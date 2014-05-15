3ad6b9416a2f8491be20acadcb6289da38f5d883
Kevin Goslar <kevin.goslar@originate.com>
2014-05-15 14:51:57 -0700

    Make terminal helpers available from the start

--- helpers/helpers.sh
+++ helpers/helpers.sh
@@ -2,7 +2,7 @@
 
 current_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
+source $current_dir/terminal_helpers.sh
 source $current_dir/configuration.sh
 source $current_dir/file_helpers.sh
-source $current_dir/terminal_helpers.sh
 source $current_dir/git_helpers.sh
