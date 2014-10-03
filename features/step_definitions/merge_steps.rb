1ae0acfee6c891e75d3fe0d2ddb933bf14f098ec
Kevin Goslar <kevin.goslar@originate.com>
2014-10-03 15:23:45 -0700

    Improve method naming

--- features/step_definitions/merge_steps.rb
+++ features/step_definitions/merge_steps.rb
@@ -5,10 +5,10 @@ end
 
 
 Then /^my repo(?: still)? has a merge in progress$/ do
-  expect(merge_in_progress).to be_truthy
+  expect(merge_in_progress?).to be_truthy
 end
 
 
 Then /^there is no merge in progress$/ do
-  expect(merge_in_progress).to be_falsy
+  expect(merge_in_progress?).to be_falsy
 end
--- features/support/merge_helpers.rb
+++ features/support/merge_helpers.rb
@@ -1,3 +1,3 @@
-def merge_in_progress
+def merge_in_progress?
   run("git status | grep 'You have unmerged paths' | wc -l")[:out] == '1'
 end
