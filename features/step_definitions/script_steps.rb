d7b0aa94f991f43499949230028a454fe956d145
Kevin Goslar <kevin.goslar@originate.com>
2014-10-16 10:47:21 -0700

    Script_exists? helper

--- features/step_definitions/script_steps.rb
+++ features/step_definitions/script_steps.rb
@@ -1,20 +1,20 @@
 Then /^there are abort and continue scripts for "(.+?)"$/  do |operation|
-  expect(File.exists? script_path(operation: operation, command: 'abort')).to be_truthy
-  expect(File.exists? script_path(operation: operation, command: 'continue')).to be_truthy
+  expect(script_exists? operation: operation, command: 'abort').to be_truthy
+  expect(script_exists? operation: operation, command: 'continue').to be_truthy
 end
 
 
 Then /^there are no abort and continue scripts for "(.*?)" anymore$/ do |operation|
-  expect(File.exists? script_path(operation: operation, command: 'abort')).to be_falsy
-  expect(File.exists? script_path(operation: operation, command: 'continue')).to be_falsy
+  expect(script_exists? operation: operation, command: 'abort').to be_falsy
+  expect(script_exists? operation: operation, command: 'continue').to be_falsy
 end
 
 
 Then /^there is an? (abort|continue) script for "(.+?)"$/ do |command, operation|
-  expect(File.exists? script_path(operation: operation, command: command)).to be_truthy
+  expect(script_exists? operation: operation, command: command).to be_truthy
 end
 
 
 Then /^there is no (abort|continue) script for "(.+?)" anymore$/ do |command, operation|
-  expect(File.exists? script_path(operation: operation, command: command)).to be_falsy
+  expect(script_exists? operation: operation, command: command).to be_falsy
 end
