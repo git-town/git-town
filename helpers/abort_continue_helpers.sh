f9f26aeee55e1b69d1ddee93b7ee78c785b0ae85
charlierudolph <charles.rudolph@originate.com>
2014-10-23 12:13:41 -0700

    DRY up abort continue scripts

--- helpers/abort_continue_helpers.sh
+++ helpers/abort_continue_helpers.sh
@@ -1,6 +1,45 @@
 # Helper methods for dealing with abort/continue scripts
 
 
+function create_pull_main_branch_abort_script {
+  echo "git rebase --abort" > $abort_script
+  echo "git checkout $initial_branch_name" >> $abort_script
+  if [ $initial_open_changes == true ]; then
+    echo "git stash pop" >> $abort_script
+  fi
+}
+
+
+function create_pull_feature_branch_abort_script {
+  echo "git merge --abort" > $abort_script
+  if [ $initial_open_changes == true ]; then
+    echo "git stash pop" >> $abort_script
+  fi
+}
+
+
+function create_merge_main_branch_abort_script {
+  echo "git merge --abort" > $abort_script
+  if [ $initial_open_changes == true ]; then
+    echo "git stash pop" >> $abort_script
+  fi
+}
+
+
+function exit_with_abort_continue_messages {
+  local cmd=$1
+
+  echo
+  if [ -n $abort_script ]; then
+    echo_red "To abort, run \"git $cmd --abort\"."
+  fi
+  if [ -n $continue_script ]; then
+    echo_red "To continue after you have resolved the conflicts, run \"git $cmd --continue\"."
+  fi
+  exit_with_error
+}
+
+
 function remove_abort_continue_scripts {
   if [ -n $abort_script ]; then rm $abort_script; fi
   if [ -n $continue_script ]; then rm $continue_script; fi
