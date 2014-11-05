a63885b26098b5ac0f527291279c2ee418d2a58a
charlierudolph <charles.rudolph@originate.com>
2014-11-04 16:08:38 -0800

    Fix unary operator expected

--- helpers/git_helpers.sh
+++ helpers/git_helpers.sh
@@ -52,7 +52,7 @@ function delete_branch {
 function delete_local_branch {
   local branch_name=$1
   local op="d"
-  if [ $2 == "force" ]; then op="D"; fi
+  if [ "$2" == "force" ]; then op="D"; fi
   run_command "git branch -$op $branch_name"
 }
 
