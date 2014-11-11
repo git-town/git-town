942800ab9834190ff0eb14d934530443b1d5dc81
Kevin Goslar <kevin.goslar@gmail.com>
2014-11-10 19:53:20 -0800

    create_and_checkout_branch helper

--- helpers/git_helpers.sh
+++ helpers/git_helpers.sh
@@ -46,12 +46,20 @@ function cherry_pick {
   if [ $? != 0 ]; then error_cherry_pick $SHAs; fi
 }
 
+
+# Cuts a new branch off the given parent branch, and checks it out.
+function create_and_checkout_branch {
+  local new_branch_name=$1
+  local parent_branch_name=$2
+  run_command "git checkout -b $new_branch_name $parent_branch_name"
+}
+
+
 # Creates a new feature branch with the given name.
 #
 # The feature branch is cut off the main development branch.
-function create_feature_branch {
-  local new_branch_name=$1
-  run_command "git checkout -b $new_branch_name $main_branch_name"
+function create_and_checkout_feature_branch {
+  create_and_checkout_branch $1 $main_branch_name
 }
 
 
