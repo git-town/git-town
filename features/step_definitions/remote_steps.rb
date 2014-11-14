8650301a3ea40a989562a991960fa0d41b26f7f7
charlierudolph <charles.rudolph@originate.com>
2014-11-14 08:33:42 -0800

    git sync-fork: Remove automatic upstream configuration

--- features/step_definitions/remote_steps.rb
+++ features/step_definitions/remote_steps.rb
@@ -8,31 +8,3 @@ Given /^my repo has an upstream repo$/ do
 
   run "git remote add upstream #{upstream_remote_repository_path}"
 end
-
-
-Given /^my remote origin is "(.*)" on GitHub$/ do |repository|
-  run "git remote set-url origin https://github.com/#{repository}.git"
-end
-
-
-Given /^my remote origin is a "rails\/rails" fork on GitHub through (.*)$/ do |protocol|
-  url = case protocol
-    when 'HTTPS' then github_rails_fork['clone_url']
-    when 'SSH' then github_rails_fork['ssh_url']
-    else raise "Unknown protocol: #{protocol}"
-  end
-
-  run "git remote set-url origin #{url}"
-end
-
-
-
-Then /^my remote upstream is "rails\/rails" on GitHub through (.*)$/ do |protocol|
-  prefix = case protocol
-    when 'HTTPS' then 'https://github.com/'
-    when 'SSH' then 'git@github.com:'
-    else raise "Unknown protocol: #{protocol}"
-  end
-
-  expect(remote_url('upstream')).to eql "#{prefix}rails/rails.git"
-end
--- helpers/helpers.sh
+++ helpers/helpers.sh
@@ -5,7 +5,6 @@ source $current_dir/abort_continue_helpers.sh
 source $current_dir/configuration.sh
 source $current_dir/file_helpers.sh
 source $current_dir/git_helpers.sh
-source $current_dir/github_helpers.sh
 source $current_dir/terminal_helpers.sh
 
 initial_branch_name=`get_current_branch_name`
