919372924b757273c613dcbb9334e1bed6462fb0
Kevin Goslar <kevin.goslar@originate.com>
2014-11-19 07:09:27 -0800

    fix sync

--- helpers/git_helpers.sh
+++ helpers/git_helpers.sh
@@ -201,7 +201,7 @@ function has_open_changes {
 # Determines whether the given branch has a remote tracking branch.
 function has_tracking_branch {
   local branch_name=$1
-  if [ $(git branch -vv | grep "$branch_name" | grep "\[origin\/$branch_name.*\]" | wc -l) == 0 ]; then
+  if [ "$(git branch -vv | grep "$branch_name" | grep -c "\[origin\/$branch_name.*\]")" == 0 ]; then
     echo false
   else
     echo true
@@ -223,7 +223,7 @@ function is_ahead_of_main {
 # Returns true if the current branch is a feature branch
 function is_feature_branch {
   local branch_name=$1
-  if [ "$branch_name" == "$main_branch_name" -o $(echo "$non_feature_branch_names" | tr ',' '\n' | grep "$branch_name" | wc -l) == 1 ]; then
+  if [ "$branch_name" == "$main_branch_name" -o "$(echo "$non_feature_branch_names" | tr ',' '\n' | grep -c "$branch_name")" == 1 ]; then
     echo false
   else
     echo true
