c7a7677ab74a8bd9a19df00e7350207b69990dd8
Kevin Goslar <kevin.goslar@originate.com>
2014-11-19 07:16:15 -0800

    fix sync

--- helpers/git_helpers.sh
+++ helpers/git_helpers.sh
@@ -212,7 +212,7 @@ function has_tracking_branch {
 # Determines whether the given branch is ahead of main
 function is_ahead_of_main {
   local branch_name=$1
-  if [ "$(git log --oneline $main_branch_name..$branch_name | wc -l | tr -d ' ')" == 0 ]; then
+  if [ "$(git log --oneline "$main_branch_name..$branch_name" | wc -l | tr -d ' ')" == 0 ]; then
     echo false
   else
     echo true
