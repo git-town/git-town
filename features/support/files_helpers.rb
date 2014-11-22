0ef56e39141457c68e35c6d1f8a7fa1c57511fe2
Kevin Goslar <kevin.goslar@gmail.com>
2014-11-22 10:03:40 -0800

    remove unused method

--- features/support/files_helpers.rb
+++ features/support/files_helpers.rb
@@ -4,12 +4,6 @@ def existing_files
 end
 
 
-# Returns whether the current workspace has uncommitted files
-def has_uncommitted_files
-  uncommitted_files.size > 0
-end
-
-
 def uncommitted_files
   array_output_of "git status --porcelain | awk '{print $2}'"
 end
