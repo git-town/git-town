19412b6883fb80b7eedf5f371975c21479885d07
charlierudolph <charles.rudolph@originate.com>
2014-11-26 17:05:34 -0800

    refactor: abort/continue/undo scripts

--- helpers/helpers.sh
+++ helpers/helpers.sh
@@ -1,10 +1,12 @@
 #!/bin/bash -e
 current_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-source "$current_dir/abort_continue_helpers.sh"
+program="$(echo "$0" | grep -o "[^/]*$")"
+
 source "$current_dir/configuration.sh"
 source "$current_dir/file_helpers.sh"
 source "$current_dir/git_helpers.sh"
+source "$current_dir/script_helpers.sh"
 source "$current_dir/terminal_helpers.sh"
 
 export initial_branch_name=$(get_current_branch_name)
