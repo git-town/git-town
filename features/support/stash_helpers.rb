1e9d8c12d08c76d42ef99c6dd37d52cbd18067ef
Kevin Goslar <kevin.goslar@originate.com>
2014-11-28 08:42:25 -0800

    Remove unused method

--- features/support/stash_helpers.rb
+++ features/support/stash_helpers.rb
@@ -1,9 +1,3 @@
-# Pops the uncommitted changes back from the stash
-def restore_open_changes
-  run "git stash pop"
-end
-
-
 # Returns the number of unpopped stashes
 def stash_size
   integer_output_of 'git stash list | wc -l'
