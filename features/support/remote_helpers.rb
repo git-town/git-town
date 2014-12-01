02b061d2adca57bdf60d6d1572f8d325e78b7b96
charlierudolph <charles.rudolph@originate.com>
2014-11-30 19:22:26 -0800

    Update feature helpers

--- features/support/remote_helpers.rb
+++ features/support/remote_helpers.rb
@@ -1,24 +1,29 @@
-# Returns the remote URL for domain / protocol
-#   domain is Github or Bitbucket
-#   protocol is HTTPS or SSH
-def remote_url domain, protocol
-  if protocol == 'HTTPS'
-    host = domain == 'Github' ? 'github.com' : 'username@bitbucket.org'
-    "https://#{host}/Originate/git-town.git"
+# Returns the remote URL for a new pull request for the given domain and branch
+def remote_pull_request_url domain, branch_name
+  if domain == 'Bitbucket'
+    sha = recent_commit_shas(1).join('')[0, 12]
+    "https://bitbucket.org/Originate/git-town/pull-request/new?source=Originate%2Fgit-town%3A#{sha}%3A#{branch_name}"
+  elsif domain == 'Github'
+    "https://github.com/Originate/git-town/compare/#{branch_name}?expand=1"
   else
-    host = domain == 'Github' ? 'github.com' : 'bitbucket.org'
-    "git@#{host}:Originate/git-town.git"
+    fail "Unknown domain: #{domain}"
   end
 end
 
 
-# Returns the remote URL for a new pull request for the given domain and branch
-#   domain is Github or Bitbucket
-def remote_pull_request_url domain, branch_name
-  if domain == 'Github'
-    "https://github.com/Originate/git-town/compare/#{branch_name}?expand=1"
-  else
-    sha = recent_commit_shas(1).join('')[0, 12]
-    "https://bitbucket.org/Originate/git-town/pull-request/new?source=Originate%2Fgit-town%3A#{sha}%3A#{branch_name}"
+# Returns the remote URL for domain / protocol
+def remote_url domain, protocol
+  "#{remote_url_prefix domain, protocol}Originate/git-town.git"
+end
+
+
+# Returns the remote URL prefix for the given domain and protocol
+def remote_url_prefix domain, protocol
+  case [domain, protocol]
+  when %w(Bitbucket HTTPS) then 'https://username@bitbucket.org/'
+  when %w(Bitbucket SSH) then 'git@bitbucket.org:'
+  when %w(Github HTTPS) then 'https://github.com/'
+  when %w(Github SSH) then 'git@github.com:'
+  else fail "Unknown domain/protocol pairing: #{domain}/#{protocol}"
   end
 end
