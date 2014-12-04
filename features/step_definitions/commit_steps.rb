f2f84155118398e0f27b263300f5eda55164d8dd
Kevin Goslar <kevin.goslar@gmail.com>
2014-12-03 20:33:52 -0800

    Remove unused step

--- features/step_definitions/commit_steps.rb
+++ features/step_definitions/commit_steps.rb
@@ -14,13 +14,6 @@ end
 
 
 
-Then(/^my branch and its remote still have (\d+) and (\d+) different commits$/) do |local_count, remote_count|
-  matches = /have (\d+) and (\d+) different commit each/.match(run('git status').out)
-  expect(matches[1]).to eql local_count
-  expect(matches[2]).to eql remote_count
-end
-
-
 Then(/^(?:now )?(?:(?:I (?:still )?(?:have|see))) the following commits$/) do |commits_table|
   verify_commits commits_table: commits_table, repository_path: local_repository_path
 end
