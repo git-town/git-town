15d9a7190df1afbf7364ea1ec04345e533333b25
Allen Wu <allen.wu@ucla.edu>
2014-12-07 14:58:15 -0800

    Added `git town (main-branch|non-feature-branches)`

--- features/git-town/configuration/configuration_actions.feature
+++ features/git-town/configuration/configuration_actions.feature
@@ -29,3 +29,33 @@ Feature: listing the configuration
     When I run `git town --config`
     Then I see "Main branch: ''"
     And I see "Non-feature branches: ''"
+
+
+  Scenario: printing just the main branch when it's not yet configured
+    Given I don't have a main branch name configured
+    When I run `git town main-branch`
+    Then I see "Main branch: ''"
+
+
+  Scenario: printing just the main branch when it's configured
+    Given I have configured the main branch name as "main"
+    When I run `git town main-branch`
+    Then I see "Main branch: 'main'"
+
+
+  Scenario: printing just the non-feature branches when they're not yet configured
+    Given my non-feature branches are not configured
+    When I run `git town non-feature-branches`
+    Then I see "Non-feature branches: ''"
+
+
+  Scenario: printing just the non-feature branches when they're configured
+    Given my non-feature branches are configured as "qa"
+    When I run `git town non-feature-branches`
+    Then I see "Non-feature branches: 'qa'"
+
+
+  Scenario: setting the main branch
+    Given I have configured the main branch name as "main-old"
+    When I run `git town main-branch main-new`
+    Then I see "main branch stored as 'main-new'"
--- helpers/configuration.sh
+++ helpers/configuration.sh
@@ -18,20 +18,22 @@ function store_configuration {
 
 
 # Persists the main branch configuration
-function store_main_branch_name {
+function store_main_branch_name_with_confirmation_text {
   store_configuration main-branch-name "$1"
+  echo "main branch stored as '$1'"
 }
 
 
 # Persists the non-feature branch configuration
-function store_non_feature_branch_names {
+function store_non_feature_branch_names_with_confirmation_text {
   store_configuration non-feature-branch-names "$1"
+  echo "non-feature branches stored as '$1'"
 }
 
 
 # Update old configuration to new one if it exists
 if [[ -f ".main_branch_name" ]]; then
-  store_main_branch_name "$(cat .main_branch_name)"
+  store_configuration main-branch-name "$(cat .main_branch_name)"
   rm .main_branch_name
 fi
 
@@ -57,18 +59,18 @@ if [[ -z "$main_branch_name" ]]; then
     echo_error "Please try again."
     exit_with_error
   fi
-  store_main_branch_name "$main_branch_name"
   echo
-  echo "main branch stored as '$main_branch_name'."
+  store_main_branch_name_with_confirmation_text
 fi
 
 # Ask and store non-feature-branch-names, if needed
 if [[ $? == '1' ]]; then
+  echo
   echo "Git Town supports non-feature branches like 'release' or 'production'."
   echo "These branches cannot be shipped and do not merge $main_branch_name when syncing."
   echo "Please enter the names of all your non-feature branches as a comma seperated list."
   echo "Example: 'qa, production'"
   read non_feature_branch_names
-  store_non_feature_branch_names "$non_feature_branch_names"
-  echo "non-feature branches stored as '$non_feature_branch_names'"
+  echo
+  store_non_feature_branch_names_with_confirmation_tex
 fi
