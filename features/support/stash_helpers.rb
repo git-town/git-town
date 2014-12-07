2807df40522044438d9f23c479ade5117de8cca3
Kevin Goslar <kevin.goslar@originate.com>
2014-12-07 09:29:40 -0800

    Cleanup

--- features/support/stash_helpers.rb
+++ features/support/stash_helpers.rb
@@ -1,15 +1,3 @@
-# Pops the uncommitted changes back from the stash
-def restore_open_changes
-  run 'git stash pop'
-end
-
-
-# Stashes away uncommitted changes
-def stash_open_changes
-  run 'git stash -u'
-end
-
-
 # Returns the number of unpopped stashes
 def stash_size
   integer_output_of 'git stash list | wc -l'
