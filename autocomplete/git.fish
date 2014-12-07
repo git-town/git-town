3fb3e1eb29fd4a1795e80f0f42d32f877c34923e
Kevin Goslar <kevin.goslar@originate.com>
2014-12-07 07:39:36 -0800

    polish

--- autocomplete/git.fish
+++ autocomplete/git.fish
@@ -4,7 +4,7 @@
 # for command in (ls (brew --repository)/Library/Homebrew/cmd | sed -e "s/\.rb//g")
 #   set commands $command $commands
 # end
-set commands extract hack kill pr prune-branches ship sync sync-fork town
+set git_town_commands extract hack kill pr prune-branches ship sync sync-fork town
 
 
 # Indicates through its error code whether the command line to auto-complete
@@ -14,7 +14,7 @@ set commands extract hack kill pr prune-branches ship sync sync-fork town
 # - has command already: exit code 1
 function __fish_complete_git_town_no_command
   for cmd in (commandline -opc)
-    if contains $cmd $commands
+    if contains $cmd $git_town_commands
       return 1
     end
   end
@@ -25,6 +25,10 @@ end
 # Returns whether the currently autocompleted Git Town command takes
 # a Git branch as the next parameter.
 function __fish_complete_git_town_command_takes_branch
+  # NOTE: Must convert command-line list to a string here
+  #       in order to use it in switch.
+  #       This is done by assigning to a temp variable.
+  #       There might be a better way.
   set arguments (commandline -opc)
   switch "$arguments"
     case 'git extract' 'git kill' 'git ship'
@@ -34,21 +38,30 @@ function __fish_complete_git_town_command_takes_branch
 end
 
 
-# define the Git Town commands
-complete --command git --arguments 'extract' --description 'Copy selected commits from the current branch into their own branch' --condition '__fish_complete_git_town_no_command' --no-files
-complete --command git --arguments 'hack' --description 'Cut a new feature branch off the main branch' --condition '__fish_complete_git_town_no_command' --no-files
-complete --command git --arguments 'kill' --description 'Remove an obsolete feature branch' --condition '__fish_complete_git_town_no_command' --no-files
-complete --command git --arguments 'pr' --description 'Create a new pull request' --condition '__fish_complete_git_town_no_command' --no-files
-complete --command git --arguments 'prune-branches' --description 'Delete merged branches' --condition '__fish_complete_git_town_no_command' --no-files
-complete --command git --arguments 'ship' --description 'Deliver a completed feature branch' --condition '__fish_complete_git_town_no_command' --no-files
-complete --command git --arguments 'sync' --description 'Update the current branch with all relevant changes' --condition '__fish_complete_git_town_no_command' --no-files
-complete --command git --arguments 'sync-fork' --description 'Pull upstream updates into a forked repository' --condition '__fish_complete_git_town_no_command' --no-files
-complete --command git --arguments 'town' --description 'Git Town management' --condition '__fish_complete_git_town_no_command' --no-files
+# Define autocompletion for the Git Town commands themselves.
+#
+# These only get autocompleted if there is no Git Town command present in the
+# command line already.
+# This is done through __fish_complete_git_town_no_command
+complete --command git --arguments 'extract'        --description 'Copy selected commits from the current branch into their own branch' --condition '__fish_complete_git_town_no_command' --no-files
+complete --command git --arguments 'hack'           --description 'Cut a new feature branch off the main branch'                        --condition '__fish_complete_git_town_no_command' --no-files
+complete --command git --arguments 'kill'           --description 'Remove an obsolete feature branch'                                   --condition '__fish_complete_git_town_no_command' --no-files
+complete --command git --arguments 'pr'             --description 'Create a new pull request'                                           --condition '__fish_complete_git_town_no_command' --no-files
+complete --command git --arguments 'prune-branches' --description 'Delete merged branches'                                              --condition '__fish_complete_git_town_no_command' --no-files
+complete --command git --arguments 'ship'           --description 'Deliver a completed feature branch'                                  --condition '__fish_complete_git_town_no_command' --no-files
+complete --command git --arguments 'sync'           --description 'Update the current branch with all relevant changes'                 --condition '__fish_complete_git_town_no_command' --no-files
+complete --command git --arguments 'sync-fork'      --description 'Pull upstream updates into a forked repository'                      --condition '__fish_complete_git_town_no_command' --no-files
+complete --command git --arguments 'town'           --description 'Git Town management'                                                 --condition '__fish_complete_git_town_no_command' --no-files
 
-# autocomplete Git branch names
+
+# Define autocompletion of Git branch names.
+#
+# This is only enabled for commands that take branch names.
+# This is achieved through __fish_complete_git_town_command_takes_branch
 complete --command git --arguments "(git branch | tr -d '* ')" --condition '__fish_complete_git_town_command_takes_branch' --no-files
 
-# command-line switches
-complete --command git -l 'abort' --description 'Abort the current command'
-complete --command git -l 'continue' --description 'Continue the current command'
-complete --command git -l 'undo' --description 'Undo the current/last command'
+
+# Define autocompletion for command-line switches
+complete --command git --long-option 'abort'    --description 'Abort the current command'     --no-files
+complete --command git --long-option 'continue' --description 'Continue the current command'  --no-files
+complete --command git --long-option 'undo'     --description 'Undo the current/last command' --no-files
