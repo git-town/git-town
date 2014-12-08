3fcc9ee806d3b84a107d4fc6019ba5c26ad90498
charlierudolph <charles.rudolph@originate.com>
2014-12-07 19:55:31 -0800

    cleanup: git-extract

--- helpers/file_helpers.sh
+++ helpers/file_helpers.sh
@@ -6,10 +6,21 @@
 # Unique string that identifies the current directory
 temp_filename_suffix="$(pwd | tr '/' '_')"
 
-# Temporary filename used for short term storage of user input
-export user_input_filename="/tmp/git-town-user-input_${temp_filename_suffix}"
-
 # Scripts filenames
 for action in "abort" "continue" "undo"; do
   declare -r ${action}_script_filename="/tmp/${program}_${action}_${temp_filename_suffix}"
 done
+
+
+function temp_filename {
+  local file=$(temp_filename_unsafe)
+  while [ -e "$file" ]; do
+    file=$(temp_filename_unsafe)
+  done
+  echo "$file"
+}
+
+
+function temp_filename_unsafe {
+  echo "/tmp/git-town$RANDOM.tmp"
+}
