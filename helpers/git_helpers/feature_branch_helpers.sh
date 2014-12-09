88602c007d987e78a9472b31a08a6db7c3b3dcae
charlierudolph <charles.rudolph@originate.com>
2014-12-08 17:26:58 -0800

    feature_branch_helpers

---
+++ helpers/git_helpers/feature_branch_helpers.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+
+# Exits if the supplied branch is not a feature branch
+function ensure_is_feature_branch {
+  local branch_name=$1
+  local error_message=$2
+  if [ "$(is_feature_branch "$branch_name")" == false ]; then
+    error_is_not_feature_branch
+
+    echo_error_header
+    echo_error "The branch '$branch_name' is not a feature branch. $error_message"
+    exit_with_error
+  fi
+}
+
+
+# Exits if the current branch is not a feature branch
+function ensure_on_feature_branch {
+  local error_message="$*"
+  local branch_name=$(get_current_branch_name)
+  ensure_is_feature_branch "$branch_name" "$error_message"
+}
+
+
+# Returns true if the current branch is a feature branch
+function is_feature_branch {
+  local branch_name=$1
+  if [ "$branch_name" == "$main_branch_name" -o "$(echo "$non_feature_branch_names" | tr ',' '\n' | grep -c "$branch_name")" == 1 ]; then
+    echo false
+  else
+    echo true
+  fi
+}
