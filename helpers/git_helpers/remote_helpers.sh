e75fa4a3e812d1c4f98394c2bd06d11347df1288
charlierudolph <charles.rudolph@originate.com>
2014-12-08 17:39:55 -0800

    Separate remote helpers

--- helpers/git_helpers/remote_helpers.sh
+++ helpers/git_helpers/remote_helpers.sh
@@ -1,57 +1,6 @@
 #!/bin/bash
 
 
-# Fetches updates from the central repository.
-#
-# It is safe to call this method multiple times per session,
-# since it makes sure that it fetches updates only once per session
-# by tracking this through the global variable $repo_fetched.
-function fetch_repo {
-  if [ "$repo_fetched" == false ]; then
-    run_command "git fetch --prune"
-    repo_fetched=true
-  fi
-}
-repo_fetched=false
-
-
-# Fetches changes from the upstream repository
-function fetch_upstream {
-  run_command "git fetch upstream"
-}
-
-
-# Returns whether the current branch has local updates
-# that haven't been pushed to the remote yet.
-# Assumes the current branch has a tracking branch
-function needs_pushing {
-  if [ "$(git status | grep -c "Your branch is ahead of")" != 0 ]; then
-    echo true
-  else
-    echo false
-  fi
-}
-
-
-# Pushes the branch with the given name to origin
-function push_branch {
-  local current_branch_name=$(get_current_branch_name)
-  if [ "$(has_tracking_branch "$current_branch_name")" == true ]; then
-    if [ "$(needs_pushing)" == true ]; then
-      run_command "git push"
-    fi
-  else
-    run_command "git push -u origin $current_branch_name"
-  fi
-}
-
-
-# Pushes tags to the remote
-function push_tags {
-  run_command "git push --tags"
-}
-
-
 # Returns the url for the remote
 function remote_url {
   git remote -v | grep "origin.*fetch" | awk '{print $2}'
