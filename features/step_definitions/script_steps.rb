58913878a9b84006660e1cff25fc07a7e104f2b8
charlierudolph <charles.rudolph@originate.com>
2014-12-10 10:00:39 -0800

    Remove unneeded files

--- features/step_definitions/script_steps.rb
+++
@@ -1,5 +0,0 @@
-Then(/^there (?:is|are) no (.+) scripts? for "(.+)"$/) do |actions, command|
-  Kappamaki.from_sentence(actions).map do |action|
-    expect(script_exists? command, action).to be_falsy, "#{action} script for #{command} should not exist"
-  end
-end
