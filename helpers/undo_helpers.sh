11e1d138820530f81dffcc3d050833f0e20690d3
charlierudolph <charles.rudolph@originate.com>
2014-12-12 09:59:19 -0800

    Break undo helpers into functions

--- helpers/undo_helpers.sh
+++ helpers/undo_helpers.sh
@@ -5,30 +5,57 @@ function undo_commands_for {
   read -a cmd_with_arguments <<< "$1"
 
   local cmd="${cmd_with_arguments[0]}"
+  local arguments="${cmd_with_arguments[*]:1}"
 
-  if [ "$cmd" = "stash_open_changes" ]; then
-    echo "restore_open_changes"
-  elif [ "$cmd" = "checkout" ] || [ "$cmd" = "checkout_main_branch" ]; then
-    local branch=$(get_current_branch_name)
-    echo "checkout $branch"
-  elif [ "$cmd" = "create_and_checkout_feature_branch" ]; then
-    local branch=$(get_current_branch_name)
-    local branch_to_create="${cmd_with_arguments[1]}"
-    echo "checkout $branch"
-    echo "delete_branch $branch_to_create"
-  elif [ "$cmd" = "delete_branch" ]; then
-    local branch_to_delete="${cmd_with_arguments[1]}"
-    local sha=$(sha_of_branch "$branch_to_delete")
-    echo "create_branch $branch_to_delete $sha"
-    if [ "$(has_tracking_branch "$branch_to_delete")" = true ]; then
-      echo "push_branch $branch_to_delete"
-    fi
-  elif [ "$cmd" = "commit_open_changes" ]; then
-    local branch=$(get_current_branch_name)
-    local sha=$(sha_of_branch "$branch")
-    echo "reset_to_sha $sha"
-    if [ "$(has_tracking_branch "$branch")" = true ]; then
-      echo "push_branch $branch force"
-    fi
+  local fn="undo_commands_for_$cmd"
+
+  if [ "$(type "$fn" 2>&1 | grep -c 'not found')" = 0 ]; then
+    eval "$fn $arguments"
   fi
+}
+
+
+
+
+function undo_commands_for_checkout {
+  local branch=$(get_current_branch_name)
+  echo "checkout $branch"
+}
+
+
+function undo_commands_for_checkout_main_branch {
+  undo_commands_for_checkout
+}
+
+
+function undo_commands_for_commit_open_changes {
+  local branch=$(get_current_branch_name)
+  local sha=$(sha_of_branch "$branch")
+  echo "reset_to_sha $sha"
+  if [ "$(has_tracking_branch "$branch")" = true ]; then
+    echo "push_branch $branch force"
+  fi
+}
+
+
+function undo_commands_for_create_and_checkout_feature_branch {
+  local branch=$(get_current_branch_name)
+  local branch_to_create="$1"
+  echo "checkout $branch"
+  echo "delete_branch $branch_to_create"
+}
+
+
+function undo_commands_for_delete_branch {
+  local branch_to_delete="$1"
+  local sha=$(sha_of_branch "$branch_to_delete")
+  echo "create_branch $branch_to_delete $sha"
+  if [ "$(has_tracking_branch "$branch_to_delete")" = true ]; then
+    echo "push_branch $branch_to_delete"
+  fi
+}
+
+
+function undo_commands_for_stash_open_changes {
+  echo "restore_open_changes"
 }
