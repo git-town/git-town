fb38853557ea3424f7e332f5ac97e76a12f54328
charlierudolph <charles.rudolph@originate.com>
2014-12-14 09:52:38 -0800

    move helpers

--- helpers/file_helpers.sh
+++ helpers/file_helpers.sh
@@ -31,6 +31,59 @@ function append_to_file {
 }
 
 
+function has_lines {
+  local file="$1"
+
+  if [ "$(has_file "$file")" = true ] && [ "$(number_of_lines "$file")" -gt 0 ]; then
+    echo true
+  else
+    echo false
+  fi
+}
+
+
+function has_file {
+  local file="$1"
+
+  if [ -n "$file" -a -f "$file" ]; then
+    echo true
+  else
+    echo false
+  fi
+}
+
+
+function number_of_lines {
+  local file="$1"
+  wc -l < "$file" | tr -d ' '
+}
+
+
+function peek_line {
+  local file="$1"
+  head -n 1 "$file"
+}
+
+
+function pop_line {
+  local file="$1"
+  peek_line "$file"
+  remove_line "$file"
+}
+
+
+function remove_line {
+  local file="$1"
+  if [ "$(number_of_lines "$file")" -gt 1 ]; then
+    local temp=$(temp_filename)
+    tail -n +2 "$file" > "$temp"
+    mv "$temp" "$file"
+  else
+    rm "$file"
+  fi
+}
+
+
 function prepend_to_file {
   local content=$1
   local file=$2
