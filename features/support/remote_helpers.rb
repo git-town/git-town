ae525b06117acf36f09bee9a1b5e8742643efe9b
charlierudolph <charles.rudolph@originate.com>
2014-12-14 09:27:51 -0800

    convert if to case

--- features/support/remote_helpers.rb
+++ features/support/remote_helpers.rb
@@ -28,10 +28,11 @@ end
 
 # Returns the remote URL for a new pull request for the given domain and branch
 def pull_request_url domain, branch_name
-  if domain == 'Bitbucket'
+  case domain
+  when 'Bitbucket'
     sha = recent_commit_shas(1).join('')[0, 12]
     "https://bitbucket.org/Originate/git-town/pull-request/new?source=Originate%2Fgit-town%3A#{sha}%3A#{branch_name}"
-  elsif domain == 'GitHub'
+  when 'GitHub'
     "https://github.com/Originate/git-town/compare/#{branch_name}?expand=1"
   else
     fail "Unknown domain: #{domain}"
