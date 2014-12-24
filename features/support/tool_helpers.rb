fa0b99daaa740313d46a1c78ba5fa1d122273b0e
charlierudolph <charles.rudolph@originate.com>
2014-12-23 17:28:07 -0700

    add comments

--- features/support/tool_helpers.rb
+++ features/support/tool_helpers.rb
@@ -1,3 +1,4 @@
+# Configures the test environment to assume only the given set of tools are installed
 def update_installed_tools tools
   File.open(TOOLS_INSTALLED_FILENAME, 'w') do |file|
     file.write tools.join("\n") + "\n"
