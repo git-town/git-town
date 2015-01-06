e665ea01646c2480ce16638956b41fdc1883ebfe
charlierudolph <charles.rudolph@originate.com>
2015-01-06 08:11:42 -0800

    remove unnecessary addition

--- features/support/run_helpers.rb
+++ features/support/run_helpers.rb
@@ -23,7 +23,7 @@ end
 
 
 def git_town_command? command
-  %w(extract hack kill pr prune-branches repo ship sync-all sync-fork sync town).any? do |subcommand|
+  %w(extract hack kill pr prune-branches repo ship sync-fork sync town).any? do |subcommand|
     command.starts_with? "git #{subcommand}"
   end
 end
