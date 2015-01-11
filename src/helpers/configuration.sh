e374e0437bc115ad8a1485920c757c4dcb7447e0
charlierudolph <charles.rudolph@originate.com>
2015-01-11 10:39:50 -0800

    updates

--- src/helpers/configuration.sh
+++ src/helpers/configuration.sh
@@ -157,7 +157,7 @@ if [[ -z "$main_branch_name" ]]; then
     exit_with_error
   fi
   echo
-  store_main_branch_name_with_confirmation_text "$main_branch_name"
+  store_configuration main-branch-name "$main_branch_name"
 fi
 
 # Ask and store non-feature-branch-names, if needed
@@ -169,5 +169,5 @@ if [[ $? == '1' ]]; then
   echo "Example: 'qa, production'"
   read non_feature_branch_names
   echo
-  store_non_feature_branch_names_with_confirmation_text "$non_feature_branch_names"
+  store_configuration non-feature-branch-names "$non_feature_branch_names"
 fi
