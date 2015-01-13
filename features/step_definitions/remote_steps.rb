696334a41642cbefd0d4f60137f34c974ece485f
charlierudolph <charles.rudolph@originate.com>
2015-01-12 22:25:28 -0800

    remove unneeded code

--- features/step_definitions/remote_steps.rb
+++ features/step_definitions/remote_steps.rb
@@ -1,11 +1,6 @@
 Given(/^my repo has an upstream repo$/) do
   clone_repository :origin, :upstream, bare: true
   clone_repository :upstream, :upstream_developer
-
-  in_repository :upstream_developer do
-    run 'git checkout main'
-  end
-
   run "git remote add upstream #{repository_path :upstream}"
 end
 
