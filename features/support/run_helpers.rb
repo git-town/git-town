fe60c0767e0f057f5ebe3a43188d9f1b8a3d89ae
Kevin Goslar <kevin.goslar@gmail.com>
2015-02-20 10:06:59 -0800

    format

--- features/support/run_helpers.rb
+++ features/support/run_helpers.rb
@@ -91,5 +91,8 @@ end
 
 # Output of last `run` without text formatting (ANSI escape sequences)
 def unformatted_last_run_output
-  @last_run_result.out.gsub(/\e[^m]*m/, '').gsub("\x0F", '').gsub(/\\u\d*F/, '')
+  @last_run_result.out
+    .gsub(/\e[^m]*m/, '')   # remove Bash colors
+    .gsub("\x0F", '')       # remove CircleCI artifacts
+    .gsub(/\\u\d*F/, '')    # remove CircleCI artifacts
 end
