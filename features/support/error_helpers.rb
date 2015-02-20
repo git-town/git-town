de5c883f616544155b8d1697447cad42e4017c86
Kevin Goslar <kevin.goslar@gmail.com>
2015-02-20 06:51:18 -0800

    fix lint errors

--- features/support/error_helpers.rb
+++ features/support/error_helpers.rb
@@ -1,7 +1,8 @@
+# rubocop:disable MethodLength
 def verify_error message
   @error_expected = true
-
   expect(@last_run_result.error).to be_truthy
+
   actual = unformatted_last_run_output
   expected = message.gsub(/\W/, '')
   expect(actual).to include(expected), %(
@@ -15,3 +16,4 @@ def verify_error message
     ***************************************************
   ).gsub(/^ {4}/, '')
 end
+# rubocop:enable MethodLength
