27ed7a8a2a0adaaf2502571afcaa30f95e2dc0e6
Kevin Goslar <kevin.goslar@gmail.com>
2015-02-26 07:21:33 -0800

    Add “Show output of command” debug step

--- features/step_definitions/debug_steps.rb
+++ features/step_definitions/debug_steps.rb
@@ -1,7 +1,14 @@
+# These steps are for debugging purposes only.
+# Please keep them around.
+
+
+# Displays the output of the given command.
+Then(/^show me the output of `(.+?)`$/) do |command|
+  puts output_of command
+end
+
+
 # Pauses the spec to let the developer inspect the test repos.
-#
-# This step is for debugging purposes only.
-# Please keep it around.
 Then(/^show me my repo$/) do
   p "Developer repo: #{repository_path :developer}"
   p "Coworker repo: #{repository_path :coworker}"
