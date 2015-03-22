4ba2714ef28b560a13ba123728c893e38491d619
Kevin Goslar <kevin.goslar@gmail.com>
2015-03-22 15:00:59 -0700

    Rename CommitListBuilder to CommitList

--- features/support/commits_list.rb
+++ features/support/commits_list.rb
@@ -2,7 +2,7 @@
 #
 # Commit lists contain very specific data about commits.
 # They are also sorted in distinct ways.
-class CommitListBuilder
+class CommitsList
 
   # commit_attributes - the attributes we want to gather for the commits found
   #                     The default value is the minimalistic set
@@ -10,7 +10,7 @@ class CommitListBuilder
   #
   # Examples
   #
-  #   CommitListBuilder.new [:branch, :location, :message, :file_name, :file_content, :author]
+  #   CommitsList.new [:branch, :location, :message, :file_name, :file_content, :author]
   def initialize commit_attributes = [:message]
     @commit_attributes = commit_attributes.map(&:upcase)
 
@@ -19,7 +19,7 @@ class CommitListBuilder
   end
 
 
-  # Adds the given commit to this CommitListBuilder instance
+  # Adds the given commit to this CommitsList instance
   #
   # rubocop:disable MethodLength
   # rubocop:disable AbcSize
@@ -29,30 +29,30 @@ class CommitListBuilder
     if @commits[local_branch_name].key? sha
       # We already have this commit in a different location --> just append the location to the existing commit
       @commits[local_branch_name][sha]['LOCATION'] << branch_location(branch_name)
-      return
+    else
+      # We don't have this commit in another location --> create an entry for this commit
+      @commits[local_branch_name][sha] = commit_data local_branch_name: local_branch_name,
+                                                     branch_name: branch_name,
+                                                     message: message,
+                                                     sha: sha,
+                                                     author: author
     end
-
-    @commits[local_branch_name][sha] = commit_data local_branch_name: local_branch_name,
-                                                   branch_name: branch_name,
-                                                   message: message,
-                                                   sha: sha,
-                                                   author: author
   end
   # rubocop:enable MethodLength
   # rubocop:enable AbcSize
 
 
-  # Returns whether this CommitListBuilder instance has found any commits so far
+  # Returns whether this CommitsList instance has found any commits so far
   def empty?
     @commits.empty?
   end
 
 
-  # Returns the currently found commits as a Cucumber compatible array
+  # Returns the currently known commits as a Cucumber compatible array
   #
   # rubocop:disable MethodLength
   # rubocop:disable AbcSize
-  def to_a
+  def to_table
     result = CucumberTableBuilder.new headers: @commit_attributes, dry: %w(BRANCH LOCATION)
     main_commits = @commits.delete 'main'
     main_commits.try(:keys).try(:each) do |sha|
@@ -73,13 +73,15 @@ class CommitListBuilder
 
   private
 
-  # Returns whether this CommitListBuilder instance is looking for
+  # Returns whether this CommitsList instance is looking for
   # the given commit attribute
   def attribute? attribute_name
     @commit_attributes.include? attribute_name
   end
 
 
+  # Returns the internally used data structure for the commit with the given data
+  #
   # rubocop:disable MethodLength
   def commit_data local_branch_name:, branch_name:, message:, sha:, author:
     result = {
