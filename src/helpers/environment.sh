b7fe18d01bdcaf390ff2ab07e0e4e6b0cc209cd2
Allen Wu <allen.wu@ucla.edu>
2015-03-31 22:58:03 -0700

    Show nightly version

--- src/helpers/environment.sh
+++ src/helpers/environment.sh
@@ -8,6 +8,18 @@ function is_git_repository {
   fi
 }
 
+
+function is_gittown_nightly {
+  local base_dir=$( dirname "${BASH_SOURCE[0]}" )
+
+  if git -C "$base_dir" remote -v > /dev/null 2>&1 | grep "https://github.com/Homebrew/homebrew.git (fetch)"; then
+    echo false
+  else
+    echo true
+  fi
+}
+
+
 function ensure_git_repository {
   if [ "$(is_git_repository)" == false ]; then
     echo_inline_error "This is not a git repository."
@@ -15,6 +27,16 @@ function ensure_git_repository {
   fi
 }
 
+
+function gittown_nightly_version {
+  local base_dir=$( dirname "${BASH_SOURCE[0]}" )
+  local git_hash=$(git -C "$base_dir" rev-parse --short HEAD)
+  local git_date=$(git -C "$base_dir" --no-pager show -s --format=%ci HEAD | cut -d ' ' -f1 | tr -d '-')
+
+  if [ "$(is_gittown_nightly)" == true ]; then
+    echo ".${git_date}-nightly (${git_hash})"
+  fi
+}
 
 if [[ ! "$@" =~ --bypass-environment-checks ]]; then
   ensure_git_repository
