4694201b7893e345025af7e19f961adedfbf25f6
Allen Wu <allen.wu@originate.com>
2015-04-02 19:18:45 -0700

    more PR refactoring

--- src/helpers/environment.sh
+++ src/helpers/environment.sh
@@ -17,15 +17,22 @@ function ensure_git_repository {
 }
 
 
-function gittown_dev_version {
+function git_town_revision {
+  local base_dir=$(dirname "${BASH_SOURCE[0]}")
+  local git_hash=$(git -C "$base_dir" rev-parse --short HEAD)
+  local git_date=$(git -C "$base_dir" show -s --format=%cD HEAD | cut -d ' ' -f 2-4)
+
+  echo "(${git_date}, ${git_hash})"
+}
+
+
+function is_git_town_installed_manually {
   local base_dir=$(dirname "${BASH_SOURCE[0]}")
 
-
   if [ "$(git -C "$base_dir" remote -v | grep -c "https://github.com/Homebrew/homebrew.git (fetch)")" == 0 ]; then
-    local git_hash=$(git -C "$base_dir" rev-parse --short HEAD)
-    local git_date=$(git -C "$base_dir" show -s --format=%cD HEAD | cut -d ' ' -f 2-4)
-
-    echo " (${git_date}, ${git_hash})"
+    echo true
+  else
+    echo false
   fi
 }
 
