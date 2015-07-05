f2822ad6e25b5489494656f37072c0532d6fd821
Kevin Goslar <kevin.goslar@originate.com>
2015-07-05 09:42:03 -0700

    Nested feature branch support for git-new-pull-request
    closes #555

--- features/step_definitions/browser_steps.rb
+++ features/step_definitions/browser_steps.rb
@@ -1,5 +1,12 @@
-Then(/^I see a new (.+?) pull request for the "(.+?)" branch in the "(.+?)" repo in my browser$/) do |domain, branch, repo|
-  expect(@last_run_result.out.strip).to eql "#{@tool} called with: #{pull_request_url domain, branch, repo}"
+Then(/^I see a new (.+?) pull request for the "([^"]+)" branch in the "(.+?)" repo in my browser$/) do |domain, branch, repo|
+  url = pull_request_url domain: domain, branch: branch, parent_branch: 'main', repo: repo
+  expect(@last_run_result.out.strip).to eql "#{@tool} called with: #{url}"
+end
+
+
+Then(/^I see a new (.+?) pull request for the "([^"]+)" branch against the "(.+?)" branch in the "(.+?)" repo in my browser$/) do |domain, child_branch, parent_branch, repo|
+  url = pull_request_url domain: domain, branch: child_branch, parent_branch: parent_branch, repo: repo
+  expect(@last_run_result.out.strip).to include "#{@tool} called with: #{url}"
 end
 
 
--- src/drivers/code_hosting/github.sh
+++ src/drivers/code_hosting/github.sh
@@ -4,8 +4,9 @@
 function create_pull_request {
   local repository=$1
   local branch=$2
+  local parent_branch=$3
 
-  open_browser "https://github.com/$repository/compare/$branch?expand=1"
+  open_browser "https://github.com/$repository/compare/$parent_branch...$branch?expand=1"
 }
 
 
