749d045374e3e2590b353ef3ba7da71c2aebe89b
Kevin Goslar <kevin.goslar@originate.com>
2015-07-05 10:31:08 -0700

    test

--- src/helpers/git_helpers/branch_configuration_helpers.sh
+++ src/helpers/git_helpers/branch_configuration_helpers.sh
@@ -17,8 +17,8 @@ function ancestor_branches {
   local branch_name=$1
 
   ancestors=$(git config "git-town.branches.ancestors.$branch_name")
-  escaped=${$ancestors/ /', '/}
-  echo $escaped
+  escaped=${ancestors/ /', '/}
+  echo "$escaped"
 }
 
 
