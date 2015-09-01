0a60a14f901ce9708e5a35ca8320c3a0df53da0e
charlierudolph <charles.rudolph@originate.com>
2015-09-01 10:08:06 -0700

    fix merge

--- src/helpers/git_helpers/push_helpers.sh
+++ src/helpers/git_helpers/push_helpers.sh
@@ -1,34 +1,6 @@
 #!/usr/bin/env bash
 
 
-<<<<<<< HEAD
-=======
-# Returns whether the given branch is in sync with its tracking branch
-function needs_push {
-  local branch_name=$1
-  local tracking_branch_name="origin/$branch_name"
-  if [ "$(git rev-list --left-right "$branch_name...$tracking_branch_name" | wc -l | tr -d ' ')" != 0 ]; then
-    echo true
-  else
-    echo false
-  fi
-}
-
-
-# Pushes the current branch with the given name to origin
-function push {
-  local current_branch_name=$(get_current_branch_name)
-  if [ "$(has_tracking_branch "$current_branch_name")" == true ]; then
-    if [ "$(needs_push "$current_branch_name")" == true ]; then
-      run_command "git push"
-    fi
-  else
-    run_command "git push -u origin $current_branch_name"
-  fi
-}
-
-
->>>>>>> master
 # Pushes tags to the remote
 function push_tags {
   run_command "git push --tags"
