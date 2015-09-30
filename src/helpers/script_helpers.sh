46071e379683d3fab1f95e2658a06c137cc28aae
Allen Wu <allen.wu@originate.com>
2015-09-30 10:46:30 -0700

    fix possible bug

--- src/helpers/script_helpers.sh
+++ src/helpers/script_helpers.sh
@@ -14,8 +14,8 @@ function command_steps {
   echo_if_all_true "change_directory $(git_root)" "$RUN_IN_GIT_ROOT" "$IN_SUB_FOLDER"
   echo_if_all_true "stash_open_changes" "$STASH_OPEN_CHANGES" "$INITIAL_OPEN_CHANGES"
   steps
-  echo_if_all_true "restore_open_changes" "$STASH_OPEN_CHANGES" "$INITIAL_OPEN_CHANGES"
   echo_if_all_true "set_previous_branch \$(expected_previous_branch)"
+  echo_if_all_true "restore_open_changes" "$STASH_OPEN_CHANGES" "$INITIAL_OPEN_CHANGES"
   echo_if_all_true "change_directory $INITIAL_DIRECTORY" "$RUN_IN_GIT_ROOT" "$IN_SUB_FOLDER"
 }
 
