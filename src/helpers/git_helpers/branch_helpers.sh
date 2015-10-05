0646f95420376188cde79f0416085460568af424
Allen Wu <allen.wu@originate.com>
2015-10-04 20:43:24 -0700

    Preserve checkout history
    closes #668
    resolves #65

--- src/helpers/git_helpers/branch_helpers.sh
+++ src/helpers/git_helpers/branch_helpers.sh
@@ -13,6 +13,13 @@ function branch_needs_push {
 }
 
 
+# Checkout a branch
+function checkout_branch_silently {
+  local branch_name=$1
+  run_command_silently "git checkout $branch_name"
+}
+
+
 # Creates a new branch with the given name off the given parent branch
 function create_branch {
   local new_branch_name=$1
@@ -72,6 +79,38 @@ function ensure_has_branch {
     echo_error_header
     echo_error "There is no branch named '$branch_name'"
     exit_with_error newline
+  fi
+}
+
+
+# Returns the branch that a user would expect when running `git checkout -`
+function expected_previous_branch {
+  local initial_previous_branch=$1
+  local initial_branch=$2
+
+  # previous branch still exists
+  if [ "$(has_local_branch "$initial_previous_branch")" = true ]; then
+
+    # current branch is unchanged
+    if [ "$(get_current_branch_name)" = "$initial_branch" ]; then
+      echo "$initial_previous_branch"
+
+    # current branch is deleted
+    elif [ "$(has_local_branch "$initial_branch")" = false ]; then
+      echo "$initial_previous_branch"
+
+    # current branch is new
+    else
+      if [ "$(has_local_branch "$initial_branch")" = true ]; then
+        echo "$initial_branch"
+      else
+        echo "$MAIN_BRANCH_NAME"
+      fi
+    fi
+
+  # previous branch is deleted
+  else
+    echo "$MAIN_BRANCH_NAME"
   fi
 }
 
@@ -146,6 +185,21 @@ function delete_local_branch_needs_force {
 }
 
 
+function preserve_checkout_history {
+  local initial_previous_branch_name=$1
+  local initial_branch_name=$2
+  local desired_previous_branch=$(expected_previous_branch "$initial_previous_branch_name" "$initial_branch_name")
+  set_previous_branch "$desired_previous_branch"
+}
+
+
+# Returns the previously checked out branch name
+function previous_branch_name {
+  # --verify and --quiet needed to suppress errors when previous branch is unresolvable
+  git rev-parse --verify --quiet --abbrev-ref "@{-1}"
+}
+
+
 # Pushes the branch with the given name to origin
 function push_branch {
   local branch_name=$1
@@ -184,6 +238,21 @@ function remote_only_merged_branches {
   comm -13 <(sort "$local_temp") <(sort "$remote_temp")
   rm "$local_temp"
   rm "$remote_temp"
+}
+
+
+function set_previous_branch {
+  local desired_previous_branch=$1
+
+  # nothing to do, exit early
+  if [ "$desired_previous_branch" = "$(previous_branch_name)" ]; then
+    return
+  fi
+
+  local current_branch="$(get_current_branch_name)"
+
+  checkout_branch_silently "$desired_previous_branch"
+  checkout_branch_silently "$current_branch"
 }
 
 
--- src/helpers/script_helpers.sh
+++ src/helpers/script_helpers.sh
@@ -13,7 +13,10 @@ function abort_command {
 function command_steps {
   echo_if_all_true "change_directory $(git_root)" "$RUN_IN_GIT_ROOT" "$IN_SUB_FOLDER"
   echo_if_all_true "stash_open_changes" "$STASH_OPEN_CHANGES" "$INITIAL_OPEN_CHANGES"
+
   steps
+
+  echo "preserve_checkout_history $INITIAL_PREVIOUS_BRANCH_NAME $INITIAL_BRANCH_NAME"
   echo_if_all_true "restore_open_changes" "$STASH_OPEN_CHANGES" "$INITIAL_OPEN_CHANGES"
   echo_if_all_true "change_directory $INITIAL_DIRECTORY" "$RUN_IN_GIT_ROOT" "$IN_SUB_FOLDER"
 }
