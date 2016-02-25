bd7499fbc6c39c5ec57b85f8ebde5484ca3e5060
allonsy <linuxbash8@gmail.com>
2016-02-24 22:22:00 -0600

    Add deb file generation

---
+++ package/debian/debian_build.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+mkdir -pv usr/lib/git-core &&
+mkdir -pv usr/share/man/man1 &&
+cp -r ../../src/* usr/lib/git-core &&
+cp -r ../../man/man1 usr/share/man/man1 &&
+chmod +x usr/lib/git-core/drivers/code_hosting/*.sh &&
+chmod +x usr/lib/git-core/helpers/*.sh &&
+chmod +x usr/lib/git-core/helpers/git_helpers/*.sh &&
+debuild -us -uc &&
+rm -rf usr
