7228b2946f3ea0d56e9d5eb1843c806f5ffe7fc4
Kevin Goslar <kevin.goslar@gmail.com>
2016-08-24 17:57:22 -0700

    Sort alphabetically

--- src/helpers/string_helpers.sh
+++ src/helpers/string_helpers.sh
@@ -3,24 +3,6 @@
 # Helper functions for working with strings
 
 
-# Split string with delimiter onto separate lines
-function split_string {
-  local string=$1
-  local delimiter=$2
-
-  echo "$string" | tr "$delimiter" '\n' | sed 's/^ *//;s/ *$//'
-}
-
-
-# Join a previously split string with delimiter
-function join_string {
-  local string=$1
-  local delimiter=$2
-
-  echo "$string" | tr '\n' "$delimiter" | sed "s/^ *$delimiter//;s/$delimiter *$//"
-}
-
-
 # Insert string into a delimiter-separated string
 function insert_string {
   local delimiter_separated_string=$1
@@ -32,14 +14,12 @@ function insert_string {
 }
 
 
-# Remove string from delimiter-separated string
-function remove_string {
-  local delimiter_separated_string=$1
+# Join a previously split string with delimiter
+function join_string {
+  local string=$1
   local delimiter=$2
-  local string_to_remove=$3
 
-  local split="$(split_string "$delimiter_separated_string" "$delimiter")"
-  join_string "$(echo "$split" | sed "s/^${string_to_remove}$//;/^$/d")" "$delimiter"
+  echo "$string" | tr '\n' "$delimiter" | sed "s/^ *$delimiter//;s/$delimiter *$//"
 }
 
 
@@ -54,3 +34,25 @@ function parameters_as_string {
   done
   echo "${str/ /}" # Remove initial space
 }
+
+
+# Remove string from delimiter-separated string
+function remove_string {
+  local delimiter_separated_string=$1
+  local delimiter=$2
+  local string_to_remove=$3
+
+  local split="$(split_string "$delimiter_separated_string" "$delimiter")"
+  join_string "$(echo "$split" | sed "s/^${string_to_remove}$//;/^$/d")" "$delimiter"
+}
+
+
+# Split string with delimiter onto separate lines
+function split_string {
+  local string=$1
+  local delimiter=$2
+
+  echo "$string" | tr "$delimiter" '\n' | sed 's/^ *//;s/ *$//'
+}
+
+
