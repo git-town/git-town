a72441020a50dbf24b71676f942934d5288cff64
David Gethings <dgethings@juniper.net>
2017-03-23 11:07:44 +0000

    set dest branch to default to $MAIN_BRANCH_NAME when creating a pull request

--- src/drivers/code_hosting/gitlab.sh
+++ src/drivers/code_hosting/gitlab.sh
@@ -4,7 +4,7 @@
 function create_pull_request {
   local repository=$1
   local src="merge_request%5Bsource_branch%5D=${2}"
-  local dest="merge_request%5Btarget_branch%5D=${3:-master}"
+  local dest="merge_request%5Btarget_branch%5D=${3:-$MAIN_BRANCH_NAME}"
 
   open_browser "https://gitlab.com/$repository/merge_requests/new?${src}\&${dest}"
 }
