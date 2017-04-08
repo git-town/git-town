4c92373a3121e2824aeeb42b6af837c5a52403a0
Charles Rudolph <charles.w.rudolph@gmail.com>
2017-04-08 09:47:47 -0500

    revert

--- lib/steps/fetch_step.go
+++ lib/steps/fetch_step.go
@@ -6,8 +6,6 @@ import (
 	"github.com/Originate/git-town/lib/script"
 )
 
-var fetched bool
-
 type FetchStep struct {
 	NoAutomaticAbort
 }
@@ -25,12 +23,9 @@ func (step FetchStep) CreateUndoStep() Step {
 }
 
 func (step FetchStep) Run() error {
-	if !fetched {
-		err := script.RunCommand("git", "fetch", "--prune")
-		if err != nil {
-			log.Fatal(err)
-		}
-		fetched = true
+	err := script.RunCommand("git", "fetch", "--prune")
+	if err != nil {
+		log.Fatal(err)
 	}
 	return nil
 }
