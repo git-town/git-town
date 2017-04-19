173ae8f9964be016eeec12272fbbb81b03b1bdb9
Charlie Rudolph <charles.w.rudolph@gmail.com>
2017-04-19 14:51:25 -0700

    implement `gt set-parent-branch` (#863)

--- cmd/shared.go
+++ cmd/shared.go
@@ -11,6 +11,21 @@ var (
 	undoFlag bool
 )
 
+func validateArgsCount(args []string, count int) error {
+	err := validateMinArgs(args, count)
+	if err != nil {
+		return err
+	}
+	return validateMaxArgs(args, count)
+}
+
+func validateMinArgs(args []string, min int) error {
+	if len(args) < min {
+		return errors.New("Too few arguments")
+	}
+	return nil
+}
+
 func validateMaxArgs(args []string, max int) error {
 	if len(args) > max {
 		return errors.New("Too many arguments")
