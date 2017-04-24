8ee1699b1ab9de6000cd08443a6fc4b5e237a193
Charles Rudolph <charles.w.rudolph@gmail.com>
2017-04-24 10:38:15 -0700

    fix lint

--- lib/git/environment.go
+++ lib/git/environment.go
@@ -2,11 +2,14 @@ package git
 
 import "github.com/Originate/git-town/lib/util"
 
+// EnsureIsRepository asserts that the current directory is in a repository
+func EnsureIsRepository() {
+	util.Ensure(isRepository(), "This is not a git repository.")
+}
+
+// Helpers
+
 func isRepository() bool {
 	_, err := util.GetFullCommandOutput("git", "rev-parse")
 	return err == nil
-}
-
-func EnsureIsRepository() {
-	util.Ensure(isRepository(), "This is not a git repository.")
 }
