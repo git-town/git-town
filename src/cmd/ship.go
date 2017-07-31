4ebbf022b46cb766d050920b2c57d25d4a47ad0d
Kevin Goslar <kevin.goslar@gmail.com>
2017-07-31 04:04:38 -0700

    Improve variable naming

--- src/cmd/ship.go
+++ src/cmd/ship.go
@@ -94,17 +94,17 @@ func ensureParentBranchIsMainBranch(branchName string) {
 }
 
 func getShipStepList(config shipConfig) (result steps.StepList) {
-	mainBranch := git.GetMainBranch()
+	targetBranch := git.GetMainBranch()
 	areInitialAndTargetDifferent := config.BranchToShip != config.InitialBranch
-	result.AppendList(steps.GetSyncBranchSteps(mainBranch))
+	result.AppendList(steps.GetSyncBranchSteps(targetBranch))
 	result.Append(steps.CheckoutBranchStep{BranchName: config.BranchToShip})
 	result.Append(steps.MergeTrackingBranchStep{})
-	result.Append(steps.MergeBranchStep{BranchName: mainBranch})
+	result.Append(steps.MergeBranchStep{BranchName: targetBranch})
 	result.Append(steps.EnsureHasShippableChangesStep{BranchName: config.BranchToShip})
-	result.Append(steps.CheckoutBranchStep{BranchName: mainBranch})
+	result.Append(steps.CheckoutBranchStep{BranchName: targetBranch})
 	result.Append(steps.SquashMergeBranchStep{BranchName: config.BranchToShip, CommitMessage: commitMessage})
 	if git.HasRemote("origin") {
-		result.Append(steps.PushBranchStep{BranchName: mainBranch, Undoable: true})
+		result.Append(steps.PushBranchStep{BranchName: targetBranch, Undoable: true})
 	}
 	childBranches := git.GetChildBranches(config.BranchToShip)
 	if git.HasTrackingBranch(config.BranchToShip) && len(childBranches) == 0 {
@@ -113,7 +113,7 @@ func getShipStepList(config shipConfig) (result steps.StepList) {
 	result.Append(steps.DeleteLocalBranchStep{BranchName: config.BranchToShip})
 	result.Append(steps.DeleteParentBranchStep{BranchName: config.BranchToShip})
 	for _, child := range childBranches {
-		result.Append(steps.SetParentBranchStep{BranchName: child, ParentBranchName: mainBranch})
+		result.Append(steps.SetParentBranchStep{BranchName: child, ParentBranchName: targetBranch})
 	}
 	result.Append(steps.DeleteAncestorBranchesStep{})
 	if areInitialAndTargetDifferent {
