e8a55dae3ab57fe281b6e864d3be4a394648c054
Kevin Goslar <kevin.goslar@gmail.com>
2017-08-06 06:35:06 -0700

    Ruby 2.4.1

--- features/support/tool_helpers.rb
+++ features/support/tool_helpers.rb
@@ -1,4 +1,5 @@
 # frozen_string_literal: true
+
 # Configures the test environment to assume only the given set of tools are installed
 def update_installed_tools tools
   File.open(TOOLS_INSTALLED_FILENAME, 'w') do |file|
