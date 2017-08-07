10980fb75fdca72927604b21b9bc41c27ccbe513
Kevin Goslar <kevin.goslar@gmail.com>
2017-08-07 07:48:25 -0700

    Improve test

--- features/step_definitions/branch_steps.rb
+++ features/step_definitions/branch_steps.rb
@@ -65,6 +65,11 @@ Given(/^my coworker has a feature branch named "(.+?)"(?: (behind|ahead of) main
 end
 
 
+Given(/^my repository knows about the remote branch$/) do
+  run "git fetch"
+end
+
+
 Given(/the "(.+?)" branch gets deleted on the remote/) do |branch_name|
   in_repository :coworker do
     run "git push origin :#{branch_name}"
