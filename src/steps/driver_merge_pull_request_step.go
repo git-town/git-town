f1fc6a4726fb6aa13a1730751b34e668ab165d57
Charles Rudolph <charles.w.rudolph@gmail.com>
2017-08-13 17:23:13 -0700

    update

--- src/steps/driver_merge_pull_request_step.go
+++ src/steps/driver_merge_pull_request_step.go
@@ -13,15 +13,41 @@ import (
 // DriverMergePullRequestStep squash merges the branch with the given name into the current branch
 type DriverMergePullRequestStep struct {
 	NoOpStep
-	BranchName    string
-	CommitMessage string
-	Driver        drivers.CodeHostingDriver
+	BranchName                string
+	CommitMessage             string
+	Driver                    drivers.CodeHostingDriver
+	enteredEmptyCommitMessage bool
+	mergeError                error
+	mergeSha                  string
+}
+
+// CreateAbortStep returns the abort step for this step.
+func (step *DriverMergePullRequestStep) CreateAbortStep() Step {
+	if step.enteredEmptyCommitMessage {
+		return &DiscardOpenChangesStep{}
+	}
+	return nil
+}
+
+// CreateUndoStepAfterRun returns the undo step for this step after it is run.
+func (step *DriverMergePullRequestStep) CreateUndoStepAfterRun() Step {
+	return &RevertCommitStep{Sha: step.mergeSha}
+}
+
+// GetAutomaticAbortErrorMessage returns the error message to display when this step
+// cause the command to automatically abort.
+func (step *DriverMergePullRequestStep) GetAutomaticAbortErrorMessage() string {
+	if step.enteredEmptyCommitMessage {
+		return "Aborted because commit exited with error"
+	}
+	return step.mergeError.Error()
 }
 
 // Run executes this step.
 func (step *DriverMergePullRequestStep) Run() error {
 	commitMessage := step.CommitMessage
 	if commitMessage != "" {
+		step.enteredEmptyCommitMessage = true
 		err := script.RunCommand("git", "merge", "--squash", step.BranchName)
 		if err != nil {
 			log.Fatal("Error squash merging:", err)
@@ -36,16 +62,18 @@ func (step *DriverMergePullRequestStep) Run() error {
 		if err != nil {
 			log.Fatal("Error resetting the main branch", err)
 		}
+		step.enteredEmptyCommitMessage = false
 	}
 	repository := git.GetURLRepositoryName(git.GetRemoteOriginURL())
 	repositoryParts := strings.SplitN(repository, "/", 2)
-	return step.Driver.MergePullRequest(drivers.MergePullRequestOptions{
+	step.mergeSha, step.mergeError = step.Driver.MergePullRequest(drivers.MergePullRequestOptions{
 		Branch:        step.BranchName,
 		CommitMessage: commitMessage,
 		ParentBranch:  git.GetCurrentBranchName(),
 		Owner:         repositoryParts[0],
 		Repository:    repositoryParts[1],
 	})
+	return step.mergeError
 }
 
 // ShouldAutomaticallyAbortOnError returns whether this step should cause the command to
