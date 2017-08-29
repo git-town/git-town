850e12c9d7c26a448c1f1313dbc994192eb0849f
Alec Snyder <linuxbash8@gmail.com>
2017-08-29 15:32:12 -0700

    update with version number

--- package/debian/make_deb.sh
+++ package/debian/make_deb.sh
@@ -1,8 +1,20 @@
 #!/bin/bash
+
+VER_NUM=$(echo $TRAVIS_TAG | cut -d 'v' -f 2) # extract version number
+TIMESTAMP=$(date "+%a, %d %b %Y %H:%M:%S %z")
+
+cat << EOF > debian/changelog
+gittown ($VER_NUM) RELEASED; urgency=low
+
+  * See https://github.com/Originate/git-town/releases for a complete changelog
+
+ -- allonsy <alec.snyder@originate.com> $TIMESTAMP
+EOF
+
 cp dist/git-town-linux-amd64 package/debian/git-town
 cd package/debian
 sudo apt-get -qq update
 sudo apt-get install -y build-essential debhelper devscripts
 ./debian_build.sh
 cd ../..
-cp package/gittown_4.2.1_amd64.deb dist
+cp package/gittown_"$VER_NUM"_amd64.deb dist
