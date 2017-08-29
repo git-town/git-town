db60db7554fa66411c3b293d18e8a8dbbbbaa050
Alec Snyder <linuxbash8@gmail.com>
2017-08-29 10:28:10 -0700

    update make deb file

--- package/debian/make_deb.sh
+++ package/debian/make_deb.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 cp dist/git-town-linux-amd64 package/debian/git-town
 cd package/debian
-sudo apt-get install build-essential debhelper devscripts
-./debian_build
-echo "success"
+sudo apt-get install -y build-essential debhelper devscripts
+./debian_build.sh
+cd ../..
+cp package/gittown_4.2.1_amd64.deb dist
