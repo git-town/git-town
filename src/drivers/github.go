bf8b4c7cb7c2e3f2621c075f4520bde3ebd1c014
d041705 <ma.kohler@sap.com>
2017-08-31 15:18:43 +0200

    support github enterprise, for now the hostname has to be github

--- src/drivers/github.go
+++ src/drivers/github.go
@@ -47,7 +47,7 @@ func (d *githubCodeHostingDriver) GetNewPullRequestURL(branch string, parentBran
 }
 
 func (d *githubCodeHostingDriver) GetRepositoryURL() string {
-	return fmt.Sprintf("https://github.com/%s/%s", d.owner, d.repository)
+	return fmt.Sprintf("https://%s/%s/%s", d.hostname, d.owner, d.repository)
 }
 
 func (d *githubCodeHostingDriver) MergePullRequest(options MergePullRequestOptions) (string, error) {
