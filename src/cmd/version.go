ce65fbaa3daf2c00a503e2f32e4add122b3c1113
Charles Rudolph <charles.w.rudolph@gmail.com>
2017-10-13 13:01:04 -0600

    update

--- src/cmd/version.go
+++ src/cmd/version.go
@@ -11,7 +11,7 @@ var versionCmd = &cobra.Command{
 	Use:   "version",
 	Short: "Displays the version",
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("Git Town 4.2.1")
+		fmt.Println("Git Town 5.0.0")
 	},
 }
 
