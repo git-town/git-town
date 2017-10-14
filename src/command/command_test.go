946c9050eb307d1d5c2de24b732f68bd8c926c35
Kevin Goslar <kevin.goslar@gmail.com>
2017-10-14 13:14:11 -0700

    tests

--- src/command/command_test.go
+++ src/command/command_test.go
@@ -1,44 +1,142 @@
 package command_test
 
 import (
+	"time"
+
 	"github.com/Originate/git-town/src/command"
 
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 )
 
+var cmd *command.Command
+
 var _ = Describe("New", func() {
 
-	Context("with a single parameter", func() {
-		It("creates a new Cmd instance with the given command", func() {
-			cmd := command.New("ls")
-			Expect(cmd.Name).To(Equal("ls"))
-			Expect(cmd.Args).To(Equal([]string{}))
-		})
+	BeforeEach(func() {
+		cmd = command.New("cd", ".")
 	})
 
-	Context("with multiple parameters", func() {
-		It("creates a new Cmd instance with the given command and arguments", func() {
-			cmd := command.New("ls", "-la", "*.go")
-			Expect(cmd.Name).To(Equal("ls"))
-			Expect(cmd.Args).To(Equal([]string{"-la", "*.go"}))
-		})
+	It("creates a new Cmd instance", func() {
+		Expect(cmd).ToNot(BeNil())
+	})
+})
+
+var _ = Describe("Run", func() {
+
+	BeforeEach(func() {
+		cmd = command.New("cd", ".")
 	})
 
+	It("runs the given command", func() {
+		cmd.Run()
+	})
+
+	Describe("Idempotency", func() {
+
+		BeforeEach(func() {
+			cmd = command.New("ruby", "-e", "puts Time.now.to_f")
+		})
+
+		It("does not re-run the command", func() {
+			cmd.Run()
+			firstOutput := cmd.Output()
+			time.Sleep(1 * time.Millisecond)
+			cmd.Run()
+			secondOutput := cmd.Output()
+			Expect(firstOutput).To(Equal(secondOutput))
+		})
+	})
 })
 
 var _ = Describe("Output", func() {
 
-	It("returns the output of this command", func() {
-		cmd := command.New("echo", "foo")
-		Expect(cmd.Output()).To(Equal("foo"))
+	Describe("return value", func() {
+
+		BeforeEach(func() {
+			cmd = command.New("echo", "foo")
+		})
+
+		It("returns the output of this command", func() {
+			cmd.Run()
+			Expect(cmd.Output()).To(Equal("foo"))
+		})
+
+		It("runs the command if it hasn't run so far", func() {
+			Expect(cmd.Output()).To(Equal("foo"))
+		})
+	})
+
+	Describe("Idempotency", func() {
+
+		BeforeEach(func() {
+			cmd = command.New("ruby", "-e", "puts Time.now.to_f")
+		})
+
+		It("does not re-run the command", func() {
+			firstOutput := cmd.Output()
+			time.Sleep(1 * time.Millisecond)
+			secondOutput := cmd.Output()
+			Expect(firstOutput).To(Equal(secondOutput))
+		})
 	})
 })
 
 var _ = Describe("OutputContainsText", func() {
 
-	It("returns whether the output contains the given text", func() {
-		cmd := command.New("echo", "hello world how are you?")
+	BeforeEach(func() {
+		cmd = command.New("echo", "hello world how are you?")
+	})
+
+	It("returns true if the output contains the given text", func() {
 		Expect(cmd.OutputContainsText("world")).To(BeTrue())
+	})
+
+	It("returns false if the output does not contain the given text", func() {
+		Expect(cmd.OutputContainsText("zonk")).To(BeFalse())
+	})
+})
+
+var _ = Describe("OutputContainsLine", func() {
+
+	BeforeEach(func() {
+		cmd = command.New("echo", "hello world")
+	})
+
+	It("returns true if the output contains the given line", func() {
+		Expect(cmd.OutputContainsLine("hello world")).To(BeTrue())
+	})
+
+	It("returns false if the output contains only parts of the given line", func() {
+		Expect(cmd.OutputContainsLine("hello")).To(BeFalse())
+	})
+
+	It("returns false if the output does not contains the given line", func() {
+		Expect(cmd.OutputContainsLine("zonk")).To(BeFalse())
+	})
+})
+
+var _ = Describe("Err", func() {
+
+	Context("command not found", func() {
+
+		BeforeEach(func() {
+			cmd = command.New("zonk")
+		})
+
+		It("returns an error", func() {
+			Expect(cmd.Err()).To(HaveOccurred())
+		})
+	})
+
+	Context("command returns exit code", func() {
+
+		BeforeEach(func() {
+			cmd = command.New("bash", "-c", "exit 2")
+		})
+
+		It("returns an error", func() {
+			Expect(cmd.Err()).To(HaveOccurred())
+		})
 	})
 })
