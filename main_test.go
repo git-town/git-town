81053b853acbcd9da19ef94fea0a8dba923d1a71
Kevin Goslar <kevin.goslar@gmail.com>
2017-11-19 15:21:55 -0800

    Main test

--- main_test.go
+++ main_test.go
@@ -1,11 +1,11 @@
 package main
 
-// This file is mandatory as otherwise the filebeat.test binary is not generated correctly.
 import (
+	"fmt"
 	"testing"
 )
 
-// Test started when the test binary is started. Only calls main.
-func TestSystem(t *testing.T) {
+func TestRunMain(t *testing.T) {
 	main()
+	fmt.Println("DONE")
 }
