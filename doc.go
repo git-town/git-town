5b03f7792ed45d82e5dbd7b98578cfa77a8706ff
Kevin Goslar <kevin.goslar@gmail.com>
2017-11-21 00:43:25 +0100

    Godoc

---
+++ doc.go
@@ -0,0 +1,19 @@
+/*
+Git Town - a high-level CLI for Git
+
+Git Town provides additional Git commands
+that make working in large teams
+easy and fun.
+
+More information at https://github.com/Originate/git-town
+
+Installation alternatives:
+	- HomeBrew (macOS): brew install git-town
+	- AUR package for Arch Linux: https://aur.archlinux.org/packages/git-town
+	- go get github.com/Originate/git-town
+	- direct download of platform-specific binaries: https://github.com/Originate/git-town/releases/latest
+
+Usage:
+  git town help
+*/
+package main
