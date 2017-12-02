d1c11cc22050089f43c4fcf800e149ffb0882895
Kevin Goslar <kevin.goslar@gmail.com>
2017-12-02 11:27:40 +0100

    Command class (#1045)

--- src/steps/preserve_checkout_history_step.go
+++ src/steps/preserve_checkout_history_step.go
@@ -1,8 +1,8 @@
 package steps
 
 import (
+	"github.com/Originate/git-town/src/command"
 	"github.com/Originate/git-town/src/git"
-	"github.com/Originate/git-town/src/util"
 )
 
 // PreserveCheckoutHistoryStep does stuff
@@ -17,8 +17,8 @@ func (step *PreserveCheckoutHistoryStep) Run() error {
 	expectedPreviouslyCheckedOutBranch := git.GetExpectedPreviouslyCheckedOutBranch(step.InitialPreviouslyCheckedOutBranch, step.InitialBranch)
 	if expectedPreviouslyCheckedOutBranch != git.GetPreviouslyCheckedOutBranch() {
 		currentBranch := git.GetCurrentBranchName()
-		util.GetCommandOutput("git", "checkout", expectedPreviouslyCheckedOutBranch)
-		util.GetCommandOutput("git", "checkout", currentBranch)
+		command.New("git", "checkout", expectedPreviouslyCheckedOutBranch).Run()
+		command.New("git", "checkout", currentBranch).Run()
 	}
 	return nil
 }
