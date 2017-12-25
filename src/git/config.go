e5e4afcde43e6bf6a245f8e32579a644563e5b56
Charles Rudolph <charles.w.rudolph@gmail.com>
2017-12-25 10:06:58 -0700

    update

--- src/git/config.go
+++ src/git/config.go
@@ -170,18 +170,10 @@ func IsAncestorBranch(branchName, ancestorBranchName string) bool {
 	return util.DoesStringArrayContain(ancestorBranches, ancestorBranchName)
 }
 
-// Remotes are cached in order to minimize the number of git commands run
-var remotes []string
-var remotesInitialized bool
-
 // HasRemote returns whether the current repository contains a Git remote
 // with the given name.
 func HasRemote(name string) bool {
-	if !remotesInitialized {
-		remotes = strings.Split(command.New("git", "remote").Output(), "\n")
-		remotesInitialized = true
-	}
-	return util.DoesStringArrayContain(remotes, name)
+	return util.DoesStringArrayContain(getRemotes(), name)
 }
 
 // IsFeatureBranch returns whether the branch with the given name is
@@ -302,6 +294,18 @@ func setGlobalConfigurationValue(key, value string) {
 func removeConfigurationValue(key string) {
 	command.New("git", "config", "--unset", key).Run()
 	configMap.Delete(key)
+}
+
+// Remotes are cached in order to minimize the number of git commands run
+var remotes []string
+var remotesInitialized bool
+
+func getRemotes() []string {
+	if !remotesInitialized {
+		remotes = strings.Split(command.New("git", "remote").Output(), "\n")
+		remotesInitialized = true
+	}
+	return remotes
 }
 
 // Init
