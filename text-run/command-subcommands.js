29875f6bb4fb6ff6863e8761799f4f5f4a5e0f8c
Kevin Goslar <kevin.goslar@gmail.com>
2018-07-02 13:03:05 -0700

    subcommands

---
+++ text-run/command-subcommands.js
@@ -0,0 +1,43 @@
+const child_process = require('child_process')
+const diff = require('jsdiff-console')
+const path = require('path')
+
+module.exports = async function (activity) {
+  const markdownCommands = await getMdCommands(activity.nodes)
+  const gitTownCommands = getGitTownCommands(activity)
+  diff(markdownCommands, gitTownCommands)
+}
+
+function getCommand (activity) {
+  return path.basename(activity.file, '.md')
+}
+
+function getGitTownCommands (activity) {
+  const result = []
+  const command = getCommand(activity)
+  const gitTownOutput = child_process
+    .execSync(`git-town help ${command}`)
+    .toString()
+  const matches = gitTownOutput.match(/\nAvailable Commands:\n(.*)/)
+  const text = matches[1]
+  for (const line of text.split('\n')) {
+    const words = line.trim().split(/\s+/)
+    const command = words[0]
+    const desc = words.slice(1).join(' ')
+    result.push([command, desc])
+  }
+  return result
+}
+
+async function getMdCommands (nodes) {
+  const result = []
+  const table = nodes.getNodeOfTypes('table_open')
+  const tableNodes = nodes.getNodesFor(table)
+  for (const tableRow of tableNodes.getNodesOfTypes('table_row_open')) {
+    const rowNodes = tableNodes.getNodesFor(tableRow)
+    const commandName = rowNodes.getNodeOfTypes('table_heading')
+    const commandDesc = rowNodes.getNodeOfTypes('table_cell')
+    result.push([commandName.content, commandDesc.content])
+  }
+  return result
+}
