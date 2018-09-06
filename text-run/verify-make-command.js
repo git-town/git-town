5bb0c66ca35f1ed4df97f896f99521d920b9d2c1
Kevin Goslar <kevin.goslar@originate.com>
2018-09-06 10:49:38 -0500

    Format

--- text-run/verify-make-command.js
+++ text-run/verify-make-command.js
@@ -1,36 +1,36 @@
-const { cyan } = require("chalk");
-const fs = require("fs");
-const os = require("os");
-const path = require("path");
-const util = require("util");
-const readFile = util.promisify(fs.readFile);
+const { cyan } = require('chalk')
+const fs = require('fs')
+const os = require('os')
+const path = require('path')
+const util = require('util')
+const readFile = util.promisify(fs.readFile)
 
-module.exports = async function(args) {
-  const expected = args.nodes
+module.exports = async function ({ configuration, formatter, nodes }) {
+  const expected = nodes
     .text()
-    .replace(/make\s+/, "")
-    .trim();
-  args.formatter.name(`verify Make command ${cyan(expected)} exists`);
-  const makefilePath = path.join(args.configuration.sourceDir, "Makefile");
-  const makefileContent = await readFile(makefilePath, "utf8");
+    .replace(/make\s+/, '')
+    .trim()
+  formatter.name(`verify Make command ${cyan(expected)} exists`)
+  const makefilePath = path.join(configuration.sourceDir, 'Makefile')
+  const makefileContent = await readFile(makefilePath, 'utf8')
   const commands = makefileContent
     .split(os.EOL)
     .filter(lineDefinesMakeCommand)
-    .map(extractMakeCommand);
+    .map(extractMakeCommand)
   if (!commands.includes(expected)) {
-    throw new Error(`Make command ${cyan(expected)} not found in ${commands}`);
+    throw new Error(`Make command ${cyan(expected)} not found in ${commands}`)
   }
-};
+}
 
 // returns whether the given line from a Makefile
 // defines a Make command
-function lineDefinesMakeCommand(line) {
-  return makeCommandRE.test(line);
+function lineDefinesMakeCommand (line) {
+  return makeCommandRE.test(line)
 }
-const makeCommandRE = /^[^ ]+:/;
+const makeCommandRE = /^[^ ]+:/
 
 // returns the defined command name
 // from a Makefile line that defines a Make command
-function extractMakeCommand(line) {
-  return line.split(":")[0];
+function extractMakeCommand (line) {
+  return line.split(':')[0]
 }
