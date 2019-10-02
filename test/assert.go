f164418079a610825f666da89e561bfb69f2bde3
Kevin Goslar <kevin.goslar@gmail.com>
2019-10-02 09:21:04 -0500

    progress

--- test/assert.go
+++ test/assert.go
@@ -1,11 +1,15 @@
 package test
 
 import (
+	"fmt"
 	"io/ioutil"
+	"log"
 	"os"
 	"path"
+	"strings"
 	"testing"
 
+	"github.com/sergi/go-diff/diffmatchpatch"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -20,4 +24,33 @@ func assertFileExistsWithContent(t *testing.T, dir, filename, expectedContent st
 	fileContent, err := ioutil.ReadFile(path.Join(dir, filename))
 	assert.Nil(t, err)
 	assert.Equal(t, expectedContent, string(fileContent))
+}
+
+func assertFolderExists(t *testing.T, dir string) {
+	_, err := os.Stat(dir)
+	assert.Falsef(t, os.IsNotExist(err), "directory %q not found", dir)
+}
+
+func assertHasGitBranches(t *testing.T, dir, expectedBranches string) {
+	runner := ShellRunner{}
+	err := os.Chdir(dir)
+	assert.Nil(t, err)
+	runResult := runner.Run("git", "branch")
+	assert.Nilf(t, runResult.Err, "cannot run 'git status' in %q", dir)
+	dmp := diffmatchpatch.New()
+	diffs := dmp.DiffMain(strings.TrimSpace(expectedBranches), strings.TrimSpace(runResult.Output), false)
+	if len(diffs) > 1 {
+		fmt.Println(dmp.DiffPrettyText(diffs))
+		log.Fatalf("folder %q has the wrong Git branches", dir)
+	}
+}
+
+func assertIsBareGitRepo(t *testing.T, dir string) {
+	assertFolderExists(t, dir)
+	entries, err := ioutil.ReadDir(dir)
+	assert.Nilf(t, err, "cannot list directory %q", dir)
+	testData := []string{"HEAD", "branches", "config", "description", "hooks", "info", "objects", "refs"}
+	for i, expected := range testData {
+		assert.Equal(t, expected, entries[i].Name())
+	}
 }
--- test/git_environment_test.go
+++ test/git_environment_test.go
@@ -10,38 +10,29 @@ import (
 	"testing"
 
 	"github.com/sergi/go-diff/diffmatchpatch"
+	"github.com/stretchr/testify/assert"
 )
 
 func TestGitEnvironmentCreateScenarioSetup(t *testing.T) {
 	gitEnvRootDir, err := ioutil.TempDir("", "")
-	if err != nil {
-		t.Error("cannot create TempDir", err)
-	}
+	assert.Nil(t, err, "cannot create TempDir")
 	gitEnv, err := NewGitEnvironment(gitEnvRootDir)
-	if err != nil {
-		t.Error("cannot create new GitEnvironment", err)
-	}
+	assert.Nil(t, err, "cannot create new GitEnvironment")
 	err = gitEnv.Populate()
-	if err != nil {
-		t.Error("cannot populate GitEnvironment", err)
-	}
-	verifyIsBareGitRepo(path.Join(gitEnvRootDir, "origin"))
+	assert.Nil(t, err, "cannot populate GitEnvironment")
+	assertIsBareGitRepo(t, path.Join(gitEnvRootDir, "origin"))
 
 	// verify the new GitEnvironment has a "developer" folder
 	devDir := path.Join(gitEnvRootDir, "developer")
-	verifyFolderExists(devDir)
+	assertFolderExists(t, devDir)
 
 	// verify the "developer" folder contains a Git repo with a main branch
-	verifyFolderExists(path.Join(devDir, ".git"))
+	assertFolderExists(t, path.Join(devDir, ".git"))
 	runner := ShellRunner{}
 	err = os.Chdir(devDir)
-	if err != nil {
-		log.Fatal("cannot enter developer dir of GitEnvironment", err)
-	}
+	assert.Nil(t, err, "cannot enter developer dir of GitEnvironment")
 	runResult := runner.Run("git", "branch")
-	if runResult.Err != nil {
-		log.Fatalf("cannot run 'git branch' in %q: %s", devDir, runResult.Err)
-	}
+	assert.Nilf(t, runResult.Err, "cannot run 'git branch' in %q", devDir)
 	dmp := diffmatchpatch.New()
 	expected := "* main"
 	diffs := dmp.DiffMain(strings.TrimSpace(expected), strings.TrimSpace(runResult.Output), false)
@@ -53,74 +44,18 @@ func TestGitEnvironmentCreateScenarioSetup(t *testing.T) {
 
 func TestGitEnvironmentCloneEnvironment(t *testing.T) {
 	dir, err := ioutil.TempDir("", "")
-	if err != nil {
-		t.Error("cannot create temp dir", err)
-	}
+	assert.Nil(t, err, "cannot create temp dir")
 	memoizedGitEnv, err := NewGitEnvironment(path.Join(dir, "memoized"))
-	if err != nil {
-		t.Error("cannot create memoized GitEnvironment", err)
-	}
+	assert.Nil(t, err, "cannot create memoized GitEnvironment")
 	err = memoizedGitEnv.Populate()
-	if err != nil {
-		t.Error("cannot populate memoized GitEnvironment", err)
-	}
+	assert.Nil(t, err, "cannot populate memoized GitEnvironment")
 	_, err = CloneGitEnvironment(memoizedGitEnv, path.Join(dir, "cloned"))
-	if err != nil {
-		log.Fatalf("cannot clone GitEnvironment: %s", err)
-	}
+	assert.Nil(t, err, "cannot clone GitEnvironment")
 
 	// verify that the GitEnvironment was properly cloned
-	verifyIsBareGitRepo(path.Join(dir, "cloned", "origin"))
+	assertIsBareGitRepo(t, path.Join(dir, "cloned", "origin"))
 	devDir := path.Join(dir, "cloned", "developer")
-	verifyFolderExists(devDir)
-	verifyFolderExists(path.Join(dir, "cloned", "developer", ".git"))
-	verifyHasGitBranches(devDir, "* main")
-}
-
-func verifyFolderExists(dir string) {
-	if _, err := os.Stat(dir); os.IsNotExist(err) {
-		log.Fatalf("directory (%s) not found", dir)
-	}
-}
-
-func verifyHasGitBranches(dir, expectedBranches string) {
-	runner := ShellRunner{}
-	err := os.Chdir(dir)
-	if err != nil {
-		log.Fatal(err)
-	}
-	runResult := runner.Run("git", "branch")
-	if runResult.Err != nil {
-		log.Fatalf("cannot run 'git status' in %q: %s", dir, runResult.Err)
-	}
-	dmp := diffmatchpatch.New()
-	diffs := dmp.DiffMain(strings.TrimSpace(expectedBranches), strings.TrimSpace(runResult.Output), false)
-	if len(diffs) > 1 {
-		fmt.Println(dmp.DiffPrettyText(diffs))
-		log.Fatalf("folder %q has the wrong Git branches", dir)
-	}
-}
-
-func verifyIsBareGitRepo(dir string) {
-	verifyFolderExists(dir)
-	runner := ShellRunner{}
-	runResult := runner.Run("/bin/ls", "-1", dir)
-	if runResult.Err != nil {
-		log.Fatalf("command failed: %s", runResult.Err)
-	}
-	dmp := diffmatchpatch.New()
-	expected := `
-branches
-config
-description
-HEAD
-hooks
-info
-objects
-refs`
-	diffs := dmp.DiffMain(strings.TrimSpace(expected), strings.TrimSpace(runResult.Output), false)
-	if len(diffs) > 1 {
-		fmt.Println(dmp.DiffPrettyText(diffs))
-		log.Fatalf("folder %q is not a bare Git repo", dir)
-	}
+	assertFolderExists(t, devDir)
+	assertFolderExists(t, path.Join(dir, "cloned", "developer", ".git"))
+	assertHasGitBranches(t, devDir, "* main")
 }
