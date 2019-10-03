8a38d350b96d82ac9fa72850acf5907f0f4dc4c2
cloned <cloned@example.com>
2019-10-03 09:20:16 -0500

    assert

--- test/assert.go
+++ test/assert.go
@@ -1,11 +1,14 @@
 package test
 
 import (
+	"fmt"
 	"io/ioutil"
 	"os"
 	"path"
 	"testing"
 
+	"github.com/DATA-DOG/godog/gherkin"
+	"github.com/sergi/go-diff/diffmatchpatch"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -52,4 +55,28 @@ func assertIsNormalGitRepo(t *testing.T, dir string) {
 	for i, expected := range testData {
 		assert.Equal(t, expected, entries[i].Name())
 	}
+}
+
+// AssertStringSliceMatchesTable compares the given string slice to the given Gherkin table.
+// If they don't match, it returns an error
+// and might print additional information to the console.
+// The comparison ignores whitespace around strings.
+func AssertStringSliceMatchesTable(actual []string, expected *gherkin.DataTable) error {
+
+	// ensure we have a valid table
+	if len(expected.Rows) == 0 {
+		return fmt.Errorf("Empty table given")
+	}
+	if len(expected.Rows[0].Cells) != 1 {
+		return fmt.Errorf("Table with more than one column given")
+	}
+
+	// render the slice
+	dmp := diffmatchpatch.New()
+	diffs := dmp.DiffMain(RenderSlice(actual), RenderTable(expected), false)
+	if len(diffs) > 1 {
+		fmt.Println(dmp.DiffPrettyText(diffs))
+		return fmt.Errorf("Found %d differences", len(diffs))
+	}
+	return nil
 }
--- test/gherkin.go
+++ test/gherkin.go
@@ -1,6 +1,11 @@
 package test
 
-import "github.com/dchest/uniuri"
+import (
+	"fmt"
+
+	"github.com/DATA-DOG/godog/gherkin"
+	"github.com/dchest/uniuri"
+)
 
 // CommitTableEntry contains the elements of a Gherkin table defining commit data.
 type CommitTableEntry struct {
@@ -20,4 +25,45 @@ func NewCommitTableEntry() CommitTableEntry {
 		branch:      "main",
 		fileContent: "default file content",
 	}
+}
+
+// RenderSlice returns the Gherkin table representation of the given slice
+func RenderSlice(data []string) string {
+
+	// determine the width of the longest string
+	width := 0
+	for _, text := range data {
+		if len(text) > width {
+			width = len(text)
+		}
+	}
+
+	// render
+	result := ""
+	formatStr := fmt.Sprintf("| %%-%dv |\n", width)
+	for _, text := range data {
+		result += fmt.Sprintf(formatStr, text)
+	}
+	return result
+}
+
+// RenderTable returns the Gherkin representation of the given Gherkin table
+func RenderTable(table *gherkin.DataTable) string {
+
+	// determine the width of the table
+	width := 0
+	for _, row := range table.Rows {
+		cellWidth := len(row.Cells[0].Value)
+		if (cellWidth) > width {
+			width = cellWidth
+		}
+	}
+
+	// convert table to slice
+	slice := []string{}
+	for i := 1; i < len(table.Rows); i++ {
+		slice = append(slice, table.Rows[i].Cells[0].Value)
+	}
+
+	return RenderSlice(slice)
 }
