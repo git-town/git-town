84cb4c706e1f7eeaef76c5fdb6b4d095ba76435d
developer <developer@example.com>
2019-10-04 16:21:19 -0500

    updates

--- test/helpers/longest_string.go
+++ test/helpers/longest_string.go
@@ -3,8 +3,7 @@ package helpers
 // LongestStringLength provides the length of the longest string in the given string collection.
 func LongestStringLength(strings []string) (result int) {
 	for i := range strings {
-		currentLen := len(strings[i])
-		if currentLen > result {
+		if currentLen := len(strings[i]); currentLen > result {
 			result = currentLen
 		}
 	}
--- test/helpers/longest_string_test.go
+++ test/helpers/longest_string_test.go
@@ -9,7 +9,7 @@ import (
 func TestLongestStringLength(t *testing.T) {
 	testData := map[int][]string{
 		5: {"one", "two", "three"},
-		0: {},
+		0: {""},
 	}
 	for expected, input := range testData {
 		assert.Equal(t, expected, LongestStringLength(input))
