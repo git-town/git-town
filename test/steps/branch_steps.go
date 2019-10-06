13b0d67a38803db51359523ddd37bb259dca09d0
developer <developer@example.com>
2019-10-06 15:32:20 -0500

    end

--- test/steps/branch_steps.go
+++ test/steps/branch_steps.go
@@ -1,6 +1,8 @@
 package steps
 
 import (
+	"fmt"
+
 	"github.com/DATA-DOG/godog"
 	"github.com/pkg/errors"
 )
@@ -11,6 +13,17 @@ func BranchSteps(suite *godog.Suite, fs *FeatureState) {
 		output, err := fs.activeScenarioState.gitEnvironment.DeveloperRepo.Run("git", "checkout", branchName)
 		if err != nil {
 			return errors.Wrapf(err, "cannot change to branch %q: %s", branchName, output)
+		}
+		return nil
+	})
+
+	suite.Step(`^I end up on the "([^"]*)" branch$`, func(expected string) error {
+		actual, err := fs.activeScenarioState.gitEnvironment.DeveloperRepo.CurrentBranch()
+		if err != nil {
+			return err
+		}
+		if actual != expected {
+			return fmt.Errorf("expected active branch %q but is %q", expected, actual)
 		}
 		return nil
 	})
