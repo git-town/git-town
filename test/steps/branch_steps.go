eedacbd8552834409e301d927dddd9d8774a8a41
developer <developer@example.com>
2019-10-10 06:38:59 -0500

    Step implementation

--- test/steps/branch_steps.go
+++ test/steps/branch_steps.go
@@ -4,11 +4,20 @@ import (
 	"fmt"
 
 	"github.com/DATA-DOG/godog"
+	"github.com/DATA-DOG/godog/gherkin"
+	"github.com/Originate/git-town/src/git"
 	"github.com/pkg/errors"
 )
 
 // BranchSteps defines Cucumber step implementations around Git branches.
 func BranchSteps(suite *godog.Suite, fs *FeatureState) {
+
+	suite.Step(`^Git Town is now aware of this branch hierarchy$`, func(data *gherkin.DataTable) error {
+		branches := git.Config.GetAncestorBranches("new-child")
+		fmt.Println(branches)
+		return nil
+	})
+
 	suite.Step(`^I am on the "([^"]*)" branch$`, func(branchName string) error {
 		output, err := fs.activeScenarioState.gitEnvironment.DeveloperRepo.Run("git", "checkout", branchName)
 		if err != nil {
