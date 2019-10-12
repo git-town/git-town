3790389ee7c568fa4fb30e94f02b5bcb6c41bb03
developer <developer@example.com>
2019-10-11 21:16:56 -0500

    Remove Ginkgo

--- src/command/command_test.go
+++ src/command/command_test.go
@@ -1,76 +1,36 @@
 package command_test
 
 import (
-	"github.com/Originate/git-town/src/command"
+	"testing"
 
-	. "github.com/onsi/ginkgo"
-	. "github.com/onsi/gomega"
+	"github.com/Originate/git-town/src/command"
+	"github.com/stretchr/testify/assert"
 )
 
-var res *command.Result
+func TestCommandRun(t *testing.T) {
+	res := command.Run("echo", "foo")
+	assert.Equal(t, "foo", res.Output())
+}
 
-var _ = Describe("Run", func() {
-	It("Runs the given command", func() {
-		res = command.Run("echo", "foo")
-		Expect(res.Output()).To(Equal("foo"))
-	})
-})
+func TestCommandOutputContainsText(t *testing.T) {
+	res := command.Run("echo", "hello world how are you?")
+	assert.True(t, res.OutputContainsText("world"), "should contain 'world'")
+	assert.False(t, res.OutputContainsText("zonk"), "should not contain 'zonk'")
+}
 
-var _ = Describe("OutputContainsText", func() {
+func TestCommandOutputContainsLine(t *testing.T) {
+	res := command.Run("echo", "hello world")
+	assert.True(t, res.OutputContainsLine("hello world"), `should contain "hello world"`)
+	assert.False(t, res.OutputContainsLine("hello"), `partial match should return false`)
+	assert.False(t, res.OutputContainsLine("zonk"), `should not contain "zonk"`)
+}
 
-	BeforeEach(func() {
-		res = command.Run("echo", "hello world how are you?")
-	})
+func TestCommandErrUnknownExecutable(t *testing.T) {
+	res := command.Run("zonk")
+	assert.Error(t, res.Err())
+}
 
-	It("returns true if the output contains the given text", func() {
-		Expect(res.OutputContainsText("world")).To(BeTrue())
-	})
-
-	It("returns false if the output does not contain the given text", func() {
-		Expect(res.OutputContainsText("zonk")).To(BeFalse())
-	})
-})
-
-var _ = Describe("OutputContainsLine", func() {
-
-	BeforeEach(func() {
-		res = command.Run("echo", "hello world")
-	})
-
-	It("returns true if the output contains the given line", func() {
-		Expect(res.OutputContainsLine("hello world")).To(BeTrue())
-	})
-
-	It("returns false if the output contains only parts of the given line", func() {
-		Expect(res.OutputContainsLine("hello")).To(BeFalse())
-	})
-
-	It("returns false if the output does not contains the given line", func() {
-		Expect(res.OutputContainsLine("zonk")).To(BeFalse())
-	})
-})
-
-var _ = Describe("Err", func() {
-
-	Context("command not found", func() {
-
-		BeforeEach(func() {
-			res = command.Run("zonk")
-		})
-
-		It("returns an error", func() {
-			Expect(res.Err()).To(HaveOccurred())
-		})
-	})
-
-	Context("command returns exit code", func() {
-
-		BeforeEach(func() {
-			res = command.Run("bash", "-c", "exit 2")
-		})
-
-		It("returns an error", func() {
-			Expect(res.Err()).To(HaveOccurred())
-		})
-	})
-})
+func TestCommandErrExitCode(t *testing.T) {
+	res := command.Run("bash", "-c", "exit 2")
+	assert.Error(t, res.Err())
+}
