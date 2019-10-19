8f7cc041d16f4da94d82202db5e21546cfa96cc8
developer <developer@example.com>
2019-10-19 10:27:15 -0500

    organize imports

--- src/cmd/alias.go
+++ src/cmd/alias.go
@@ -1,9 +1,8 @@
 package cmd
 
 import (
-	"github.com/Originate/git-town/src/script"
-
 	"github.com/Originate/git-town/src/git"
+	"github.com/Originate/git-town/src/script"
 	"github.com/Originate/git-town/src/util"
 	"github.com/spf13/cobra"
 )
