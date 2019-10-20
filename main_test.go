b0ec976bf8b6609ac40dc035e5288c948b3b8bd8
developer <developer@example.com>
2019-10-20 07:54:33 -0500

    extract

--- main_test.go
+++ main_test.go
@@ -1,9 +1,6 @@
 package main_test
 
 import (
-	"os"
-	"testing"
-
 	"github.com/DATA-DOG/godog"
 	"github.com/Originate/git-town/test/steps"
 )
@@ -20,17 +17,4 @@ func FeatureContext(suite *godog.Suite) {
 	steps.PrintSteps(suite, state)
 	steps.RunSteps(suite, state)
 	steps.WorkspaceSteps(suite, state)
-}
-
-func TestMain(m *testing.M) {
-	status := godog.RunWithOptions("godog", func(s *godog.Suite) {
-		FeatureContext(s)
-	}, godog.Options{
-		Format: "progress",
-		Paths:  []string{"features/git-town-append/on-perennial-branch.feature"},
-	})
-	if st := m.Run(); st > status {
-		status = st
-	}
-	os.Exit(status)
 }
