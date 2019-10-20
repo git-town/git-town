aa7281d511e85516bd4927f437998312ea62bf54
developer <developer@example.com>
2019-10-20 09:36:25 -0500

    fix formatting

--- test/git_town_output_parser_test.go
+++ test/git_town_output_parser_test.go
@@ -9,16 +9,16 @@ import (
 func TestGitCommandsInGitTownOutput(t *testing.T) {
 	testData := map[string][]ExecutedGitCommand{
 		// simple
-		"\x1b[1m[mybranch] foo bar": []ExecutedGitCommand{
+		"\x1b[1m[mybranch] foo bar": {
 			{Command: "foo bar", Branch: "mybranch"}},
 
 		// multiline
-		"\x1b[1m[branch1] command one\n\n\x1b[1m[branch2] command two\n\n": []ExecutedGitCommand{
+		"\x1b[1m[branch1] command one\n\n\x1b[1m[branch2] command two\n\n": {
 			{Command: "command one", Branch: "branch1"},
 			{Command: "command two", Branch: "branch2"}},
 
 		// no branch
-		"\x1b[1mcommand one": []ExecutedGitCommand{
+		"\x1b[1mcommand one": {
 			{Command: "command one", Branch: ""}},
 	}
 	for input, expected := range testData {
