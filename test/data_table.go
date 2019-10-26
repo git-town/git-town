c778cfcf2c5079dd5b02caabacd3bc783071c3bd
developer <developer@example.com>
2019-10-26 16:17:19 -0500

    Address comments

--- test/data_table.go
+++ test/data_table.go
@@ -18,26 +18,26 @@ type DataTable struct {
 // FromGherkin provides a DataTable instance populated with data from the given Gherkin table.
 func FromGherkin(table *gherkin.DataTable) (result DataTable) {
 	for _, tableRow := range table.Rows {
-		mortaRow := make([]string, len(tableRow.Cells))
+		resultRow := make([]string, len(tableRow.Cells))
 		for i, tableCell := range tableRow.Cells {
-			mortaRow[i] = tableCell.Value
+			resultRow[i] = tableCell.Value
 		}
-		result.AddRow(mortaRow...)
+		result.AddRow(resultRow...)
 	}
 	return result
 }
 
 // AddRow adds the given row of table data to this table.
-func (morta *DataTable) AddRow(elements ...string) {
-	morta.cells = append(morta.cells, elements)
+func (table *DataTable) AddRow(elements ...string) {
+	table.cells = append(table.cells, elements)
 }
 
 // columns provides the table data organized into columns.
-func (morta *DataTable) columns() (result [][]string) {
-	for column := range morta.cells[0] {
+func (table *DataTable) columns() (result [][]string) {
+	for column := range table.cells[0] {
 		colData := []string{}
-		for row := range morta.cells {
-			colData = append(colData, morta.cells[row][column])
+		for row := range table.cells {
+			colData = append(colData, table.cells[row][column])
 		}
 		result = append(result, colData)
 	}
@@ -47,13 +47,13 @@ func (morta *DataTable) columns() (result [][]string) {
 // Equal indicates whether this DataTable instance is equal to the given Gherkin table.
 // If both are equal it returns an empty string,
 // otherwise a diff printable on the console.
-func (morta *DataTable) Equal(table *gherkin.DataTable) (diff string, errorCount int) {
-	if len(morta.cells) == 0 {
+func (table *DataTable) Equal(other *gherkin.DataTable) (diff string, errorCount int) {
+	if len(table.cells) == 0 {
 		return "your data is empty", 1
 	}
-	gherkinTable := FromGherkin(table)
+	gherkinTable := FromGherkin(other)
 	dmp := diffmatchpatch.New()
-	diffs := dmp.DiffMain(gherkinTable.String(), morta.String(), false)
+	diffs := dmp.DiffMain(gherkinTable.String(), table.String(), false)
 	if len(diffs) == 1 && diffs[0].Type == 0 {
 		return "", 0
 	}
@@ -61,17 +61,17 @@ func (morta *DataTable) Equal(table *gherkin.DataTable) (diff string, errorCount
 }
 
 // String provides the data in this DataTable instance formatted in Gherkin table format.
-func (morta *DataTable) String() (result string) {
+func (table *DataTable) String() (result string) {
 	// determine how to format each column
 	formatStrings := []string{}
-	for _, width := range morta.widths() {
+	for _, width := range table.widths() {
 		formatStrings = append(formatStrings, fmt.Sprintf("| %%-%dv ", width))
 	}
 
 	// render the table using this format
-	for row := range morta.cells {
-		for col := range morta.cells[row] {
-			result += fmt.Sprintf(formatStrings[col], morta.cells[row][col])
+	for row := range table.cells {
+		for col := range table.cells[row] {
+			result += fmt.Sprintf(formatStrings[col], table.cells[row][col])
 		}
 		result += "|\n"
 	}
@@ -79,8 +79,8 @@ func (morta *DataTable) String() (result string) {
 }
 
 // widths provides the widths of all columns.
-func (morta *DataTable) widths() (result []int) {
-	for _, column := range morta.columns() {
+func (table *DataTable) widths() (result []int) {
+	for _, column := range table.columns() {
 		result = append(result, helpers.LongestStringLength(column))
 	}
 	return result
