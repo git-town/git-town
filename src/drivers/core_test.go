fec0e0167d3719955d3a4cb332c110b49af85f3a
developer <developer@example.com>
2019-10-29 15:25:26 -0500

    convert

--- src/drivers/core_test.go
+++ src/drivers/core_test.go
@@ -1,75 +1,69 @@
 package drivers_test
 
 import (
+	"testing"
+
 	"github.com/Originate/git-town/src/drivers"
-	. "github.com/onsi/ginkgo"
-	. "github.com/onsi/gomega"
+	"github.com/stretchr/testify/assert"
 )
 
-var _ = Describe("CodeHostingDriver", func() {
-	Describe("GetDriver", func() {
-		Describe("with driver override", func() {
-			It("works with bitbucket", func() {
-				result := drivers.GetDriver(drivers.DriverOptions{
-					DriverType: "bitbucket",
-					OriginURL:  "git@self-hosted-bitbucket.com:Originate/git-town.git",
-				})
-				Expect(result).NotTo(BeNil())
-				Expect(result.HostingServiceName()).To(Equal("Bitbucket"))
-				Expect(result.GetRepositoryURL()).To(Equal("https://self-hosted-bitbucket.com/Originate/git-town"))
-			})
+func TestGetDriver_DriverOverride_Bitbucket(t *testing.T) {
 
-			It("works with github", func() {
-				result := drivers.GetDriver(drivers.DriverOptions{
-					DriverType: "github",
-					OriginURL:  "git@self-hosted-github.com:Originate/git-town.git",
-				})
-				Expect(result).NotTo(BeNil())
-				Expect(result.HostingServiceName()).To(Equal("GitHub"))
-				Expect(result.GetRepositoryURL()).To(Equal("https://self-hosted-github.com/Originate/git-town"))
-			})
-
-			It("works with gitlab", func() {
-				result := drivers.GetDriver(drivers.DriverOptions{
-					DriverType: "gitlab",
-					OriginURL:  "git@self-hosted-gitlab.com:Originate/git-town.git",
-				})
-				Expect(result).NotTo(BeNil())
-				Expect(result.HostingServiceName()).To(Equal("GitLab"))
-				Expect(result.GetRepositoryURL()).To(Equal("https://self-hosted-gitlab.com/Originate/git-town"))
-			})
-		})
-
-		Describe("with origin hostname override", func() {
-			It("works with bitbucket", func() {
-				result := drivers.GetDriver(drivers.DriverOptions{
-					OriginURL:      "git@my-ssh-identity.com:Originate/git-town.git",
-					OriginHostname: "bitbucket.org",
-				})
-				Expect(result).NotTo(BeNil())
-				Expect(result.HostingServiceName()).To(Equal("Bitbucket"))
-				Expect(result.GetRepositoryURL()).To(Equal("https://bitbucket.org/Originate/git-town"))
-			})
-
-			It("works with github", func() {
-				result := drivers.GetDriver(drivers.DriverOptions{
-					OriginURL:      "git@my-ssh-identity.com:Originate/git-town.git",
-					OriginHostname: "github.com",
-				})
-				Expect(result).NotTo(BeNil())
-				Expect(result.HostingServiceName()).To(Equal("GitHub"))
-				Expect(result.GetRepositoryURL()).To(Equal("https://github.com/Originate/git-town"))
-			})
-
-			It("works with gitlab", func() {
-				result := drivers.GetDriver(drivers.DriverOptions{
-					OriginURL:      "git@my-ssh-identity.com:Originate/git-town.git",
-					OriginHostname: "gitlab.com",
-				})
-				Expect(result).NotTo(BeNil())
-				Expect(result.HostingServiceName()).To(Equal("GitLab"))
-				Expect(result.GetRepositoryURL()).To(Equal("https://gitlab.com/Originate/git-town"))
-			})
-		})
+	result := drivers.GetDriver(drivers.DriverOptions{
+		DriverType: "bitbucket",
+		OriginURL:  "git@self-hosted-bitbucket.com:Originate/git-town.git",
 	})
-})
+	assert.NotNil(t, result)
+	assert.Equal(t, "Bitbucket", result.HostingServiceName())
+	assert.Equal(t, "https://self-hosted-bitbucket.com/Originate/git-town", result.GetRepositoryURL())
+}
+
+func TestGetDriver_DriverOverride_GitHub(t *testing.T) {
+	result := drivers.GetDriver(drivers.DriverOptions{
+		DriverType: "github",
+		OriginURL:  "git@self-hosted-github.com:Originate/git-town.git",
+	})
+	assert.NotNil(t, result)
+	assert.Equal(t, "GitHub", result.HostingServiceName())
+	assert.Equal(t, "https://self-hosted-github.com/Originate/git-town", result.GetRepositoryURL())
+}
+
+func TestGetDriver_DriverOverride_GitLab(t *testing.T) {
+	result := drivers.GetDriver(drivers.DriverOptions{
+		DriverType: "gitlab",
+		OriginURL:  "git@self-hosted-gitlab.com:Originate/git-town.git",
+	})
+	assert.NotNil(t, result)
+	assert.Equal(t, "GitLab", result.HostingServiceName())
+	assert.Equal(t, "https://self-hosted-gitlab.com/Originate/git-town", result.GetRepositoryURL())
+}
+
+func TestGetDriver_OriginHostnameOverride_Bitbucket(t *testing.T) {
+	result := drivers.GetDriver(drivers.DriverOptions{
+		OriginURL:      "git@my-ssh-identity.com:Originate/git-town.git",
+		OriginHostname: "bitbucket.org",
+	})
+	assert.NotNil(t, result)
+	assert.Equal(t, "Bitbucket", result.HostingServiceName())
+	assert.Equal(t, "https://bitbucket.org/Originate/git-town", result.GetRepositoryURL())
+}
+
+func TestGetDriver_OriginHostnameOverride_GitHub(t *testing.T) {
+	result := drivers.GetDriver(drivers.DriverOptions{
+		OriginURL:      "git@my-ssh-identity.com:Originate/git-town.git",
+		OriginHostname: "github.com",
+	})
+	assert.NotNil(t, result)
+	assert.Equal(t, "GitHub", result.HostingServiceName())
+	assert.Equal(t, "https://github.com/Originate/git-town", result.GetRepositoryURL())
+}
+
+func TestGetDriver_OriginHostnameOverride_GitLab(t *testing.T) {
+	result := drivers.GetDriver(drivers.DriverOptions{
+		OriginURL:      "git@my-ssh-identity.com:Originate/git-town.git",
+		OriginHostname: "gitlab.com",
+	})
+	assert.NotNil(t, result)
+	assert.Equal(t, "GitLab", result.HostingServiceName())
+	assert.Equal(t, "https://gitlab.com/Originate/git-town", result.GetRepositoryURL())
+}
