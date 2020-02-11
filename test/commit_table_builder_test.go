446a6e1970a45320d9257120831fe1ff50c72556
Kevin Goslar <kevin.goslar@gmail.com>
2020-02-11 11:16:23 -0500

    List the main branch first in Gherkin (#1285)

--- test/commit_table_builder_test.go
+++ test/commit_table_builder_test.go
@@ -9,7 +9,7 @@ import (
 func TestCommitTableBuilder(t *testing.T) {
 	builder := NewCommitTableBuilder()
 	commit1 := Commit{SHA: "sha1", Branch: "branch1", Message: "commit1"}
-	commit2 := Commit{SHA: "sha2", Branch: "branch2", Message: "commit2"}
+	commit2 := Commit{SHA: "sha2", Branch: "main", Message: "commit2"}
 	commit3 := Commit{SHA: "sha3", Branch: "branch3", Message: "commit3"}
 	builder.Add(commit1, "local")
 	builder.Add(commit1, "remote")
@@ -17,8 +17,8 @@ func TestCommitTableBuilder(t *testing.T) {
 	builder.Add(commit3, "remote")
 	table := builder.Table([]string{"BRANCH", "LOCATION", "MESSAGE"})
 	expected := `| BRANCH  | LOCATION      | MESSAGE |
+| main    | local         | commit2 |
 | branch1 | local, remote | commit1 |
-| branch2 | local         | commit2 |
 | branch3 | remote        | commit3 |
 `
 	assert.Equal(t, expected, table.String())
