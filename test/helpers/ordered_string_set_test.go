e8eb1be5f989455d864113ea0dcf25d7da1f6c02
Kevin Goslar <kevin.goslar@gmail.com>
2020-05-02 08:04:17 -0500

    Make helper tests external

--- test/helpers/ordered_string_set_test.go
+++ test/helpers/ordered_string_set_test.go
@@ -1,13 +1,14 @@
-package helpers
+package helpers_test
 
 import (
 	"testing"
 
+	"github.com/git-town/git-town/test/helpers"
 	"github.com/stretchr/testify/assert"
 )
 
 func TestOrderedStringSet(t *testing.T) {
-	set1 := NewOrderedStringSet("one")
+	set1 := helpers.NewOrderedStringSet("one")
 	set2 := set1.Add("two")
 	set2 = set2.Add("two")
 	assert.Equal(t, []string{"one", "two"}, set2.Slice())
