8866447a8d8dc9769419391adde317caa64d047d
Kevin Goslar <kevin.goslar@gmail.com>
2020-05-16 10:06:35 -0500

    Replace

--- test/git_repository.go
+++ test/git_repository.go
@@ -6,7 +6,6 @@ import (
 	"os"
 	"os/exec"
 	"path/filepath"
-	"regexp"
 	"sort"
 	"strconv"
 	"strings"
@@ -71,31 +70,6 @@ func InitGitRepository(workingDir, homeDir, binDir string) (GitRepository, error
 // The directory must contain an existing Git repo.
 func NewGitRepository(workingDir string, homeDir string, shell command.Shell) GitRepository {
 	return GitRepository{Dir: workingDir, Shell: shell}
-}
-
-// HasBranchesOutOfSync returns if true if and only if one or more local branches are out of sync with their remote
-func (repo *GitRepository) HasBranchesOutOfSync() (bool, error) {
-	res, err := repo.Shell.Run("git", "branch", "-vv")
-	if err != nil {
-		return false, fmt.Errorf("cannot determine if any branches are out of sync in %q: %w %q", repo.Dir, err, res.Output())
-	}
-	lines := strings.Split(res.OutputSanitized(), "\n")
-	regex := regexp.MustCompile(`\[.*\]`)
-	for l := range lines {
-		regex.FindString(
-
-	res, err := repo.Shell.Run("bash", "-c", `git branch -vv | grep -o "\[.*\]" | tr -d "[]" | awk "{ print \$2 }" | grep . | wc -l`)
-	if err != nil {
-		return false, fmt.Errorf("cannot determine if any branches are out of sync in %q: %w %q", repo.Dir, err, res.Output())
-	}
-	count, err := strconv.ParseInt(res.OutputSanitized(), 0, 64)
-	if err != nil {
-		return false, fmt.Errorf("cannot parse output as int in %q: %w %q", repo.Dir, err, res.Output())
-	}
-	return count != 0, nil
-}
-
-func parseBranchVvLine(line string) bool {
 }
 
 // AddRemote adds the given Git remote to this repository.
@@ -418,6 +392,15 @@ func (repo *GitRepository) FilesInBranches() (result DataTable, err error) {
 		}
 	}
 	return result, err
+}
+
+// HasBranchesOutOfSync indicates whether one or more local branches are out of sync with their remote
+func (repo *GitRepository) HasBranchesOutOfSync() (bool, error) {
+	res, err := repo.Shell.Run("git", "for-each-ref", "--format=%(refname:short) %(upstream:track)", "refs/heads")
+	if err != nil {
+		return false, fmt.Errorf("cannot determine if branches are out of sync in %q: %w %q", repo.Dir, err, res.Output())
+	}
+	return strings.Contains(res.Output(), "["), nil
 }
 
 // HasFile indicates whether this repository contains a file with the given name and content.
