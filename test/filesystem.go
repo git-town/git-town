68edc9333bb1843eafa1993cab7043c89135d721
Kevin Goslar <kevin.goslar@gmail.com>
2020-05-19 13:57:33 -0500

    Improve variable names

--- test/filesystem.go
+++ test/filesystem.go
@@ -17,7 +17,6 @@ import (
 func CopyDirectory(src, dst string) error {
 	return filepath.Walk(src, func(srcPath string, fi os.FileInfo, e error) error {
 		dstPath := strings.Replace(srcPath, src, dst, 1)
-		// handle directory
 		if fi.IsDir() {
 			err := os.Mkdir(dstPath, fi.Mode())
 			if err != nil {
@@ -25,20 +24,19 @@ func CopyDirectory(src, dst string) error {
 			}
 			return nil
 		}
-		// handle file
-		sourceContent, err := os.Open(srcPath)
+		sourceFile, err := os.Open(srcPath)
 		if err != nil {
-			return fmt.Errorf("cannot read source file %q: %w", srcPath, err)
+			return fmt.Errorf("cannot open source file %q: %w", srcPath, err)
 		}
 		destFile, err := os.OpenFile(dstPath, os.O_CREATE|os.O_WRONLY, fi.Mode())
 		if err != nil {
 			return fmt.Errorf("cannot create target file %q: %w", srcPath, err)
 		}
-		_, err = io.Copy(destFile, sourceContent)
+		_, err = io.Copy(destFile, sourceFile)
 		if err != nil {
 			return fmt.Errorf("cannot copy %q into %q: %w", srcPath, dstPath, err)
 		}
-		err = sourceContent.Close()
+		err = sourceFile.Close()
 		if err != nil {
 			return fmt.Errorf("cannot close source file %q: %w", srcPath, err)
 		}
