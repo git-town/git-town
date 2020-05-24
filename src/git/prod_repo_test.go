9a5a3ea130bff3d011184982fbc4f7e1b13ffbd3
Kevin Goslar <kevin.goslar@gmail.com>
2020-05-24 18:25:11 -0500

    tests

---
+++ src/git/prod_repo_test.go
@@ -0,0 +1,13 @@
+package git_test
+
+import (
+	"testing"
+
+	"github.com/git-town/git-town/src/git"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestNewProdRepo(t *testing.T) {
+	repo := git.NewProdRepo()
+	assert.Equal(t, repo.Configuration, repo.Silent.Configuration)
+}
