e9b2f3264f00e8ee0226127cf4ab90c5842d1cf1
Kevin Goslar <kevin.goslar@gmail.com>
2020-05-24 17:45:47 -0500

    fix

--- test/steps/offline_steps.go
+++ test/steps/offline_steps.go
@@ -9,6 +9,7 @@ import (
 // OfflineSteps defines Cucumber step implementations around offline functionality.
 func OfflineSteps(suite *godog.Suite, state *ScenarioState) {
 	suite.Step(`^offline mode is disabled$`, func() error {
+		state.gitEnv.DevRepo.Configuration.Reload()
 		offline := state.gitEnv.DevRepo.IsOffline()
 		if offline {
 			return fmt.Errorf("expected to not be offline but am")
@@ -16,6 +17,7 @@ func OfflineSteps(suite *godog.Suite, state *ScenarioState) {
 		return nil
 	})
 	suite.Step(`^offline mode is enabled$`, func() error {
+		state.gitEnv.DevRepo.Configuration.Reload()
 		offline := state.gitEnv.DevRepo.IsOffline()
 		if !offline {
 			return fmt.Errorf("expected to be offline but am not")
