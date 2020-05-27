4e75793aa5ddc705677a656e0e41ef2bad12f094
Kevin Goslar <kevin.goslar@gmail.com>
2020-05-27 15:30:14 -0500

    Merge all feature steps

--- test/scenario_state.go
+++ test/scenario_state.go
@@ -1,15 +1,14 @@
-package steps
+package test
 
 import (
 	"github.com/cucumber/messages-go/v10"
 	"github.com/git-town/git-town/src/command"
-	"github.com/git-town/git-town/test"
 )
 
 // ScenarioState constains the state that is shared by all steps within a scenario.
 type ScenarioState struct {
 	// the GitEnvironment used in the current scenario
-	gitEnv *test.GitEnvironment
+	gitEnv *GitEnvironment
 
 	// the error of the last run of Git Town
 	runErr error
@@ -28,7 +27,7 @@ type ScenarioState struct {
 }
 
 // Reset restores the null value of this ScenarioState.
-func (state *ScenarioState) Reset(gitEnv *test.GitEnvironment) {
+func (state *ScenarioState) Reset(gitEnv *GitEnvironment) {
 	state.gitEnv = gitEnv
 	state.initialCommits = nil
 	state.runRes = nil
