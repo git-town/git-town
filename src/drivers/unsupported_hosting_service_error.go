8068cb31d611cc857a813749fe024395b367f675
Kevin Goslar <kevin.goslar@gmail.com>
2020-06-02 22:11:55 -0500

    cleanup

--- src/drivers/unsupported_hosting_service_error.go
+++
@@ -1,7 +0,0 @@
-package drivers
-
-// UnsupportedHostingServiceError represents the error condition
-// when no suitable hosting service is found in the respective Registry.
-type UnsupportedHostingServiceError struct {
-	registry *Registry
-}
