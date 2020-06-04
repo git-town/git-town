fd5a288029ab1b0fa0a5e25d365fee4c712e79c2
Charles Rudolph <charles.w.rudolph@gmail.com>
2020-06-03 19:42:41 -0700

    periods

--- src/git/printable.go
+++ src/git/printable.go
@@ -10,7 +10,7 @@ import (
 
 var noneString = "[none]"
 
-// getBranchAncestryRoots returns the branches with children and no parents
+// getBranchAncestryRoots returns the branches with children and no parents.
 func getBranchAncestryRoots() []string {
 	parentMap := Config().GetParentBranchMap()
 	roots := []string{}
@@ -23,7 +23,7 @@ func getBranchAncestryRoots() []string {
 	return roots
 }
 
-// GetPrintableMainBranch returns a user printable main branch
+// GetPrintableMainBranch returns a user printable main branch.
 func GetPrintableMainBranch() string {
 	output := Config().GetMainBranch()
 	if output == "" {
@@ -41,12 +41,12 @@ func GetPrintablePerennialBranches() string {
 	return output
 }
 
-// GetPrintableNewBranchPushFlag returns a user printable new branch push flag
+// GetPrintableNewBranchPushFlag returns a user printable new branch push flag.
 func GetPrintableNewBranchPushFlag() string {
 	return strconv.FormatBool(Config().ShouldNewBranchPush())
 }
 
-// getPrintableBranchTree returns a user printable branch tree
+// getPrintableBranchTree returns a user printable branch tree.
 func getPrintableBranchTree(branchName string) (result string) {
 	result += branchName
 	childBranches := Config().GetChildBranches(branchName)
@@ -57,7 +57,7 @@ func getPrintableBranchTree(branchName string) (result string) {
 	return
 }
 
-// GetPrintableBranchAncestry returns a user printable branch ancestry
+// GetPrintableBranchAncestry returns a user printable branch ancestry.
 func GetPrintableBranchAncestry() string {
 	roots := getBranchAncestryRoots()
 	trees := make([]string, len(roots))
@@ -67,7 +67,7 @@ func GetPrintableBranchAncestry() string {
 	return strings.Join(trees, "\n\n")
 }
 
-// GetPrintableOfflineFlag returns a user printable offline flag
+// GetPrintableOfflineFlag returns a user printable offline flag.
 func GetPrintableOfflineFlag() string {
 	return strconv.FormatBool(Config().IsOffline())
 }
