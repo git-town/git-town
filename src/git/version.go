2fcc1528f6c93f2944bbd39642252833b1cffaa0
Kevin Goslar <kevin.goslar@gmail.com>
2020-06-06 15:22:41 -0500

    Cleaner Git version check

--- src/git/version.go
+++ src/git/version.go
@@ -8,25 +8,21 @@ import (
 	"github.com/git-town/git-town/src/command"
 )
 
-// CheckVersion indicates whether the needed Git version is installed.
-func CheckVersion() error {
+// GetVersion indicates whether the needed Git version is installed.
+func GetVersion() (int, int, error) {
 	versionRegexp := regexp.MustCompile(`git version (\d+).(\d+).(\d+)`)
 	res := command.MustRun("git", "version")
 	matches := versionRegexp.FindStringSubmatch(res.OutputSanitized())
 	if matches == nil {
-		return fmt.Errorf("'git version' returned unexpected output: %q.\nPlease open an issue and supply the output of running 'git version'", res.Output())
+		return 0, 0, fmt.Errorf("'git version' returned unexpected output: %q.\nPlease open an issue and supply the output of running 'git version'", res.Output())
 	}
 	majorVersion, err := strconv.Atoi(matches[1])
 	if err != nil {
-		return fmt.Errorf("cannot convert major version (%v) to int: %w", matches[1], err)
+		return 0, 0, fmt.Errorf("cannot convert major version (%v) to int: %w", matches[1], err)
 	}
 	minorVersion, err := strconv.Atoi(matches[2])
 	if err != nil {
-		return fmt.Errorf("cannot convert minor version (%v) to int: %w", matches[2], err)
+		return 0, 0, fmt.Errorf("cannot convert minor version (%v) to int: %w", matches[2], err)
 	}
-	if majorVersion*100+minorVersion < 207 {
-		// nolint:stylecheck // sentence begins with a proper noun
-		return fmt.Errorf("Git Town requires Git 2.7.0 or higher")
-	}
-	return nil
+	return majorVersion, minorVersion, nil
 }
