592e0c8a9562d6a12d956ccaf389d5b549ef21a2
Kevin Goslar <kevin.goslar@gmail.com>
2020-06-08 09:39:06 -0500

    Make cli interface more specific (#1546)

--- src/cli/config.go
+++
@@ -1,7 +0,0 @@
-package cli
-
-// Config defines the configuration values needed by the `cli` package.
-type Config interface {
-	GetBranchAncestryRoots() []string
-	GetChildBranches(string) []string
-}
