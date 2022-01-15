cff3d9dad70c9e315ff5e9676c17e412cf26d988
Kevin Goslar <kevin.goslar@gmail.com>
2022-01-15 17:29:45 -0600

    helpers.go

--- src/cli/helpers.go
+++ src/cli/helpers.go
@@ -5,5 +5,5 @@ import "strings"
 // Indent outputs the given string with the given level of indentation
 // on each line. Each level of indentation is two spaces.
 func Indent(message string) string {
-	return "  " + strings.Replace(message, "\n", "\n  ", -1)
+	return "  " + strings.ReplaceAll(message, "\n", "\n  ")
 }
