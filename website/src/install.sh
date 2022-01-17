0710baaa8891e7e82c8febcddb8e6fdbdae8609b
Kevin Goslar <kevin.goslar@gmail.com>
2022-01-17 12:44:28 -0600

    install.sh

--- website/src/install.sh
+++ website/src/install.sh
@@ -5,32 +5,39 @@ MACOS_URL=https://github.com/git-town/git-town/releases/download/v7.6.0/git-town
 MSWIN_URL=https://github.com/git-town/git-town/releases/download/v7.6.0/git-town_7.6.0_windows_intel_64.zip
 
 TARGET=$HOME/.local/bin
+TMP=.git-town-download
 
 function os_name {
   case "$OSTYPE" in
     darwin*)  echo "macos" ;;
     linux*)   echo "linux" ;;
-    msys*)    echo "windows" ;;
-    cygwin*)  echo "windows" ;;
-    *)        echo "unknown" ;;
+    msys*)    echo "mswin" ;;
+    cygwin*)  echo "mswin" ;;
+    *)        echo "other" ;;
   esac
 }
 
 function download_linux {
-	curl -L $LINUX_URL | tar xvz --directory "$TARGET"
+	curl -L $LINUX_URL | tar xvz --directory "$TMP"
+  echo
+  mv $TMP/git-town "$TARGET"
 }
 
 function download_macos {
   echo Note: this is untested, please submit bug reports and fixes if it is broken
-	curl -L $MACOS_URL | tar xvz --directory "$TARGET"
+	curl -L $MACOS_URL | tar xvz --directory "$TMP"
+  echo
+  mv $TMP/git-town "$TARGET"
 }
 
-function download_win {
+function download_mswin {
   echo Note: this is untested, please submit bug reports and fixes if it is broken
 	curl -L $MSWIN_URL | tar xvz --directory "$TARGET"
+  echo
+  mv $TMP/git-town.exe "$TARGET"
 }
 
-function download_unknown {
+function download_other {
   echo "Error: unsupported operating system."
   echo "Please compile mdBook from source."
   exit 1
@@ -39,12 +46,16 @@ function download_unknown {
 function check_path {
   echo "I have downloaded the Git Town executable into $TARGET."
   if [[ ":$PATH:" == *":$TARGET:"* ]]; then
-    echo "This directory is already in your $PATH so you should be good to go."
+    echo "This directory is already in your PATH so you should be good to go."
   else
-    echo "This directory is not in your $PATH, please add it."
+    echo "This directory is not in your PATH, please add it."
   fi
 }
 
+os="$(os_name)"
+mkdir -p "$TMP"
 mkdir -p "$TARGET"
-download_"$(os_name)"
+download_"$os"
+echo
+rm -rf "$TMP"
 check_path
