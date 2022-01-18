73c544df24fc761e662237ac9d98e70cb443fd6b
Kevin Goslar <kevin.goslar@gmail.com>
2022-01-17 20:37:27 -0600

    install.sh

--- website/src/install.sh
+++ website/src/install.sh
@@ -1,61 +1,160 @@
-#!/usr/bin/env bash
+#!/bin/sh
+set -e
 
-LINUX_URL=https://github.com/git-town/git-town/releases/download/v7.6.0/git-town_7.6.0_linux_intel_64.tar.gz
-MACOS_URL=https://github.com/git-town/git-town/releases/download/v7.6.0/git-town_7.6.0_macOS_intel_64.tar.gz
-MSWIN_URL=https://github.com/git-town/git-town/releases/download/v7.6.0/git-town_7.6.0_windows_intel_64.zip
+# This script installs the Git Town executable.
+# It is inspired by https://github.com/rust-lang/rustup/blob/master/rustup-init.sh.
 
-TARGET=$HOME/.local/bin
-TMP=.git-town-download
+VERSION=7.6.0           # the version of Git Town to install
+DEST=$HOME/.local/bin   # the folder into which to install the Git Town executable
+TMP_DIR=.git-town-download  # temporary folder to use
 
-function os_name {
-  case "$OSTYPE" in
-    darwin*)  echo "macos" ;;
-    linux*)   echo "linux" ;;
-    msys*)    echo "mswin" ;;
-    cygwin*)  echo "mswin" ;;
-    *)        echo "other" ;;
+
+main() {
+  print_welcome
+
+  # verify the environment
+  need_cmd uname
+  need_cmd curl
+  OS="$(os_name)"
+  CPU="$(cpu_name)"
+  say "you seem to run %s_%s\n" "$OS" "$CPU"
+  EXECUTABLE_FILENAME=$(executable_filename "$OS")
+  DEST_PATH=$DEST/$EXECUTABLE_FILENAME
+  ensure_no_other_git_town "$DEST_PATH"
+  say "no other Git Town installation found in the path, proceeding with the installation\n"
+
+  # download the executable
+  URL="$(download_url "$OS" "$CPU")"
+  say "downloading %s\n" "$URL"
+  download_and_extract "$URL" "$OS"
+  echo
+
+  # unpack the archive
+  rm -rf "$TMP_DIR"
+  check_path
+}
+
+print_welcome() {
+  echo "Git Town installer script"
+  echo "This installer is under development. Please report bugs at https://github.com/git-town/git-town/issues/new."
+  echo
+}
+
+
+# provides the name of the operating system in the format used by release assets
+os_name() {
+  case "$(uname -s)" in
+    darwin*)  echo "macOS"   ;;
+    Linux*)   echo "linux"   ;;
+    msys*)    echo "windows" ;;
+    cygwin*)  echo "windows" ;;
+    *)        err "unknown operating system"
   esac
 }
 
-function download_linux {
-	curl -L $LINUX_URL | tar xvz --directory "$TMP"
-  echo
-  mv $TMP/git-town "$TARGET"
+# provides the CPU architecture name
+cpu_name() {
+  cpu_name=$(uname -m)
+  case $cpu_name in
+    x86_64 | x86-64 | x64 | amd64)  echo "intel_64" ;;
+    aarch64 | arm64)                echo "arm_64"   ;;
+    *)                              err "unknown cpu type"
+  esac
 }
 
-function download_macos {
-  echo Note: this is untested, please submit bug reports and fixes if it is broken
-	curl -L $MACOS_URL | tar xvz --directory "$TMP"
-  echo
-  mv $TMP/git-town "$TARGET"
+# provides the URL from which to download the Git Town asset for the given OS and cpu type
+download_url() {
+  OS=$1
+  CPU=$2
+  EXT=tar.gz
+  if [ "$OS" = windows ]; then
+    # only Intel binaries for Windows right now
+    CPU=intel_64
+    EXT=zip
+  fi
+  if [ "$OS" = macOS ]; then
+    # only Intel binaries for macOS right now
+    CPU=intel_64
+  fi
+  printf "https://github.com/git-town/git-town/releases/download/v%s/git-town_%s_%s_%s.%s" $VERSION $VERSION $OS $CPU $EXT
 }
 
-function download_mswin {
-  echo Note: this is untested, please submit bug reports and fixes if it is broken
-	curl -L $MSWIN_URL | tar xvz --directory "$TARGET"
-  echo
-  mv $TMP/git-town.exe "$TARGET"
-}
-
-function download_other {
-  echo "Error: unsupported operating system."
-  echo "Please compile mdBook from source."
-  exit 1
-}
-
-function check_path {
-  echo "I have downloaded the Git Town executable into $TARGET."
-  if [[ ":$PATH:" == *":$TARGET:"* ]]; then
-    echo "This directory is already in your PATH so you should be good to go."
+download_and_extract() {
+  URL=$1
+  OS=$2
+  echo "OS: $OS"
+  mkdir -p "$TMP_DIR"
+  if [ "$OS" = "windows" ]; then
+    need_cmd unzip
+    curl -L "$URL" | unzip --directory "$TMP_DIR"
+    FILENAME=git-town.exe
   else
-    echo "This directory is not in your PATH, please add it."
+    need_cmd tar
+    curl -L "$URL" | tar xvz --directory "$TMP_DIR"
+    FILENAME=git-town
+  fi
+
+  mkdir -p "$DEST"
+  mv "$TMP_DIR/$FILENAME" "$DEST"
+  say "Git Town installed as $DEST/$FILENAME"
+}
+
+executable_filename() {
+  OS=$1
+  if [ "$OS" = "windows" ]; then
+    echo "git-town.exe"
+  else
+    echo "git-town"
   fi
 }
 
-os="$(os_name)"
-mkdir -p "$TMP"
-mkdir -p "$TARGET"
-download_"$os"
-echo
-rm -rf "$TMP"
-check_path
+# indicates whether the Git Town executable is in the path
+executable_in_path() {
+  which git-town > /dev/null 2>&1
+}
+
+# prints output of the installer
+say() {
+  printf "installer: "
+  # shellcheck disable=SC2059
+  printf "$@"
+}
+
+# aborts with the given error message
+err() {
+  say "$1" >&2
+  exit 1
+}
+
+# verifies that Git Town is in the PATH
+check_path() {
+  if ! check_cmd "git-town"; then
+    say ""
+  fi
+}
+
+# verifies that no existing installation of Git Town outside of the destination exists
+ensure_no_other_git_town() {
+  DEST_PATH=$1
+  if ! check_cmd "git-town"; then
+    return
+  fi
+  if [ -f "$DEST_PATH" ]; then
+    return
+  fi
+    err "You already have Git Town installed. Please uninstall it and then run this installer again."
+}
+
+# verifies that the command with the given name exists on this system
+need_cmd() {
+  if ! check_cmd "$1"; then
+    err "need '$1' (command not found)"
+  fi
+}
+
+# indicates whether the command with the given name exists
+check_cmd() {
+  command -v "$1" > /dev/null 2>&1
+}
+
+main || exit 1
