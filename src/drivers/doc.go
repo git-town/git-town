4dbde1cc16df94f8857e1d84364103fdfc1b019c
Kevin Goslar <kevin.goslar@gmail.com>
2022-01-19 13:14:47 -0600

    core.go doc.go

--- src/drivers/doc.go
+++ src/drivers/doc.go
@@ -1,8 +1,5 @@
 // Package drivers provides support for interacting with code hosting services.
-// Commands like "git new-pull-request", "git-repo", and "git ship"
-// use this package to know how to perform Git Town operations on GitHub, Gitlab, Bitbucket, etc.
-//
+// Commands like "new-pull-request", "repo", and "ship" use this package
+// to know how to perform Git Town operations on GitHub, Gitlab, Bitbucket, etc.
 // Drivers implement the CodeHostingDriver interface.
-// Driver implementations are available for GitHub,
-// Gitea, Bitbucket, and GitLab.
 package drivers
