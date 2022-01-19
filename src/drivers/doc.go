05c1cca06ade69a0f311075e05af41736aa9c7de
Kevin Goslar <kevin.goslar@gmail.com>
2022-01-19 13:10:29 -0600

    drivers doc.go

--- src/drivers/doc.go
+++ src/drivers/doc.go
@@ -1,2 +1,8 @@
-// Package drivers provides support for communicating with the API of common code hosting services.
+// Package drivers provides support for interacting with code hosting services.
+// Commands like "git new-pull-request", "git-repo", and "git ship"
+// use this package to know how to perform Git Town operations on GitHub, Gitlab, Bitbucket, etc.
+//
+// Drivers implement the CodeHostingDriver interface.
+// Driver implementations are available for GitHub,
+// Gitea, Bitbucket, and GitLab.
 package drivers
