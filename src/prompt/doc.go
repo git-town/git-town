85fbe0c7c3a385f1328a6d5732f16b8aaec2cf24
Kevin Goslar <kevin.goslar@gmail.com>
2022-01-19 12:38:20 -0600

    doc.go

--- src/prompt/doc.go
+++ src/prompt/doc.go
@@ -1,2 +1,2 @@
-// Package prompt provides sophisticated CLI-based dialogs through which the user can enter configuration data.
+// Package prompt provides CLI-based dialogs through which the user can enter configuration data.
 package prompt
