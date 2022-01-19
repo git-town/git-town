9cdc70f28f9f934b7460f74711f9559e2bf7e4c6
Kevin Goslar <kevin.goslar@gmail.com>
2022-01-19 16:06:16 -0600

    Add unit tests for cli/indent (#1720)

---
+++ src/cli/indent_test.go
@@ -0,0 +1,26 @@
+package cli_test
+
+import (
+	"testing"
+
+	"github.com/git-town/git-town/v7/src/cli"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestIndent_empty(t *testing.T) {
+	t.Parallel()
+	have := cli.Indent("")
+	assert.Equal(t, have, "  ")
+}
+
+func TestIndent_singleLine(t *testing.T) {
+	t.Parallel()
+	have := cli.Indent("hello")
+	assert.Equal(t, have, "  hello")
+}
+
+func TestIndent_multiLine(t *testing.T) {
+	t.Parallel()
+	have := cli.Indent("hello\nworld")
+	assert.Equal(t, have, "  hello\n  world")
+}
