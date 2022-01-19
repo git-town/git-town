d90bd861713ea40d73b7f657ee76c1c4008cf0a8
Kevin Goslar <kevin.goslar@gmail.com>
2022-01-19 12:17:07 -0600

    ttt

--- src/config/config.go
+++ src/config/config.go
@@ -1,6 +1,7 @@
 // Package config provides facilities to read and write the Git Town configuration.
 // Git Town stores its configuration in the Git configuration under the prefix "git-town".
-// You can manually read it by running `git config --get-regexp git-town`.
+// It supports both the Git configuration for the local repository as well as the global Git configuration in `~/.gitconfig`.
+// You can manually read the Git configuration entries for Git Town by running `git config --get-regexp git-town`.
 package config
 
 import (
---
+++ src/drivers/doc.go
@@ -0,0 +1,2 @@
+// Package drivers provides support for communicating with the API of common code hosting services.
+package drivers
