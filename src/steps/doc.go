a268e7a3d3605d0e539e1c32886f23151e01c886
Kevin Goslar <kevin.goslar@gmail.com>
2022-01-19 13:00:27 -0600

    architecture main.go doc.go step.go

--- src/steps/doc.go
+++ src/steps/doc.go
@@ -1,2 +1,5 @@
-// Package steps defines all the Git and CLI operations that Git Town can execute.
+// Package steps defines the individual CLI operations that Git Town can execute.
+// All steps implement the Step interface defined in step.go.
+// Git Town doesn't execute steps directly.
+// It organizes all activities it wants to perform in a StepList and executes that StepList instance.
 package steps
