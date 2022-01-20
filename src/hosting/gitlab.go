312ce9734aa28e1610cb208fab213d6d351e1ef8
Kevin Goslar <kevin.goslar@gmail.com>
2022-01-19 20:26:51 -0600

    gitlab.go

--- src/hosting/gitlab.go
+++ src/hosting/gitlab.go
@@ -8,8 +8,8 @@ import (
 	"github.com/git-town/git-town/v7/src/hosting/helpers"
 )
 
-// GitlabCodeHostingDriver provides access to the API of GitLab installations.
-type GitlabCodeHostingDriver struct {
+// GitlabDriver provides access to the API of GitLab installations.
+type GitlabDriver struct {
 	originURL  string
 	hostname   string
 	repository string
@@ -17,7 +17,7 @@ type GitlabCodeHostingDriver struct {
 
 // NewGitlabDriver provides a GitLab driver instance if the given repo configuration is for a GitLab repo,
 // otherwise nil.
-func NewGitlabDriver(config config) *GitlabCodeHostingDriver {
+func NewGitlabDriver(config config) *GitlabDriver {
 	driverType := config.HostingService()
 	originURL := config.OriginURL()
 	hostname := helpers.URLHostname(originURL)
@@ -28,32 +28,32 @@ func NewGitlabDriver(config config) *GitlabCodeHostingDriver {
 	if driverType != "gitlab" && hostname != "gitlab.com" {
 		return nil
 	}
-	return &GitlabCodeHostingDriver{
+	return &GitlabDriver{
 		originURL:  originURL,
 		hostname:   hostname,
 		repository: helpers.URLRepositoryName(originURL),
 	}
 }
 
-func (d *GitlabCodeHostingDriver) LoadPullRequestInfo(branch, parentBranch string) (result PullRequestInfo, err error) {
+func (d *GitlabDriver) LoadPullRequestInfo(branch, parentBranch string) (result PullRequestInfo, err error) {
 	return result, nil
 }
 
-func (d *GitlabCodeHostingDriver) NewPullRequestURL(branch, parentBranch string) (string, error) {
+func (d *GitlabDriver) NewPullRequestURL(branch, parentBranch string) (string, error) {
 	query := url.Values{}
 	query.Add("merge_request[source_branch]", branch)
 	query.Add("merge_request[target_branch]", parentBranch)
 	return fmt.Sprintf("%s/merge_requests/new?%s", d.RepositoryURL(), query.Encode()), nil
 }
 
-func (d *GitlabCodeHostingDriver) RepositoryURL() string {
+func (d *GitlabDriver) RepositoryURL() string {
 	return fmt.Sprintf("https://%s/%s", d.hostname, d.repository)
 }
 
-func (d *GitlabCodeHostingDriver) MergePullRequest(options MergePullRequestOptions) (mergeSha string, err error) {
+func (d *GitlabDriver) MergePullRequest(options MergePullRequestOptions) (mergeSha string, err error) {
 	return "", errors.New("shipping pull requests via the GitLab API is currently not supported. If you need this functionality, please vote for it by opening a ticket at https://github.com/git-town/git-town/issues")
 }
 
-func (d *GitlabCodeHostingDriver) HostingServiceName() string {
+func (d *GitlabDriver) HostingServiceName() string {
 	return "GitLab"
 }
