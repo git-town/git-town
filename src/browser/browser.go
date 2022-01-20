b5610cd89917e7604b8d19b300855bd375556b3e
Kevin Goslar <kevin.goslar@gmail.com>
2022-01-20 09:54:53 -0600

    browser.go

--- src/browser/browser.go
+++ src/browser/browser.go
@@ -11,15 +11,14 @@ import (
 // OpenBrowserCommand provides the console command to open the default browser.
 func OpenBrowserCommand() string {
 	if runtime.GOOS == "windows" {
-		// NOTE: the "explorer" command cannot handle special characters
-		//       like "?" and "=".
+		// NOTE: the "explorer" command cannot handle special characters like "?" and "=".
 		//       In particular, "?" can be escaped via "\", but "=" cannot.
 		//       So we are using "start" here.
 		return "start"
 	}
 	openBrowserCommands := []string{
 		"wsl-open",           // for Windows Subsystem for Linux, see https://github.com/git-town/git-town/issues/1344
-		"garcon-url-handler", // opens links in native browser on ChromeOS
+		"garcon-url-handler", // opens links in the native browser from Crostini on ChromeOS
 		"xdg-open",
 		"open",
 		"cygstart",
