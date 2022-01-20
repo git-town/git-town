38be37e4a8957afb85badb451db29e4266992a01
Kevin Goslar <kevin.goslar@gmail.com>
2022-01-19 20:19:46 -0600

    core.go

--- src/hosting/core.go
+++ src/hosting/core.go
@@ -25,7 +25,7 @@ type Driver interface {
 
 // config defines the configuration data needed by the driver package.
 type config interface {
-	// OriginOverride provides the override for the orgin URL in the Git Town configuration.
+	// OriginOverride provides the override for the orgin URL from the Git Town configuration.
 	OriginOverride() string
 
 	// HostingService provides the name of the hosting service that runs at the origin remote.
