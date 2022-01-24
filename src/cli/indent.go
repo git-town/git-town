6e4ec250ef8db7e9b0491e32f777db248c6f3ec2
Kevin Goslar <kevin.goslar@gmail.com>
2022-01-24 13:33:55 -0600

    indent.go

--- src/cli/indent.go
+++ src/cli/indent.go
@@ -14,7 +14,5 @@ var (
 // Indent outputs the given string with the given level of indentation
 // on each line. Each level of indentation is two spaces.
 func Indent(message string) string {
-	result := "  " + strings.ReplaceAll(message, "\n", "\n  ")
-	indentOnce.Do(func() { identRE = regexp.MustCompile("\n  \n") })
-	return identRE.ReplaceAllString(result, "\n\n")
+	return "  " + strings.ReplaceAll(message, "\n", "\n  ")
 }
