e6fb705b40ce7b51ffb6f58562bb06569f4cff4f
Kevin Goslar <kevin.goslar@gmail.com>
2022-02-08 12:37:24 -0600

    prepend.go

--- src/cmd/prepend.go
+++ src/cmd/prepend.go
@@ -29,11 +29,11 @@ var prependCommand = &cobra.Command{
 Syncs the parent branch,
 cuts a new feature branch with the given name off the parent branch,
 makes the new branch the parent of the current branch,
-pushes the new feature branch to the remote repository
+pushes the new feature branch to the origin repository
 (if "new-branch-push-flag" is true),
 and brings over all uncommitted changes to the new feature branch.
 
-See "sync" for remote upstream options.
+See "sync" for upstream remote options.
 `,
 	Run: func(cmd *cobra.Command, args []string) {
 		config, err := createPrependConfig(args, prodRepo)
