c619cf9cade372071518a9d9f591a19b21f6a513
Kevin Goslar <kevin.goslar@gmail.com>
2022-02-09 21:48:18 -0600

    longest_string_length.go

--- test/helpers/longest_string_length.go
+++ test/helpers/longest_string_length.go
@@ -2,8 +2,8 @@ package helpers
 
 // LongestStringLength provides the length of the longest string in the given string collection.
 func LongestStringLength(strings []string) (result int) {
-	for i := range strings {
-		if currentLen := len(strings[i]); currentLen > result {
+	for _, s := range strings {
+		if currentLen := len(s); currentLen > result {
 			result = currentLen
 		}
 	}
