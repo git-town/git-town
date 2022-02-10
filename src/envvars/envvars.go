2784163dd7f5c9cf72e2169094b8faaf7ac13c9b
Kevin Goslar <kevin.goslar@gmail.com>
2022-02-09 21:21:39 -0600

    Range

--- src/envvars/envvars.go
+++ src/envvars/envvars.go
@@ -9,15 +9,15 @@ import (
 
 // PrependPath provides a new envvars with the given directory appended to the PATH entry of the given envvars.
 // This function assumes there is only one PATH entry.
-func PrependPath(envvars []string, directory string) []string {
-	for i, envVar := range envvars {
+func PrependPath(envVars []string, directory string) []string {
+	for e, envVar := range envVars {
 		if strings.HasPrefix(envVar, "PATH=") {
 			parts := strings.SplitN(envVar, "=", 2)
-			envvars[i] = "PATH=" + directory + string(os.PathListSeparator) + parts[1]
-			return envvars
+			envVars[e] = "PATH=" + directory + string(os.PathListSeparator) + parts[1]
+			return envVars
 		}
 	}
-	return append(envvars, fmt.Sprintf("PATH=%s", directory))
+	return append(envVars, fmt.Sprintf("PATH=%s", directory))
 }
 
 // Replace provides a new envvars in which the entry with the given key contains the given value instead of its original value.
