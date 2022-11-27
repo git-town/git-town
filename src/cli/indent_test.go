bbe3d154a636f610d9231baedadc99c412b222ec
Kevin Goslar <kevin.goslar@gmail.com>
2022-11-27 13:25:57 -0600

    indent_test.go config_test.go envvars_test.go

--- src/cli/indent_test.go
+++ src/cli/indent_test.go
@@ -7,28 +7,30 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestIndent_empty(t *testing.T) {
-	t.Parallel()
-	have := cli.Indent("")
-	assert.Equal(t, have, "  ")
-}
+func TestIndent(t *testing.T) {
+	t.Run("no indent", func(t *testing.T) {
+		t.Parallel()
+		have := cli.Indent("")
+		assert.Equal(t, have, "  ")
+	})
 
-func TestIndent_singleLine(t *testing.T) {
-	t.Parallel()
-	have := cli.Indent("hello")
-	assert.Equal(t, have, "  hello")
-}
+	t.Run("single line of text", func(t *testing.T) {
+		t.Parallel()
+		have := cli.Indent("hello")
+		assert.Equal(t, have, "  hello")
+	})
 
-func TestIndent_multiLine(t *testing.T) {
-	t.Parallel()
-	have := cli.Indent("hello\nworld")
-	assert.Equal(t, have, "  hello\n  world")
-}
+	t.Run("multi-line text", func(t *testing.T) {
+		t.Parallel()
+		have := cli.Indent("hello\nworld")
+		assert.Equal(t, have, "  hello\n  world")
+	})
 
-func TestIndent_multipleNewlines(t *testing.T) {
-	t.Parallel()
-	give := "hello\n\nworld"
-	want := "  hello\n\n  world"
-	have := cli.Indent(give)
-	assert.Equal(t, want, have)
+	t.Run("multiple newlines", func(t *testing.T) {
+		t.Parallel()
+		give := "hello\n\nworld"
+		want := "  hello\n\n  world"
+		have := cli.Indent(give)
+		assert.Equal(t, want, have)
+	})
 }
