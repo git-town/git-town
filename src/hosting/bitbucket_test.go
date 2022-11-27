545a184f33f081c1de861a65ee100996585aa35f
Kevin Goslar <kevin.goslar@gmail.com>
2022-11-27 13:50:46 -0600

    bitbucket_test.go

--- src/hosting/bitbucket_test.go
+++ src/hosting/bitbucket_test.go
@@ -9,33 +9,35 @@ import (
 
 //nolint:paralleltest  // mocks HTTP
 func TestLoadBitbucket(t *testing.T) {
-	driver := hosting.NewBitbucketDriver(mockConfig{
-		hostingService: "bitbucket",
-		originURL:      "git@self-hosted-bitbucket.com:git-town/git-town.git",
-	}, nil)
-	assert.NotNil(t, driver)
-	assert.Equal(t, "Bitbucket", driver.HostingServiceName())
-	assert.Equal(t, "https://self-hosted-bitbucket.com/git-town/git-town", driver.RepositoryURL())
-}
+	t.Run("normal example", func(t *testing.T) {
+		driver := hosting.NewBitbucketDriver(mockConfig{
+			hostingService: "bitbucket",
+			originURL:      "git@self-hosted-bitbucket.com:git-town/git-town.git",
+		}, nil)
+		assert.NotNil(t, driver)
+		assert.Equal(t, "Bitbucket", driver.HostingServiceName())
+		assert.Equal(t, "https://self-hosted-bitbucket.com/git-town/git-town", driver.RepositoryURL())
+	})
 
-//nolint:paralleltest  // mocks HTTP
-func TestLoadBitbucket_customHostName(t *testing.T) {
-	driver := hosting.NewBitbucketDriver(mockConfig{
-		originURL:      "git@my-ssh-identity.com:git-town/git-town.git",
-		originOverride: "bitbucket.org",
-	}, nil)
-	assert.NotNil(t, driver)
-	assert.Equal(t, "Bitbucket", driver.HostingServiceName())
-	assert.Equal(t, "https://bitbucket.org/git-town/git-town", driver.RepositoryURL())
-}
+	//nolint:paralleltest  // mocks HTTP
+	t.Run("custom hostname", func(t *testing.T) {
+		driver := hosting.NewBitbucketDriver(mockConfig{
+			originURL:      "git@my-ssh-identity.com:git-town/git-town.git",
+			originOverride: "bitbucket.org",
+		}, nil)
+		assert.NotNil(t, driver)
+		assert.Equal(t, "Bitbucket", driver.HostingServiceName())
+		assert.Equal(t, "https://bitbucket.org/git-town/git-town", driver.RepositoryURL())
+	})
 
-//nolint:paralleltest  // mocks HTTP
-func TestLoadBitbucket_customUserName(t *testing.T) {
-	driver := hosting.NewBitbucketDriver(mockConfig{
-		hostingService: "bitbucket",
-		originURL:      "username@bitbucket.org:git-town/git-town.git",
-	}, nil)
-	assert.NotNil(t, driver)
-	assert.Equal(t, "Bitbucket", driver.HostingServiceName())
-	assert.Equal(t, "https://bitbucket.org/git-town/git-town", driver.RepositoryURL())
+	//nolint:paralleltest  // mocks HTTP
+	t.Run("custom username", func(t *testing.T) {
+		driver := hosting.NewBitbucketDriver(mockConfig{
+			hostingService: "bitbucket",
+			originURL:      "username@bitbucket.org:git-town/git-town.git",
+		}, nil)
+		assert.NotNil(t, driver)
+		assert.Equal(t, "Bitbucket", driver.HostingServiceName())
+		assert.Equal(t, "https://bitbucket.org/git-town/git-town", driver.RepositoryURL())
+	})
 }
