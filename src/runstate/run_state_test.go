d94d28ebb618731f35a8d20705caf9d061719f01
Kevin Goslar <kevin.goslar@gmail.com>
2022-11-27 20:14:42 -0600

    run_state_test.go

--- src/runstate/run_state_test.go
+++ src/runstate/run_state_test.go
@@ -9,24 +9,26 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestRunState_Marshal(t *testing.T) {
-	t.Parallel()
-	runState := &runstate.RunState{
-		AbortStepList: runstate.StepList{
-			List: []steps.Step{&steps.ResetToShaStep{Sha: "abc"}},
-		},
-		Command: "sync",
-		RunStepList: runstate.StepList{
-			List: []steps.Step{&steps.ResetToShaStep{Sha: "abc"}},
-		},
-		UndoStepList: runstate.StepList{
-			List: []steps.Step{&steps.ResetToShaStep{Sha: "abc"}},
-		},
-	}
-	data, err := json.Marshal(runState)
-	assert.NoError(t, err)
-	newRunState := &runstate.RunState{}
-	err = json.Unmarshal(data, &newRunState)
-	assert.NoError(t, err)
-	assert.Equal(t, runState, newRunState)
+func TestRunState(t *testing.T) {
+	t.Run(".Marshal()", func(t *testing.T) {
+		t.Parallel()
+		runState := &runstate.RunState{
+			AbortStepList: runstate.StepList{
+				List: []steps.Step{&steps.ResetToShaStep{Sha: "abc"}},
+			},
+			Command: "sync",
+			RunStepList: runstate.StepList{
+				List: []steps.Step{&steps.ResetToShaStep{Sha: "abc"}},
+			},
+			UndoStepList: runstate.StepList{
+				List: []steps.Step{&steps.ResetToShaStep{Sha: "abc"}},
+			},
+		}
+		data, err := json.Marshal(runState)
+		assert.NoError(t, err)
+		newRunState := &runstate.RunState{}
+		err = json.Unmarshal(data, &newRunState)
+		assert.NoError(t, err)
+		assert.Equal(t, runState, newRunState)
+	})
 }
