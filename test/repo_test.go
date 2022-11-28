c2bab3ac016d9b2ab4c8eb64a79546e327d5f988
Kevin Goslar <kevin.goslar@gmail.com>
2022-11-27 20:20:32 -0600

    repo_test.go

--- test/repo_test.go
+++ test/repo_test.go
@@ -8,24 +8,26 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestNewRepo(t *testing.T) {
-	t.Parallel()
-	dir := CreateTempDir(t)
-	workingDir := filepath.Join(dir, "working")
-	homeDir := filepath.Join(dir, "home")
-	binDir := filepath.Join(dir, "bin")
-	repo := NewRepo(workingDir, homeDir, binDir)
-	assert.Equal(t, workingDir, repo.shell.workingDir)
-	assert.Equal(t, homeDir, repo.shell.homeDir)
-	assert.Equal(t, binDir, repo.shell.binDir)
-}
+func TestRepo(t *testing.T) {
+	t.Run("NewRepo", func(t *testing.T) {
+		t.Parallel()
+		dir := CreateTempDir(t)
+		workingDir := filepath.Join(dir, "working")
+		homeDir := filepath.Join(dir, "home")
+		binDir := filepath.Join(dir, "bin")
+		repo := NewRepo(workingDir, homeDir, binDir)
+		assert.Equal(t, workingDir, repo.shell.workingDir)
+		assert.Equal(t, homeDir, repo.shell.homeDir)
+		assert.Equal(t, binDir, repo.shell.binDir)
+	})
 
-func TestRepo_Clone(t *testing.T) {
-	t.Parallel()
-	origin := CreateRepo(t)
-	clonedPath := filepath.Join(origin.shell.workingDir, "cloned")
-	cloned, err := origin.Clone(clonedPath)
-	assert.NoError(t, err)
-	assert.Equal(t, clonedPath, cloned.shell.workingDir)
-	assertIsNormalGitRepo(t, clonedPath)
+	t.Run(".Clone()", func(t *testing.T) {
+		t.Parallel()
+		origin := CreateRepo(t)
+		clonedPath := filepath.Join(origin.shell.workingDir, "cloned")
+		cloned, err := origin.Clone(clonedPath)
+		assert.NoError(t, err)
+		assert.Equal(t, clonedPath, cloned.shell.workingDir)
+		assertIsNormalGitRepo(t, clonedPath)
+	})
 }
