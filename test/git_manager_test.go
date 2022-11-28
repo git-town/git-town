e734c50feb3009b8163ffba9e970069c2fc4787d
Kevin Goslar <kevin.goslar@gmail.com>
2022-11-27 20:15:58 -0600

    git_manager_test.go

--- test/git_manager_test.go
+++ test/git_manager_test.go
@@ -9,23 +9,25 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestGitManager_CreateMemoizedEnvironment(t *testing.T) {
-	t.Parallel()
-	dir := CreateTempDir(t)
-	_, err := NewGitManager(dir)
-	assert.Nil(t, err, "creating memoized environment failed")
-	memoizedPath := filepath.Join(dir, "memoized")
-	_, err = os.Stat(memoizedPath)
-	assert.Falsef(t, os.IsNotExist(err), "memoized directory %q not found", memoizedPath)
-}
+func TestGitManager(t *testing.T) {
+	t.Run("NewGitManager()", func(t *testing.T) {
+		t.Parallel()
+		dir := CreateTempDir(t)
+		_, err := NewGitManager(dir)
+		assert.Nil(t, err, "creating memoized environment failed")
+		memoizedPath := filepath.Join(dir, "memoized")
+		_, err = os.Stat(memoizedPath)
+		assert.Falsef(t, os.IsNotExist(err), "memoized directory %q not found", memoizedPath)
+	})
 
-func TestGitManager_CreateScenarioEnvironment(t *testing.T) {
-	t.Parallel()
-	dir := CreateTempDir(t)
-	gm, err := NewGitManager(dir)
-	assert.Nil(t, err, "creating memoized environment failed")
-	result, err := gm.CreateScenarioEnvironment("foo")
-	assert.Nil(t, err, "cannot create scenario environment")
-	_, err = os.Stat(result.DevRepo.shell.workingDir)
-	assert.False(t, os.IsNotExist(err), "scenario environment directory %q not found", result.DevRepo.WorkingDir)
+	t.Run(".CreateScenarioEnvironment()", func(t *testing.T) {
+		t.Parallel()
+		dir := CreateTempDir(t)
+		gm, err := NewGitManager(dir)
+		assert.Nil(t, err, "creating memoized environment failed")
+		result, err := gm.CreateScenarioEnvironment("foo")
+		assert.Nil(t, err, "cannot create scenario environment")
+		_, err = os.Stat(result.DevRepo.shell.workingDir)
+		assert.False(t, os.IsNotExist(err), "scenario environment directory %q not found", result.DevRepo.WorkingDir)
+	})
 }
