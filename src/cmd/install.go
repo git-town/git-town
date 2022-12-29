41a7f678841f0569bdf53e78fb0dede7a09ecb66
Kevin Goslar <kevin.goslar@gmail.com>
2022-12-29 08:39:56 -0600

    install.go

--- src/cmd/install.go
+++ src/cmd/install.go
@@ -8,9 +8,6 @@ var installCommand = &cobra.Command{
 	Use:   "install",
 	Short: "Set up Git Town on your computer",
 	Args:  cobra.NoArgs,
-	PreRunE: func(cmd *cobra.Command, args []string) error {
-		return ValidateIsRepository(prodRepo)
-	},
 }
 
 func init() {
