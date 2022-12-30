7b9c403b3309f253f2ec594198956845aeeee278
Kevin Goslar <kevin.goslar@gmail.com>
2022-12-30 12:55:46 -0600

    install-completions.go

--- src/cmd/install-completions.go
+++ src/cmd/install-completions.go
@@ -20,31 +20,31 @@ To enable completions:
 
 Bash:
 
-$ source <(git-town completions bash)
+$ source <(git-town install completions bash)
 
 Persist and autoload on each session:
 
-Linux: $ git-town completions bash > /etc/bash_completion.d/git-town
+Linux: $ git-town install completions bash > /etc/bash_completion.d/git-town
 
-MacOS: $ git-town completions bash > /usr/local/etc/bash_completion.d/git-town
+MacOS: $ git-town install completions bash > /usr/local/etc/bash_completion.d/git-town
 
 Zsh:
 
-$ source <(git-town completions zsh)
+$ source <(git-town install completions zsh)
 
 Persist and autoload on each session:
 
-$ git-town completions zsh > /usr/share/zsh/vendor-completions/_git-town
+$ git-town install completions zsh > /usr/share/zsh/vendor-completions/_git-town
 
 Fish:
 
 CAUTION: pending upstream issue breaks this: https://github.com/spf13/cobra/pull/1122
 
-$ git-town completions fish | source
+$ git-town install completions fish | source
 
 Persist and autoload on each session:
 
-$ git-town completions fish > /etc/fish/completions/git-town.fish
+$ git-town install completions fish > /etc/fish/completions/git-town.fish
 
 You might be a power user who has their dotfiles under version control. Or you
 might have another motivation to keep those scripts in your home folder.
@@ -77,5 +77,6 @@ func init() {
 		&completionsNoDesc,
 		"no-descriptions", false,
 		"disable completions description for shells that support it")
-	RootCmd.AddCommand(completionsCmd)
+	installCommand.AddCommand(completionsCmd)
+	RootCmd.CompletionOptions.DisableDefaultCmd = true
 }
