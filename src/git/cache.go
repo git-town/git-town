8025bda823608c7bc4ed6207ef9e8bfe8a1385a2
Kevin Goslar <kevin.goslar@gmail.com>
2023-01-15 17:27:58 -0600

    cache.go

--- src/git/cache.go
+++ src/git/cache.go
@@ -14,7 +14,7 @@ func (c *Cache[T]) Set(newValue T) {
 }
 
 // Value provides the current value.
-func (c *Cache[T]) Value() T {
+func (c *Cache[T]) Value() T { //nolint:ireturn
 	if !c.initialized {
 		panic("using current branch before initialization")
 	}
