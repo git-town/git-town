e1c3af40096db4d9edd17625eb784a114b554a39
Kevin Goslar <kevin.goslar@gmail.com>
2023-01-16 11:08:27 -0600

    silent_shell.go

--- src/run/silent_shell.go
+++ src/run/silent_shell.go
@@ -10,21 +10,21 @@ import (
 type SilentShell struct{}
 
 // WorkingDir provides the directory that this Shell operates in.
-func (shell SilentShell) WorkingDir() string {
+func (s SilentShell) WorkingDir() string {
 	return "."
 }
 
 // Run runs the given command in this ShellRunner's directory.
-func (shell SilentShell) Run(cmd string, args ...string) (*Result, error) {
+func (s SilentShell) Run(cmd string, args ...string) (*Result, error) {
 	return Exec(cmd, args...)
 }
 
 // RunMany runs all given commands in current directory.
 // Commands are provided as a list of argv-style strings.
 // Failed commands abort immediately with the encountered error.
-func (shell SilentShell) RunMany(commands [][]string) error {
+func (s SilentShell) RunMany(commands [][]string) error {
 	for _, argv := range commands {
-		_, err := shell.Run(argv[0], argv[1:]...)
+		_, err := s.Run(argv[0], argv[1:]...)
 		if err != nil {
 			return fmt.Errorf("error running command %q: %w", argv, err)
 		}
@@ -33,17 +33,17 @@ func (shell SilentShell) RunMany(commands [][]string) error {
 }
 
 // RunString runs the given command (including possible arguments) in this ShellInDir's directory.
-func (shell SilentShell) RunString(fullCmd string) (*Result, error) {
+func (s SilentShell) RunString(fullCmd string) (*Result, error) {
 	parts, err := shellquote.Split(fullCmd)
 	if err != nil {
 		return nil, fmt.Errorf("cannot split command %q: %w", fullCmd, err)
 	}
 	cmd, args := parts[0], parts[1:]
-	return shell.Run(cmd, args...)
+	return s.Run(cmd, args...)
 }
 
 // RunStringWith runs the given command (including possible arguments) in this ShellInDir's directory.
-func (shell SilentShell) RunStringWith(fullCmd string, options *Options) (*Result, error) {
+func (s SilentShell) RunStringWith(fullCmd string, options *Options) (*Result, error) {
 	parts, err := shellquote.Split(fullCmd)
 	if err != nil {
 		return nil, fmt.Errorf("cannot split command %q: %w", fullCmd, err)
