3e4fadf20e3d488019ed55b8c5303dc3146a3e0c
Kevin Goslar <kevin.goslar@gmail.com>
2023-02-20 07:33:41 -0600

    Streamline internal documentation (#2072)

--- src/steps/core.go
+++ src/steps/core.go
@@ -1,3 +1,7 @@
+// Package steps defines the individual CLI operations that Git Town can execute.
+// All steps implement the Step interface defined in step.go.
+// Git Town doesn't execute steps directly.
+// It organizes all Step instances it wants to perform in a StepList and executes that StepList.
 package steps
 
 import (
