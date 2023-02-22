d05062bd76fc0b09531835f1bf348c20a6c64f85
Kevin Goslar <kevin.goslar@gmail.com>
2023-02-21 21:24:33 -0600

    core.go

--- src/hosting/core.go
+++ src/hosting/core.go
@@ -1,7 +1,7 @@
 // Package hosting provides support for interacting with code hosting services.
 // Commands like "new-pull-request", "repo", and "ship" use this package
 // to know how to perform Git Town operations on GitHub, Gitlab, Bitbucket, etc.
-// Implementations of connectors for particular code hosting platforms implement the Connector interface.
+// Implementations of connectors for particular code hosting platforms conform to the Connector interface.
 package hosting
 
 import (
