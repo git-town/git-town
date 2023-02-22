e1728df20ebcdad7904710ed563ddfad79c56d32
Kevin Goslar <kevin.goslar@gmail.com>
2023-02-22 11:07:46 -0600

    doctor.go

--- src/cmd/doctor.go
+++ src/cmd/doctor.go
@@ -1,6 +1,8 @@
 package cmd
 
 import (
+	"strings"
+
 	"github.com/git-town/git-town/v7/src/cli"
 	"github.com/git-town/git-town/v7/src/git"
 	"github.com/git-town/git-town/v7/src/hosting"
@@ -12,27 +14,14 @@ func doctorCommand(repo *git.ProdRepo) *cobra.Command {
 		Use:   "doctor",
 		Short: "Displays diagnostic information",
 		Run: func(cmd *cobra.Command, args []string) {
-			cli.Println("Git Town:")
-			cli.Printf("- version: %s\n", version)
-			cli.Printf("- built: %s\n\n", buildDate)
+			cli.Printf("Git Town v%s, built %s\n\n", version, buildDate)
 			isRepo := repo.Silent.IsRepository()
 			if isRepo {
-				cli.Println("Git repository:")
+				cli.Println("Git repository detected")
 				// print origin
 				// print upstream
 			} else {
 				cli.Println("The current folder does not seem to contain a Git repository")
-			}
-			cli.Print("- main branch: ")
-			isOffline, err := repo.Config.IsOffline()
-			if err == nil {
-				if isOffline {
-					cli.Println("enabled")
-				} else {
-					cli.Println("disabled")
-				}
-			} else {
-				cli.Println("(cannot determine: %v)", err)
 			}
 			cli.Print("- main branch: ")
 			mainbranch := repo.Config.MainBranch()
@@ -41,11 +30,19 @@ func doctorCommand(repo *git.ProdRepo) *cobra.Command {
 			} else {
 				cli.Println(mainbranch)
 			}
-			cli.Println("\nhosting service:")
+			cli.Print("- perennial branches: ")
+			perennialBranches := repo.Config.PerennialBranches()
+			if len(perennialBranches) == 0 {
+				cli.Println("(none)")
+			} else {
+				cli.Println(strings.Join(perennialBranches, ", "))
+			}
+			cli.Print("\nHosting service: ")
 			driver, err := hosting.NewDriver(&repo.Config, &repo.Silent, cli.PrintDriverAction)
 			if err == nil {
-				cli.Println("- name: ", driver.HostingServiceName())
-				cli.Println("- repo: ", driver.RepositoryURL())
+				cli.Println(driver.HostingServiceName())
+				cli.Println("- repo URL: ", driver.RepositoryURL())
+				cli.Println("- API token: %s", repo.)
 			} else {
 				cli.Println("(cannot determine: %v)", err)
 			}
