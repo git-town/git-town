8e5ae76022915c9ea89a1c9e9097d4fe5d044635
Kevin Goslar <kevin.goslar@gmail.com>
2023-02-22 07:31:12 -0600

    Package core pattern

--- src/cmd/core.go
+++ src/cmd/core.go
@@ -1,3 +1,23 @@
+// Package cmd defines the Git Town commands.
+//
+// Each Git Town command begins by inspecting the current state of the Git
+// repository (which branch you are on, whether you have open changes). If there
+// are no errors, it generates a StepList instance containing the steps to run.
+//
+// Steps, located in src/steps, implement the individual steps that
+// each Git Town command performs. Examples are steps to
+// change to a different Git branch or to pull updates for the current branch.
+//
+// When executing a step, the runstate.Execute function goes through each step in the StepList.
+// It executes the step. If it succeeded, it asks the current step to provide the undo step
+// for what it just did and appends it to the undo StepList.
+// If a Git command fails (for example due to a merge conflict), then the program
+// asks the step to create it's corresponding abort and continue steps, adds them to the respective StepLists,
+// saves the entire runstate to disk, informs the user, and exits.
+//
+// When running "git town continue", Git Town loads the runstate and executes the "continue" StepList in it.
+// When running "git town abort", Git Town loads the runstate and executes the "abort" StepList in it.
+// When running "git town undo", Git Town loads the runstate and executes the "undo" StepList in it.
 package cmd
 
 import (
