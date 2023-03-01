31529aaa9e8f084aaf9652d5db35d611e80b105d
Kevin Goslar <kevin.goslar@gmail.com>
2023-03-01 10:48:32 -0600

    config_runstate_reset.go

--- src/cmd/config_runstate_reset.go
+++ src/cmd/config_runstate_reset.go
@@ -34,10 +34,7 @@ func resetRunstateCommand(repo *git.ProdRepo) *cobra.Command {
 		},
 		Args: cobra.NoArgs,
 		PreRunE: func(cmd *cobra.Command, args []string) error {
-			if err := ValidateIsRepository(repo); err != nil {
-				return err
-			}
-			return validateIsConfigured(repo)
+			return ValidateIsRepository(repo)
 		},
 	}
 }
