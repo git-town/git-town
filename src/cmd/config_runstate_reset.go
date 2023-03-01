067f25ec6b35955225f0c9a27ae789344c3ceaf7
Kevin Goslar <kevin.goslar@gmail.com>
2023-03-01 13:22:39 -0600

    Simplify runstate reset

--- src/cmd/config_runstate_reset.go
+++ src/cmd/config_runstate_reset.go
@@ -1,9 +1,7 @@
 package cmd
 
 import (
-	"errors"
 	"fmt"
-	"os"
 
 	"github.com/git-town/git-town/v7/src/cli"
 	"github.com/git-town/git-town/v7/src/git"
@@ -16,13 +14,9 @@ func resetRunstateCommand(repo *git.ProdRepo) *cobra.Command {
 		Use:   "reset",
 		Short: "Resets the current suspended Git Town command",
 		Run: func(cmd *cobra.Command, args []string) {
-			config, err := loadResetStatusConfig(repo)
+			err := runstate.Delete(repo)
 			if err != nil {
 				cli.Exit(err)
-			}
-			err = resetStatus(config)
-			if err != nil {
-				cli.Exit(fmt.Errorf("cannot delete runstate file: %w", err))
 			}
 			fmt.Println("Runstate file deleted.")
 		},
@@ -31,30 +25,4 @@ func resetRunstateCommand(repo *git.ProdRepo) *cobra.Command {
 			return ValidateIsRepository(repo)
 		},
 	}
-}
-
-type resetStatusConfig struct {
-	filepath string // filepath of the runstate file
-}
-
-func loadResetStatusConfig(repo *git.ProdRepo) (*resetStatusConfig, error) {
-	filepath, err := runstate.PersistenceFilename(repo)
-	if err != nil {
-		return nil, fmt.Errorf("cannot determine the runstate file path: %w", err)
-	}
-	return &resetStatusConfig{
-		filepath: filepath,
-	}, nil
-}
-
-func resetStatus(config *resetStatusConfig) error {
-	err := os.Remove(config.filepath)
-	if err != nil {
-		if errors.Is(err, os.ErrNotExist) {
-			fmt.Println("Runstate doesn't exist.")
-			return nil
-		}
-		return fmt.Errorf("cannot delete runstate file: %w", err)
-	}
-	return nil
 }
