d8b2b0edc1d6e0c70bece79409092e61e3ac4f67
Kevin Goslar <kevin.goslar@gmail.com>
2023-03-10 21:28:12 -0600

    core.go

--- src/validate/core.go
+++ src/validate/core.go
@@ -1,3 +1,3 @@
 // Package validate provides functionality to validate the status of Git repositories.
-// These validators are used by Cobra packages.
+// These validators are used mostly inside Cobra commands.
 package validate
