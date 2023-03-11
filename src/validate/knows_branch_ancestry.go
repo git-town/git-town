2f2ec7c626376d0a6d2302bf2cc7a9a66737d8d4
Kevin Goslar <kevin.goslar@gmail.com>
2023-03-11 17:03:05 -0600

    knows_branch_ancestry.go knows_branches_ancestry.go

--- src/validate/knows_branch_ancestry.go
+++ src/validate/knows_branch_ancestry.go
@@ -5,6 +5,19 @@ import (
 	"github.com/git-town/git-town/v7/src/git"
 )
 
+// KnowsBranchesAncestry asserts that the entire ancestry for all given branches
+// is known to Git Town.
+// Missing ancestry information is queried from the user.
+func KnowsBranchesAncestry(branches []string, repo *git.ProdRepo) error {
+	for _, branch := range branches {
+		err := KnowsBranchAncestry(branch, repo.Config.MainBranch(), repo)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
 // KnowsBranchAncestry prompts the user for all unknown ancestors of the given branch.
 func KnowsBranchAncestry(branch, defaultBranch string, repo *git.ProdRepo) (err error) { //nolint:nonamedreturns // return value names are useful here
 	headerShown := false
