d048f662be9249c1c39f9b6f1555b9be7212bb5b
Kevin Goslar <kevin.goslar@gmail.com>
2023-03-11 15:44:10 -0600

    enter_main_branch.go

--- src/dialog/enter_main_branch.go
+++ src/dialog/enter_main_branch.go
@@ -8,22 +8,17 @@ import (
 
 // EnterMainBranch lets the user enter the main branch.
 func EnterMainBranch(mainBranch string, localBranches []string) (string, error) {
-	newMainBranch, err := Select(SelectArgs{
+	return Select(SelectArgs{
 		Options: localBranches,
 		Message: mainBranchPrompt(mainBranch),
 		Default: mainBranch,
 	})
-	if err != nil {
-		return "", err
-	}
-	return newMainBranch, nil
 }
 
 func mainBranchPrompt(mainBranch string) string {
 	result := "Please specify the main development branch:"
-	currentMainBranch := mainBranch
-	if currentMainBranch != "" {
-		coloredBranch := color.New(color.Bold).Add(color.FgCyan).Sprintf(currentMainBranch)
+	if mainBranch != "" {
+		coloredBranch := color.New(color.Bold).Add(color.FgCyan).Sprintf(mainBranch)
 		result += fmt.Sprintf(" (current value: %s)", coloredBranch)
 	}
 	return result
