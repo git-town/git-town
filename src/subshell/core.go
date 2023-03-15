c08f0fc4790c0b444a19957abfbcdd3235a4d493
Kevin Goslar <kevin.goslar@gmail.com>
2023-03-15 07:54:41 -0500

    core.go

--- src/subshell/core.go
+++ src/subshell/core.go
@@ -1,4 +1,15 @@
-// Package run provides facilities to execute CLI commands in subshells.
+// Package subshell provides facilities to execute CLI commands in subshells.
+//
+// There are are two types of shell commands in Git Town:
+//
+//  1. Internal shell commands.
+//     Git Town runs these silently to determine the state of a Git repository.
+//     Git Town needs to know the output that they generated.
+//     These commands don't change the Git repository, they only investigate it.
+//
+//  2. Public shell commands.
+//     These are the commands that Git Town runs for the end user to change their Git repository.
+//     Git Town doesn't need to know their output, only whether they failed.
 package subshell
 
 import (
