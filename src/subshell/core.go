51c2376cc92c659b894e2d634bb6a196d0d809fc
Kevin Goslar <kevin.goslar@gmail.com>
2023-03-23 11:29:08 -0500

    core.go

--- src/subshell/core.go
+++ src/subshell/core.go
@@ -1,13 +1,2 @@
 // Package subshell provides facilities to execute CLI commands in subshells.
-//
-// There are two types of shell commands in Git Town:
-//
-//  1. Backend shell commands.
-//     Git Town runs these silently to determine the state of a Git repository.
-//     Git Town needs to know the output that they generated.
-//     These commands don't change the Git repository, they only investigate it.
-//
-//  2. Frontend shell commands.
-//     These are the commands that Git Town runs for the end user to change their Git repository.
-//     Git Town doesn't need to know their output, only whether they failed.
 package subshell
