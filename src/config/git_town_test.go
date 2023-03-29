af50f1f08826cc15e3fab93d4acb286de0e3b14d
Kevin Goslar <kevin.goslar@gmail.com>
2023-03-28 21:52:36 -0500

    OriginURL tests

--- src/config/git_town_test.go
+++ src/config/git_town_test.go
@@ -1,14 +1,29 @@
 package config_test
 
 import (
+	"os"
 	"testing"
 
+	"github.com/git-town/git-town/v7/src/giturl"
 	"github.com/git-town/git-town/v7/test"
 	"github.com/stretchr/testify/assert"
 )
 
 func TestGitTown(t *testing.T) {
 	t.Parallel()
+	t.Run("OriginURL()", func(t *testing.T) {
+		tests := map[string]giturl.Parts{
+			"http://github.com/organization/repository": {Host: "github.com", Org: "organization", Repo: "repository"},
+		}
+		for give, want := range tests {
+			repo := test.CreateTestGitTownRunner(t)
+			os.Setenv("GIT_TOWN_REMOTE", give)
+			defer os.Unsetenv("GIT_TOWN_REMOTE")
+			have := repo.Config.OriginURL()
+			assert.Equal(t, &want, have)
+		}
+	})
+
 	t.Run(".SetOffline()", func(t *testing.T) {
 		t.Parallel()
 		repo := test.CreateTestGitTownRunner(t)
