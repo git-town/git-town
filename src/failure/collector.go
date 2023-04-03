cd6747b81231ae939f579c06bd5de187c7538ff6
Kevin Goslar <kevin.goslar@gmail.com>
2023-04-03 05:23:02 -0500

    Extract error handling into "failure" package (#2193)

--- src/failure/collector.go
+++ src/failure/collector.go
@@ -1,4 +1,4 @@
-package runstate
+package failure
 
 import (
 	"fmt"
@@ -6,7 +6,7 @@ import (
 	"github.com/git-town/git-town/v7/src/config"
 )
 
-// ErrorChecker helps avoid excessive error checking
+// Collector helps avoid excessive error checking
 // while gathering a larger number of values through fallible operations.
 //
 // This is based on ideas outlined in https://go.dev/blog/errors-are-values.
@@ -14,13 +14,13 @@ import (
 // Using this technique can lead to executing logic that would normally not run,
 // using potentially invalid data, and potentially leading to unexpected runtime exceptions and side effects.
 // Use with care and only if it's abundantly clear and obvious that there are no negative side effects.
-type ErrorChecker struct {
+type Collector struct {
 	Err error `exhaustruct:"optional"`
 }
 
 // Check registers the given error and indicates
 // whether this ErrorChecker contains an error now.
-func (ec *ErrorChecker) Check(err error) bool {
+func (ec *Collector) Check(err error) bool {
 	if ec.Err == nil {
 		ec.Err = err
 	}
@@ -28,48 +28,48 @@ func (ec *ErrorChecker) Check(err error) bool {
 }
 
 // Fail registers the error constructed using the given format arguments.
-func (ec *ErrorChecker) Fail(format string, a ...any) {
+func (ec *Collector) Fail(format string, a ...any) {
 	ec.Check(fmt.Errorf(format, a...))
 }
 
 // Bool provides the bool part of the given fallible function result
 // while registering the given error.
-func (ec *ErrorChecker) Bool(value bool, err error) bool {
+func (ec *Collector) Bool(value bool, err error) bool {
 	ec.Check(err)
 	return value
 }
 
 // HostingService provides the config.HostingService part of the given fallible function result
 // while registering the given error.
-func (ec *ErrorChecker) HostingService(value config.HostingService, err error) config.HostingService {
+func (ec *Collector) HostingService(value config.HostingService, err error) config.HostingService {
 	ec.Check(err)
 	return value
 }
 
 // PullBranchStrategy provides the string part of the given fallible function result
 // while registering the given error.
-func (ec *ErrorChecker) PullBranchStrategy(value config.PullBranchStrategy, err error) config.PullBranchStrategy {
+func (ec *Collector) PullBranchStrategy(value config.PullBranchStrategy, err error) config.PullBranchStrategy {
 	ec.Check(err)
 	return value
 }
 
 // String provides the string part of the given fallible function result
 // while registering the given error.
-func (ec *ErrorChecker) String(value string, err error) string {
+func (ec *Collector) String(value string, err error) string {
 	ec.Check(err)
 	return value
 }
 
 // Strings provides the []string part of the given fallible function result
 // while registering the given error.
-func (ec *ErrorChecker) Strings(value []string, err error) []string {
+func (ec *Collector) Strings(value []string, err error) []string {
 	ec.Check(err)
 	return value
 }
 
 // SyncStrategy provides the string part of the given fallible function result
 // while registering the given error.
-func (ec *ErrorChecker) SyncStrategy(value config.SyncStrategy, err error) config.SyncStrategy {
+func (ec *Collector) SyncStrategy(value config.SyncStrategy, err error) config.SyncStrategy {
 	ec.Check(err)
 	return value
 }
