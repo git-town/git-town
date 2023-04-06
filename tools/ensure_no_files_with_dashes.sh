61b9e57e19daa750445b5a4c3aec8cc8e085b714
Kevin Goslar <kevin.goslar@gmail.com>
2023-04-06 08:52:05 -0500

    ensure_no_files_with_dashes.sh golangci_lint_1.50.1

--- tools/ensure_no_files_with_dashes.sh
+++ tools/ensure_no_files_with_dashes.sh
@@ -3,9 +3,9 @@
 # This script verifies that there are no files or folders that contain dashes.
 # Git Town uses underscores in file paths.
 
-files_with_dashes=$(find . -name '*-*' | grep -v node_modules | grep -v vendor | grep -v '.git' | grep -v website)
+files_with_dashes=$(find . -name '*-*' | grep -v node_modules | grep -v vendor | grep -v '.git' | grep -v './website' | grep -v 'text-run.yml' | grep -v './.gherkin-*')
 count=$(echo "$files_with_dashes" | wc -l)
 if [ ! "$count" -eq 0 ]; then
-  echo "Found $count files/folders containing dashes:"
+  echo "ERROR: Found $count files/folders containing dashes:"
   echo "$files_with_dashes"
 fi
