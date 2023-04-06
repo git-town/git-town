6d0a24d777f12237be9a6fbed86bae109aad8825
Kevin Goslar <kevin.goslar@gmail.com>
2023-04-06 08:47:40 -0500

    ensure_no_files_with_dashes.sh

--- tools/ensure_no_files_with_dashes.sh
+++ tools/ensure_no_files_with_dashes.sh
@@ -3,7 +3,7 @@
 # This script verifies that there are no files or folders that contain dashes.
 # Git Town uses underscores in file paths.
 
-files_with_dashes=$(find . -name '*-*' | grep -v node_modules | grep -v vendor | grep -v '.git')
+files_with_dashes=$(find . -name '*-*' | grep -v node_modules | grep -v vendor | grep -v '.git' | grep -v website)
 count=$(echo "$files_with_dashes" | wc -l)
 if [ ! "$count" -eq 0 ]; then
   echo "Found $count files/folders containing dashes:"
