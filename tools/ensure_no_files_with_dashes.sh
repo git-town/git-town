97b026191175dcb14e1ad1f116ab457bce9fe720
Kevin Goslar <kevin.goslar@gmail.com>
2023-04-06 11:04:58 -0500

    ensure_no_files_with_dashes.sh

--- tools/ensure_no_files_with_dashes.sh
+++ tools/ensure_no_files_with_dashes.sh
@@ -4,7 +4,6 @@
 # Git Town uses underscores in file paths.
 
 files_with_dashes=$(find . -name '*-*' | grep -v node_modules | grep -v vendor | grep -v '.git' | grep -v './website' | grep -v 'text-run.yml' | grep -v './.gherkin-*')
-echo "$files_with_dashes"
 if [ -n "$files_with_dashes" ]; then
   count=$(echo "$files_with_dashes" | wc -l)
   tput setaf 1
