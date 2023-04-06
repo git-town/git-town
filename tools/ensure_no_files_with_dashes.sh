9059471eecc78ab21927e6597b195d0df90e9c71
Kevin Goslar <kevin.goslar@gmail.com>
2023-04-06 10:52:57 -0500

    ensure_no_files_with_dashes.sh

--- tools/ensure_no_files_with_dashes.sh
+++ tools/ensure_no_files_with_dashes.sh
@@ -6,9 +6,10 @@
 files_with_dashes=$(find . -name '*-*' | grep -v node_modules | grep -v vendor | grep -v '.git' | grep -v './website' | grep -v 'text-run.yml' | grep -v './.gherkin-*')
 count=$(echo "$files_with_dashes" | wc -l)
 if [ ! "$count" -eq 0 ]; then
-	tput setaf 1
-	echo
-	echo "ERROR: Found $count files/folders containing dashes:"
-	tput init
-	echo "$files_with_dashes"
+  tput setaf 1
+  echo
+  echo "ERROR: Found $count files/folders containing dashes:"
+  tput sgr0
+  echo "$files_with_dashes"
+  exit 1
 fi
