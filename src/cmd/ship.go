06ec762ee0d69dcb7442830c48bbeb7e4e8dfcd2
Kevin Goslar <kevin.goslar@gmail.com>
2023-04-26 10:09:34 -0500

    Improve feature spec for continuing

--- src/cmd/ship.go
+++ src/cmd/ship.go
@@ -273,7 +273,6 @@ func shipStepList(config *shipConfig, commitMessage string, run *git.ProdRunner)
 		list.Add(&steps.SetParentStep{Branch: child, ParentBranch: config.targetBranch})
 	}
 	if !config.isShippingInitialBranch {
-		// TODO: check out the main branch here?
 		list.Add(&steps.CheckoutStep{Branch: config.initialBranch})
 	}
 	list.Wrap(runstate.WrapOptions{RunInGitRoot: true, StashOpenChanges: !config.isShippingInitialBranch}, &run.Backend, config.mainBranch)
