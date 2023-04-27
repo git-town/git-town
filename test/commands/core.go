be219439a7ce57c79c6b20a65ef9825fdce95fd2
Kevin Goslar <kevin.goslar@gmail.com>
2023-04-27 07:35:17 -0500

    Extract commands

--- test/commands/core.go
+++ test/commands/core.go
@@ -1,5 +1,17 @@
 package commands
 
+import (
+	prodgit "github.com/git-town/git-town/v8/src/git"
+	"github.com/git-town/git-town/v8/test/subshell"
+)
+
+// TestCommands defines Git commands used only in test code.
+type TestCommands struct {
+	subshell.Mocking
+	Config prodgit.RepoConfig
+	*prodgit.BackendCommands
+}
+
 type Shell interface {
 	Run(string, ...string) (string, error)
 	RunMany([][]string) error
