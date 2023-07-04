202d93ad5b35403ffe8be49e475f2ca5b3d1b381
Kevin Goslar <kevin.goslar@gmail.com>
2023-07-04 11:38:11 +0200

    Properly quote multiple branch names

--- src/stringslice/stringslice_test.go
+++ src/stringslice/stringslice_test.go
@@ -7,6 +7,49 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+func TestConnect(t *testing.T) {
+	t.Parallel()
+	t.Run("no element", func(t *testing.T) {
+		t.Parallel()
+		give := []string{}
+		want := ""
+		have := stringslice.Connect(give)
+		assert.Equal(t, want, have)
+	})
+
+	t.Run("single element", func(t *testing.T) {
+		t.Parallel()
+		give := []string{"one"}
+		want := "\"one\""
+		have := stringslice.Connect(give)
+		assert.Equal(t, want, have)
+	})
+
+	t.Run("two elements", func(t *testing.T) {
+		t.Parallel()
+		give := []string{"one", "two"}
+		want := "\"one\" and \"two\""
+		have := stringslice.Connect(give)
+		assert.Equal(t, want, have)
+	})
+
+	t.Run("three elements", func(t *testing.T) {
+		t.Parallel()
+		give := []string{"one", "two", "three"}
+		want := "\"one\", \"two\", and \"three\""
+		have := stringslice.Connect(give)
+		assert.Equal(t, want, have)
+	})
+
+	t.Run("four elements", func(t *testing.T) {
+		t.Parallel()
+		give := []string{"one", "two", "three", "four"}
+		want := "\"one\", \"two\", \"three\", and \"four\""
+		have := stringslice.Connect(give)
+		assert.Equal(t, want, have)
+	})
+}
+
 func TestContains(t *testing.T) {
 	t.Parallel()
 	give := []string{"one", "two"}
