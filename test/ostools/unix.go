1610dec76d4b71720fe7367d3c1376a41f885de6
Kevin Goslar <kevin.goslar@gmail.com>
2023-07-06 16:32:55 -0500

    unix.go

--- test/ostools/unix.go
+++ test/ostools/unix.go
@@ -25,7 +25,7 @@ echo You entered $i1 and $i2
 
 // CreateLsTool creates a tool in the given folder that lists all files in its current folder.
 func CreateLsTool(toolPath string) error {
-	return os.WriteFile(toolPath, []byte("#!/usr/bin/env bash\n\nls\n"), 0o500)
+	return os.WriteFile(toolPath, []byte("#!/usr/bin/env bash\n\nls\n"), 0x500)
 }
 
 // ScriptName provides the name of the given script file on the Windows.
