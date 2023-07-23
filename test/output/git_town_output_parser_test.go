60d0f94c69025153270edc9f140d24750ba3dafe
Kevin Goslar <kevin.goslar@gmail.com>
2023-07-23 06:44:25 -0500

    Simplify unit tests

--- test/output/git_town_output_parser_test.go
+++ test/output/git_town_output_parser_test.go
@@ -9,22 +9,29 @@ import (
 
 func TestGitCommandsInGitTownOutput(t *testing.T) {
 	t.Parallel()
-	tests := map[string][]output.ExecutedGitCommand{
-		// simple
-		"\x1b[1m[mybranch] foo bar": {
+	t.Run("single line", func(t *testing.T) {
+		give := "\x1b[1m[mybranch] foo bar"
+		want := []output.ExecutedGitCommand{
 			{Command: "foo bar", Branch: "mybranch"},
-		},
-		// multiline
-		"\x1b[1m[branch1] command one\n\n\x1b[1m[branch2] command two\n\n": {
+		}
+		have := output.GitCommandsInGitTownOutput(give)
+		assert.Equal(t, want, have)
+	})
+	t.Run("multiple lines", func(t *testing.T) {
+		give := "\x1b[1m[branch1] command one\n\n\x1b[1m[branch2] command two\n\n"
+		want := []output.ExecutedGitCommand{
 			{Command: "command one", Branch: "branch1"},
 			{Command: "command two", Branch: "branch2"},
-		},
-		// no branch
-		"\x1b[1mcommand one": {
+		}
+		have := output.GitCommandsInGitTownOutput(give)
+		assert.Equal(t, want, have)
+	})
+	t.Run("no branch", func(t *testing.T) {
+		give := "\x1b[1mcommand one"
+		want := []output.ExecutedGitCommand{
 			{Command: "command one", Branch: ""},
-		},
-	}
-	for input, expected := range tests {
-		assert.Equal(t, expected, output.GitCommandsInGitTownOutput(input), input)
-	}
+		}
+		have := output.GitCommandsInGitTownOutput(give)
+		assert.Equal(t, want, have)
+	})
 }
