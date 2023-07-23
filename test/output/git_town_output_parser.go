6acb74374f2636fccadd9c936b65d3a70eaf52e9
Kevin Goslar <kevin.goslar@gmail.com>
2023-07-23 06:10:10 -0500

    Simplify E2E test implementation

--- test/output/git_town_output_parser.go
+++ test/output/git_town_output_parser.go
@@ -17,7 +17,6 @@ type ExecutedGitCommand struct {
 func GitCommandsInGitTownOutput(output string) []ExecutedGitCommand {
 	result := []ExecutedGitCommand{}
 	for _, line := range strings.Split(output, "\n") {
-		line = strings.TrimSpace(line)
 		if lineContainsGitTownCommand(line) {
 			result = append(result, parseLine(line))
 		}
