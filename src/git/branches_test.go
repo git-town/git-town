dff8c2c311168d0cffef3b9c4bacf1b397875f4f
Kevin Goslar <kevin.goslar@gmail.com>
2023-07-25 15:42:33 -0500

    branches_test.go

--- src/git/branches_test.go
+++ src/git/branches_test.go
@@ -7,8 +7,25 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestAncestry(t *testing.T) {
+func TestBranches(t *testing.T) {
 	t.Parallel()
+
+	t.Run("BranchNames", func(t *testing.T) {
+		t.Parallel()
+		bs := git.BranchesSyncStatus{
+			git.BranchSyncStatus{
+				Name:       "one",
+				SyncStatus: git.SyncStatusUpToDate,
+			},
+			git.BranchSyncStatus{
+				Name:       "two",
+				SyncStatus: git.SyncStatusAhead,
+			},
+		}
+		have := bs.BranchNames()
+		want := []string{"one", "two"}
+		assert.Equal(t, want, have)
+	})
 
 	t.Run("Contains", func(t *testing.T) {
 		t.Parallel()
@@ -60,6 +77,39 @@ func TestAncestry(t *testing.T) {
 		assert.Equal(t, want, have)
 	})
 
+	t.Run("LocalBranchesWithDeletedTrackingBranches", func(t *testing.T) {
+		t.Parallel()
+		bs := git.BranchesSyncStatus{
+			git.BranchSyncStatus{
+				Name:       "up-to-date",
+				SyncStatus: git.SyncStatusUpToDate,
+			},
+			git.BranchSyncStatus{
+				Name:       "ahead",
+				SyncStatus: git.SyncStatusAhead,
+			},
+			git.BranchSyncStatus{
+				Name:       "behind",
+				SyncStatus: git.SyncStatusBehind,
+			},
+			git.BranchSyncStatus{
+				Name:       "local-only",
+				SyncStatus: git.SyncStatusLocalOnly,
+			},
+			git.BranchSyncStatus{
+				Name:       "remote-only",
+				SyncStatus: git.SyncStatusRemoteOnly,
+			},
+			git.BranchSyncStatus{
+				Name:       "deleted-at-remote",
+				SyncStatus: git.SyncStatusDeletedAtRemote,
+			},
+		}
+		have := bs.LocalBranchesWithDeletedTrackingBranches().BranchNames()
+		want := []string{"deleted-at-remote"}
+		assert.Equal(t, want, have)
+	})
+
 	t.Run("Lookup", func(t *testing.T) {
 		t.Parallel()
 		bs := git.BranchesSyncStatus{
@@ -75,6 +125,54 @@ func TestAncestry(t *testing.T) {
 		assert.Equal(t, "one", bs.Lookup("one").Name)
 		assert.Equal(t, "two", bs.Lookup("two").Name)
 		assert.Nil(t, bs.Lookup("zonk"))
+	})
+
+	t.Run("Remove", func(t *testing.T) {
+		t.Parallel()
+		t.Run("contains the removed element", func(t *testing.T) {
+			bs := git.BranchesSyncStatus{
+				git.BranchSyncStatus{
+					Name:       "one",
+					SyncStatus: git.SyncStatusUpToDate,
+				},
+				git.BranchSyncStatus{
+					Name:       "two",
+					SyncStatus: git.SyncStatusUpToDate,
+				},
+			}
+			have := bs.Remove("two")
+			want := git.BranchesSyncStatus{
+				git.BranchSyncStatus{
+					Name:       "one",
+					SyncStatus: git.SyncStatusUpToDate,
+				},
+			}
+			assert.Equal(t, want, have)
+		})
+	})
+	t.Run("does not contain the removed element", func(t *testing.T) {
+		bs := git.BranchesSyncStatus{
+			git.BranchSyncStatus{
+				Name:       "one",
+				SyncStatus: git.SyncStatusUpToDate,
+			},
+			git.BranchSyncStatus{
+				Name:       "two",
+				SyncStatus: git.SyncStatusUpToDate,
+			},
+		}
+		have := bs.Remove("zonk")
+		want := git.BranchesSyncStatus{
+			git.BranchSyncStatus{
+				Name:       "one",
+				SyncStatus: git.SyncStatusUpToDate,
+			},
+			git.BranchSyncStatus{
+				Name:       "two",
+				SyncStatus: git.SyncStatusUpToDate,
+			},
+		}
+		assert.Equal(t, want, have)
 	})
 
 	t.Run("Select", func(t *testing.T) {
