8b3227317d847a90e874ef79f538b4507268eae9
Kevin Goslar <kevin.goslar@gmail.com>
2023-08-08 10:18:41 -0500

    aliases.go

--- src/cmd/aliases.go
+++ src/cmd/aliases.go
@@ -62,8 +62,8 @@ func aliases(arg string, debug bool) error {
 }
 
 func addAliases(run *git.ProdRunner) error {
-	for _, aliasType := range config.Aliases() {
-		err := run.Frontend.AddGitAlias(aliasType)
+	for _, alias := range config.Aliases() {
+		err := run.Frontend.AddGitAlias(alias)
 		if err != nil {
 			return err
 		}
@@ -73,10 +73,10 @@ func addAliases(run *git.ProdRunner) error {
 }
 
 func removeAliases(run *git.ProdRunner) error {
-	for _, aliasType := range config.Aliases() {
-		existingAlias := run.Config.GitAlias(aliasType)
-		if existingAlias == "town "+string(aliasType) {
-			err := run.Frontend.RemoveGitAlias(aliasType)
+	for _, alias := range config.Aliases() {
+		existingAlias := run.Config.GitAlias(alias)
+		if existingAlias == "town "+string(alias) {
+			err := run.Frontend.RemoveGitAlias(alias)
 			if err != nil {
 				return err
 			}
