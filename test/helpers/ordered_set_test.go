11ef1501c830fc9a528bff9574c30e87c4d456aa
Kevin Goslar <kevin.goslar@gmail.com>
2023-08-18 15:26:18 -0500

    ordered_set_test.go

--- test/helpers/ordered_set_test.go
+++ test/helpers/ordered_set_test.go
@@ -4,13 +4,48 @@ import (
 	"testing"
 
 	"github.com/git-town/git-town/v9/test/helpers"
+	"github.com/stretchr/testify/assert"
 )
 
 func TestOrderedSet(t *testing.T) {
 	t.Parallel()
-	t.Run("strings", func(t *testing.T) {
+	t.Run("Add", func(t *testing.T) {
 		t.Parallel()
+		t.Run("element doesn't exist in set", func(t *testing.T) {
+			set := helpers.NewOrderedSet("one", "two")
+			set = set.Add("three")
+			have := set.Elements()
+			want := []string{"one", "two", "three"}
+			assert.Equal(t, want, have)
+		})
+		t.Run("element already exists in set", func(t *testing.T) {
+			set := helpers.NewOrderedSet("one", "two")
+			set = set.Add("two")
+			have := set.Elements()
+			want := []string{"one", "two"}
+			assert.Equal(t, want, have)
+		})
+	})
+
+	t.Run("Contains", func(t *testing.T) {
 		set := helpers.NewOrderedSet("one", "two")
-		set.Add("three")
+		assert.True(t, set.Contains("one"))
+		assert.True(t, set.Contains("two"))
+		assert.False(t, set.Contains("zonk"))
+	})
+
+	t.Run("Elements", func(t *testing.T) {
+		set := helpers.NewOrderedSet("one", "two")
+		have := set.Elements()
+		want := []string{"one", "two"}
+		assert.Equal(t, want, have)
+	})
+
+	t.Run("Join", func(t *testing.T) {
+		set := helpers.NewOrderedSet("one", "two", "three")
+		have := set.Join(", ")
+		want := "one, two, three"
+		assert.Equal(t, want, have)
+
 	})
 }
