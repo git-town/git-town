e3d2dd915b08e709d5ca8f361220e89c73261d39
Kevin Goslar <kevin.goslar@gmail.com>
2023-08-18 09:35:47 -0500

    core.go

--- src/domain/core.go
+++ src/domain/core.go
@@ -1,3 +1,3 @@
-// Package domain contains strong type support for modeling the domain of Git Town.
-// This helps lean on the typesystem to prevent bugs, stringly-typed code, and primitive obsession.
+// Package domain provides strong type support for modeling the domain of Git Town.
+// This helps lean on the typesystem to prevent bugs, stringly-typed code, and primitive obsession and improve maintainability.
 package domain
