4321c3f27c0091cbc5ea78ce0dc0359e044327af
Kevin Goslar <kevin.goslar@gmail.com>
2023-08-17 20:14:59 -0500

    location.go

--- src/domain/location.go
+++ src/domain/location.go
@@ -5,7 +5,7 @@ import "encoding/json"
 // Location describes a location within a Git repo.
 // This could be either a branch or a SHA.
 type Location struct {
-	id string // TODO: rename to id
+	id string
 }
 
 func (l Location) MarshalJSON() ([]byte, error) {
