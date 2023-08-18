98c9000142a617029225d812d28f840b3b1d4f1a
Kevin Goslar <kevin.goslar@gmail.com>
2023-08-17 19:54:46 -0500

    core.go

--- src/cache/core.go
+++ src/cache/core.go
@@ -6,8 +6,10 @@ import "github.com/git-town/git-town/v9/src/domain"
 // Bool is a cache for bool variables.
 type Bool = Cache[bool]
 
+// LocalBranch is a cache for domain.LocalBranchName variables.
 type LocalBranch = Cache[domain.LocalBranchName]
 
+// RemoteBranch is a cache for domain.RemoteBranchName variables.
 type RemoteBranch = Cache[domain.RemoteBranchName]
 
 // String is a cache for string variables.
