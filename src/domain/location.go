b2c48003bcd0fd1ca07fe03b4c23de3c14613c52
Kevin Goslar <kevin.goslar@gmail.com>
2023-08-18 09:57:40 -0500

    location.go

--- src/domain/location.go
+++ src/domain/location.go
@@ -2,12 +2,13 @@ package domain
 
 import "encoding/json"
 
-// Location describes a location within a Git repo.
-// This could be either a branch or a SHA.
+// Location is a location within a Git repo.
+// Examples for locations are SHA addresses of commits or branch names.
 type Location struct {
-	id string
+	id string // the textual description of the location
 }
 
+// MarshalJSON is used when serializing this Location to JSON.
 func (l Location) MarshalJSON() ([]byte, error) {
 	return json.Marshal(l.id)
 }
@@ -15,12 +16,7 @@ func (l Location) MarshalJSON() ([]byte, error) {
 // Implementation of the fmt.Stringer interface.
 func (l Location) String() string { return l.id }
 
+// UnmarshalJSON is used when de-serializing JSON into a Location.
 func (l *Location) UnmarshalJSON(b []byte) error {
-	var t string
-	err := json.Unmarshal(b, &t)
-	if err != nil {
-		return err
-	}
-	l.id = t
-	return nil
+	return json.Unmarshal(b, &l.id)
 }
