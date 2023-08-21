c78a77756ac428c4712a73ef1b942791ed78c4c4
Kevin Goslar <kevin.goslar@gmail.com>
2023-08-20 21:13:54 -0500

    steplist_test.go

--- src/runstate/steplist_test.go
+++ src/runstate/steplist_test.go
@@ -29,6 +29,50 @@ func TestStepList(t *testing.T) {
 		})
 	})
 
+	t.Run("AppendList", func(t *testing.T) {
+		t.Run("append a populated list", func(t *testing.T) {
+			list := runstate.StepList{List: []steps.Step{&steps.AbortMergeStep{}}}
+			other := runstate.StepList{List: []steps.Step{&steps.StashOpenChangesStep{}}}
+			list.AppendList(other)
+			want := []steps.Step{&steps.AbortMergeStep{}, &steps.StashOpenChangesStep{}}
+			assert.Equal(t, want, list.List)
+		})
+		t.Run("append an empty list", func(t *testing.T) {
+			list := runstate.StepList{List: []steps.Step{&steps.AbortMergeStep{}}}
+			other := runstate.StepList{List: []steps.Step{}}
+			list.AppendList(other)
+			assert.Equal(t, []steps.Step{&steps.AbortMergeStep{}}, list.List)
+		})
+	})
+
+	t.Run("IsEmpty", func(t *testing.T) {
+		t.Run("list is empty", func(t *testing.T) {
+			list := runstate.StepList{List: []steps.Step{}}
+			assert.True(t, list.IsEmpty())
+		})
+		t.Run("list is not empty", func(t *testing.T) {
+			list := runstate.StepList{List: []steps.Step{&steps.AbortMergeStep{}}}
+			assert.False(t, list.IsEmpty())
+		})
+	})
+
+	t.Run("Peek", func(t *testing.T) {
+		t.Run("populated list", func(t *testing.T) {
+			list := runstate.StepList{List: []steps.Step{&steps.AbortMergeStep{}, &steps.StashOpenChangesStep{}}}
+			have := list.Peek()
+			assert.Equal(t, &steps.AbortMergeStep{}, have, "returns the last element of the list")
+			wantList := runstate.StepList{List: []steps.Step{&steps.AbortMergeStep{}, &steps.StashOpenChangesStep{}}}
+			assert.Equal(t, wantList, list, "does not modify the list")
+		})
+		t.Run("empty list", func(t *testing.T) {
+			list := runstate.StepList{List: []steps.Step{}}
+			have := list.Peek()
+			assert.Equal(t, nil, have)
+			wantList := runstate.StepList{List: []steps.Step{}}
+			assert.Equal(t, wantList, list)
+		})
+	})
+
 	t.Run("Prepend", func(t *testing.T) {
 		t.Run("prepend a single step", func(t *testing.T) {
 			list := runstate.StepList{List: []steps.Step{&steps.AbortMergeStep{}}}
