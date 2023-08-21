b853ab174376cdec6150d11f4888c16ff5f206ab
Kevin Goslar <kevin.goslar@gmail.com>
2023-08-21 18:09:20 -0500

    persistence_test.go

--- src/runstate/persistence_test.go
+++ src/runstate/persistence_test.go
@@ -1,13 +1,17 @@
 package runstate_test
 
 import (
+	"encoding/json"
 	"testing"
 
+	"github.com/git-town/git-town/v9/src/domain"
 	"github.com/git-town/git-town/v9/src/runstate"
+	"github.com/git-town/git-town/v9/src/steps"
 	"github.com/stretchr/testify/assert"
 )
 
 func TestSanitizePath(t *testing.T) {
+	t.Parallel()
 	t.Run("SanitizePath", func(t *testing.T) {
 		t.Parallel()
 		tests := map[string]string{
@@ -19,6 +23,30 @@ func TestSanitizePath(t *testing.T) {
 			assert.Equal(t, want, have)
 		}
 	})
-	t.Run("Save", func(t *testing.T) {
+	t.Run("Serialization format", func(t *testing.T) {
+		t.Parallel()
+		t.Run("PushBranchStep", func(t *testing.T) {
+			t.Parallel()
+			step := steps.PushBranchStep{
+				Branch:         domain.NewLocalBranchName("main"),
+				ForceWithLease: true,
+				NoPushHook:     true,
+				Undoable:       true,
+			}
+			want := `
+{
+  "Branch": "main",
+  "ForceWithLease": true,
+  "NoPushHook": true,
+  "Undoable": true
+}`[1:]
+			bytes, err := json.MarshalIndent(step, "", "  ")
+			assert.NoError(t, err)
+			assert.Equal(t, want, string(bytes))
+			newStep := steps.PushBranchStep{}
+			err = json.Unmarshal(bytes, &newStep)
+			assert.NoError(t, err)
+			assert.Equal(t, step, newStep)
+		})
 	})
 }
