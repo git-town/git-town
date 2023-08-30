a698cc4e2a2a864e45a6c65fba0f0b83b024a43c
Kevin Goslar <kevin.goslar@gmail.com>
2023-08-29 21:13:04 -0500

    snapshot_test.go

--- src/runstate/snapshot_test.go
+++ src/runstate/snapshot_test.go
@@ -95,6 +95,53 @@ func TestSnapshot(t *testing.T) {
 		})
 		t.Run("branches updated", func(t *testing.T) {
 			t.Parallel()
+			before := runstate.Snapshot{
+				Branches: domain.BranchInfos{
+					domain.BranchInfo{
+						Name:       domain.NewLocalBranchName("branch-1"),
+						InitialSHA: domain.NewSHA("111111"),
+					},
+					domain.BranchInfo{
+						Name:       domain.NewLocalBranchName("branch-2"),
+						InitialSHA: domain.NewSHA("222222"),
+					},
+					domain.BranchInfo{
+						Name:       domain.NewLocalBranchName("branch-3"),
+						InitialSHA: domain.NewSHA("333333"),
+					},
+				},
+			}
+			after := runstate.Snapshot{
+				Branches: domain.BranchInfos{
+					domain.BranchInfo{
+						Name:       domain.NewLocalBranchName("branch-1"),
+						InitialSHA: domain.NewSHA("111111"),
+					},
+					domain.BranchInfo{
+						Name:       domain.NewLocalBranchName("branch-2"),
+						InitialSHA: domain.NewSHA("444444"),
+					},
+					domain.BranchInfo{
+						Name:       domain.NewLocalBranchName("branch-3"),
+						InitialSHA: domain.NewSHA("333333"),
+					},
+				},
+			}
+			have := after.Diff(before)
+			want := runstate.Diff{
+				BranchesUpdated: map[domain.BranchName]runstate.BranchUpdate{
+					domain.NewBranchName("branch-2"): runstate.BranchUpdate{
+						OriginalSHA: domain.NewSHA("222222"),
+						FinalSHA:    domain.NewSHA("444444"),
+					},
+				},
+				BranchesAdded:   map[domain.BranchName]domain.SHA{},
+				BranchesRemoved: map[domain.BranchName]domain.SHA{},
+				PartialDiff:     runstate.NewPartialDiff(),
+			}
+			fmt.Printf("WANT: %#v\n", want)
+			fmt.Printf("HAVE: %#v\n", have)
+			assert.Equal(t, want, have)
 		})
 		t.Run("config added", func(t *testing.T) {
 			t.Parallel()
