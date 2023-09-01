95badec0921d2c7c44df122ee9a16a62e06e2e4b
Kevin Goslar <kevin.goslar@gmail.com>
2023-09-01 09:48:09 -0500

    key_test.go

--- src/config/key_test.go
+++ src/config/key_test.go
@@ -20,27 +20,33 @@ func TestKey(t *testing.T) {
 		t.Run("lineage keys", func(t *testing.T) {
 			t.Parallel()
 			t.Run("valid lineage key", func(t *testing.T) {
+				t.Parallel()
 				give := "git-town-branch.branch-1.parent"
 				have := config.ParseKey(give)
 				want := &config.Key{give}
 				assert.Equal(t, want, have)
 			})
 			t.Run("lineage key without suffix", func(t *testing.T) {
+				t.Parallel()
 				have := config.ParseKey("git-town-branch.branch-1")
 				assert.Nil(t, have)
 			})
 			t.Run("lineage key without prefix", func(t *testing.T) {
+				t.Parallel()
 				have := config.ParseKey("git-town.branch-1.parent")
 				assert.Nil(t, have)
 			})
 		})
 		t.Run("alias key", func(t *testing.T) {
+			t.Parallel()
 			t.Run("valid alias", func(t *testing.T) {
+				t.Parallel()
 				have := config.ParseKey("alias.append")
 				want := &config.KeyAliasAppend
 				assert.Equal(t, want, have)
 			})
 			t.Run("invalid alias", func(t *testing.T) {
+				t.Parallel()
 				have := config.ParseKey("alias.zonk")
 				assert.Nil(t, have)
 			})
