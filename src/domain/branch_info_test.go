1d2d0db76e70465393a5f17b552ae0e084efa3b4
Kevin Goslar <kevin.goslar@gmail.com>
2023-09-13 13:31:23 -0500

    branch_info_test.go

--- src/domain/branch_info_test.go
+++ src/domain/branch_info_test.go
@@ -2,8 +2,36 @@ package domain_test
 
 import (
 	"testing"
+
+	"github.com/git-town/git-town/v9/src/domain"
+	"github.com/stretchr/testify/assert"
 )
 
 func TestBranchInfo(t *testing.T) {
 	t.Parallel()
+	t.Run("IsOmniBranch", func(t *testing.T) {
+		t.Parallel()
+		t.Run("is an omnibranch", func(t *testing.T) {
+			t.Parallel()
+			bi := domain.BranchInfo{
+				LocalName:  domain.NewLocalBranchName("branch-1"),
+				LocalSHA:   domain.NewSHA("111111"),
+				SyncStatus: domain.SyncStatusUpToDate,
+				RemoteName: domain.NewRemoteBranchName("origin/branch-1"),
+				RemoteSHA:  domain.NewSHA("111111"),
+			}
+			assert.True(t, bi.IsOmniBranch())
+		})
+		t.Run("not an omnibranch", func(t *testing.T) {
+			t.Parallel()
+			bi := domain.BranchInfo{
+				LocalName:  domain.NewLocalBranchName("branch-1"),
+				LocalSHA:   domain.NewSHA("111111"),
+				SyncStatus: domain.SyncStatusUpToDate,
+				RemoteName: domain.NewRemoteBranchName("origin/branch-1"),
+				RemoteSHA:  domain.NewSHA("222222"),
+			}
+			assert.False(t, bi.IsOmniBranch())
+		})
+	})
 }
