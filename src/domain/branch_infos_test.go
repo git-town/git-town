ebd3b57c2e1d3fa2fc499c45e8bd7c879ff26216
Kevin Goslar <kevin.goslar@gmail.com>
2023-09-13 13:20:04 -0500

    branch_infos_test.go

--- src/domain/branch_infos_test.go
+++ src/domain/branch_infos_test.go
@@ -8,6 +8,7 @@ import (
 )
 
 func TestBranchInfos(t *testing.T) {
+	t.Parallel()
 	t.Run("HasLocalBranch", func(t *testing.T) {
 		t.Parallel()
 		t.Run("has a matching local branch", func(t *testing.T) {
