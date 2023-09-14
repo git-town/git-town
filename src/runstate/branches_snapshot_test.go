87051e83ede313f98c90d1dbff2ddc99e485e7a1
Kevin Goslar <kevin.goslar@gmail.com>
2023-09-13 20:35:55 -0500

    branches_snapshot_test.go

--- src/runstate/branches_snapshot_test.go
+++ src/runstate/branches_snapshot_test.go
@@ -1018,6 +1018,47 @@ func TestBranchesSnapshot(t *testing.T) {
 
 		t.Run("omnibranch changed locally and remotely to same SHA", func(t *testing.T) {
 			t.Parallel()
+			before := runstate.BranchesSnapshot{
+				Branches: domain.BranchInfos{
+					domain.BranchInfo{
+						LocalName:  domain.NewLocalBranchName("branch-1"),
+						LocalSHA:   domain.NewSHA("111111"),
+						SyncStatus: domain.SyncStatusUpToDate,
+						RemoteName: domain.NewRemoteBranchName("origin/branch-1"),
+						RemoteSHA:  domain.NewSHA("111111"),
+					},
+				},
+			}
+			after := runstate.BranchesSnapshot{
+				Branches: domain.BranchInfos{
+					domain.BranchInfo{
+						LocalName:  domain.NewLocalBranchName("branch-1"),
+						LocalSHA:   domain.NewSHA("222222"),
+						SyncStatus: domain.SyncStatusUpToDate,
+						RemoteName: domain.NewRemoteBranchName("origin/branch-1"),
+						RemoteSHA:  domain.NewSHA("222222"),
+					},
+				},
+			}
+			changes := before.Changes(after)
+			have := changes.Diff()
+			want := runstate.Changes{
+				LocalAdded:    domain.LocalBranchNames{},
+				LocalRemoved:  map[domain.LocalBranchName]domain.SHA{},
+				LocalChanged:  map[domain.LocalBranchName]runstate.Change[domain.SHA]{},
+				RemoteAdded:   []domain.RemoteBranchName{},
+				RemoteRemoved: map[domain.RemoteBranchName]domain.SHA{},
+				RemoteChanged: map[domain.RemoteBranchName]runstate.Change[domain.SHA]{},
+				BothAdded:     domain.NewLocalBranchNames(),
+				BothRemoved:   map[domain.LocalBranchName]domain.SHA{},
+				BothChanged: map[domain.LocalBranchName]runstate.Change[domain.SHA]{
+					domain.NewLocalBranchName("branch-1"): {
+						Before: domain.NewSHA("111111"),
+						After:  domain.NewSHA("222222"),
+					},
+				},
+			}
+			assert.Equal(t, want, have)
 		})
 		t.Run("omnibranch changed locally and remotely to different SHAs", func(t *testing.T) {
 			t.Parallel()
