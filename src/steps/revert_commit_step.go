3a5857acf241e595b0bb74464f28499820d03eb3
Kevin Goslar <kevin.goslar@gmail.com>
2023-09-14 19:08:00 -0500

    Improve comment

--- src/steps/revert_commit_step.go
+++ src/steps/revert_commit_step.go
@@ -4,12 +4,15 @@ import (
 	"github.com/git-town/git-town/v9/src/domain"
 )
 
-// RevertCommitStep reverts the commit with the given sha.
+// RevertCommitStep adds a commit to the current branch
+// that reverts the commit with the given SHA.
 type RevertCommitStep struct {
 	SHA domain.SHA
 	EmptyStep
 }
 
 func (step *RevertCommitStep) Run(args RunArgs) error {
+	commits := args.Runner.Backend.Commits
+	// Ensure that the current branch contains the given commit?
 	return args.Runner.Frontend.RevertCommit(step.SHA)
 }
