af03ddaf92eb5c69b73235749c3aa6fee2316540
Kevin Goslar <kevin.goslar@gmail.com>
2023-09-19 14:15:23 -0500

    Export TypeName to gohacks

---
+++ src/gohacks/type_name.go
@@ -0,0 +1,11 @@
+package gohacks
+
+import "reflect"
+
+func TypeName(myvar interface{}) string {
+	t := reflect.TypeOf(myvar)
+	if t.Kind() == reflect.Ptr {
+		return t.Elem().Name()
+	}
+	return t.Name()
+}
---
+++ src/gohacks/type_name_test.go
@@ -0,0 +1,23 @@
+package gohacks_test
+
+import (
+	"testing"
+
+	"github.com/git-town/git-town/v9/src/domain"
+	"github.com/git-town/git-town/v9/src/gohacks"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestTypeName(t *testing.T) {
+	sha := domain.NewSHA("123456")
+	tests := map[any]string{
+		"hello":                          "string",
+		123:                              "int",
+		domain.NewLocalBranchName("foo"): "LocalBranchName",
+		&sha:                             "SHA",
+	}
+	for give, want := range tests {
+		have := gohacks.TypeName(give)
+		assert.Equal(t, want, have)
+	}
+}
