426f40c2648525404e701eb9115400601d96954a
Kevin Goslar <kevin.goslar@gmail.com>
2023-09-21 17:51:16 -0500

    location.go location_test.go

---
+++ src/domain/location_test.go
@@ -0,0 +1,31 @@
+package domain_test
+
+import (
+	"encoding/json"
+	"testing"
+
+	"github.com/git-town/git-town/v9/src/domain"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestLocation(t *testing.T) {
+	t.Parallel()
+
+	t.Run("MarshalJSON", func(t *testing.T) {
+		t.Parallel()
+		location := domain.NewLocation("branch-1")
+		have, err := json.MarshalIndent(location, "", "  ")
+		assert.Nil(t, err)
+		want := `"branch-1"`
+		assert.Equal(t, want, string(have))
+	})
+
+	t.Run("UnmarshalJSON", func(t *testing.T) {
+		t.Parallel()
+		give := `"branch-1"`
+		have := domain.Location{}
+		json.Unmarshal([]byte(give), &have)
+		want := domain.NewLocation("branch-1")
+		assert.Equal(t, want, have)
+	})
+}
