b7ba62cb7cf2dab558b1e956dc3375a4ab9c749d
Kevin Goslar <kevin.goslar@gmail.com>
2023-09-22 09:15:42 -0500

    format.go

--- tools/format.go
+++ tools/format.go
@@ -1,7 +1,55 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io/fs"
+	"os"
+	"path/filepath"
+	"strings"
+)
+
+func isTestLine(line string) bool {
+	return strings.HasPrefix(line, "  func Test") && strings.HasSuffix(line, "(t *testing.T) {")
+}
+
+func formatFile(path string, perm os.FileMode) error {
+	content, err := os.ReadFile(path)
+	if err != nil {
+		return err
+	}
+	newContent := []string{}
+	for _, line := range strings.Split(string(content), "\n") {
+		if isTestLine(line) {
+			newContent = append(newContent, line)
+			newContent = append(newContent, "")
+		} else {
+			newContent = append(newContent, line)
+		}
+	}
+	err = os.WriteFile(path, []byte(strings.Join(newContent, "\n")), perm)
+	if err != nil {
+		return err
+	}
+	fmt.Print(".")
+	return nil
+}
 
 func main() {
-	fmt.Println("hello")
+	err := filepath.WalkDir(".", func(path string, dirEntry fs.DirEntry, err error) error {
+		if err != nil {
+			return err
+		}
+		if dirEntry.IsDir() {
+			return nil
+		}
+		if !strings.HasSuffix(dirEntry.Name(), "_test.go") {
+			return nil
+		}
+		return formatFile(path, dirEntry.Type().Perm())
+	})
+	fmt.Println()
+	if err != nil {
+		fmt.Printf("ERROR: %s\n", err)
+		os.Exit(1)
+	}
 }
