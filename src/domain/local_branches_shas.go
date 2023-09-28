efce9391db5eba838a09cf193a3a0b2eabd975e5
Kevin Goslar <kevin.goslar@gmail.com>
2023-09-28 18:24:54 -0500

    local_branches_shas.go

--- src/domain/local_branches_shas.go
+++ src/domain/local_branches_shas.go
@@ -1,11 +1,9 @@
 package domain
 
+import "golang.org/x/exp/maps"
+
 type LocalBranchesSHAs map[LocalBranchName]SHA
 
 func (lbs LocalBranchesSHAs) BranchNames() LocalBranchNames {
-	result := make(LocalBranchNames, 0, len(lbs))
-	for branch := range lbs {
-		result = append(result, branch)
-	}
-	return result
+	return maps.Keys(lbs)
 }
