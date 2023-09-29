bdd842599b62d77dd7113fc1c7afe46fd1093636
Kevin Goslar <kevin.goslar@gmail.com>
2023-09-29 12:15:28 -0500

    checkout_if_exists_step.go

--- src/steps/checkout_if_exists_step.go
+++ src/steps/checkout_if_exists_step.go
@@ -15,12 +15,5 @@ func (step *CheckoutIfExistsStep) Run(args RunArgs) error {
 	if !args.Runner.Backend.HasLocalBranch(step.Branch) {
 		return nil
 	}
-	previousBranch, err := args.Runner.Backend.CurrentBranch()
-	if err != nil {
-		return err
-	}
-	if previousBranch == step.Branch {
-		return nil
-	}
-	return args.Runner.Frontend.CheckoutBranch(step.Branch)
+	return (&CheckoutStep{Branch: step.Branch}).Run(args)
 }
