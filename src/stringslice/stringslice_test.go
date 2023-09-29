75e783a08cac877a90ff59c88b73014d7888daf3
Kevin Goslar <kevin.goslar@gmail.com>
2023-09-29 11:27:07 -0500

    stringslice_test.go

--- src/stringslice/stringslice_test.go
+++ src/stringslice/stringslice_test.go
@@ -54,17 +54,14 @@ func TestStringSlice(t *testing.T) {
 
 	t.Run("Lines", func(t *testing.T) {
 		t.Parallel()
-		t.Run("has lines", func(t *testing.T) {
-			t.Parallel()
-			tests := map[string][]string{
-				"":                {},
-				"single line":     {"single line"},
-				"multiple\nlines": {"multiple", "lines"},
-			}
-			for give, want := range tests {
-				have := stringslice.Lines(give)
-				assert.Equal(t, want, have)
-			}
-		})
+		tests := map[string][]string{
+			"":                {},
+			"single line":     {"single line"},
+			"multiple\nlines": {"multiple", "lines"},
+		}
+		for give, want := range tests {
+			have := stringslice.Lines(give)
+			assert.Equal(t, want, have)
+		}
 	})
 }
