4d99751429352007a8ddc539c42be24202efca7e
Kevin Goslar <kevin.goslar@gmail.com>
2023-09-28 21:14:39 -0500

    Sort Runstate semantically (#2481)

--- src/runstate/runstate.go
+++ src/runstate/runstate.go
@@ -11,10 +11,10 @@ import (
 // including which operations are left to do,
 // and how to undo what has been done so far.
 type RunState struct {
-	AbortStepList     StepList                   `exhaustruct:"optional" json:"AbortStepList"`
 	Command           string                     `json:"Command"`
 	IsAbort           bool                       `exhaustruct:"optional" json:"IsAbort"`
 	IsUndo            bool                       `exhaustruct:"optional" json:"IsUndo"`
+	AbortStepList     StepList                   `exhaustruct:"optional" json:"AbortStepList"`
 	RunStepList       StepList                   `json:"RunStepList"`
 	UndoStepList      StepList                   `exhaustruct:"optional" json:"UndoStepList"`
 	UnfinishedDetails *UnfinishedRunStateDetails `exhaustruct:"optional" json:"UnfinishedDetails"`
