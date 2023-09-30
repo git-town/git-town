9cc0690aba56e1386db9f6c1cba5523728ece367
Kevin Goslar <kevin.goslar@gmail.com>
2023-09-30 08:16:55 -0500

    remote_branch_names.go remote_branch_names_test.go

--- src/domain/remote_branch_names.go
+++ src/domain/remote_branch_names.go
@@ -4,18 +4,18 @@ import "sort"
 
 type RemoteBranchNames []RemoteBranchName
 
+// Sort orders the branches in this collection alphabetically.
+func (rbns RemoteBranchNames) Sort() {
+	sort.Slice(rbns, func(a, b int) bool {
+		return rbns[a].id < rbns[b].id
+	})
+}
+
 // Strings provides these remote branch names as strings.
-func (r RemoteBranchNames) Strings() []string {
-	result := make([]string, len(r))
-	for b, branch := range r {
+func (rbns RemoteBranchNames) Strings() []string {
+	result := make([]string, len(rbns))
+	for b, branch := range rbns {
 		result[b] = branch.String()
 	}
 	return result
-}
-
-// Sort orders the branches in this collection alphabetically.
-func (l RemoteBranchNames) Sort() {
-	sort.Slice(l, func(a, b int) bool {
-		return l[a].id < l[b].id
-	})
 }
---
+++ src/domain/remote_branch_names_test.go
@@ -0,0 +1,11 @@
+package domain_test
+
+import "testing"
+
+func TestRemoteBranchNames(t *testing.T) {
+	t.Parallel()
+
+	t.Run("Sort", func(t *testing.T) {
+		t.Parallel()
+	})
+}
