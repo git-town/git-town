e34004f9ff9bf5b8b3abf0fc076b4732fd3ee84e
Kevin Goslar <kevin.goslar@gmail.com>
2023-09-30 13:54:58 -0500

    Rename ForcePushBranchStep to ForcePushCurrentBranchStep (#2497)

--- src/steps/force_push_current_branch_step.go
+++ src/steps/force_push_current_branch_step.go
@@ -4,15 +4,15 @@ import (
 	"github.com/git-town/git-town/v9/src/domain"
 )
 
-// ForcePushBranchStep force-pushes the branch with the given name to the origin remote.
+// ForcePushCurrentBranchStep force-pushes the branch with the given name to the origin remote.
 // TODO: rename to ForcePushCurrentBranchStep and determine the current branch.
-type ForcePushBranchStep struct {
+type ForcePushCurrentBranchStep struct {
 	Branch     domain.LocalBranchName
 	NoPushHook bool
 	EmptyStep
 }
 
-func (step *ForcePushBranchStep) Run(args RunArgs) error {
+func (step *ForcePushCurrentBranchStep) Run(args RunArgs) error {
 	shouldPush, err := args.Runner.Backend.ShouldPushBranch(step.Branch, step.Branch.TrackingBranch())
 	if err != nil {
 		return err
