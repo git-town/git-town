53c8c9a7e2850971bf09e081442d5e8c0155847f
Kevin Goslar <kevin.goslar@gmail.com>
2023-10-04 07:04:53 -0500

    file_has_content.go

--- test/asserts/file_has_content.go
+++ test/asserts/file_has_content.go
@@ -12,5 +12,5 @@ func FileHasContent(t *testing.T, dir, filename, expectedContent string) {
 	t.Helper()
 	fileContent, err := os.ReadFile(filepath.Join(dir, filename))
 	must.NoError(t, err)
-	must.EqOp(t, string(fileContent), expectedContent)
+	must.EqOp(t, expectedContent, string(fileContent))
 }
