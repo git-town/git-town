e6fc0a61e978bbf3c6429f0ccf4c0f1ce889b01b
Kevin Goslar <kevin.goslar@gmail.com>
2023-10-04 17:22:29 -0500

    is_checkout_step_test.go

--- src/steps/is_checkout_step_test.go
+++ src/steps/is_checkout_step_test.go
@@ -11,16 +11,19 @@ import (
 
 func TestIsCheckoutStep(t *testing.T) {
 	t.Parallel()
+
 	t.Run("given a step.Checkout", func(t *testing.T) {
 		t.Parallel()
 		give := &step.Checkout{Branch: domain.NewLocalBranchName("branch")}
 		must.True(t, steps.IsCheckoutStep(give))
 	})
+
 	t.Run("given a step.CheckoutIfExists", func(t *testing.T) {
 		t.Parallel()
 		give := &step.CheckoutIfExists{Branch: domain.NewLocalBranchName("branch")}
 		must.True(t, steps.IsCheckoutStep(give))
 	})
+
 	t.Run("given another step", func(t *testing.T) {
 		t.Parallel()
 		give := &step.AbortMerge{}
