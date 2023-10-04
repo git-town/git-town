90e906e66703fd7a2c69f88065083e6be7df655a
Kevin Goslar <kevin.goslar@gmail.com>
2023-10-04 17:11:35 -0500

    core.go is_checkout_step.go

--- src/steps/core.go
+++ src/steps/core.go
@@ -1,11 +1,2 @@
+// Package steps provides facilities to represent and build collections of steps.
 package steps
-
-import (
-	"github.com/git-town/git-town/v9/src/gohacks"
-	"github.com/git-town/git-town/v9/src/step"
-)
-
-func IsCheckoutStep(step step.Step) bool {
-	typeName := gohacks.TypeName(step)
-	return typeName == "Checkout" || typeName == "CheckoutIfExists"
-}
