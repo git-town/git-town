b6ec716ef2011252c399edc00d45b4ef90d0c256
Kevin Goslar <kevin.goslar@gmail.com>
2023-10-05 15:00:53 -0500

    Move LongestStringLength into stringslice (#2537)

--- src/gohacks/stringslice/stringslice.go
+++ src/gohacks/stringslice/stringslice.go
@@ -38,3 +38,14 @@ func Lines(text string) []string {
 	}
 	return strings.Split(text, "\n")
 }
+
+// Longest provides the length of the longest string in the given string slice.
+func Longest(strings []string) int {
+	result := 0
+	for _, s := range strings {
+		if currentLen := len(s); currentLen > result {
+			result = currentLen
+		}
+	}
+	return result
+}
