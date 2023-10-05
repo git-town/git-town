74742faf86fdc28d1f7a64a680a782a54a263046
Kevin Goslar <kevin.goslar@gmail.com>
2023-10-05 14:56:46 -0500

    stringslice_test.go

--- src/gohacks/stringslice/stringslice_test.go
+++ src/gohacks/stringslice/stringslice_test.go
@@ -67,12 +67,23 @@ func TestStringSlice(t *testing.T) {
 
 	t.Run("Longest", func(t *testing.T) {
 		t.Parallel()
-		tests := map[int][]string{
-			5: {"one", "two", "three"},
-			0: {""},
-		}
-		for expected, input := range tests {
-			must.EqOp(t, expected, stringslice.Longest(input))
-		}
+		t.Run("various strings", func(t *testing.T) {
+			t.Parallel()
+			give := []string{"one", "two", "three"}
+			have := stringslice.Longest(give)
+			must.Eq(t, 5, have)
+		})
+		t.Run("empty string", func(t *testing.T) {
+			t.Parallel()
+			give := []string{""}
+			have := stringslice.Longest(give)
+			must.Eq(t, 0, have)
+		})
+		t.Run("empty slice", func(t *testing.T) {
+			t.Parallel()
+			give := []string{}
+			have := stringslice.Longest(give)
+			must.Eq(t, 0, have)
+		})
 	})
 }
