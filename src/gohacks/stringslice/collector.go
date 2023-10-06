19ef1a52a544b438dd0078189752562f90d01f80
Kevin Goslar <kevin.goslar@gmail.com>
2023-10-06 11:32:50 -0500

    collector.go

--- src/gohacks/stringslice/collector.go
+++ src/gohacks/stringslice/collector.go
@@ -1,13 +1,16 @@
 package stringslice
 
+// Collector accumulates string instances one at a time.
 type Collector struct {
 	data []string
 }
 
+// Add appends a string instance to this collector.
 func (c *Collector) Add(text string) {
 	c.data = append(c.data, text)
 }
 
+// Result provides all accumulated string instances.
 func (c *Collector) Result() []string {
 	if c.data == nil {
 		return []string{}
