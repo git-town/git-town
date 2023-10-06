387b4d45cca9cd50392bb2eb1892bb97c5762a49
Kevin Goslar <kevin.goslar@gmail.com>
2023-10-06 09:54:17 -0500

    Surround empty backend arguments with quotes (#2548)

--- src/gohacks/stringslice/stringslice.go
+++ src/gohacks/stringslice/stringslice.go
@@ -49,3 +49,16 @@ func Longest(strings []string) int {
 	}
 	return result
 }
+
+// SurroundEmptyWith surrounds all empty strings in the given list with the given character.
+func SurroundEmptyWith(strings []string, surround string) []string {
+	result := make([]string, len(strings))
+	for t, text := range strings {
+		if text == "" {
+			result[t] = surround + surround
+		} else {
+			result[t] = text
+		}
+	}
+	return result
+}
