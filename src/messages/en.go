de4c76cddd91d5f633fb5341030e2405632438ea
Kevin Goslar <kevin.goslar@gmail.com>
2023-10-06 11:36:33 -0500

    en.go

--- src/messages/en.go
+++ src/messages/en.go
@@ -8,7 +8,7 @@ const (
 	BranchAlreadyExistsRemotely       = "there is already a branch %q at the \"origin\" remote"
 	BranchCheckoutProblem             = "cannot check out branch %q: %w"
 	BranchCurrentProblem              = "cannot determine current branch: %w"
-	BranchDeletedHasUnmergedChanges   = "Branch %q was deleted at the remote but the local branch contains unshipped changes.\nI am therefore not removing this branch. Run \"git town diff-parent\" to see the changes."
+	BranchDeletedHasUnmergedChanges   = "Branch %q was deleted at the remote but the local branch contains unshipped changes.\nI am therefore not removing this branch. You can see the unshipped changes by running \"git town diff-parent\"."
 	BranchDiffProblem                 = "cannot determine if branch %q has unmerged commits: %w"
 	BranchDoesntContainCommit         = "branch %q does not contain commit %q. Found commits %s"
 	BranchDoesntExist                 = "there is no branch %q"
