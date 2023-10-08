88c5dc71f5e449b4aa796f1eacf2013c37812c60
Kevin Goslar <kevin.goslar@gmail.com>
2023-10-07 20:20:55 -0500

    change_parent.go set_parent.go

--- src/step/set_parent.go
+++ src/step/set_parent.go
@@ -3,6 +3,7 @@ package step
 import "github.com/git-town/git-town/v9/src/domain"
 
 // SetParent sets the given parent branch as the parent of the given branch.
+// To change an existing parent, use ChangeParent.
 type SetParent struct {
 	Branch domain.LocalBranchName
 	Parent domain.LocalBranchName
