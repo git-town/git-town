b3ad5e97f0b75472a4a6ae6ac6142e7f8445cded
Kevin Goslar <kevin.goslar@gmail.com>
2023-10-08 16:07:05 -0500

    core.go

--- src/vm/runstate/core.go
+++ src/vm/runstate/core.go
@@ -1,2 +1,2 @@
-// Package state represents the current execution state of the interpreter within a Git Town command.
+// Package runstate represents the current execution state of the interpreter within a Git Town command.
 package runstate
