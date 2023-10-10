d82ad4647c3597943f2fb0998ef4bf50f2922650
Kevin Goslar <kevin.goslar@gmail.com>
2023-10-10 13:27:56 -0500

    checkout_parent.go merge.go merge_parent.go

--- src/step/merge.go
+++ src/step/merge.go
@@ -11,7 +11,7 @@ type Merge struct {
 }
 
 func (step *Merge) CreateAbortSteps() []Step {
-	return []Step{&AbortMerge{}}
+	return []Step{&AbortMerge{}} // TODO: move the step on a new line for readability
 }
 
 func (step *Merge) CreateContinueSteps() []Step {
---
+++ src/step/merge_parent.go
@@ -0,0 +1,27 @@
+package step
+
+// MergeParent merges the current parent of the current branch into the current branch.
+type MergeParent struct {
+	Empty
+}
+
+func (step *MergeParent) CreateAbortSteps() []Step {
+	return []Step{
+		&AbortMerge{},
+	}
+}
+
+func (step *MergeParent) CreateContinueSteps() []Step {
+	return []Step{
+		&ContinueMerge{},
+	}
+}
+
+func (step *MergeParent) Run(args RunArgs) error {
+	currentBranch, err := args.Runner.Backend.CurrentBranch()
+	if err != nil {
+		return err
+	}
+	parent := args.Lineage.Parent(currentBranch)
+	return args.Runner.Frontend.MergeBranchNoEdit(parent.BranchName())
+}
