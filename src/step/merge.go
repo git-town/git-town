76e5f8951a49c33d2ecd13d04493da2fd36568a0
Kevin Goslar <kevin.goslar@gmail.com>
2023-10-12 10:17:06 -0500

    merge.go

--- src/step/merge.go
+++ src/step/merge.go
@@ -1,6 +1,8 @@
 package step
 
-import "github.com/git-town/git-town/v9/src/domain"
+import (
+	"github.com/git-town/git-town/v9/src/domain"
+)
 
 // Merge merges the branch with the given name into the current branch.
 type Merge struct {
