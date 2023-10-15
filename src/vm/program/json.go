421094d114dfcd9e6f8c4d8761edbd24f605d72c
Kevin Goslar <kevin.goslar@gmail.com>
2023-10-14 21:07:23 -0500

    .golangci.yml core.go json.go

--- src/vm/program/json.go
+++ src/vm/program/json.go
@@ -35,9 +35,111 @@ func (js *JSON) UnmarshalJSON(b []byte) error {
 	if err != nil {
 		return err
 	}
-	js.Step = opcode.DetermineStep(stepType)
+	js.Step = DetermineStep(stepType)
 	if js.Step == nil {
 		return fmt.Errorf(messages.RunstateStepUnknown, stepType)
 	}
 	return json.Unmarshal(mapping["data"], &js.Step)
+}
+
+func DetermineStep(stepType string) shared.Opcode { //nolint:ireturn
+	switch stepType {
+	case "AbortMerge":
+		return &opcode.AbortMerge{}
+	case "AbortRebase":
+		return &opcode.AbortRebase{}
+	case "AddToPerennialBranches":
+		return &opcode.AddToPerennialBranches{}
+	case "ChangeParent":
+		return &opcode.ChangeParent{}
+	case "Checkout":
+		return &opcode.Checkout{}
+	case "CheckoutIfExists":
+		return &opcode.CheckoutIfExists{}
+	case "CommitOpenChanges":
+		return &opcode.CommitOpenChanges{}
+	case "ConnectorMergeProposal":
+		return &opcode.ConnectorMergeProposal{}
+	case "ContinueMerge":
+		return &opcode.ContinueMerge{}
+	case "ContinueRebase":
+		return &opcode.ContinueRebase{}
+	case "CreateBranch":
+		return &opcode.CreateBranch{}
+	case "CreateBranchExistingParent":
+		return &opcode.CreateBranchExistingParent{}
+	case "CreateProposal":
+		return &opcode.CreateProposal{}
+	case "CreateRemoteBranch":
+		return &opcode.CreateRemoteBranch{}
+	case "CreateTrackingBranch":
+		return &opcode.CreateTrackingBranch{}
+	case "DeleteLocalBranch":
+		return &opcode.DeleteLocalBranch{}
+	case "DeleteParentBranch":
+		return &opcode.DeleteParentBranch{}
+	case "DeleteRemoteBranch":
+		return &opcode.DeleteRemoteBranch{}
+	case "DeleteTrackingBranch":
+		return &opcode.DeleteTrackingBranch{}
+	case "DiscardOpenChanges":
+		return &opcode.DiscardOpenChanges{}
+	case "EnsureHasShippableChanges":
+		return &opcode.EnsureHasShippableChanges{}
+	case "FetchUpstream":
+		return &opcode.FetchUpstream{}
+	case "ForcePushCurrentBranch":
+		return &opcode.ForcePushCurrentBranch{}
+	case "Merge":
+		return &opcode.Merge{}
+	case "MergeParent":
+		return &opcode.MergeParent{}
+	case "PreserveCheckoutHistory":
+		return &opcode.PreserveCheckoutHistory{}
+	case "PullCurrentBranch":
+		return &opcode.PullCurrentBranch{}
+	case "PushCurrentBranch":
+		return &opcode.PushCurrentBranch{}
+	case "PushTags":
+		return &opcode.PushTags{}
+	case "RebaseBranch":
+		return &opcode.RebaseBranch{}
+	case "RebaseParent":
+		return &opcode.RebaseParent{}
+	case "RemoveFromPerennialBranches":
+		return &opcode.RemoveFromPerennialBranches{}
+	case "RemoveGlobalConfig":
+		return &opcode.RemoveGlobalConfig{}
+	case "RemoveLocalConfig":
+		return &opcode.RemoveLocalConfig{}
+	case "ResetCurrentBranchToSHA":
+		return &opcode.ResetCurrentBranchToSHA{}
+	case "ResetRemoteBranchToSHA":
+		return &opcode.ResetRemoteBranchToSHA{}
+	case "RestoreOpenChanges":
+		return &opcode.RestoreOpenChanges{}
+	case "RevertCommit":
+		return &opcode.RevertCommit{}
+	case "SetExistingParent":
+		return &opcode.SetExistingParent{}
+	case "SetGlobalConfig":
+		return &opcode.SetGlobalConfig{}
+	case "SetLocalConfig":
+		return &opcode.SetLocalConfig{}
+	case "SetParent":
+		return &opcode.SetParent{}
+	case "SetParentIfBranchExists":
+		return &opcode.SetParentIfBranchExists{}
+	case "SquashMerge":
+		return &opcode.SquashMerge{}
+	case "SkipCurrentBranch":
+		return &opcode.SkipCurrentBranch{}
+	case "StashOpenChanges":
+		return &opcode.StashOpenChanges{}
+	case "UndoLastCommit":
+		return &opcode.UndoLastCommit{}
+	case "UpdateProposalTarget":
+		return &opcode.UpdateProposalTarget{}
+	}
+	return nil
 }
