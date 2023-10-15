2f27c162559dc033cc54ade8720bd57646b23f2f
Kevin Goslar <kevin.goslar@gmail.com>
2023-10-15 07:21:28 -0500

    core.go

--- src/vm/persistence/core.go
+++ src/vm/persistence/core.go
@@ -1,12 +1,2 @@
 // Package persistence stores Git Town runstate on disk.
 package persistence
-
-import "github.com/git-town/git-town/v9/src/vm/shared"
-
-type Opcode interface {
-	CreateAbortProgram() []Opcode
-	CreateContinueProgram() []Opcode
-	CreateAutomaticAbortError() error
-	Run(args shared.RunArgs) error
-	ShouldAutomaticallyAbortOnError() bool
-}
