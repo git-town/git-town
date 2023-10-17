61a951204778fa4205d7bbae10c4897b538a401b
Kevin Goslar <kevin.goslar@gmail.com>
2023-10-17 10:08:52 -0500

    branch_tree.go

--- src/cli/format/branch_tree.go
+++ src/cli/format/branch_tree.go
@@ -5,7 +5,7 @@ import (
 	"github.com/git-town/git-town/v9/src/domain"
 )
 
-// BranchTree returns a user printable branch tree.
+// BranchTree provids a printable version of the given branch tree.
 func BranchTree(branch domain.LocalBranchName, lineage config.Lineage) string {
 	result := branch.String()
 	childBranches := lineage.Children(branch)
