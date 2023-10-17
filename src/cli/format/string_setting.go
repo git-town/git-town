8493cafb44c0f265d65955a3063110f055050342
Kevin Goslar <kevin.goslar@gmail.com>
2023-10-17 10:09:58 -0500

    string_setting.go

--- src/cli/format/string_setting.go
+++ src/cli/format/string_setting.go
@@ -1,5 +1,6 @@
 package format
 
+// StringSetting provides a printable version of the given string configuration value.
 func StringSetting(text string) string {
 	if text == "" {
 		return "(not set)"
