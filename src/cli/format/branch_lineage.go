8964638ad7a183d70d3a1c2a8bcec53ad649d3af
Kevin Goslar <kevin.goslar@gmail.com>
2023-10-17 10:07:47 -0500

    branch_lineage.go

--- src/cli/format/branch_lineage.go
+++ src/cli/format/branch_lineage.go
@@ -6,7 +6,7 @@ import (
 	"github.com/git-town/git-town/v9/src/config"
 )
 
-// BranchLineage provides a CLI printable formatting of the given branch lineage.
+// BranchLineage provides printable formatting of the given branch lineage.
 func BranchLineage(lineage config.Lineage) string {
 	roots := lineage.Roots()
 	trees := make([]string, len(roots))
