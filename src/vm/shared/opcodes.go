3b32e76dee62c227a21605591233827deff6264a
Kevin Goslar <kevin.goslar@gmail.com>
2023-10-20 08:31:44 -0500

    opcodes.go

--- src/vm/shared/opcodes.go
+++ src/vm/shared/opcodes.go
@@ -1,14 +1,18 @@
 package shared
 
-import "encoding/json"
+import (
+	"encoding/json"
+
+	"github.com/git-town/git-town/v9/src/vm/program"
+)
 
 type Opcodes []Opcode
 
 // MarshalJSON marshals this program to JSON.
 func (self Opcodes) MarshalJSON() ([]byte, error) {
-	jsonOpcodes := make([]JSON, len(self.Opcodes))
-	for o, opcode := range self.Opcodes {
-		jsonOpcodes[o] = JSON{Opcode: opcode}
+	jsonOpcodes := make([]program.JSON, len(self))
+	for o, opcode := range self {
+		jsonOpcodes[o] = program.JSON{Opcode: opcode}
 	}
 	return json.Marshal(jsonOpcodes)
 }
