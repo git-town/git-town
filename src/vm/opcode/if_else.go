7078959a40c93667aeef8d1181aa5fe3001859aa
Kevin Goslar <kevin.goslar@gmail.com>
2023-10-23 12:37:47 -0500

    Manual comparison for IfElse opcode (#2609)

--- src/vm/opcode/if_else.go
+++ src/vm/opcode/if_else.go
@@ -1,6 +1,8 @@
 package opcode
 
 import (
+	"reflect"
+
 	"github.com/git-town/git-town/v9/src/config"
 	"github.com/git-town/git-town/v9/src/git"
 	"github.com/git-town/git-town/v9/src/vm/shared"
@@ -12,6 +14,12 @@ type IfElse struct {
 	WhenTrue  []shared.Opcode // the opcodes to execute if the given branch is empty
 	WhenFalse []shared.Opcode // the opcodes to execute if the given branch is not empty
 	undeclaredOpcodeMethods
+}
+
+// This method makes comparison work in unit tests.
+func (self IfElse) Equal(other IfElse) bool {
+	return reflect.DeepEqual(self.WhenFalse, other.WhenFalse) &&
+		reflect.DeepEqual(self.WhenTrue, other.WhenTrue)
 }
 
 func (self *IfElse) Run(args shared.RunArgs) error {
