35a40ff4b8801fbad334f4318033674e512fdd92
Kevin Goslar <kevin.goslar@gmail.com>
2023-10-25 22:12:06 -0500

    if_branch_has_unmerged_changes.go if_branch_has_unmerged_changes_test.go

--- src/vm/opcode/if_branch_has_unmerged_changes.go
+++ src/vm/opcode/if_branch_has_unmerged_changes.go
@@ -1,8 +1,6 @@
 package opcode
 
 import (
-	"reflect"
-
 	"github.com/git-town/git-town/v9/src/domain"
 	"github.com/git-town/git-town/v9/src/vm/shared"
 )
@@ -13,12 +11,6 @@ type IfBranchHasUnmergedChanges struct {
 	WhenTrue  []shared.Opcode // the opcodes to execute if the given branch is empty
 	WhenFalse []shared.Opcode // the opcodes to execute if the given branch is not empty
 	undeclaredOpcodeMethods
-}
-
-// This method makes comparison via https://github.com/google/go-cmp work in unit tests.
-func (self IfBranchHasUnmergedChanges) Equal(other IfBranchHasUnmergedChanges) bool {
-	return reflect.DeepEqual(self.WhenFalse, other.WhenFalse) &&
-		reflect.DeepEqual(self.WhenTrue, other.WhenTrue)
 }
 
 func (self *IfBranchHasUnmergedChanges) Run(args shared.RunArgs) error {
