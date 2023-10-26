de59d23bf086379af105f56fc25635b3b79acc49
Kevin Goslar <kevin.goslar@gmail.com>
2023-10-26 15:13:50 -0500

    sync_status.go

--- src/domain/sync_status.go
+++ src/domain/sync_status.go
@@ -6,9 +6,7 @@ import (
 
 // SyncStatus encodes the places a branch can exist at.
 // This is a type-safe enum, see https://npf.io/2022/05/safer-enums.
-type SyncStatus struct {
-	name string
-}
+type SyncStatus string
 
 // IsLocal indicates whether a branch with this SyncStatus exists in the local repo.
 func (self SyncStatus) IsLocal() bool {
@@ -21,14 +19,16 @@ func (self SyncStatus) IsLocal() bool {
 	panic(fmt.Sprintf("uncaptured sync status: %v", self))
 }
 
-func (self SyncStatus) String() string { return self.name }
+func (self SyncStatus) String() string {
+	return string(self)
+}
 
 var (
-	SyncStatusUpToDate        = SyncStatus{"up to date"}        //nolint:gochecknoglobals // the branch exists locally and remotely, the local branch is up to date
-	SyncStatusBehind          = SyncStatus{"behind"}            //nolint:gochecknoglobals // the branch exists locally and remotely, the local branch is behind the remote tracking branch
-	SyncStatusAhead           = SyncStatus{"ahead"}             //nolint:gochecknoglobals // the branch exists locally and remotely, the local branch is ahead of its remote branch
-	SyncStatusAheadAndBehind  = SyncStatus{"ahead and behind"}  //nolint:gochecknoglobals // the branch exists locally and remotely, both ends have different commits
-	SyncStatusLocalOnly       = SyncStatus{"local only"}        //nolint:gochecknoglobals // the branch was created locally and hasn't been pushed to the remote yet
-	SyncStatusRemoteOnly      = SyncStatus{"remote only"}       //nolint:gochecknoglobals // the branch exists only at the remote
-	SyncStatusDeletedAtRemote = SyncStatus{"deleted at remote"} //nolint:gochecknoglobals // the branch was deleted on the remote
+	SyncStatusUpToDate        SyncStatus = "up to date"        //nolint:gochecknoglobals // the branch exists locally and remotely, the local branch is up to date
+	SyncStatusBehind          SyncStatus = "behind"            //nolint:gochecknoglobals // the branch exists locally and remotely, the local branch is behind the remote tracking branch
+	SyncStatusAhead           SyncStatus = "ahead"             //nolint:gochecknoglobals // the branch exists locally and remotely, the local branch is ahead of its remote branch
+	SyncStatusAheadAndBehind  SyncStatus = "ahead and behind"  //nolint:gochecknoglobals // the branch exists locally and remotely, both ends have different commits
+	SyncStatusLocalOnly       SyncStatus = "local only"        //nolint:gochecknoglobals // the branch was created locally and hasn't been pushed to the remote yet
+	SyncStatusRemoteOnly      SyncStatus = "remote only"       //nolint:gochecknoglobals // the branch exists only at the remote
+	SyncStatusDeletedAtRemote SyncStatus = "deleted at remote" //nolint:gochecknoglobals // the branch was deleted on the remote
 )
