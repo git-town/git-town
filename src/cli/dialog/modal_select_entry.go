c03451917a1ecb615fd013f91ca7568cf163e0eb
Kevin Goslar <kevin.goslar@gmail.com>
2023-10-27 13:41:21 -0500

    modal_select.go modal_select_entries.go modal_select_entry.go modal_select_status.go switch.go

---
+++ src/cli/dialog/modal_select_entry.go
@@ -0,0 +1,7 @@
+package dialog
+
+// ModalSelectEntry contains one of the many entries that the user can choose from.
+type ModalSelectEntry struct {
+	Text  string // the text to display
+	Value string // the return value
+}
