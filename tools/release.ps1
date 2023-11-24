d2c8bdf982ca17c1e764845276acc3611774bc2e
Kevin Goslar <kevin.goslar@gmail.com>
2023-11-24 13:08:06 -0600

    release.ps1

--- tools/release.ps1
+++ tools/release.ps1
@@ -8,7 +8,7 @@ param (
 $ErrorActionPreference = "Stop"
 
 # the Git Town version to release
-Set-Variable -Name "GitTownVersion" -Value "v0.0.1" -Option Constant
+Set-Variable -Name "GitTownVersion" -Value "v10.0.2" -Option Constant
 
 # dependencies
 Set-Variable -Name "GoMsiVersion" -Value "1.0.2" -Option Constant
