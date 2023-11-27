f06ef375ab8cd783068b76845b8031926c7c245d
Kevin Goslar <kevin.goslar@gmail.com>
2023-11-27 10:17:57 -0600

    core.go new_pull_request.go

---
+++ src/cmd/new_pull_request.go
@@ -0,0 +1,34 @@
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/git-town/git-town/v10/src/cli/flags"
+	"github.com/git-town/git-town/v10/src/config"
+	"github.com/spf13/cobra"
+)
+
+func newPullRequestCommand() *cobra.Command {
+	addVerboseFlag, readVerboseFlag := flags.Verbose()
+	cmd := cobra.Command{
+		Use:     "new-pull-request",
+		GroupID: "basic",
+		Args:    cobra.NoArgs,
+		Short:   proposeDesc,
+		Long:    long(proposeDesc, fmt.Sprintf(proposeHelp, config.KeyCodeHostingDriver, config.KeyCodeHostingOriginHostname)),
+		RunE: func(cmd *cobra.Command, args []string) error {
+			printDeprecationNotice()
+			result := executePropose(readVerboseFlag(cmd))
+			printDeprecationNotice()
+			return result
+		},
+	}
+	addVerboseFlag(&cmd)
+	return &cmd
+}
+
+func printDeprecationNotice() {
+	fmt.Println("DEPRECATION NOTICE")
+	fmt.Println("This command has been renamed to \"git town propose\"")
+	fmt.Println("and will be removed in future versions of Git Town.")
+}
