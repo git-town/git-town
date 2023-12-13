8af6cca38d0a7aed8ef6adc221bd5aca1f81f914
Kevin Goslar <kevin.goslar@gmail.com>
2023-12-13 16:19:45 -0600

    ship_delete_tracking_branch.go

--- src/config/configdomain/ship_delete_tracking_branch.go
+++ src/config/configdomain/ship_delete_tracking_branch.go
@@ -1,5 +1,6 @@
 package configdomain
 
+// ShipDeleteTrackingBranch contains the configuration setting about whether to delete the remote branch when shipping.
 type ShipDeleteTrackingBranch bool
 
 func (self ShipDeleteTrackingBranch) Bool() bool {
