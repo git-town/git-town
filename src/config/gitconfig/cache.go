5e4d0444d9fdc888de0345279211d716492b9898
Kevin Goslar <kevin.goslar@gmail.com>
2023-12-14 06:51:16 -0600

    key.go git_town.go cache.go

--- src/config/gitconfig/cache.go
+++ src/config/gitconfig/cache.go
@@ -1,6 +1,7 @@
 package gitconfig
 
 import (
+	"fmt"
 	"regexp"
 	"sort"
 	"strings"
@@ -55,7 +56,12 @@ func LoadGitConfigCache(runner Runner, global bool) (config configdomain.Partial
 		key, value := parts[0], parts[1]
 		configKey := configdomain.ParseKey(key)
 		if configKey == nil {
+			// this part of the Git configuration is not a Git Town specific config setting --> ignore
 			continue
+		}
+		newConfigKey := updateDeprecatedKey(*configKey, value, runner)
+		if newConfigKey != nil {
+			configKey = newConfigKey
 		}
 		if config.Add(*configKey, value) {
 			continue
@@ -63,4 +69,51 @@ func LoadGitConfigCache(runner Runner, global bool) (config configdomain.Partial
 		cache[*configKey] = value
 	}
 	return config, cache
+}
+
+// updateDeprecatedKey updates the given deprecated config key to its up-to-date counterpart and returns the latter.
+func updateDeprecatedKey(key configdomain.Key, value string, global bool, runner Runner) configdomain.Key {
+	newKey, isDeprecated := configdomain.DeprecatedKeys[key]
+	if !isDeprecated {
+		return key
+	}
+	if global {
+		updateDeprecatedGlobalSetting(key, newKey, value, runner)
+	}
+	return newKey
+}
+
+func updateDeprecatedGlobalSetting(deprecatedKey, newKey configdomain.Key, value string, runner Runner) error {
+	fmt.Printf("I found the deprecated global setting %q.\n", deprecatedKey)
+	fmt.Printf("I am upgrading this setting to the new format %q.\n", newKey)
+	err := RemoveGlobalConfigValue(deprecatedKey)
+	if err != nil {
+		return err
+	}
+	err = self.SetGlobalConfigValue(newKey, deprecatedSetting)
+	return err
+	return nil
+}
+
+func (self *GitTown) updateDeprecatedLocalSetting(deprecatedKey, newKey configdomain.Key) error {
+	deprecatedSetting := self.LocalConfigValue(deprecatedKey)
+	if deprecatedSetting != "" {
+		fmt.Printf("I found the deprecated local setting %q.\n", deprecatedKey)
+		fmt.Printf("I am upgrading this setting to the new format %q.\n", newKey)
+		err := self.RemoveLocalConfigValue(deprecatedKey)
+		if err != nil {
+			return err
+		}
+		err = self.SetLocalConfigValue(newKey, deprecatedSetting)
+		return err
+	}
+	return nil
+}
+
+func (self *GitTown) updateDeprecatedSetting(deprecatedKey, newKey configdomain.Key) error {
+	err := self.updateDeprecatedLocalSetting(deprecatedKey, newKey)
+	if err != nil {
+		return err
+	}
+	return self.updateDeprecatedGlobalSetting(deprecatedKey, newKey)
 }
