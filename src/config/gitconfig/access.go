fb165920afc787c17d0adcf9e770864b812a0ff4
Kevin Goslar <kevin.goslar@gmail.com>
2023-12-14 08:40:26 -0600

    access.go

--- src/config/gitconfig/access.go
+++ src/config/gitconfig/access.go
@@ -16,26 +16,6 @@ type Access struct {
 	Runner
 }
 
-func (self *Access) RemoveGlobalConfigValue(key configdomain.Key) error {
-	return self.Run("git", "config", "--global", "--unset", key.String())
-}
-
-// removeLocalConfigurationValue deletes the configuration value with the given key from the local Git Town configuration.
-func (self *Access) RemoveLocalConfigValue(key configdomain.Key) error {
-	err := self.Run("git", "config", "--unset", key.String())
-	return err
-}
-
-// SetGlobalConfigValue sets the given configuration setting in the global Git configuration.
-func (self *Access) SetGlobalConfigValue(key configdomain.Key, value string) error {
-	return self.Run("git", "config", "--global", key.String(), value)
-}
-
-// SetLocalConfigValue sets the local configuration with the given key to the given value.
-func (self *Access) SetLocalConfigValue(key configdomain.Key, value string) error {
-	return self.Run("git", "config", key.String(), value)
-}
-
 // LoadGit provides the Git configuration from the given directory or the global one if the global flag is set.
 func (self *Access) LoadCache(global bool) SingleCache {
 	result := SingleCache{}
@@ -64,4 +44,24 @@ func (self *Access) LoadCache(global bool) SingleCache {
 		}
 	}
 	return result
+}
+
+func (self *Access) RemoveGlobalConfigValue(key configdomain.Key) error {
+	return self.Run("git", "config", "--global", "--unset", key.String())
+}
+
+// removeLocalConfigurationValue deletes the configuration value with the given key from the local Git Town configuration.
+func (self *Access) RemoveLocalConfigValue(key configdomain.Key) error {
+	err := self.Run("git", "config", "--unset", key.String())
+	return err
+}
+
+// SetGlobalConfigValue sets the given configuration setting in the global Git configuration.
+func (self *Access) SetGlobalConfigValue(key configdomain.Key, value string) error {
+	return self.Run("git", "config", "--global", key.String(), value)
+}
+
+// SetLocalConfigValue sets the local configuration with the given key to the given value.
+func (self *Access) SetLocalConfigValue(key configdomain.Key, value string) error {
+	return self.Run("git", "config", key.String(), value)
 }
