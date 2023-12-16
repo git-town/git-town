497fb6af3053b36f1fa8f00ebb38a18f7e36cd2c
Kevin Goslar <kevin.goslar@gmail.com>
2023-12-16 06:27:47 -0600

    steps.go

--- test/cucumber/steps.go
+++ test/cucumber/steps.go
@@ -326,6 +326,17 @@ func Steps(suite *godog.Suite, state *ScenarioState) {
 		return fmt.Errorf(`expected global setting "perennial-branches" to be %v, but was %v`, want, *have)
 	})
 
+	suite.Step(`^global Git Town setting "push-new-branches" is (?:now|still) "([^"]*)"$`, func(wantStr string) error {
+		have := state.fixture.DevRepo.GitTown.GlobalConfig.NewBranchPush
+		wantBool, err := strconv.ParseBool(wantStr)
+		asserts.NoError(err)
+		want := configdomain.NewBranchPush(wantBool)
+		if cmp.Equal(*have, want) {
+			return nil
+		}
+		return fmt.Errorf(`expected global setting "push-new-branches" to be %v, but was %v`, want, *have)
+	})
+
 	suite.Step(`^global Git Town setting "([^"]*)" is (?:now|still) "([^"]*)"$`, func(name, want string) error {
 		configKey := configdomain.ParseKey("git-town." + name)
 		have := state.fixture.DevRepo.GitTown.GlobalConfigValue(*configKey)
