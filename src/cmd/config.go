424b8d6ccc68e7daf9551d6b24a0aa7a151812a4
Kevin Goslar <kevin.goslar@gmail.com>
2023-12-18 13:23:34 +0000

    Sort alphabetically (#2819)

--- src/cmd/config.go
+++ src/cmd/config.go
@@ -31,12 +31,12 @@ func configCmd() *cobra.Command {
 	configCmd.AddCommand(mainbranchConfigCmd())
 	configCmd.AddCommand(offlineCmd())
 	configCmd.AddCommand(perennialBranchesCmd())
-	configCmd.AddCommand(syncPerennialStrategyCommand())
-	configCmd.AddCommand(pushNewBranchesCommand())
 	configCmd.AddCommand(pushHookCommand())
+	configCmd.AddCommand(pushNewBranchesCommand())
 	configCmd.AddCommand(resetConfigCommand())
 	configCmd.AddCommand(setupConfigCommand())
 	configCmd.AddCommand(syncFeatureStrategyCommand())
+	configCmd.AddCommand(syncPerennialStrategyCommand())
 	return &configCmd
 }
 
