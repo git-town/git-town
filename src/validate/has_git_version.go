a25e20996d902458485ae3bc99b748aea4094f41
Kevin Goslar <kevin.goslar@gmail.com>
2023-12-21 14:03:31 -0600

    Improve variable names

--- src/validate/has_git_version.go
+++ src/validate/has_git_version.go
@@ -6,8 +6,8 @@ import (
 	"github.com/git-town/git-town/v11/src/messages"
 )
 
-// HasGitVersion verifies that the system has Git of version 2.7 or newer installed.
-func HasGitVersion(majorVersion, minorVersion int) error {
+// HasAcceptableGitVersion verifies that the system has Git of version 2.7 or newer installed.
+func HasAcceptableGitVersion(majorVersion, minorVersion int) error {
 	if !IsAcceptableGitVersion(majorVersion, minorVersion) {
 		return errors.New(messages.GitVersionTooLow)
 	}
