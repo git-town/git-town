c07504d07e4241800359110b20ade2384041d631
Kevin Goslar <kevin.goslar@gmail.com>
2023-12-21 14:57:23 -0600

    Remove snapshot for current directory

--- src/undo/config_snapshot.go
+++ src/undo/config_snapshot.go
@@ -4,6 +4,5 @@ import "github.com/git-town/git-town/v11/src/config/gitconfig"
 
 // ConfigSnapshot is a snapshot of the Git configuration at a particular point in time.
 type ConfigSnapshot struct {
-	Cwd       string // the current working directory
 	GitConfig gitconfig.FullCache
 }
