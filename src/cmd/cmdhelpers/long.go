e4d632b7b91e5b387fe0cc9ff236b0740154b3c5
Kevin Goslar <kevin.goslar@gmail.com>
2023-12-22 06:32:21 -0600

    long.go

--- src/cmd/cmdhelpers/long.go
+++ src/cmd/cmdhelpers/long.go
@@ -1,5 +1,7 @@
 package cmdhelpers
 
+// Long automatically compiles the long description of Cobra commands
+// out of the given short summary and description.
 func Long(summary string, desc ...string) string {
 	if len(desc) == 1 {
 		return summary + ".\n" + desc[0]
