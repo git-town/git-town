8866d96b2fb3669b2e7af7c4bcc089cc7d19c2ca
Kevin Goslar <kevin.goslar@gmail.com>
2023-12-28 16:19:45 -0600

    single_cache.go single_cache_test.go

--- src/config/configdomain/single_cache.go
+++ src/config/configdomain/single_cache.go
@@ -1,15 +1,4 @@
 package configdomain
 
-import (
-	"golang.org/x/exp/maps"
-)
-
 // SingleCache caches a single Git configuration type (local or global).
 type SingleCache map[Key]string
-
-// Clone provides a copy of this GitConfiguration instance.
-func (self SingleCache) Clone() SingleCache {
-	result := SingleCache{}
-	maps.Copy(result, self)
-	return result
-}
