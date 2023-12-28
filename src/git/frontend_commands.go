7047450bae46e16ab43e7e2ef63ce73f65aa5e53
Kevin Goslar <kevin.goslar@gmail.com>
2023-12-28 10:23:51 -0600

    Always store current branch

--- src/git/frontend_commands.go
+++ src/git/frontend_commands.go
@@ -37,10 +37,10 @@ func (self *FrontendCommands) AbortRebase() error {
 // CheckoutBranch checks out the Git branch with the given name in this repo.
 func (self *FrontendCommands) CheckoutBranch(name gitdomain.LocalBranchName) error {
 	err := self.Run("git", "checkout", name.String())
+	self.SetCachedCurrentBranch(name)
 	if err != nil {
 		return fmt.Errorf(messages.BranchCheckoutProblem, name, err)
 	}
-	self.SetCachedCurrentBranch(name)
 	return nil
 }
 
