56e06cd823739558cf49319f35389d877aa46afa
Kevin Goslar <kevin.goslar@gmail.com>
2023-12-28 16:15:39 -0600

    Remove SingleCache.KeysMatching

--- src/config/configdomain/single_cache.go
+++ src/config/configdomain/single_cache.go
@@ -1,9 +1,6 @@
 package configdomain
 
 import (
-	"regexp"
-	"sort"
-
 	"golang.org/x/exp/maps"
 )
 
@@ -14,18 +11,5 @@ type SingleCache map[Key]string
 func (self SingleCache) Clone() SingleCache {
 	result := SingleCache{}
 	maps.Copy(result, self)
-	return result
-}
-
-// KeysMatching provides the keys in this GitConfigCache that match the given regex.
-func (self SingleCache) KeysMatching(pattern string) []Key {
-	result := []Key{}
-	re := regexp.MustCompile(pattern)
-	for key := range self {
-		if re.MatchString(key.String()) {
-			result = append(result, key)
-		}
-	}
-	sort.Slice(result, func(a, b int) bool { return result[a].String() < result[b].String() })
 	return result
 }
