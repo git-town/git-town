f3a6d582c102175b464b4d6ccac7f6183cbd133a
Kevin Goslar <kevin.goslar@gmail.com>
2023-12-29 17:06:03 -0600

    Only use subshell to run start on Windows

--- src/subshell/frontend_runner.go
+++ src/subshell/frontend_runner.go
@@ -67,10 +67,7 @@ func (self *FrontendRunner) Run(cmd string, args ...string) (err error) {
 	if self.PrintCommands {
 		PrintCommand(branchName, self.OmitBranchNames, cmd, args...)
 	}
-	// Windows commands run inside CMD
-	// because opening browsers is done via "start"
-	// TODO: do this only when actually running the "start" command
-	if runtime.GOOS == "windows" {
+	if runtime.GOOS == "windows" && args[0] == "start" {
 		args = append([]string{"/C", cmd}, args...)
 		cmd = "cmd"
 	}
