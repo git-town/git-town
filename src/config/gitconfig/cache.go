016f3b90d3a1eac5b9e531ea371cd3c337be3a06
Kevin Goslar <kevin.goslar@gmail.com>
2024-01-01 16:06:49 -0600

    cache.go

--- src/config/gitconfig/cache.go
+++ src/config/gitconfig/cache.go
@@ -1,4 +1,4 @@
 package gitconfig
 
-// Cache caches a single Git configuration type (local or global).
+// Cache buffers all of the local or global Git metadata config settings.
 type Cache map[Key]string
