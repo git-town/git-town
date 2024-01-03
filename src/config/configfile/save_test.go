e1da7f90d5350b5e402d9948006020885216160e
Kevin Goslar <kevin.goslar@gmail.com>
2024-01-03 06:51:05 -0600

    .git-branches.toml save_test.go

--- src/config/configfile/save_test.go
+++ src/config/configfile/save_test.go
@@ -1,6 +1,7 @@
 package configfile_test
 
 import (
+	"os"
 	"testing"
 
 	"github.com/git-town/git-town/v11/src/config/configdomain"
@@ -36,6 +37,51 @@ func TestSave(t *testing.T) {
 		}
 		have, err := configfile.Encode(&config)
 		must.NoError(t, err)
+		want := `
+push-new-branches = false
+ship-delete-remote-branch = false
+sync-before-ship = false
+sync-upstream = true
+
+[branches]
+  main = "main"
+  perennials = ["one", "two"]
+
+[sync-strategy]
+  feature-branches = "merge"
+  perennial-branches = "rebase"
+`[1:]
+		must.EqOp(t, want, have)
+	})
+
+	t.Run("Save", func(t *testing.T) {
+		t.Parallel()
+		mainBranch := gitdomain.LocalBranchName("main")
+		newBranchPush := configdomain.NewBranchPush(false)
+		perennialBranches := gitdomain.NewLocalBranchNames("one", "two")
+		pushHook := configdomain.PushHook(true)
+		shipDeleteTrackingBranch := configdomain.ShipDeleteTrackingBranch(false)
+		syncBeforeShip := configdomain.SyncBeforeShip(false)
+		syncFeatureStrategy := configdomain.SyncFeatureStrategyMerge
+		syncPerennialStrategy := configdomain.SyncPerennialStrategyRebase
+		syncUpstream := configdomain.SyncUpstream(true)
+		config := configdomain.PartialConfig{ //nolint:exhaustruct
+			Aliases:                  map[configdomain.AliasableCommand]string{},
+			MainBranch:               &mainBranch,
+			NewBranchPush:            &newBranchPush,
+			PerennialBranches:        &perennialBranches,
+			PushHook:                 &pushHook,
+			ShipDeleteTrackingBranch: &shipDeleteTrackingBranch,
+			SyncBeforeShip:           &syncBeforeShip,
+			SyncFeatureStrategy:      &syncFeatureStrategy,
+			SyncPerennialStrategy:    &syncPerennialStrategy,
+			SyncUpstream:             &syncUpstream,
+		}
+		err := configfile.Save(&config)
+		must.NoError(t, err)
+		bytes, err := os.ReadFile(configfile.FileName)
+		must.NoError(t, err)
+		have := string(bytes)
 		want := `
 push-new-branches = false
 ship-delete-remote-branch = false
