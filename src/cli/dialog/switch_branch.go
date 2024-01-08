a47d36f6f0e16ca88d639eba6d4466119f7f61db
Kevin Goslar <kevin.goslar@gmail.com>
2024-01-08 11:25:38 -0600

    switch_branch.go switch_branch_test.go

--- src/cli/dialog/switch_branch.go
+++ src/cli/dialog/switch_branch.go
@@ -1,12 +1,9 @@
 package dialog
 
 import (
-	"fmt"
 	"strings"
 
 	tea "github.com/charmbracelet/bubbletea"
-	"github.com/git-town/git-town/v11/src/config/configdomain"
-	"github.com/git-town/git-town/v11/src/git/gitdomain"
 )
 
 type Model struct {
@@ -24,14 +21,15 @@ func (m Model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 	switch msg := msg.(type) {
 	case tea.KeyMsg:
 		switch msg.Type {
-		case tea.KeyUp:
+		case tea.KeyUp, tea.KeyShiftTab:
 			m.MoveCursorUp()
-		case tea.KeyDown:
+		case tea.KeyDown, tea.KeyTab:
 			m.MoveCursorDown()
 		case tea.KeyEnter:
 			m.SelectedBranch = m.Branches[m.cursor]
 			return m, tea.Quit
 		case tea.KeyCtrlC:
+			m.SelectedBranch = m.InitialBranch
 			return m, tea.Quit
 		case tea.KeyRunes:
 			switch string(msg.Runes) {
@@ -67,83 +65,19 @@ func (m *Model) MoveCursorDown() {
 func (m Model) View() string {
 	s := strings.Builder{}
 	for _, branch := range m.Branches {
-		cursor := " "
-		if m.cursor == i {
-			cursor = ">"
+		if branch == m.InitialBranch {
+			s.WriteString("> ")
+			s.WriteString(branch)
+		} else if branch == m.SelectedBranch {
+			s.WriteString("* ")
+			s.WriteString(branch)
+		} else {
+			s.WriteString("  ")
+			s.WriteString(branch)
 		}
-
-		checked := " "
-		if _, ok := m.selected[i]; ok {
-			checked = "x"
-		}
-
-		s += fmt.Sprintf("%s [%s] %s\n", cursor, checked, choice)
+		s.WriteRune('\n')
 	}
 
-	s += "\nPress q to quit.\n"
-
-	return s
-}
-
-func NewBuilder(lineage configdomain.Lineage) Builder {
-	return Builder{
-		Entries: ModalSelectEntries{},
-		Lineage: lineage,
-	}
-}
-
-// queryBranch lets the user select a new branch via a visual dialog.
-// Indicates via `validSelection` whether the user made a valid selection.
-func SwitchBranch(roots gitdomain.LocalBranchNames, selected gitdomain.LocalBranchName, lineage configdomain.Lineage) (selection gitdomain.LocalBranchName, validSelection bool, err error) {
-	builder := NewBuilder(lineage)
-	err = builder.CreateEntries(roots, selected)
-	if err != nil {
-		return gitdomain.EmptyLocalBranchName(), false, err
-	}
-	choice, err := ModalSelect(builder.Entries, selected.String())
-	if err != nil {
-		return gitdomain.EmptyLocalBranchName(), false, err
-	}
-	if choice == nil {
-		return gitdomain.EmptyLocalBranchName(), false, nil
-	}
-	return gitdomain.NewLocalBranchName(*choice), true, nil
-}
-
-// Builder builds up the switch-branch dialog entries.
-type Builder struct {
-}
-
-// AddEntryAndChildren adds the given branch and all its child branches to the given entries collection.
-func (self *Builder) AddEntryAndChildren(branch gitdomain.LocalBranchName, indent int) error {
-	self.Entries = append(self.Entries, ModalSelectEntry{
-		Text:  strings.Repeat("  ", indent) + branch.String(),
-		Value: branch.String(),
-	})
-	var err error
-	for _, child := range self.Lineage.Children(branch) {
-		err = self.AddEntryAndChildren(child, indent+1)
-		if err != nil {
-			return err
-		}
-	}
-	return nil
-}
-
-// createEntries provides all the entries for the branch dialog.
-func (self *Builder) CreateEntries(roots gitdomain.LocalBranchNames, selected gitdomain.LocalBranchName) error {
-	var err error
-	for _, root := range roots {
-		err = self.AddEntryAndChildren(root, 0)
-		if err != nil {
-			return err
-		}
-	}
-	if len(self.Entries) == 0 {
-		self.Entries = append(self.Entries, ModalSelectEntry{
-			Text:  string(selected),
-			Value: string(selected),
-		})
-	}
-	return nil
+	s.WriteString("Press Ctrl-C to quit")
+	return s.String()
 }
