afeed41aef2fc8dca440f2040f14bee29c5739cf
Kevin Goslar <kevin.goslar@gmail.com>
2024-01-09 13:41:15 -0600

    Make slice package pure

--- src/gohacks/slice/append_all_missing.go
+++ src/gohacks/slice/append_all_missing.go
@@ -1,10 +1,12 @@
 package slice
 
 // AppendAllMissing appends all elements of `additional` that aren't contained in `existing` to `existing`.
-func AppendAllMissing[S ~[]C, C comparable](existing *S, additional S) {
+func AppendAllMissing[S ~[]C, C comparable](existing S, additional S) S { //nolint:ireturn
+	result := existing
 	for a := range additional {
-		if !Contains(*existing, additional[a]) {
-			*existing = append(*existing, additional[a])
+		if !Contains(result, additional[a]) {
+			result = append(result, additional[a])
 		}
 	}
+	return result
 }
