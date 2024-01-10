a37f85d84bf3d335cd1f0325be3fe5dcecc32585
Kevin Goslar <kevin.goslar@gmail.com>
2024-01-10 16:15:14 -0600

    enter_main_branch.go

--- src/cmd/debug/enter_main_branch.go
+++ src/cmd/debug/enter_main_branch.go
@@ -12,7 +12,7 @@ func enterMainBranchCmd() *cobra.Command {
 	return &cobra.Command{
 		Use: "enter-main-branch",
 		RunE: func(cmd *cobra.Command, args []string) error {
-			localBranches := gitdomain.NewLocalBranchNames("branch-1", "branch-2", "branch-3", "branch-4", "branch-5", "branch-6", "branch-7", "branch-8", "branch-9", "branch-A", "branch-B", "main")
+			localBranches := gitdomain.NewLocalBranchNames("main", "branch-1", "branch-2", "branch-3", "branch-4", "branch-5", "branch-6", "branch-7", "branch-8", "branch-9", "branch-A", "branch-B")
 			main := gitdomain.NewLocalBranchName("main")
 			selected, aborted, err := dialog.EnterMainBranch(localBranches, main)
 			if err != nil {
