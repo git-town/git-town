ea8b6f36915cdb07f3ef7b7c61ea3a4e6a63b629
Kevin Goslar <kevin.goslar@gmail.com>
2024-01-10 16:00:18 -0600

    enter_main_branch.go

--- src/cmd/debug/enter_main_branch.go
+++ src/cmd/debug/enter_main_branch.go
@@ -3,14 +3,26 @@ package debug
 import (
 	"fmt"
 
+	"github.com/git-town/git-town/v11/src/cli/dialog"
+	"github.com/git-town/git-town/v11/src/git/gitdomain"
 	"github.com/spf13/cobra"
 )
 
 func enterMainBranchCmd() *cobra.Command {
 	return &cobra.Command{
 		Use: "enter-main-branch",
-		Run: func(cmd *cobra.Command, args []string) {
-			fmt.Println("hello")
+		RunE: func(cmd *cobra.Command, args []string) error {
+			localBranches := gitdomain.NewLocalBranchNames("branch-1", "branch-2", "branch-3", "branch-4", "branch-5", "branch-6", "branch-7", "branch-8", "branch-9", "branch-A", "branch-B", "main")
+			main := gitdomain.NewLocalBranchName("main")
+			selected, aborted, err := dialog.EnterMainBranch(localBranches, main)
+			if err != nil {
+				return err
+			}
+			if aborted {
+				fmt.Println("ABORTED")
+			}
+			fmt.Println("SELECTED:", selected)
+			return nil
 		},
 	}
 }
