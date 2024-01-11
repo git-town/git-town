9b9e053d2d0e66d7471921170ccef7f730bb99a0
Kevin Goslar <kevin.goslar@gmail.com>
2024-01-10 19:28:10 -0600

    select_squash_commit_author.go

--- src/cli/dialog/select_squash_commit_author.go
+++ src/cli/dialog/select_squash_commit_author.go
@@ -3,27 +3,18 @@ package dialog
 import (
 	"fmt"
 
-	"github.com/git-town/git-town/v11/src/cli/io"
 	"github.com/git-town/git-town/v11/src/git/gitdomain"
 	"github.com/git-town/git-town/v11/src/messages"
-	survey "gopkg.in/AlecAivazis/survey.v1"
 )
 
 // SelectSquashCommitAuthor allows the user to select an author amongst a given list of authors.
-func SelectSquashCommitAuthor(branch gitdomain.LocalBranchName, authors []string) (string, error) {
+func SelectSquashCommitAuthor(branch gitdomain.LocalBranchName, authors []string) (string, bool, error) {
 	if len(authors) == 1 {
-		return authors[0], nil
+		return authors[0], false, nil
 	}
-	io.Printf(messages.BranchAuthorMultiple, branch)
-	fmt.Println()
-	result := ""
-	prompt := &survey.Select{
-		Message: messages.SquashCommitAuthorQuery,
-		Options: authors,
-	}
-	err := survey.AskOne(prompt, &result, nil)
-	if err != nil {
-		return result, fmt.Errorf(messages.DialogCannotReadAuthor, err)
-	}
-	return result, nil
+	return radioList(radioListArgs{
+		entries:      authors,
+		defaultEntry: "",
+		help:         fmt.Sprintf(messages.BranchAuthorMultiple, branch),
+	})
 }
