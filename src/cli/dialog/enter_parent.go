0764e234c8e57684d7be83c2c333315e2ee8e9b7
Kevin Goslar <kevin.goslar@gmail.com>
2024-01-10 19:24:21 -0600

    Extract radio list dialog (#2962)

--- src/cli/dialog/enter_parent.go
+++ src/cli/dialog/enter_parent.go
@@ -1,30 +1,29 @@
 package dialog
 
 import (
-	"strings"
+	"fmt"
 
-	tea "github.com/charmbracelet/bubbletea"
 	"github.com/git-town/git-town/v11/src/config/configdomain"
 	"github.com/git-town/git-town/v11/src/git/gitdomain"
 )
 
 const PerennialBranchOption = "<none> (perennial branch)"
 
+const enterParentHelpTemplate = `
+Please select the parent of branch %q or enter its number.
+Most of the time this is the main development branch (%v).
+
+
+`
+
 // EnterParent lets the user select the parent branch for the given branch.
 func EnterParent(args EnterParentArgs) (gitdomain.LocalBranchName, bool, error) {
-	parentCandidates := EnterParentEntries(args)
-	dialogData := enterParentModel{
-		bubbleList: newBubbleList(parentCandidates, args.MainBranch.String()),
-		branch:     args.Branch.String(),
-		mainBranch: args.MainBranch.String(),
-	}
-	dialogResult, err := tea.NewProgram(dialogData).Run()
-	if err != nil {
-		return gitdomain.EmptyLocalBranchName(), false, err
-	}
-	result := dialogResult.(enterParentModel) //nolint:forcetypeassert // we know the type for sure here
-	selectedBranch := gitdomain.LocalBranchName(result.selectedEntry())
-	return selectedBranch, result.aborted, nil
+	selection, aborted, err := radioList(radioListArgs{
+		entries:      EnterParentEntries(args),
+		defaultEntry: args.MainBranch.String(),
+		help:         fmt.Sprintf(enterParentHelpTemplate, args.Branch, args.MainBranch),
+	})
+	return gitdomain.LocalBranchName(selection), aborted, err
 }
 
 type EnterParentArgs struct {
@@ -32,76 +31,6 @@ type EnterParentArgs struct {
 	LocalBranches gitdomain.LocalBranchNames
 	Lineage       configdomain.Lineage
 	MainBranch    gitdomain.LocalBranchName
-}
-
-type enterParentModel struct {
-	bubbleList
-	branch     string // the branch for which to enter the parent
-	mainBranch string // name of the main branch
-}
-
-func (self enterParentModel) Init() tea.Cmd {
-	return nil
-}
-
-func (self enterParentModel) Update(msg tea.Msg) (tea.Model, tea.Cmd) { //nolint:ireturn
-	keyMsg, isKeyMsg := msg.(tea.KeyMsg)
-	if !isKeyMsg {
-		return self, nil
-	}
-	if handled, cmd := self.bubbleList.handleKey(keyMsg); handled {
-		return self, cmd
-	}
-	if keyMsg.Type == tea.KeyEnter {
-		return self, tea.Quit
-	}
-	if keyMsg.String() == "o" {
-		return self, tea.Quit
-	}
-	return self, nil
-}
-
-func (self enterParentModel) View() string {
-	s := strings.Builder{}
-	s.WriteString("\nPlease select the parent of branch \"" + self.branch + "\" or enter its number.\n")
-	s.WriteString("Most of the time this is the main development branch (" + self.mainBranch + ").\n\n")
-	for i, branch := range self.entries {
-		if i == self.cursor {
-			s.WriteString(self.entryNumberStr(i))
-			s.WriteString(self.colors.selection.Styled("> " + branch))
-		} else {
-			s.WriteString(self.entryNumberStr(i))
-			s.WriteString("  " + branch)
-		}
-		s.WriteRune('\n')
-	}
-	s.WriteString("\n\n  ")
-	// up
-	s.WriteString(self.colors.helpKey.Styled("↑"))
-	s.WriteString(self.colors.help.Styled("/"))
-	s.WriteString(self.colors.helpKey.Styled("k"))
-	s.WriteString(self.colors.help.Styled(" up   "))
-	// down
-	s.WriteString(self.colors.helpKey.Styled("↓"))
-	s.WriteString(self.colors.help.Styled("/"))
-	s.WriteString(self.colors.helpKey.Styled("j"))
-	s.WriteString(self.colors.help.Styled(" down   "))
-	// accept
-	s.WriteString(self.colors.helpKey.Styled("enter"))
-	s.WriteString(self.colors.help.Styled("/"))
-	s.WriteString(self.colors.helpKey.Styled("o"))
-	s.WriteString(self.colors.help.Styled(" accept   "))
-	// numbers
-	s.WriteString(self.colors.helpKey.Styled("0"))
-	s.WriteString(self.colors.help.Styled("-"))
-	s.WriteString(self.colors.helpKey.Styled("9"))
-	s.WriteString(self.colors.help.Styled(" jump   "))
-	// abort
-	s.WriteString(self.colors.helpKey.Styled("ctrl-c"))
-	s.WriteString(self.colors.help.Styled("/"))
-	s.WriteString(self.colors.helpKey.Styled("q"))
-	s.WriteString(self.colors.help.Styled(" abort"))
-	return s.String()
 }
 
 func EnterParentEntries(args EnterParentArgs) []string {
---
+++ src/cli/dialog/radiolist.go
@@ -0,0 +1,94 @@
+package dialog
+
+import (
+	"strings"
+
+	tea "github.com/charmbracelet/bubbletea"
+)
+
+// EnterMainBranch lets the user select a new main branch for this repo.
+func radioList(args radioListArgs) (selected string, aborted bool, err error) {
+	model := radioListModel{
+		bubbleList: newBubbleList(args.entries, args.defaultEntry),
+		help:       args.help,
+	}
+	dialogResult, err := tea.NewProgram(model).Run()
+	if err != nil {
+		return "", false, err
+	}
+	result := dialogResult.(radioListModel) //nolint:forcetypeassert
+	return result.selectedEntry(), result.aborted, nil
+}
+
+type radioListArgs struct {
+	entries      []string
+	defaultEntry string
+	help         string
+}
+
+type radioListModel struct {
+	bubbleList
+	help string // help text to display before the radio list
+}
+
+func (self radioListModel) Init() tea.Cmd {
+	return nil
+}
+
+func (self radioListModel) Update(msg tea.Msg) (tea.Model, tea.Cmd) { //nolint:ireturn
+	keyMsg, isKeyMsg := msg.(tea.KeyMsg)
+	if !isKeyMsg {
+		return self, nil
+	}
+	if handled, cmd := self.bubbleList.handleKey(keyMsg); handled {
+		return self, cmd
+	}
+	if keyMsg.Type == tea.KeyEnter {
+		return self, tea.Quit
+	}
+	if keyMsg.String() == "o" {
+		return self, tea.Quit
+	}
+	return self, nil
+}
+
+func (self radioListModel) View() string {
+	s := strings.Builder{}
+	s.WriteString(self.help)
+	for i, branch := range self.entries {
+		s.WriteString(self.entryNumberStr(i))
+		if i == self.cursor {
+			s.WriteString(self.colors.selection.Styled("> " + branch))
+		} else {
+			s.WriteString("  " + branch)
+		}
+		s.WriteRune('\n')
+	}
+	s.WriteString("\n\n  ")
+	// up
+	s.WriteString(self.colors.helpKey.Styled("↑"))
+	s.WriteString(self.colors.help.Styled("/"))
+	s.WriteString(self.colors.helpKey.Styled("k"))
+	s.WriteString(self.colors.help.Styled(" up   "))
+	// down
+	s.WriteString(self.colors.helpKey.Styled("↓"))
+	s.WriteString(self.colors.help.Styled("/"))
+	s.WriteString(self.colors.helpKey.Styled("j"))
+	s.WriteString(self.colors.help.Styled(" down   "))
+	// numbers
+	s.WriteString(self.colors.helpKey.Styled("0"))
+	s.WriteString(self.colors.help.Styled("-"))
+	s.WriteString(self.colors.helpKey.Styled("9"))
+	s.WriteString(self.colors.help.Styled(" jump   "))
+	// accept
+	s.WriteString(self.colors.helpKey.Styled("enter"))
+	s.WriteString(self.colors.help.Styled("/"))
+	s.WriteString(self.colors.helpKey.Styled("o"))
+	s.WriteString(self.colors.help.Styled(" accept   "))
+	// abort
+	s.WriteString(self.colors.helpKey.Styled("esc"))
+	s.WriteString(self.colors.help.Styled("/"))
+	s.WriteString(self.colors.helpKey.Styled("q"))
+	s.WriteString(self.colors.help.Styled(" abort"))
+	return s.String()
+}
