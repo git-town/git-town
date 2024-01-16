a3f07cad08a524e83b1f89871c240dae0623bb7d
Kevin Goslar <kevin.goslar@gmail.com>
2024-01-15 18:25:52 -0600

    unfinished_run_state.go unfinished_state.go

--- src/cmd/debug/unfinished_state.go
+++ src/cmd/debug/unfinished_state.go
@@ -1,7 +1,6 @@
 package debug
 
 import (
-	"fmt"
 	"os"
 	"time"
 
@@ -16,16 +15,8 @@ func unfinishedStateCommitAuthorCmd() *cobra.Command {
 		RunE: func(cmd *cobra.Command, args []string) error {
 			branch := gitdomain.NewLocalBranchName("feature-branch")
 			dialogTestInputs := dialog.LoadTestInputs(os.Environ())
-			selected, aborted, err := dialog.AskHowToHandleUnfinishedRunState("sync", branch, time.Now().Add(time.Second*-1), true, dialogTestInputs.Next())
-			if err != nil {
-				return err
-			}
-			if aborted {
-				fmt.Println("ABORTED")
-			} else {
-				fmt.Println("SELECTED:", selected)
-			}
-			return nil
+			_, _, err := dialog.AskHowToHandleUnfinishedRunState("sync", branch, time.Now().Add(time.Second*-1), true, dialogTestInputs.Next())
+			return err
 		},
 	}
 }
