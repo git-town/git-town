d449997fd2868e945557e595e05506aac8c2c8aa
Kevin Goslar <kevin.goslar@gmail.com>
2024-01-25 16:47:57 -0600

    index_or_start_test.go index_test.go

--- src/gohacks/stringers/index_or_start_test.go
+++ src/gohacks/stringers/index_or_start_test.go
@@ -7,16 +7,16 @@ import (
 	"github.com/shoenig/test/must"
 )
 
-func TestIndex(t *testing.T) {
+func TestIndexOrStart(t *testing.T) {
 	t.Parallel()
 	t.Run("haystack contains the needle", func(t *testing.T) {
 		t.Parallel()
 		one := testEntry("one")
 		two := testEntry("two")
 		list := []testEntry{one, two}
-		have := stringers.Index(list, one)
+		have := stringers.IndexOrStart(list, one)
 		must.Eq(t, 0, have)
-		have = stringers.Index(list, two)
+		have = stringers.IndexOrStart(list, two)
 		must.Eq(t, 1, have)
 	})
 	t.Run("haystack does not contain the needle", func(t *testing.T) {
@@ -24,20 +24,14 @@ func TestIndex(t *testing.T) {
 		one := testEntry("one")
 		two := testEntry("two")
 		list := []testEntry{one}
-		have := stringers.Index(list, two)
-		must.Eq(t, -1, have)
+		have := stringers.IndexOrStart(list, two)
+		must.Eq(t, 0, have)
 	})
 	t.Run("empty haystack", func(t *testing.T) {
 		t.Parallel()
 		one := testEntry("one")
 		list := []testEntry{}
-		have := stringers.Index(list, one)
-		must.Eq(t, -1, have)
+		have := stringers.IndexOrStart(list, one)
+		must.Eq(t, 0, have)
 	})
-}
-
-type testEntry string
-
-func (self testEntry) String() string {
-	return string(self)
 }
