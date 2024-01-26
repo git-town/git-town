f53f71dd41999290d2857853efb6e80b25e58e98
Kevin Goslar <kevin.goslar@gmail.com>
2024-01-25 18:00:01 -0600

    setup.go

--- src/cmd/config/setup.go
+++ src/cmd/config/setup.go
@@ -58,7 +58,7 @@ func executeConfigSetup(verbose bool) error {
 	if err != nil || aborted {
 		return err
 	}
-	aborted, err = setupHosting(config.HostingPlatform, repo.Runner, config.dialogInputs.Next())
+	aborted, err = setupHostingPlatform(config.HostingPlatform, repo.Runner, config.dialogInputs.Next())
 	if err != nil || aborted {
 		return err
 	}
@@ -140,7 +140,7 @@ func setupAliases(existingValue configdomain.Aliases, allAliasableCommands confi
 	return aborted, nil
 }
 
-func setupHosting(existingValue configdomain.HostingPlatform, runner *git.ProdRunner, inputs dialog.TestInput) (bool, error) {
+func setupHostingPlatform(existingValue configdomain.HostingPlatform, runner *git.ProdRunner, inputs dialog.TestInput) (bool, error) {
 	newValue, aborted, err := dialog.EnterHostingPlatform(existingValue, inputs)
 	if err != nil || aborted {
 		return aborted, err
