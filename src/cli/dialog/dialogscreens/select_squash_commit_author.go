9eaebb4215cc0e83323ef05cf0ae3d45fb84301c
Kevin Goslar <kevin.goslar@gmail.com>
2024-01-26 08:17:15 -0600

    select_squash_commit_author.go

--- src/cli/dialog/dialogscreens/select_squash_commit_author.go
+++ src/cli/dialog/dialogscreens/select_squash_commit_author.go
@@ -3,18 +3,19 @@ package dialogscreens
 import (
 	"fmt"
 
+	"github.com/git-town/git-town/v11/src/cli/dialog/dialogcomponents"
 	"github.com/git-town/git-town/v11/src/git/gitdomain"
 	"github.com/git-town/git-town/v11/src/messages"
 )
 
 // SelectSquashCommitAuthor allows the user to select an author amongst a given list of authors.
-func SelectSquashCommitAuthor(branch gitdomain.LocalBranchName, authors []string, dialogTestInputs TestInput) (string, bool, error) {
+func SelectSquashCommitAuthor(branch gitdomain.LocalBranchName, authors []string, dialogTestInputs dialogcomponents.TestInput) (string, bool, error) {
 	if len(authors) == 1 {
 		return authors[0], false, nil
 	}
 	authorsList := squashCommitAuthorList(authors)
-	selection, aborted, err := radioList(authorsList, 0, fmt.Sprintf(messages.BranchAuthorMultiple, branch), dialogTestInputs)
-	fmt.Printf("Selected squash commit author: %s\n", formattedSelection(selection.String(), aborted))
+	selection, aborted, err := dialogcomponents.RadioList(authorsList, 0, fmt.Sprintf(messages.BranchAuthorMultiple, branch), dialogTestInputs)
+	fmt.Printf("Selected squash commit author: %s\n", dialogcomponents.FormattedSelection(selection.String(), aborted))
 	return selection.String(), aborted, err
 }
 
