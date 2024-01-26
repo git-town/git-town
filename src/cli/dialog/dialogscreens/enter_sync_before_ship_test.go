54f240dec2b5cc88f61ee856934f3cab9826d11c
Kevin Goslar <kevin.goslar@gmail.com>
2024-01-26 07:33:15 -0600

    enter_sync_before_ship_test.go

--- src/cli/dialog/dialogscreens/enter_sync_before_ship_test.go
+++ src/cli/dialog/dialogscreens/enter_sync_before_ship_test.go
@@ -3,7 +3,7 @@ package dialogscreens_test
 import (
 	"testing"
 
-	"github.com/git-town/git-town/v11/src/cli/dialog"
+	"github.com/git-town/git-town/v11/src/cli/dialog/dialogscreens"
 	"github.com/shoenig/test/must"
 )
 
@@ -14,8 +14,8 @@ func TestEnterSyncBeforeShip(t *testing.T) {
 		t.Parallel()
 		t.Run("Short", func(t *testing.T) {
 			t.Parallel()
-			must.Eq(t, "yes", dialog.SyncBeforeShipEntryYes.Short())
-			must.Eq(t, "no", dialog.SyncBeforeShipEntryNo.Short())
+			must.Eq(t, "yes", dialogscreens.SyncBeforeShipEntryYes.Short())
+			must.Eq(t, "no", dialogscreens.SyncBeforeShipEntryNo.Short())
 		})
 	})
 }
