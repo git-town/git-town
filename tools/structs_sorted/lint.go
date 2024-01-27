7dfdda0127d26b03e5a781625af4f39ab3d90d43
Kevin Goslar <kevin.goslar@gmail.com>
2024-01-27 07:09:01 -0600

    lint.go

--- tools/structs_sorted/lint.go
+++ tools/structs_sorted/lint.go
@@ -3,8 +3,10 @@ package main
 import (
 	"fmt"
 	"go/ast"
+	"go/format"
 	"go/parser"
 	"go/token"
+	"os"
 	"sort"
 )
 
@@ -27,17 +29,26 @@ func main() {
 			return true
 		}
 
-		fields := make([]string, len(structType.Fields.List))
-		for f, field := range structType.Fields.List {
-			if field.Names != nil {
-				fields[f] = field.Names[0].Name
+		sort.Slice(structType.Fields.List, func(a, b int) bool {
+			if structType.Fields.List[a].Names == nil || structType.Fields.List[b].Names == nil {
+				return false
 			}
-		}
-
-		if !sort.StringsAreSorted(fields) {
-			fmt.Printf("Struct %q has unsorted fields\n", typeSpec.Name.Name)
-		}
+			return structType.Fields.List[a].Names[0].Name < structType.Fields.List[b].Names[0].Name
+		})
 
 		return true
 	})
+
+	file, err := os.Create("test_sorted.go")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer file.Close()
+
+	err = format.Node(file, fset, node)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
 }
