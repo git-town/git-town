5d45c0161ea2f9770068c66b6bbad84d1266e484
Kevin Goslar <kevin.goslar@gmail.com>
2024-01-27 07:25:45 -0600

    test.go

--- tools/structs_sorted/test.go
+++ tools/structs_sorted/test.go
@@ -1,11 +1,22 @@
+// This file is test data.
 package main
 
-type Test1 struct {
+// Test1 is an unsorted struct definition.
+type Unsorted1 struct {
+	field2 int // this field should not be first
+	field1 int // this field should not be last
+}
+
+// Test1 is an unsorted struct definition.
+type Unsorted2 struct {
+	// this field should not be first
 	field2 int
+	// this field should not be last
 	field1 int
 }
 
-type Test2 struct {
-	fieldA int
-	fieldB int
+// Test2 is a sorted struct definition.
+type Sorted struct {
+	fieldA int // this field is okay
+	fieldB int // this field is also okay
 }
