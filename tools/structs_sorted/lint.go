0d750e2446d03fbd1212e869a7304ac72e41c4df
Kevin Goslar <kevin.goslar@gmail.com>
2024-01-27 07:23:26 -0600

    lint.go

--- tools/structs_sorted/lint.go
+++ tools/structs_sorted/lint.go
@@ -7,46 +7,48 @@ import (
 	"go/parser"
 	"go/token"
 	"os"
+	"path/filepath"
 	"sort"
 )
 
-func main() {
-	fset := token.NewFileSet()
-	node, err := parser.ParseFile(fset, "test.go", nil, parser.ParseComments)
+func processFile(path string, fileSet *token.FileSet) error {
+	astFile, err := parser.ParseFile(fileSet, path, nil, parser.ParseComments)
 	if err != nil {
-		fmt.Println(err)
-		return
+		return err
 	}
-
-	ast.Inspect(node, func(n ast.Node) bool {
-		typeSpec, ok := n.(*ast.TypeSpec)
+	ast.Inspect(astFile, func(astNode ast.Node) bool {
+		typeSpec, ok := astNode.(*ast.TypeSpec)
 		if !ok || typeSpec.Type == nil {
 			return true
 		}
-
 		structType, ok := typeSpec.Type.(*ast.StructType)
 		if !ok || structType.Fields == nil {
 			return true
 		}
-
 		sort.Slice(structType.Fields.List, func(a, b int) bool {
 			if structType.Fields.List[a].Names == nil || structType.Fields.List[b].Names == nil {
 				return false
 			}
 			return structType.Fields.List[a].Names[0].Name < structType.Fields.List[b].Names[0].Name
 		})
-
 		return true
 	})
-
-	file, err := os.Create("test_sorted.go")
+	file, err := os.Create(path)
 	if err != nil {
-		fmt.Println(err)
-		return
+		return err
 	}
 	defer file.Close()
+	return format.Node(file, fileSet, astFile)
+}
 
-	err = format.Node(file, fset, node)
+func main() {
+	fileSet := token.NewFileSet()
+	err := filepath.Walk("src", func(path string, fileInfo os.FileInfo, err error) error {
+		if err != nil || fileInfo.IsDir() || filepath.Ext(path) != ".go" {
+			return err
+		}
+		return processFile(path, fileSet)
+	})
 	if err != nil {
 		fmt.Println(err)
 		return
