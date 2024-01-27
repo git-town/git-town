35e41897b798f84a8c0ea859e985b9463c1430fb
Kevin Goslar <kevin.goslar@gmail.com>
2024-01-27 16:23:15 -0600

    lint.go

--- tools/structs_sorted/lint.go
+++ tools/structs_sorted/lint.go
@@ -77,7 +77,7 @@ This tool lints Go code for alphabetic sorting of struct fields.
 Usage: lint <command>
 
 Available commands:
-   run   Formats the test files
+   run   Lints the files
    test  Verifies that this tool works
 `[1:])
 }
