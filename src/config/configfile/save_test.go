d3c24a467a6c2c69441c2e54330f88b70ec3b659
Kevin Goslar <kevin.goslar@gmail.com>
2024-01-31 07:07:50 -0600

    save_test.go

--- src/config/configfile/save_test.go
+++ src/config/configfile/save_test.go
@@ -106,6 +106,8 @@ sync-upstream = true
 	t.Run("Save", func(t *testing.T) {
 		t.Parallel()
 		give := configdomain.DefaultConfig()
+		give.MainBranch = gitdomain.NewLocalBranchName("main")
+		give.PerennialBranches = gitdomain.NewLocalBranchNames("one", "two")
 		err := configfile.Save(&give)
 		defer os.Remove(configfile.FileName)
 		must.NoError(t, err)
@@ -113,18 +115,84 @@ sync-upstream = true
 		must.NoError(t, err)
 		have := string(bytes)
 		want := `
+# Git Town configuration file
+#
+# The "push-hook" setting determines whether Git Town
+# permits or prevents Git hooks while pushing branches.
+# Hooks are enabled by default. If your Git hooks are slow,
+# you can disable them to speed up branch syncing.
+#
+# When disabled, Git Town pushes using the "--no-verify" switch.
+# More info at https://www.git-town.com/preferences/push-hook.
 push-hook = true
+
+# Should Git Town push the new branches it creates
+# immediately to origin even if they are empty?
+#
+# When enabled, you can run "git push" right away
+# but creating new branches is slower and
+# it triggers an unnecessary CI run on the empty branch.
+#
+# When disabled, many Git Town commands execute faster
+# and Git Town will create the missing tracking branch
+# on the first run of "git sync".
 push-new-branches = false
-ship-delete-tracking-branch = false
+
+# Should "git ship" delete the tracking branch?
+# You want to disable this if your code hosting system
+# (GitHub, GitLab, etc) deletes head branches when
+# merging pull requests through its UI.
+ship-delete-tracking-branch = true
+
+# Should "git ship" sync branches before shipping them?
+#
+# Guidance: enable when shipping branches locally on your machine
+# and disable when shipping feature branches via the code hosting
+# API or web UI.
+#
+# When enabled, branches are always fully up to date when shipped
+# and you get a chance to resolve merge conflicts
+# between the feature branch to ship and the main development branch
+# on the feature branch. This helps keep the main branch green.
+# But this also triggers another CI run and delays shipping.
 sync-before-ship = false
+
+# Should "git sync" also fetch updates from the upstream remote?
+#
+# If an "upstream" remote exists, and this setting is enabled,
+# "git sync" will also update the local main branch
+# with commits from the main branch at the upstream remote.
+#
+# This is useful if the repository you work on is a fork,
+# and you want to keep it in sync with the repo it was forked from.
 sync-upstream = true
 
 [branches]
+
+  # The main branch is the branch from which you cut new feature branches,
+  # and into which you ship feature branches when they are done.
+  # This branch is often called "main", "master", or "development".
   main = "main"
+
+  # Perennial branches are long-lived branches.
+  # They are never shipped and have no ancestors.
+  # Typically, perennial branches have names like
+  # "development", "staging", "qa", "production", etc.
   perennials = ["one", "two"]
 
 [sync-strategy]
+
+  # How should Git Town synchronize feature branches?
+  # Feature branches are short-lived branches cut from
+  # the main branch and shipped back into the main branch.
+  # Typically you develop features and bug fixes on them,
+  # hence their name.
   feature-branches = "merge"
+
+  # How should Git Town synchronize perennial branches?
+  # Perennial branches have no parent branch.
+  # The only updates they receive are additional commits
+  # made to their tracking branch somewhere else.
   perennial-branches = "rebase"
 `[1:]
 		must.EqOp(t, want, have)
@@ -165,13 +233,7 @@ sync-upstream = true
 		t.Run("multiple lines with terminating newline", func(t *testing.T) {
 			t.Parallel()
 			have := configfile.TOMLComment("line 1\nline 2\nline 3\n", "  ")
-			want := "  # line 1\n  # line 2\n  # line 3\n"
-			must.Eq(t, want, have)
-		})
-		t.Run("don't comment initial empty line", func(t *testing.T) {
-			t.Parallel()
-			have := configfile.TOMLComment("\nline 1\nline 2\nline 3\n", "  ")
-			want := "\n  # line 1\n  # line 2\n  # line 3\n"
+			want := "  # line 1\n  # line 2\n  # line 3\n  #"
 			must.Eq(t, want, have)
 		})
 	})
