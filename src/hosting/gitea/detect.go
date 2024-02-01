0af4119a11af8a1418dea2cf214c00145091ab42
Kevin Goslar <kevin.goslar@gmail.com>
2024-01-31 20:53:21 -0600

    detect.go detect.go

---
+++ src/hosting/gitea/detect.go
@@ -0,0 +1,11 @@
+package gitea
+
+import (
+	"github.com/git-town/git-town/v11/src/config/configdomain"
+	"github.com/git-town/git-town/v11/src/git/giturl"
+)
+
+// Detect indicates whether the current repository is hosted on a GitHub server.
+func Detect(originURL *giturl.Parts, hostingPlatform configdomain.HostingPlatform) bool {
+	return originURL != nil && (originURL.Host == "gitea.com" || hostingPlatform == configdomain.HostingPlatformGitea)
+}
