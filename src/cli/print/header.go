a5ce63d14a223fb26c94fe597de63fafa18ef652
Kevin Goslar <kevin.goslar@gmail.com>
2024-02-02 19:59:25 -0600

    header.go

--- src/cli/print/header.go
+++ src/cli/print/header.go
@@ -1,11 +1,12 @@
 package print
 
 import (
-	"github.com/fatih/color"
-	"github.com/git-town/git-town/v11/src/cli/io"
+	"fmt"
+
+	"github.com/muesli/termenv"
 )
 
 func Header(text string) {
-	boldUnderline := color.New(color.Bold).Add(color.Underline)
-	io.PrintlnColor(boldUnderline, text+":")
+	boldUnderline := termenv.String().Bold().Underline()
+	fmt.Println(boldUnderline.Styled(text + ":"))
 }
