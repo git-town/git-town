4186cc5c027480620edaaa46ff183cae9cdea889
Kevin Goslar <kevin.goslar@gmail.com>
2024-02-05 15:09:58 -0600

    save.go save_test.go

--- src/config/configfile/save.go
+++ src/config/configfile/save.go
@@ -24,39 +24,39 @@ func RenderTOML(config *configdomain.FullConfig) string {
 	result.WriteString("# Run \"git town config setup\" to add additional entries\n")
 	result.WriteString("# to this file after updating Git Town.\n")
 	result.WriteString("#\n")
-	result.WriteString(TOMLComment(strings.TrimSpace(dialog.PushHookHelp), "") + "\n")
+	result.WriteString(TOMLComment(strings.TrimSpace(dialog.PushHookHelp)) + "\n")
 	result.WriteString(fmt.Sprintf("push-hook = %t\n\n", config.PushHook))
-	result.WriteString(TOMLComment(strings.TrimSpace(dialog.PushNewBranchesHelp), "") + "\n")
+	result.WriteString(TOMLComment(strings.TrimSpace(dialog.PushNewBranchesHelp)) + "\n")
 	result.WriteString(fmt.Sprintf("push-new-branches = %t\n\n", config.PushNewBranches))
-	result.WriteString(TOMLComment(strings.TrimSpace(dialog.ShipDeleteTrackingBranchHelp), "") + "\n")
+	result.WriteString(TOMLComment(strings.TrimSpace(dialog.ShipDeleteTrackingBranchHelp)) + "\n")
 	result.WriteString(fmt.Sprintf("ship-delete-tracking-branch = %t\n\n", config.ShipDeleteTrackingBranch))
-	result.WriteString(TOMLComment(strings.TrimSpace(dialog.SyncBeforeShipHelp), "") + "\n")
+	result.WriteString(TOMLComment(strings.TrimSpace(dialog.SyncBeforeShipHelp)) + "\n")
 	result.WriteString(fmt.Sprintf("sync-before-ship = %t\n\n", config.SyncBeforeShip))
-	result.WriteString(TOMLComment(strings.TrimSpace(dialog.SyncUpstreamHelp), "") + "\n")
+	result.WriteString(TOMLComment(strings.TrimSpace(dialog.SyncUpstreamHelp)) + "\n")
 	result.WriteString(fmt.Sprintf("sync-upstream = %t\n", config.SyncUpstream))
 	result.WriteString("\n[branches]\n\n")
-	result.WriteString(TOMLComment(strings.TrimSpace(dialog.MainBranchHelp), "  ") + "\n")
-	result.WriteString(fmt.Sprintf("  main = %q\n\n", config.MainBranch))
-	result.WriteString(TOMLComment(strings.TrimSpace(dialog.PerennialBranchesHelp), "  ") + "\n")
-	result.WriteString(fmt.Sprintf("  perennials = %s\n", RenderPerennialBranches(config.PerennialBranches)))
+	result.WriteString(TOMLComment(strings.TrimSpace(dialog.MainBranchHelp)) + "\n")
+	result.WriteString(fmt.Sprintf("main = %q\n\n", config.MainBranch))
+	result.WriteString(TOMLComment(strings.TrimSpace(dialog.PerennialBranchesHelp)) + "\n")
+	result.WriteString(fmt.Sprintf("perennials = %s\n", RenderPerennialBranches(config.PerennialBranches)))
 	result.WriteString("\n[hosting]\n\n")
-	result.WriteString(TOMLComment(strings.TrimSpace(dialog.HostingPlatformHelp), "  ") + "\n")
+	result.WriteString(TOMLComment(strings.TrimSpace(dialog.HostingPlatformHelp)) + "\n")
 	if config.HostingPlatform == configdomain.HostingPlatformNone {
-		result.WriteString("  # platform = \"\"\n\n")
+		result.WriteString("# platform = \"\"\n\n")
 	} else {
-		result.WriteString(fmt.Sprintf("  platform = %q\n\n", config.HostingPlatform))
+		result.WriteString(fmt.Sprintf("platform = %q\n\n", config.HostingPlatform))
 	}
-	result.WriteString(TOMLComment(strings.TrimSpace(dialog.OriginHostnameHelp), "  ") + "\n")
+	result.WriteString(TOMLComment(strings.TrimSpace(dialog.OriginHostnameHelp)) + "\n")
 	if config.HostingOriginHostname == "" {
-		result.WriteString("  # origin-hostname = \"\"\n")
+		result.WriteString("# origin-hostname = \"\"\n")
 	} else {
-		result.WriteString(fmt.Sprintf("  origin-hostname = %q\n", config.HostingOriginHostname))
+		result.WriteString(fmt.Sprintf("origin-hostname = %q\n", config.HostingOriginHostname))
 	}
 	result.WriteString("\n[sync-strategy]\n\n")
-	result.WriteString(TOMLComment(strings.TrimSpace(dialog.SyncFeatureStrategyHelp), "  ") + "\n")
-	result.WriteString(fmt.Sprintf("  feature-branches = %q\n\n", config.SyncFeatureStrategy))
-	result.WriteString(TOMLComment(strings.TrimSpace(dialog.SyncPerennialStrategyHelp), "  ") + "\n")
-	result.WriteString(fmt.Sprintf("  perennial-branches = %q\n", config.SyncPerennialStrategy))
+	result.WriteString(TOMLComment(strings.TrimSpace(dialog.SyncFeatureStrategyHelp)) + "\n")
+	result.WriteString(fmt.Sprintf("feature-branches = %q\n\n", config.SyncFeatureStrategy))
+	result.WriteString(TOMLComment(strings.TrimSpace(dialog.SyncPerennialStrategyHelp)) + "\n")
+	result.WriteString(fmt.Sprintf("perennial-branches = %q\n", config.SyncPerennialStrategy))
 	return result.String()
 }
 
@@ -64,16 +64,16 @@ func Save(config *configdomain.FullConfig) error {
 	return os.WriteFile(FileName, []byte(RenderTOML(config)), 0o600)
 }
 
-func TOMLComment(text string, linePrefix string) string {
+func TOMLComment(text string) string {
 	if text == "" {
 		return ""
 	}
 	result := []string{}
 	for _, line := range strings.Split(text, "\n") {
 		if line == "" {
-			result = append(result, linePrefix+"#")
+			result = append(result, "#")
 		} else {
-			result = append(result, linePrefix+"# "+line)
+			result = append(result, "# "+line)
 		}
 	}
 	return strings.Join(result, "\n")
