3075cb0392376a68fa5fe9858d44732b7a186d66
Kevin Goslar <kevin.goslar@gmail.com>
2024-02-07 14:55:50 -0600

    go.work format.go format_unittests.go format_unittests_test.go go.mod go.sum

---
+++ tools/format_unittests/format_unittests.go
@@ -0,0 +1,84 @@
+package main
+
+import (
+	"fmt"
+	"io/fs"
+	"os"
+	"path/filepath"
+	"strings"
+)
+
+func main() {
+	switch {
+	case len(os.Args) == 1 || len(os.Args) > 2:
+		displayUsage()
+	case len(os.Args) == 2 && os.Args[1] == "run":
+		formatFiles()
+	default:
+		fmt.Printf("Error: unknown argument: %s", os.Args[1])
+		os.Exit(1)
+	}
+}
+
+func displayUsage() {
+	fmt.Println(`
+This tool formats Go unit tests to have an empty line before top-level subtests.
+
+Usage: format <command>
+
+Available commands:
+   run   Formats the test files
+   test  Verifies that this tool works
+`[1:])
+}
+
+// shouldIgnorePath indicates whether the file with the given path should be ignored (not formatted).
+func shouldIgnorePath(path string) bool {
+	return path == "main_test.go"
+}
+
+func IsTopLevelRunLine(line string) bool {
+	return strings.HasPrefix(line, "\tt.Run(\"") && strings.HasSuffix(line, ", func(t *testing.T) {")
+}
+
+func isEmptyLine(line string) bool {
+	return line == ""
+}
+
+func IsGoTestFile(path string) bool {
+	return strings.HasSuffix(path, "_test.go")
+}
+
+func FormatFileContent(content string) string {
+	lines := strings.Split(content, "\n")
+	newLines := []string{}
+	previousLineEmpty := false
+	for _, line := range lines {
+		if IsTopLevelRunLine(line) && !previousLineEmpty {
+			newLines = append(newLines, "")
+		}
+		newLines = append(newLines, line)
+		previousLineEmpty = isEmptyLine(line)
+	}
+	return strings.Join(newLines, "\n")
+}
+
+func formatFiles() {
+	err := filepath.WalkDir(".", func(path string, dirEntry fs.DirEntry, err error) error {
+		if err != nil || dirEntry.IsDir() || !IsGoTestFile(path) || shouldIgnorePath(path) {
+			return err
+		}
+		fmt.Print(".")
+		content, err := os.ReadFile(path)
+		if err != nil {
+			return err
+		}
+		newContent := FormatFileContent(string(content))
+		return os.WriteFile(path, []byte(newContent), dirEntry.Type().Perm())
+	})
+	fmt.Println()
+	if err != nil {
+		fmt.Printf("ERROR: %s\n", err)
+		os.Exit(1)
+	}
+}
---
+++ tools/format_unittests/format_unittests_test.go
@@ -0,0 +1,174 @@
+package main_test
+
+import (
+	"testing"
+
+	formatUnittests "github.com/git-town/git-town/tools/format_unittests"
+	"github.com/shoenig/test/must"
+)
+
+func TestFormatUnittests(t *testing.T) {
+	t.Run("IsTopLevelRunLine", func(t *testing.T) {
+		tests := map[string]bool{
+			"\tt.Run(\"HasLocalBranch\", func(t *testing.T) {":   true,
+			"\t\tt.Run(\"HasLocalBranch\", func(t *testing.T) {": false,
+		}
+		for give, want := range tests {
+			have := formatUnittests.IsTopLevelRunLine(give)
+			must.EqOp(t, want, have)
+		}
+	})
+
+	t.Run("FormatContentWithSubtests", func(t *testing.T) {
+		give := `
+	package hosting_test
+
+	import (
+		"code.gitea.io/sdk/gitea"
+	)
+
+	func TestNewGiteaConnector(t *testing.T) {
+		t.Parallel()
+		t.Run("top-level test 1", func(t *testing.T) {
+			t.Parallel()
+			give := 123
+		})
+		t.Run("top-level test 2", func(t *testing.T) {
+			t.Parallel()
+			give := 123
+		})
+	}`
+		want := `
+	package hosting_test
+
+	import (
+		"code.gitea.io/sdk/gitea"
+	)
+
+	func TestNewGiteaConnector(t *testing.T) {
+		t.Parallel()
+
+		t.Run("top-level test 1", func(t *testing.T) {
+			t.Parallel()
+			give := 123
+		})
+
+		t.Run("top-level test 2", func(t *testing.T) {
+			t.Parallel()
+			give := 123
+		})
+	}`
+		have := formatUnittests.FormatFileContent(give)
+		must.EqOp(t, want, have)
+	})
+
+	t.Run("FormatContentWithNestedSubtests", func(t *testing.T) {
+		give := `
+	package hosting_test
+
+	import (
+		"code.gitea.io/sdk/gitea"
+	)
+
+	func TestNewGiteaConnector(t *testing.T) {
+		t.Parallel()
+		t.Run("top-level test 1", func(t *testing.T) {
+			t.Parallel()
+			t.Run("nested test 1a", func(t *testing.T) {
+				t.Parallel()
+				give := 123
+			})
+			t.Run("nested test 1b", func(t *testing.T) {
+				t.Parallel()
+				give := 123
+			})
+		})
+		t.Run("top-level test 2", func(t *testing.T) {
+			t.Parallel()
+			t.Run("nested test 2a", func(t *testing.T) {
+				t.Parallel()
+				give := 123
+			})
+			t.Run("nested test 2b", func(t *testing.T) {
+				t.Parallel()
+				give := 123
+			})
+		})
+	}`
+		want := `
+	package hosting_test
+
+	import (
+		"code.gitea.io/sdk/gitea"
+	)
+
+	func TestNewGiteaConnector(t *testing.T) {
+		t.Parallel()
+
+		t.Run("top-level test 1", func(t *testing.T) {
+			t.Parallel()
+			t.Run("nested test 1a", func(t *testing.T) {
+				t.Parallel()
+				give := 123
+			})
+			t.Run("nested test 1b", func(t *testing.T) {
+				t.Parallel()
+				give := 123
+			})
+		})
+
+		t.Run("top-level test 2", func(t *testing.T) {
+			t.Parallel()
+			t.Run("nested test 2a", func(t *testing.T) {
+				t.Parallel()
+				give := 123
+			})
+			t.Run("nested test 2b", func(t *testing.T) {
+				t.Parallel()
+				give := 123
+			})
+		})
+	}`
+		have := formatUnittests.FormatFileContent(give)
+		must.EqOp(t, want, have)
+	})
+
+	t.Run("FormatContentWithoutSubTests", func(t *testing.T) {
+		give := `
+	package hosting_test
+
+	import (
+		"code.gitea.io/sdk/gitea"
+	)
+
+	func TestNewGiteaConnector(t *testing.T) {
+		t.Parallel()
+		give := "123"
+	}`
+		want := `
+	package hosting_test
+
+	import (
+		"code.gitea.io/sdk/gitea"
+	)
+
+	func TestNewGiteaConnector(t *testing.T) {
+		t.Parallel()
+		give := "123"
+	}`
+		have := formatUnittests.FormatFileContent(give)
+		must.EqOp(t, want, have)
+	})
+
+	t.Run("IsGoTestFile", func(t *testing.T) {
+		tests := map[string]bool{
+			"/one/two/three_test.go": true,
+			"/one/two/three.go":      false,
+			"/one/two_test/three.go": false,
+		}
+		for give, want := range tests {
+			have := formatUnittests.IsGoTestFile(give)
+			must.EqOp(t, want, have)
+		}
+	})
+}
