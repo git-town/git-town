923d4c65c344b23c1958d96c93ecfa798a24ab82
Kevin Goslar <kevin.goslar@gmail.com>
2024-02-08 06:40:14 -0600

    list_contributors.go

--- tools/list_contributors/list_contributors.go
+++ tools/list_contributors/list_contributors.go
@@ -35,7 +35,6 @@ func main() {
 	// load and categorize all closed issues
 	issues := []*github.Issue{}
 	pullRequests := []*github.Issue{}
-	total := 0
 	page := 0
 	query := fmt.Sprintf("repo:git-town/git-town closed:>=%s", tagTime.Format("2006-01-02"))
 	for {
@@ -55,7 +54,6 @@ func main() {
 		if len(results.Issues) == 0 {
 			break
 		}
-		total = *results.Total
 		for _, issue := range results.Issues {
 			if issue.IsPullRequest() {
 				pullRequests = append(pullRequests, issue)
@@ -65,11 +63,10 @@ func main() {
 		}
 		page += 1
 	}
-	fmt.Printf("found %d issues and %d pull requests (%d total)\n", len(issues), len(pullRequests), total)
+	fmt.Printf("found %d issues and %d pull requests\n", len(issues), len(pullRequests))
 
 	// register the creators of pull requests
 	for _, pullRequest := range pullRequests {
-		fmt.Printf("%s submitted pull request %d (%s)\n", *pullRequest.User.Login, *pullRequest.Number, *pullRequest.Title)
 		users.AddUser(*pullRequest.User.Login)
 	}
 
