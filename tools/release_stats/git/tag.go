a0e9fbbb06e2f24b6ee77ad1b48f334ca5c7e80a
Kevin Goslar <kevin.goslar@gmail.com>
2024-02-09 11:16:58 -0600

    tag.go

--- tools/release_stats/git/tag.go
+++ tools/release_stats/git/tag.go
@@ -26,8 +26,8 @@ func LoadTag(name string) Tag {
 		panic(err.Error())
 	}
 	return Tag{
-		ISOTime: gitTime.Format("2006-01-02"),
-		Name:    name,
-		Time:    gitTime,
+		ISOTime: gitTime.Format("2006-01-02"), // the time this tag was created, in ISO format
+		Name:    name,                         // name of the tag
+		Time:    gitTime,                      // the time this tag was created
 	}
 }
