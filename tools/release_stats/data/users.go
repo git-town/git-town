fdd6d3c707c7a130a02f6c746c4b44c8f4f5d5d1
Kevin Goslar <kevin.goslar@gmail.com>
2024-02-09 11:13:46 -0600

    users.go

--- tools/release_stats/data/users.go
+++ tools/release_stats/data/users.go
@@ -9,17 +9,17 @@ import (
 
 // collection of unique GitHub usernames
 type Users struct {
-	users map[string]struct{}
+	list map[string]struct{}
 }
 
 func NewUsers() Users {
 	return Users{
-		users: map[string]struct{}{},
+		list: map[string]struct{}{},
 	}
 }
 
 func (self *Users) AddUser(id string) {
-	self.users[id] = struct{}{}
+	self.list[id] = struct{}{}
 }
 
 func (self *Users) AddUsers(users Users) {
@@ -29,7 +29,7 @@ func (self *Users) AddUsers(users Users) {
 }
 
 func (self *Users) Users() []string {
-	result := maps.Keys(self.users)
+	result := maps.Keys(self.list)
 	sort.Slice(result, func(i, j int) bool { return strings.ToLower(result[i]) < strings.ToLower(result[j]) })
 	return result
 }
