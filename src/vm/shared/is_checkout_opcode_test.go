9f5693b0b143901d6f644997135ef53830c1fe98
Kevin Goslar <kevin.goslar@gmail.com>
2024-02-12 08:53:48 -0600

    Use table-based test

--- src/vm/shared/is_checkout_opcode_test.go
+++ src/vm/shared/is_checkout_opcode_test.go
@@ -11,22 +11,13 @@ import (
 
 func TestIsCheckout(t *testing.T) {
 	t.Parallel()
-
-	t.Run("given an opcode.Checkout", func(t *testing.T) {
-		t.Parallel()
-		give := &opcode.Checkout{Branch: gitdomain.NewLocalBranchName("branch")}
-		must.True(t, shared.IsCheckoutOpcode(give))
-	})
-
-	t.Run("given an opcode.CheckoutIfExists", func(t *testing.T) {
-		t.Parallel()
-		give := &opcode.CheckoutIfExists{Branch: gitdomain.NewLocalBranchName("branch")}
-		must.True(t, shared.IsCheckoutOpcode(give))
-	})
-
-	t.Run("given another opcode", func(t *testing.T) {
-		t.Parallel()
-		give := &opcode.AbortMerge{}
-		must.False(t, shared.IsCheckoutOpcode(give))
-	})
+	tests := map[shared.Opcode]bool{
+		&opcode.Checkout{Branch: gitdomain.NewLocalBranchName("branch")}:         true,
+		&opcode.CheckoutIfExists{Branch: gitdomain.NewLocalBranchName("branch")}: true,
+		&opcode.AbortMerge{}: false,
+	}
+	for give, want := range tests {
+		have := shared.IsCheckoutOpcode(give)
+		must.Eq(t, want, have)
+	}
 }
