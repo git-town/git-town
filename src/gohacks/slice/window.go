5dedf89cde617522229012cba4c17680949441f1
Kevin Goslar <kevin.goslar@gmail.com>
2024-02-13 07:21:28 -0600

    window.go window_test.go

---
+++ src/gohacks/slice/window.go
@@ -0,0 +1,6 @@
+package slice
+
+// Window provides size elements surrounding the given cursor.
+func Window[S ~[]C, C comparable](elements S, cursor int, size int) S {
+
+}
---
+++ src/gohacks/slice/window_test.go
@@ -0,0 +1,56 @@
+package slice_test
+
+import "testing"
+
+func TestWindow(t *testing.T) {
+	t.Parallel()
+	t.Run("no elements", func(t *testing.T) {
+		t.Parallel()
+	})
+	t.Run("one element", func(t *testing.T) {
+		t.Parallel()
+	})
+	t.Run("fewer elements than window size", func(t *testing.T) {
+		t.Parallel()
+		t.Run("cursor at first element", func(t *testing.T) {
+			t.Parallel()
+		})
+		t.Run("cursor at second element", func(t *testing.T) {
+			t.Parallel()
+		})
+		t.Run("cursor at third element", func(t *testing.T) {
+			t.Parallel()
+		})
+		t.Run("cursor at middle element", func(t *testing.T) {
+			t.Parallel()
+		})
+		t.Run("cursor at second to last element", func(t *testing.T) {
+			t.Parallel()
+		})
+		t.Run("cursor at last element", func(t *testing.T) {
+			t.Parallel()
+		})
+	})
+	t.Run("more elements than window size", func(t *testing.T) {
+		t.Parallel()
+		t.Run("cursor at first element", func(t *testing.T) {
+			t.Parallel()
+		})
+		t.Run("cursor at second element", func(t *testing.T) {
+			t.Parallel()
+		})
+		t.Run("cursor at third element", func(t *testing.T) {
+			t.Parallel()
+		})
+		t.Run("cursor at middle element", func(t *testing.T) {
+			t.Parallel()
+		})
+		t.Run("cursor at second to last element", func(t *testing.T) {
+			t.Parallel()
+		})
+		t.Run("cursor at last element", func(t *testing.T) {
+			t.Parallel()
+		})
+	})
+
+}
