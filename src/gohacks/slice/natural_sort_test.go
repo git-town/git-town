d94494199c2099b8cee6b42cd9b23c71a67f2fa5
Kevin Goslar <kevin.goslar@gmail.com>
2024-02-14 14:44:37 -0600

    natural_sort_test.go

--- src/gohacks/slice/natural_sort_test.go
+++ src/gohacks/slice/natural_sort_test.go
@@ -11,7 +11,7 @@ func TestNaturalSort(t *testing.T) {
 	t.Parallel()
 	tests := map[*[]stringer]*[]stringer{
 		{}:                       {},                       // empty
-		{"a"}:                    {"a"},                    // single element
+		{"1"}:                    {"1"},                    // single element
 		{"a100a", "a20b", "a3c"}: {"a3c", "a20b", "a100a"}, // ordering by numeric value
 		{"a10b10", "a10b2"}:      {"a10b2", "a10b10"},      // multiple parts of numbers and characters
 	}
