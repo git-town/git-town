7aa9eea53ba5baabeed24992bf22db3176fd935d
Kevin Goslar <kevin.goslar@gmail.com>
2024-02-13 20:42:10 -0600

    natsort.go

--- src/gohacks/slice/natsort.go
+++ src/gohacks/slice/natsort.go
@@ -7,26 +7,12 @@ import (
 	"unicode"
 )
 
-type StringerSlice []fmt.Stringer
-
-func (s StringerSlice) Len() int {
-	return len(s)
-}
-
-func (s StringerSlice) Less(a, b int) bool {
-	return naturalLess(s[a].String(), s[b].String())
-}
-
-func (s StringerSlice) Swap(a, b int) {
-	s[a], s[b] = s[b], s[a]
-}
-
 func NatSort[T fmt.Stringer](elements []T) []T {
-	stringerSlice := make(StringerSlice, len(elements))
+	stringerSlice := make(Stringers, len(elements))
 	for e, element := range elements {
 		stringerSlice[e] = element
 	}
-	SortStringers(stringerSlice)
+	sort.Sort(stringerSlice)
 	result := make([]T, len(stringerSlice))
 	for s, stringer := range stringerSlice {
 		result[s] = stringer.(T)
@@ -34,47 +20,57 @@ func NatSort[T fmt.Stringer](elements []T) []T {
 	return result
 }
 
-func SortStringers(stringerSlice StringerSlice) {
-	sort.Sort(stringerSlice)
+type Stringers []fmt.Stringer
+
+func (self Stringers) Len() int {
+	return len(self)
 }
 
-func naturalLess(a, b string) bool {
-	ai, bi := 0, 0
-	for ai < len(a) && bi < len(b) {
-		var an, bn string
-		if unicode.IsDigit(rune(a[ai])) {
-			an, ai = extractNumber(a, ai)
+func (self Stringers) Less(a, b int) bool {
+	return naturalLess(self[a].String(), self[b].String())
+}
+
+func (self Stringers) Swap(a, b int) {
+	self[a], self[b] = self[b], self[a]
+}
+
+func naturalLess(text1, text2 string) bool {
+	index1, index2 := 0, 0
+	for index1 < len(text1) && index2 < len(text2) {
+		var part1, part2 string
+		if unicode.IsDigit(rune(text1[index1])) {
+			part1, index1 = extractNumber(text1, index1)
 		} else {
-			an, ai = extractNonNumber(a, ai)
+			part1, index1 = extractNonNumber(text1, index1)
 		}
-		if unicode.IsDigit(rune(b[bi])) {
-			bn, bi = extractNumber(b, bi)
+		if unicode.IsDigit(rune(text2[index2])) {
+			part2, index2 = extractNumber(text2, index2)
 		} else {
-			bn, bi = extractNonNumber(b, bi)
+			part2, index2 = extractNonNumber(text2, index2)
 		}
-		if an != bn {
-			if unicode.IsDigit(rune(an[0])) && unicode.IsDigit(rune(bn[0])) {
-				// Compare numbers by their numeric value.
-				anInt, _ := strconv.Atoi(an)
-				bnInt, _ := strconv.Atoi(bn)
-				return anInt < bnInt
+		if part1 != part2 {
+			if unicode.IsDigit(rune(part1[0])) && unicode.IsDigit(rune(part2[0])) {
+				// compare numbers by their numeric value
+				int1, _ := strconv.Atoi(part1)
+				int2, _ := strconv.Atoi(part2)
+				return int1 < int2
 			}
-			// Compare non-numbers lexicographically.
-			return an < bn
+			// compare non-numbers lexicographically
+			return part1 < part2
 		}
 	}
-	// The strings are equal up to the end of one of them.
-	return len(a) < len(b)
+	// the strings are equal up to the end of one of them
+	return len(text1) < len(text2)
 }
 
-func extractNumber(stringerSlice string, i int) (number string, next int) {
-	for next = i; next < len(stringerSlice) && unicode.IsDigit(rune(stringerSlice[next])); next++ {
+func extractNumber(text string, index int) (number string, nextIndex int) {
+	for nextIndex = index; nextIndex < len(text) && unicode.IsDigit(rune(text[nextIndex])); nextIndex++ {
 	}
-	return stringerSlice[i:next], next
+	return text[index:nextIndex], nextIndex
 }
 
-func extractNonNumber(stringerSlice string, i int) (nonNumber string, next int) {
-	for next = i; next < len(stringerSlice) && !unicode.IsDigit(rune(stringerSlice[next])); next++ {
+func extractNonNumber(text string, index int) (nonNumber string, nextIndex int) {
+	for nextIndex = index; nextIndex < len(text) && !unicode.IsDigit(rune(text[nextIndex])); nextIndex++ {
 	}
-	return stringerSlice[i:next], next
+	return text[index:nextIndex], nextIndex
 }
