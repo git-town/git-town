e7c3ffac22a0281c6c5a26c041430b5549e94de0
Kevin Goslar <kevin.goslar@gmail.com>
2024-02-13 19:50:32 -0600

    max.go max_test.go min.go min_test.go

--- src/gohacks/math/max.go
+++
@@ -1,9 +0,0 @@
-package math
-
-// Generic max function that should be in the Go standard library.
-func Max(element1, element2 int) int {
-	if element1 > element2 {
-		return element1
-	}
-	return element2
-}
--- src/gohacks/math/min.go
+++
@@ -1,9 +0,0 @@
-package math
-
-// Generic min function that should be in the Go standard library.
-func Min(element1, element2 int) int {
-	if element1 < element2 {
-		return element1
-	}
-	return element2
-}
