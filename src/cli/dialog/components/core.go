46d72ec9381b45f68864df700e6f34b75baff0b3
Kevin Goslar <kevin.goslar@gmail.com>
2024-02-14 18:08:48 -0600

    core.go

--- src/cli/dialog/components/core.go
+++ src/cli/dialog/components/core.go
@@ -3,6 +3,7 @@ package components
 
 import tea "github.com/charmbracelet/bubbletea"
 
+// SendInputs sends the given keystrokes to the given bubbletea program.
 func SendInputs(inputs TestInput, program *tea.Program) {
 	if len(inputs) > 0 {
 		go func() {
