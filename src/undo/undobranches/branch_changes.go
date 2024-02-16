b6490b9dcb9fbec1d3f6d57fc47f7346fa14e663
Kevin Goslar <kevin.goslar@gmail.com>
2024-02-16 15:40:47 -0600

    additional_commits_before_undo.feature branch_changes.go

--- src/undo/undobranches/branch_changes.go
+++ src/undo/undobranches/branch_changes.go
@@ -189,6 +189,7 @@ func (self BranchChanges) UndoProgram(args BranchChangesUndoProgramArgs) program
 
 	// This must be a CheckoutIfExists opcode because this branch might not exist
 	// when a Git Town command fails, stores this undo opcode, then gets continued and deletes this branch.
+	// TODO: remove this?
 	result.Add(&opcodes.CheckoutIfExists{Branch: args.InitialBranch})
 	return result
 }
