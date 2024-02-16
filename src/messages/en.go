afffee5e8b5fe54c4672693fdfeed1f559446c61
Kevin Goslar <kevin.goslar@gmail.com>
2024-02-16 16:24:09 -0600

    colors.go en.go

--- src/messages/en.go
+++ src/messages/en.go
@@ -14,7 +14,7 @@ const (
 	BranchDiffProblem                  = "cannot determine if branch %q has unmerged commits: %w"
 	BranchDoesntContainCommit          = "branch %q does not contain commit %q. Found commits %s"
 	BranchDoesntExist                  = "there is no branch %q"
-	BranchHasWrongSHA                  = "cannot reset branch %q to %q because it received additional commits in the meantime. It should have SHA %q but has %q."
+	BranchHasWrongSHA                  = "cannot reset branch %q to %q because it received additional commits in the meantime: it should have SHA %q but has %q"
 	BranchLocalSHAProblem              = "cannot determine SHA of local branch %q: %w"
 	BranchLocalProblem                 = "cannot determine whether the local branch %q exists: %w"
 	BranchParentChanged                = "branch %q is now a child of %q"
