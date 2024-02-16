fb2bbad43f50e564b7f332a4528996e64f46a859
Kevin Goslar <kevin.goslar@gmail.com>
2024-02-15 19:53:16 -0600

    execute.go

---
+++ src/undo/execute.go
@@ -0,0 +1,81 @@
+package undo
+
+import (
+	"fmt"
+
+	"github.com/git-town/git-town/v12/src/cmd/cmdhelpers"
+	"github.com/git-town/git-town/v12/src/config/configdomain"
+	"github.com/git-town/git-town/v12/src/git"
+	"github.com/git-town/git-town/v12/src/git/gitdomain"
+	"github.com/git-town/git-town/v12/src/undo/undobranches"
+	"github.com/git-town/git-town/v12/src/undo/undoconfig"
+	"github.com/git-town/git-town/v12/src/undo/undostash"
+	"github.com/git-town/git-town/v12/src/vm/opcodes"
+	"github.com/git-town/git-town/v12/src/vm/program"
+	"github.com/git-town/git-town/v12/src/vm/runstate"
+	"github.com/git-town/git-town/v12/src/vm/shared"
+)
+
+// undoes the persisted runstate
+func Execute(args ExecuteArgs) {
+	undoProgram := program.Program{}
+	undoProgram.AddProgram(args.RunState.AbortProgram)
+
+	// undo branch changes
+	branchSpans := undobranches.NewBranchSpans(args.RunState.BeforeBranchesSnapshot, args.RunState.AfterBranchesSnapshot)
+	branchChanges := branchSpans.Changes()
+	undoBranchesProgram := branchChanges.UndoProgram(undobranches.BranchChangesUndoProgramArgs{
+		Config:                   args.FullConfig,
+		FinalBranch:              args.RunState.AfterBranchesSnapshot.Active,
+		InitialBranch:            args.RunState.BeforeBranchesSnapshot.Active,
+		UndoablePerennialCommits: []gitdomain.SHA{},
+	})
+	undoProgram.AddProgram(undoBranchesProgram)
+
+	// undo config changes
+	configSpans := undoconfig.NewConfigDiffs(args.RunState.BeforeConfigSnapshot, args.RunState.AfterConfigSnapshot)
+	configUndoProgram := configSpans.UndoProgram()
+	undoProgram.AddProgram(configUndoProgram)
+
+	// undo stash changes
+	stashDiff := undostash.NewStashDiff(args.RunState.BeforeStashSize, args.InitialStashSize)
+	undoStashProgram := stashDiff.Program()
+	undoProgram.AddProgram(undoStashProgram)
+
+	undoProgram.AddProgram(args.RunState.FinalUndoProgram)
+	undoProgram.Add(&opcodes.Checkout{Branch: args.RunState.BeforeBranchesSnapshot.Active})
+	undoProgram.RemoveDuplicateCheckout()
+
+	cmdhelpers.Wrap(&undoProgram, cmdhelpers.WrapOptions{
+		DryRun:                   args.RunState.DryRun,
+		RunInGitRoot:             true,
+		StashOpenChanges:         args.HasOpenChanges,
+		PreviousBranchCandidates: gitdomain.LocalBranchNames{args.RunState.BeforeBranchesSnapshot.Active},
+	})
+
+	// execute the undo program
+	for _, opcode := range undoBranchesProgram {
+		err := opcode.Run(shared.RunArgs{
+			Connector:                       nil,
+			DialogTestInputs:                nil,
+			Lineage:                         args.Lineage,
+			PrependOpcodes:                  nil,
+			RegisterUndoablePerennialCommit: nil,
+			Runner:                          args.Runner,
+			UpdateInitialBranchLocalSHA:     nil,
+		})
+		if err != nil {
+			fmt.Println("ERROR: " + err.Error())
+		}
+	}
+}
+
+type ExecuteArgs struct {
+	FullConfig       *configdomain.FullConfig
+	HasOpenChanges   bool
+	InitialStashSize gitdomain.StashSize
+	Lineage          configdomain.Lineage
+	RootDir          gitdomain.RepoRootDir
+	RunState         runstate.RunState
+	Runner           *git.ProdRunner
+}
