9b3aba05f3e7f7ecda06fe768dd11dd0cf4b68e4
Kevin Goslar <kevin.goslar@gmail.com>
2024-02-17 14:55:25 -0600

    status_reset.go

--- src/cmd/status_reset.go
+++ src/cmd/status_reset.go
@@ -30,12 +30,12 @@ func resetRunstateCommand() *cobra.Command {
 
 func executeStatusReset(verbose bool) error {
 	repo, err := execute.OpenRepo(execute.OpenRepoArgs{
-		Verbose:          verbose,
 		DryRun:           false,
 		OmitBranchNames:  false,
 		PrintCommands:    true,
-		ValidateIsOnline: false,
 		ValidateGitRepo:  true,
+		ValidateIsOnline: false,
+		Verbose:          verbose,
 	})
 	if err != nil {
 		return err
