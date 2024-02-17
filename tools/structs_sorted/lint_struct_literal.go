68a5d6c95c52c89d8d6db5e28dd65d26bd41013e
Kevin Goslar <kevin.goslar@gmail.com>
2024-02-17 12:01:47 -0600

    lint_struct_definition.go lint_struct_literal.go structs_sorted.go

---
+++ tools/structs_sorted/lint_struct_literal.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"fmt"
+	"go/ast"
+	"go/token"
+	"reflect"
+	"slices"
+)
+
+func lintStructLiteral(compositeLit *ast.CompositeLit, fileSet *token.FileSet) Issues {
+	structType, ok := compositeLit.Type.(*ast.Ident)
+	if !ok {
+		return Issues{}
+	}
+	pos := fileSet.Position(compositeLit.Pos())
+	fmt.Printf("%s:%d  %s\n", pos.Filename, pos.Line, structType.Name)
+	structName := structType.Name
+	if slices.Contains(ignoreTypes, structName) {
+		return Issues{}
+	}
+	fieldNames := structInstantiationFieldNames(compositeLit)
+	if len(fieldNames) == 0 {
+		return Issues{}
+	}
+	sortedFields := make([]string, len(fieldNames))
+	copy(sortedFields, fieldNames)
+	slices.Sort(sortedFields)
+	if reflect.DeepEqual(fieldNames, sortedFields) {
+		return Issues{}
+	}
+	return Issues{
+		issue{
+			expected:   sortedFields,
+			position:   fileSet.Position(compositeLit.Pos()),
+			structName: structName,
+		},
+	}
+}
+
+func structInstantiationFieldNames(compositeLit *ast.CompositeLit) []string {
+	result := make([]string, 0, len(compositeLit.Elts))
+	for _, expr := range compositeLit.Elts {
+		kvExpr, ok := expr.(*ast.KeyValueExpr)
+		if !ok {
+			continue
+		}
+		ident, ok := kvExpr.Key.(*ast.Ident)
+		if !ok {
+			continue
+		}
+		result = append(result, ident.Name)
+	}
+	return result
+}
--- tools/structs_sorted/structs_sorted.go
+++ tools/structs_sorted/structs_sorted.go
@@ -7,8 +7,6 @@ import (
 	"go/token"
 	"os"
 	"path/filepath"
-	"reflect"
-	"slices"
 	"strings"
 )
 
@@ -56,73 +54,15 @@ func LintFile(path string) Issues {
 		return result
 	}
 	ast.Inspect(file, func(node ast.Node) bool {
-		switch n := node.(type) {
+		switch typedNode := node.(type) {
 		case *ast.TypeSpec:
-			result = append(result, lintStructDefinition(n, fileSet)...)
+			result = append(result, lintStructDefinition(typedNode, fileSet)...)
 		case *ast.CompositeLit:
+			result = append(result, lintStructLiteral(typedNode, fileSet)...)
 		}
-		result = append(result, lintStructLiteral(node, fileSet)...)
 		return true
 	})
 	return result
-}
-
-func lintStructDefinition(typeSpec *ast.TypeSpec, fileSet *token.FileSet) Issues {
-	structName := typeSpec.Name.Name
-	if slices.Contains(ignoreTypes, structName) {
-		return Issues{}
-	}
-	structType, ok := typeSpec.Type.(*ast.StructType)
-	if !ok {
-		return Issues{}
-	}
-	fields := structDefFieldNames(structType)
-	if len(fields) == 0 {
-		return Issues{}
-	}
-	sortedFields := make([]string, len(fields))
-	copy(sortedFields, fields)
-	slices.Sort(sortedFields)
-	if reflect.DeepEqual(fields, sortedFields) {
-		return Issues{}
-	}
-	return Issues{
-		issue{
-			expected:   sortedFields,
-			position:   fileSet.Position(typeSpec.Pos()),
-			structName: structName,
-		},
-	}
-}
-
-func lintStructLiteral(compositeLit *ast.CompositeLit, fileSet *token.FileSet) Issues {
-	structType, ok := compositeLit.Type.(*ast.Ident)
-	if !ok {
-		return Issues{}
-	}
-	pos := fileSet.Position(compositeLit.Pos())
-	fmt.Printf("%s:%d  %s\n", pos.Filename, pos.Line, structType.Name)
-	structName := structType.Name
-	if slices.Contains(ignoreTypes, structName) {
-		return Issues{}
-	}
-	fieldNames := structInstantiationFieldNames(compositeLit)
-	if len(fieldNames) == 0 {
-		return Issues{}
-	}
-	sortedFields := make([]string, len(fieldNames))
-	copy(sortedFields, fieldNames)
-	slices.Sort(sortedFields)
-	if reflect.DeepEqual(fieldNames, sortedFields) {
-		return Issues{}
-	}
-	return Issues{
-		issue{
-			expected:   sortedFields,
-			position:   fileSet.Position(compositeLit.Pos()),
-			structName: structName,
-		},
-	}
 }
 
 func isIgnoredPath(path string) bool {
@@ -132,30 +72,4 @@ func isIgnoredPath(path string) bool {
 		}
 	}
 	return false
-}
-
-func structDefFieldNames(structType *ast.StructType) []string {
-	var result []string
-	for _, field := range structType.Fields.List {
-		if field.Names != nil {
-			result = append(result, field.Names[0].Name)
-		}
-	}
-	return result
-}
-
-func structInstantiationFieldNames(compositeLit *ast.CompositeLit) []string {
-	result := make([]string, 0, len(compositeLit.Elts))
-	for _, expr := range compositeLit.Elts {
-		kvExpr, ok := expr.(*ast.KeyValueExpr)
-		if !ok {
-			continue
-		}
-		ident, ok := kvExpr.Key.(*ast.Ident)
-		if !ok {
-			continue
-		}
-		result = append(result, ident.Name)
-	}
-	return result
 }
