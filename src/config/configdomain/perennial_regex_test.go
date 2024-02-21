878d3af24ef089722f70a71b95fe6adbd5ac3055
Kevin Goslar <kevin.goslar@gmail.com>
2024-02-21 16:16:49 -0600

    perennial_regex_test.go

--- src/config/configdomain/perennial_regex_test.go
+++ src/config/configdomain/perennial_regex_test.go
@@ -4,6 +4,7 @@ import (
 	"testing"
 
 	"github.com/git-town/git-town/v12/src/config/configdomain"
+	"github.com/git-town/git-town/v12/src/git/gitdomain"
 	"github.com/shoenig/test/must"
 )
 
@@ -15,10 +16,36 @@ func TestPerennialRegex(t *testing.T) {
 		must.False(t, perennialRegex.MatchBranch(""))
 		must.False(t, perennialRegex.MatchBranch("foo"))
 	})
-	t.Run("only characters", func(t *testing.T) {
+	t.Run("only characters, no wildcards matches all branch names that contain that phrase", func(t *testing.T) {
 		t.Parallel()
+		perennialRegex := configdomain.PerennialRegex("release")
+		tests := map[string]bool{
+			"":                false,
+			"release":         true,
+			"release-1":       true,
+			"another-release": true,
+			"main":            false,
+		}
+		for give, want := range tests {
+			have := perennialRegex.MatchBranch(gitdomain.LocalBranchName(give))
+			must.Eq(t, want, have)
+		}
 	})
 	t.Run("with wildcards", func(t *testing.T) {
 		t.Parallel()
+		perennialRegex := configdomain.PerennialRegex("release-.*")
+		tests := map[string]bool{
+			"":                false,
+			"release":         false,
+			"release-1":       true,
+			"release-2":       true,
+			"release-30":      true,
+			"another-release": false,
+			"main":            false,
+		}
+		for give, want := range tests {
+			have := perennialRegex.MatchBranch(gitdomain.LocalBranchName(give))
+			must.Eq(t, want, have)
+		}
 	})
 }
