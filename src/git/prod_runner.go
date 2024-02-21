3cc15892251ad79042998a45f6a1c632c259776a
Kevin Goslar <kevin.goslar@gmail.com>
2024-02-21 10:10:38 -0600

    Sort alphabetically

--- src/git/prod_runner.go
+++ src/git/prod_runner.go
@@ -9,8 +9,8 @@ import (
 // ProdRunner provides Git functionality for production code.
 type ProdRunner struct {
 	Backend         BackendCommands
-	Config          *config.Config
-	Frontend        FrontendCommands
 	CommandsCounter *gohacks.Counter
+	Config          *config.Config
 	FinalMessages   *stringslice.Collector
+	Frontend        FrontendCommands
 }
