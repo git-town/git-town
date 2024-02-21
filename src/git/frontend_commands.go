a06566a568b34bf179a4ae79aa46beda1d78b46b
Kevin Goslar <kevin.goslar@gmail.com>
2024-02-21 10:53:09 -0600

    Make composition explicit

--- src/git/frontend_commands.go
+++ src/git/frontend_commands.go
@@ -19,7 +19,7 @@ type FrontendRunner interface {
 // They can take a while to execute (fetch, push) and stream their output to the user.
 // Git Town only needs to know the exit code of frontend commands.
 type FrontendCommands struct {
-	FrontendRunner
+	Runner                 FrontendRunner
 	SetCachedCurrentBranch SetCachedCurrentBranchFunc
 }
 
@@ -27,17 +27,17 @@ type SetCachedCurrentBranchFunc func(gitdomain.LocalBranchName)
 
 // AbortMerge cancels a currently ongoing Git merge operation.
 func (self *FrontendCommands) AbortMerge() error {
-	return self.Run("git", "merge", "--abort")
+	return self.Runner.Run("git", "merge", "--abort")
 }
 
 // AbortRebase cancels a currently ongoing Git rebase operation.
 func (self *FrontendCommands) AbortRebase() error {
-	return self.Run("git", "rebase", "--abort")
+	return self.Runner.Run("git", "rebase", "--abort")
 }
 
 // CheckoutBranch checks out the Git branch with the given name in this repo.
 func (self *FrontendCommands) CheckoutBranch(name gitdomain.LocalBranchName) error {
-	err := self.Run("git", "checkout", name.String())
+	err := self.Runner.Run("git", "checkout", name.String())
 	self.SetCachedCurrentBranch(name)
 	if err != nil {
 		return fmt.Errorf(messages.BranchCheckoutProblem, name, err)
@@ -54,32 +54,32 @@ func (self *FrontendCommands) Commit(message, author string) error {
 	if author != "" {
 		gitArgs = append(gitArgs, "--author", author)
 	}
-	return self.Run("git", gitArgs...)
+	return self.Runner.Run("git", gitArgs...)
 }
 
 // CommitNoEdit commits all staged files with the default commit message.
 func (self *FrontendCommands) CommitNoEdit() error {
-	return self.Run("git", "commit", "--no-edit")
+	return self.Runner.Run("git", "commit", "--no-edit")
 }
 
 // CommitStagedChanges commits the currently staged changes.
 func (self *FrontendCommands) CommitStagedChanges(message string) error {
 	if message != "" {
-		return self.Run("git", "commit", "-m", message)
+		return self.Runner.Run("git", "commit", "-m", message)
 	}
-	return self.Run("git", "commit", "--no-edit")
+	return self.Runner.Run("git", "commit", "--no-edit")
 }
 
 // ContinueRebase continues the currently ongoing rebase.
 func (self *FrontendCommands) ContinueRebase() error {
-	return self.Run("git", "rebase", "--continue")
+	return self.Runner.Run("git", "rebase", "--continue")
 }
 
 // CreateBranch creates a new branch with the given name.
 // The created branch is a normal branch.
 // To create feature branches, use CreateFeatureBranch.
 func (self *FrontendCommands) CreateBranch(name gitdomain.LocalBranchName, parent gitdomain.Location) error {
-	return self.Run("git", "branch", name.String(), parent.String())
+	return self.Runner.Run("git", "branch", name.String(), parent.String())
 }
 
 // CreateRemoteBranch creates a remote branch from the given local SHA.
@@ -89,7 +89,7 @@ func (self *FrontendCommands) CreateRemoteBranch(localSHA gitdomain.SHA, branch 
 		args = append(args, "--no-verify")
 	}
 	args = append(args, gitdomain.OriginRemote.String(), localSHA.String()+":refs/heads/"+branch.String())
-	return self.Run("git", args...)
+	return self.Runner.Run("git", args...)
 }
 
 // PushBranch pushes the branch with the given name to origin.
@@ -100,17 +100,17 @@ func (self *FrontendCommands) CreateTrackingBranch(branch gitdomain.LocalBranchN
 	}
 	args = append(args, "-u", remote.String())
 	args = append(args, branch.String())
-	return self.Run("git", args...)
+	return self.Runner.Run("git", args...)
 }
 
 // DeleteHostingPlatform removes the hosting platform config entry.
 func (self *FrontendCommands) DeleteHostingPlatform() error {
-	return self.Run("git", "config", "--unset", gitconfig.KeyHostingPlatform.String())
+	return self.Runner.Run("git", "config", "--unset", gitconfig.KeyHostingPlatform.String())
 }
 
 // DeleteLastCommit resets HEAD to the previous commit.
 func (self *FrontendCommands) DeleteLastCommit() error {
-	return self.Run("git", "reset", "--hard", "HEAD~1")
+	return self.Runner.Run("git", "reset", "--hard", "HEAD~1")
 }
 
 // DeleteLocalBranch removes the local branch with the given name.
@@ -119,38 +119,38 @@ func (self *FrontendCommands) DeleteLocalBranch(name gitdomain.LocalBranchName, 
 	if force {
 		args[1] = "-D"
 	}
-	return self.Run("git", args...)
+	return self.Runner.Run("git", args...)
 }
 
 // DeleteOriginHostname removes the origin hostname override
 func (self *FrontendCommands) DeleteOriginHostname() error {
-	return self.Run("git", "config", "--unset", gitconfig.KeyHostingOriginHostname.String())
+	return self.Runner.Run("git", "config", "--unset", gitconfig.KeyHostingOriginHostname.String())
 }
 
 // DeleteTrackingBranch removes the tracking branch of the given local branch.
 func (self *FrontendCommands) DeleteTrackingBranch(name gitdomain.RemoteBranchName) error {
 	remote, localBranchName := name.Parts()
-	return self.Run("git", "push", remote.String(), ":"+localBranchName.String())
+	return self.Runner.Run("git", "push", remote.String(), ":"+localBranchName.String())
 }
 
 // DiffParent displays the diff between the given branch and its given parent branch.
 func (self *FrontendCommands) DiffParent(branch, parentBranch gitdomain.LocalBranchName) error {
-	return self.Run("git", "diff", parentBranch.String()+".."+branch.String())
+	return self.Runner.Run("git", "diff", parentBranch.String()+".."+branch.String())
 }
 
 // DiscardOpenChanges deletes all uncommitted changes.
 func (self *FrontendCommands) DiscardOpenChanges() error {
-	return self.Run("git", "reset", "--hard")
+	return self.Runner.Run("git", "reset", "--hard")
 }
 
 // Fetch retrieves the updates from the origin repo.
 func (self *FrontendCommands) Fetch() error {
-	return self.Run("git", "fetch", "--prune", "--tags")
+	return self.Runner.Run("git", "fetch", "--prune", "--tags")
 }
 
 // FetchUpstream fetches updates from the upstream remote.
 func (self *FrontendCommands) FetchUpstream(branch gitdomain.LocalBranchName) error {
-	return self.Run("git", "fetch", gitdomain.UpstreamRemote.String(), branch.String())
+	return self.Runner.Run("git", "fetch", gitdomain.UpstreamRemote.String(), branch.String())
 }
 
 // PushBranch pushes the branch with the given name to origin.
@@ -159,13 +159,13 @@ func (self *FrontendCommands) ForcePushBranch(noPushHook configdomain.NoPushHook
 	if noPushHook {
 		args = append(args, "--no-verify")
 	}
-	return self.Run("git", args...)
+	return self.Runner.Run("git", args...)
 }
 
 // MergeBranchNoEdit merges the given branch into the current branch,
 // using the default commit message.
 func (self *FrontendCommands) MergeBranchNoEdit(branch gitdomain.BranchName) error {
-	return self.Run("git", "merge", "--no-edit", branch.String())
+	return self.Runner.Run("git", "merge", "--no-edit", branch.String())
 }
 
 // NavigateToDir changes into the root directory of the current repository.
@@ -175,12 +175,12 @@ func (self *FrontendCommands) NavigateToDir(dir gitdomain.RepoRootDir) error {
 
 // PopStash restores stashed-away changes into the workspace.
 func (self *FrontendCommands) PopStash() error {
-	return self.Run("git", "stash", "pop")
+	return self.Runner.Run("git", "stash", "pop")
 }
 
 // Pull fetches updates from origin and updates the currently checked out branch.
 func (self *FrontendCommands) Pull() error {
-	return self.Run("git", "pull")
+	return self.Runner.Run("git", "pull")
 }
 
 // PushCurrentBranch pushes the current branch to its tracking branch.
@@ -189,23 +189,23 @@ func (self *FrontendCommands) PushCurrentBranch(noPushHook configdomain.NoPushHo
 	if noPushHook {
 		args = append(args, "--no-verify")
 	}
-	return self.Run("git", args...)
+	return self.Runner.Run("git", args...)
 }
 
 // PushTags pushes new the Git tags to origin.
 func (self *FrontendCommands) PushTags() error {
-	return self.Run("git", "push", "--tags")
+	return self.Runner.Run("git", "push", "--tags")
 }
 
 // Rebase initiates a Git rebase of the current branch against the given branch.
 func (self *FrontendCommands) Rebase(target gitdomain.BranchName) error {
-	return self.Run("git", "rebase", target.String())
+	return self.Runner.Run("git", "rebase", target.String())
 }
 
 // RemoveGitAlias removes the given Git alias.
 func (self *FrontendCommands) RemoveGitAlias(aliasableCommand configdomain.AliasableCommand) error {
 	aliasKey := gitconfig.KeyForAliasableCommand(aliasableCommand)
-	return self.Run("git", "config", "--global", "--unset", aliasKey.String())
+	return self.Runner.Run("git", "config", "--global", "--unset", aliasKey.String())
 }
 
 // ResetCurrentBranchToSHA undoes all commits on the current branch all the way until the given SHA.
@@ -215,73 +215,73 @@ func (self *FrontendCommands) ResetCurrentBranchToSHA(sha gitdomain.SHA, hard bo
 		args = append(args, "--hard")
 	}
 	args = append(args, sha.String())
-	return self.Run("git", args...)
+	return self.Runner.Run("git", args...)
 }
 
 // ResetRemoteBranchToSHA sets the given remote branch to the given SHA.
 func (self *FrontendCommands) ResetRemoteBranchToSHA(branch gitdomain.RemoteBranchName, sha gitdomain.SHA) error {
-	return self.Run("git", "push", "--force-with-lease", gitdomain.OriginRemote.String(), sha.String()+":"+branch.LocalBranchName().String())
+	return self.Runner.Run("git", "push", "--force-with-lease", gitdomain.OriginRemote.String(), sha.String()+":"+branch.LocalBranchName().String())
 }
 
 // RevertCommit reverts the commit with the given SHA.
 func (self *FrontendCommands) RevertCommit(sha gitdomain.SHA) error {
-	return self.Run("git", "revert", sha.String())
+	return self.Runner.Run("git", "revert", sha.String())
 }
 
 // SetGitAlias sets the given Git alias.
 func (self *FrontendCommands) SetGitAlias(aliasableCommand configdomain.AliasableCommand) error {
-	return self.Run("git", "config", "--global", gitconfig.KeyForAliasableCommand(aliasableCommand).String(), "town "+aliasableCommand.String())
+	return self.Runner.Run("git", "config", "--global", gitconfig.KeyForAliasableCommand(aliasableCommand).String(), "town "+aliasableCommand.String())
 }
 
 // SetGitHubToken sets the given API token for the GitHub API.
 func (self *FrontendCommands) SetGitHubToken(value configdomain.GitHubToken) error {
-	return self.Run("git", "config", "git-town.github-token", value.String())
+	return self.Runner.Run("git", "config", "git-town.github-token", value.String())
 }
 
 // SetGitLabToken sets the given API token for the GitHub API.
 func (self *FrontendCommands) SetGitLabToken(value configdomain.GitLabToken) error {
-	return self.Run("git", "config", "git-town.gitlab-token", value.String())
+	return self.Runner.Run("git", "config", "git-town.gitlab-token", value.String())
 }
 
 // SetGiteaToken sets the given API token for the Gitea API.
 func (self *FrontendCommands) SetGiteaToken(value configdomain.GiteaToken) error {
-	return self.Run("git", "config", "git-town.gitea-token", value.String())
+	return self.Runner.Run("git", "config", "git-town.gitea-token", value.String())
 }
 
 // SetHostingPlatform sets the given code hosting platform.
 func (self *FrontendCommands) SetHostingPlatform(platform configdomain.HostingPlatform) error {
-	return self.Run("git", "config", gitconfig.KeyHostingPlatform.String(), platform.String())
+	return self.Runner.Run("git", "config", gitconfig.KeyHostingPlatform.String(), platform.String())
 }
 
 // SetHostingPlatform sets the given code hosting platform.
 func (self *FrontendCommands) SetOriginHostname(hostname configdomain.HostingOriginHostname) error {
-	return self.Run("git", "config", gitconfig.KeyHostingOriginHostname.String(), hostname.String())
+	return self.Runner.Run("git", "config", gitconfig.KeyHostingOriginHostname.String(), hostname.String())
 }
 
 // SquashMerge squash-merges the given branch into the current branch.
 func (self *FrontendCommands) SquashMerge(branch gitdomain.LocalBranchName) error {
-	return self.Run("git", "merge", "--squash", branch.String())
+	return self.Runner.Run("git", "merge", "--squash", branch.String())
 }
 
 // StageFiles adds the file with the given name to the Git index.
 func (self *FrontendCommands) StageFiles(names ...string) error {
 	args := append([]string{"add"}, names...)
-	return self.Run("git", args...)
+	return self.Runner.Run("git", args...)
 }
 
 // StartCommit starts a commit and stops at asking the user for the commit message.
 func (self *FrontendCommands) StartCommit() error {
-	return self.Run("git", "commit")
+	return self.Runner.Run("git", "commit")
 }
 
 // Stash adds the current files to the Git stash.
 func (self *FrontendCommands) Stash() error {
-	return self.RunMany([][]string{
+	return self.Runner.RunMany([][]string{
 		{"git", "add", "-A"},
 		{"git", "stash"},
 	})
 }
 
 func (self *FrontendCommands) UndoLastCommit() error {
-	return self.Run("git", "reset", "--soft", "HEAD~1")
+	return self.Runner.Run("git", "reset", "--soft", "HEAD~1")
 }
