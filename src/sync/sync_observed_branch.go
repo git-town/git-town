2985842325c0f93a9a6a72b85417acec5ad27d5f
Kevin Goslar <kevin.goslar@gmail.com>
2024-02-22 10:11:02 -0600

    sync_observed_branch.go

--- src/sync/sync_observed_branch.go
+++ src/sync/sync_observed_branch.go
@@ -5,7 +5,7 @@ import (
 	"github.com/git-town/git-town/v12/src/vm/opcodes"
 )
 
-// PerennialBranchProgram adds the opcodes to sync the perennial branch with the given name.
+// PerennialBranchProgram adds the opcodes to sync the observed branch with the given name.
 func ObservedBranchProgram(branch gitdomain.BranchInfo, args BranchProgramArgs) {
 	if branch.HasTrackingBranch() {
 		args.Program.Add(&opcodes.RebaseBranch{Branch: branch.RemoteName.BranchName()})
