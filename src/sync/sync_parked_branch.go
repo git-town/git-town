23cbf0b73a1563af9f98cde122256c661fe571cb
Kevin Goslar <kevin.goslar@gmail.com>
2024-02-22 19:28:35 -0600

    sync_parked_branch.go

--- src/sync/sync_parked_branch.go
+++ src/sync/sync_parked_branch.go
@@ -4,7 +4,7 @@ import (
 	"github.com/git-town/git-town/v12/src/git/gitdomain"
 )
 
-// PerennialBranchProgram adds the opcodes to sync the observed branch with the given name.
+// ParkedBranchProgram adds the opcodes to sync the observed branch with the given name.
 func ParkedBranchProgram(initialBranch gitdomain.LocalBranchName, args featureBranchArgs) {
 	if args.branch.LocalName == initialBranch {
 		FeatureBranchProgram(args)
