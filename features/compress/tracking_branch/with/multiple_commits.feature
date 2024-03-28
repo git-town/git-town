6bd52929e5b31f7f67dc18a70f84d3a31e92e7ff
Kevin Goslar <kevin.goslar@gmail.com>
2024-03-28 14:37:25 -0500

    on_perennial_branch.feature one_commit.feature multiple_commits.feature compress.go

---
+++ features/compress/tracking_branch/with/multiple_commits.feature
@@ -0,0 +1,44 @@
+Feature: compress the commits on a feature branch
+
+  Background:
+    Given the current branch is a feature branch "feature"
+    And the commits
+      | BRANCH  | LOCATION      | MESSAGE  |
+      | feature | local, origin | commit 1 |
+      |         |               | commit 2 |
+      |         |               | commit 3 |
+    When I run "git-town compress" and enter "compressed commit" for the commit message
+
+  @debug @this
+  Scenario: result
+    Then it runs the commands
+      | BRANCH  | COMMAND                                         |
+      | feature | git fetch --prune --tags                        |
+      |         | git reset main                                  |
+      |         | git commit                                      |
+      |         | git push --force-with-lease --force-if-includes |
+    And all branches are now synchronized
+    And the current branch is still "feature"
+    And these commits exist now
+      | BRANCH  | LOCATION      | MESSAGE                                                    |
+      | feature | local, origin | local feature commit                                       |
+      |         |               | origin feature commit                                      |
+      |         |               | Merge remote-tracking branch 'origin/feature' into feature |
+      |         |               | origin main commit                                         |
+      |         |               | local main commit                                          |
+      |         |               | Merge branch 'main' into feature                           |
+
+  Scenario: undo
+    When I run "git-town undo"
+    Then it runs the commands
+      | BRANCH  | COMMAND                                                                      |
+      | feature | git reset --hard {{ sha 'local feature commit' }}                            |
+      |         | git push --force-with-lease origin {{ sha 'origin feature commit' }}:feature |
+    And the current branch is still "feature"
+    And these commits exist now
+      | BRANCH  | LOCATION      | MESSAGE               |
+      | main    | local, origin | origin main commit    |
+      |         |               | local main commit     |
+      | feature | local         | local feature commit  |
+      |         | origin        | origin feature commit |
+    And the initial branches and lineage exist
---
+++ src/cmd/compress.go
@@ -0,0 +1,128 @@
+package cmd
+
+import (
+	"os"
+
+	"github.com/git-town/git-town/v13/src/cli/dialog/components"
+	"github.com/git-town/git-town/v13/src/cli/flags"
+	"github.com/git-town/git-town/v13/src/cmd/cmdhelpers"
+	"github.com/git-town/git-town/v13/src/config/configdomain"
+	"github.com/git-town/git-town/v13/src/execute"
+	"github.com/git-town/git-town/v13/src/git/gitdomain"
+	"github.com/git-town/git-town/v13/src/undo/undoconfig"
+	fullInterpreter "github.com/git-town/git-town/v13/src/vm/interpreter/full"
+	"github.com/git-town/git-town/v13/src/vm/opcodes"
+	"github.com/git-town/git-town/v13/src/vm/program"
+	"github.com/git-town/git-town/v13/src/vm/runstate"
+	"github.com/spf13/cobra"
+)
+
+const compressDesc = "Squashes all commits on a feature branch down to a single commit"
+
+const compressHelp = `
+Compress is a more convenient way of running "git rebase -i" and squashing all commits down to one.
+The branches must be synced before you compress them.
+`
+
+func compressCmd() *cobra.Command {
+	addVerboseFlag, readVerboseFlag := flags.Verbose()
+	addDryRunFlag, readDryRunFlag := flags.DryRun()
+	cmd := cobra.Command{
+		Use:   "compress",
+		Args:  cobra.NoArgs,
+		Short: compressDesc,
+		Long:  cmdhelpers.Long(compressDesc, compressHelp),
+		RunE: func(cmd *cobra.Command, args []string) error {
+			return executeCompress(args, readDryRunFlag(cmd), readVerboseFlag(cmd))
+		},
+	}
+	addDryRunFlag(&cmd)
+	addVerboseFlag(&cmd)
+	return &cmd
+}
+
+func executeCompress(_ []string, dryRun, verbose bool) error {
+	repo, err := execute.OpenRepo(execute.OpenRepoArgs{
+		DryRun:           dryRun,
+		OmitBranchNames:  false,
+		PrintCommands:    true,
+		ValidateGitRepo:  true,
+		ValidateIsOnline: false,
+		Verbose:          verbose,
+	})
+	if err != nil {
+		return err
+	}
+	config, initialBranchesSnapshot, initialStashSize, exit, err := determineCompressConfig(repo, dryRun, verbose)
+	if err != nil || exit {
+		return err
+	}
+	steps := compressProgram(config)
+	runState := runstate.RunState{
+		BeginBranchesSnapshot: initialBranchesSnapshot,
+		BeginConfigSnapshot:   repo.ConfigSnapshot,
+		BeginStashSize:        initialStashSize,
+		Command:               "compress",
+		DryRun:                dryRun,
+		EndBranchesSnapshot:   gitdomain.EmptyBranchesSnapshot(),
+		EndConfigSnapshot:     undoconfig.EmptyConfigSnapshot(),
+		EndStashSize:          0,
+		RunProgram:            steps,
+	}
+	return fullInterpreter.Execute(fullInterpreter.ExecuteArgs{
+		Connector:               nil,
+		DialogTestInputs:        &config.dialogTestInputs,
+		FullConfig:              config.FullConfig,
+		HasOpenChanges:          config.hasOpenChanges,
+		InitialBranchesSnapshot: initialBranchesSnapshot,
+		InitialConfigSnapshot:   repo.ConfigSnapshot,
+		InitialStashSize:        initialStashSize,
+		RootDir:                 repo.RootDir,
+		Run:                     repo.Runner,
+		RunState:                &runState,
+		Verbose:                 verbose,
+	})
+}
+
+type compressConfig struct {
+	*configdomain.FullConfig
+	dialogTestInputs components.TestInputs
+	dryRun           bool
+	hasOpenChanges   bool
+	previousBranch   gitdomain.LocalBranchName
+}
+
+func determineCompressConfig(repo *execute.OpenRepoResult, dryRun, verbose bool) (*compressConfig, gitdomain.BranchesSnapshot, gitdomain.StashSize, bool, error) {
+	dialogTestInputs := components.LoadTestInputs(os.Environ())
+	branchesSnapshot, stashSize, repoStatus, exit, err := execute.LoadRepoSnapshot(execute.LoadRepoSnapshotArgs{
+		DialogTestInputs:      dialogTestInputs,
+		Fetch:                 true,
+		FullConfig:            &repo.Runner.Config.FullConfig,
+		HandleUnfinishedState: true,
+		Repo:                  repo,
+		ValidateIsConfigured:  true,
+		ValidateNoOpenChanges: false,
+		Verbose:               verbose,
+	})
+	if err != nil || exit {
+		return nil, branchesSnapshot, stashSize, exit, err
+	}
+	return &compressConfig{
+		dialogTestInputs: dialogTestInputs,
+		dryRun:           dryRun,
+		FullConfig:       &repo.Runner.Config.FullConfig,
+		hasOpenChanges:   repoStatus.OpenChanges,
+	}, branchesSnapshot, stashSize, false, nil
+}
+
+func compressProgram(config *compressConfig) program.Program {
+	prog := program.Program{}
+	cmdhelpers.Wrap(&prog, cmdhelpers.WrapOptions{
+		DryRun:                   config.dryRun,
+		RunInGitRoot:             true,
+		StashOpenChanges:         config.hasOpenChanges,
+		PreviousBranchCandidates: gitdomain.LocalBranchNames{config.previousBranch},
+	})
+	prog.Add(&opcodes.CreateProposal{Branch: config.previousBranch})
+	return prog
+}
