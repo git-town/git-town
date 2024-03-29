c4b12fa5d2275ee4984a1f71cd8af05c69bc951e
Kevin Goslar <kevin.goslar@gmail.com>
2024-03-29 10:00:17 -0500

    test_commands.go

--- test/commands/test_commands.go
+++ test/commands/test_commands.go
@@ -399,8 +399,7 @@ func (self *TestCommands) SHAsForCommit(name string) gitdomain.SHAs {
 	if output == "" {
 		panic(fmt.Sprintf("cannot find the SHA of commit %q", name))
 	}
-	parts := strings.Split(output, "\n")
-	return gitdomain.NewSHAs(parts...)
+	return gitdomain.NewSHAs(strings.Split(output, "\n")...)
 }
 
 // SetColorUI configures whether Git output contains color codes.
