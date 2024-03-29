935efcae680f3a0f67f3240de83e2a58c38ce40c
Kevin Goslar <kevin.goslar@gmail.com>
2024-03-29 07:28:16 -0500

    commit_message_test.go

---
+++ src/cli/flags/commit_message_test.go
@@ -0,0 +1,33 @@
+package flags_test
+
+import (
+	"testing"
+
+	"github.com/git-town/git-town/v13/src/cli/flags"
+	"github.com/shoenig/test/must"
+	"github.com/spf13/cobra"
+)
+
+func TestCommitMessage(t *testing.T) {
+	t.Parallel()
+
+	t.Run("long version", func(t *testing.T) {
+		t.Parallel()
+		cmd := cobra.Command{}
+		addFlag, readFlag := flags.CommitMessage("myflag", "m", "default", "desc")
+		addFlag(&cmd)
+		err := cmd.ParseFlags([]string{"--myflag", "my-value"})
+		must.NoError(t, err)
+		must.EqOp(t, "my-value", readFlag(&cmd))
+	})
+
+	t.Run("short version", func(t *testing.T) {
+		t.Parallel()
+		cmd := cobra.Command{}
+		addFlag, readFlag := flags.CommitMessage("myflag", "m", "default", "desc")
+		addFlag(&cmd)
+		err := cmd.ParseFlags([]string{"-m", "my-value"})
+		must.NoError(t, err)
+		must.EqOp(t, "my-value", readFlag(&cmd))
+	})
+}
