ca10204037b2e0147cc86395c0a34c3999208a81
Kevin Goslar <kevin.goslar@gmail.com>
2024-03-29 07:45:17 -0500

    steps.go

--- test/cucumber/steps.go
+++ test/cucumber/steps.go
@@ -1670,12 +1670,7 @@ func updateInitialSHAs(state *ScenarioState) {
 	if len(state.initialOriginSHAs) == 0 && state.insideGitRepo && state.fixture.OriginRepo != nil {
 		state.initialOriginSHAs = state.fixture.OriginRepo.TestCommands.CommitSHAs()
 	}
-	fmt.Println("111111111111111")
 	if len(state.initialWorktreeSHAs) == 0 && state.insideGitRepo && state.fixture.SecondWorktree != nil {
-		fmt.Println("222222222222222222")
 		state.initialWorktreeSHAs = state.fixture.SecondWorktree.TestCommands.CommitSHAs()
-		fmt.Printf("worktree: %#v\n", state.initialWorktreeSHAs)
-		fmt.Printf("dev: %#v\n", state.initialDevSHAs)
-		fmt.Printf("origin: %#v\n", state.initialOriginSHAs)
 	}
 }
