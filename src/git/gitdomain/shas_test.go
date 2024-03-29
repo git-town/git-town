ac320b234450bf77ad9c850c8c723138ecdc4172
Kevin Goslar <kevin.goslar@gmail.com>
2024-03-29 09:53:01 -0500

    shas_test.go

--- src/git/gitdomain/shas_test.go
+++ src/git/gitdomain/shas_test.go
@@ -10,6 +10,17 @@ import (
 func TestSHAs(t *testing.T) {
 	t.Parallel()
 
+	t.Run("NewSHAs", func(t *testing.T) {
+		t.Parallel()
+		have := gitdomain.NewSHAs("111111", "222222", "333333")
+		want := gitdomain.SHAs{
+			gitdomain.SHA("111111"),
+			gitdomain.SHA("222222"),
+			gitdomain.SHA("333333"),
+		}
+		must.Eq(t, want, have)
+	})
+
 	t.Run("Join", func(t *testing.T) {
 		t.Parallel()
 		t.Run("contains elements", func(t *testing.T) {
