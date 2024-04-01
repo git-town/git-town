da212786ed412e9eefc480fcd29f7ddcb89abbb4
Kevin Goslar <kevin.goslar@gmail.com>
2024-04-01 08:09:42 -0500

    longest_test.go

--- src/gohacks/stringslice/longest_test.go
+++ src/gohacks/stringslice/longest_test.go
@@ -9,22 +9,25 @@ import (
 
 func TestLongest(t *testing.T) {
 	t.Parallel()
-	t.Run("various strings", func(t *testing.T) {
-		t.Parallel()
-		give := []string{"one", "two", "three"}
-		have := stringslice.Longest(give)
-		must.Eq(t, 5, have)
-	})
-	t.Run("empty string", func(t *testing.T) {
-		t.Parallel()
-		give := []string{""}
-		have := stringslice.Longest(give)
-		must.Eq(t, 0, have)
-	})
-	t.Run("empty slice", func(t *testing.T) {
-		t.Parallel()
-		give := []string{}
-		have := stringslice.Longest(give)
-		must.Eq(t, 0, have)
-	})
+	tests := []struct {
+		give []string
+		want int
+	}{
+		{
+			give: []string{"one", "two", "three"},
+			want: 5,
+		},
+		{
+			give: []string{""},
+			want: 0,
+		},
+		{
+			give: []string{},
+			want: 0,
+		},
+	}
+	for _, test := range tests {
+		have := stringslice.Longest(test.give)
+		must.EqOp(t, test.want, have)
+	}
 }
