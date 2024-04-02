22424367e04a009d4d0c7c7b80a61d4255ca5572
Kevin Goslar <kevin.goslar@gmail.com>
2024-04-02 06:29:20 -0500

    locations_test.go

---
+++ test/git/locations_test.go
@@ -0,0 +1,23 @@
+package git_test
+
+import (
+	"testing"
+
+	testgit "github.com/git-town/git-town/v13/test/git"
+	"github.com/shoenig/test/must"
+)
+
+func TestNewLocations(t *testing.T) {
+	t.Parallel()
+	tests := map[string]testgit.Locations{
+		"local":           {testgit.LocationLocal},
+		"origin":          {testgit.LocationOrigin},
+		"upstream":        {testgit.LocationUpstream},
+		"local, origin":   {testgit.LocationLocal, testgit.LocationOrigin},
+		"local, upstream": {testgit.LocationLocal, testgit.LocationUpstream},
+	}
+	for give, want := range tests {
+		have := testgit.NewLocations(give)
+		must.Eq(t, want, have)
+	}
+}
