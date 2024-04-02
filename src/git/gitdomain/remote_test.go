86cb05f12506ea555c05451d7c18a58cee2cb4ae
Kevin Goslar <kevin.goslar@gmail.com>
2024-04-02 05:56:27 -0500

    offline.feature remote_test.go

---
+++ src/git/gitdomain/remote_test.go
@@ -0,0 +1,20 @@
+package gitdomain_test
+
+import (
+	"testing"
+
+	"github.com/git-town/git-town/v13/src/git/gitdomain"
+	"github.com/shoenig/test/must"
+)
+
+func TestNewRemote(t *testing.T) {
+	t.Parallel()
+	tests := map[string]gitdomain.Remote{
+		"origin":   gitdomain.RemoteOrigin,
+		"upstream": gitdomain.RemoteUpstream,
+	}
+	for give, want := range tests {
+		have := gitdomain.NewRemote(give)
+		must.Eq(t, want, have)
+	}
+}
