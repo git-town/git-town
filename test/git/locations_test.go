06170169e44309af33b57fc0ed0f10b55b52cad0
Kevin Goslar <kevin.goslar@gmail.com>
2024-04-02 06:29:50 -0500

    locations_test.go

--- test/git/locations_test.go
+++ test/git/locations_test.go
@@ -10,11 +10,12 @@ import (
 func TestNewLocations(t *testing.T) {
 	t.Parallel()
 	tests := map[string]testgit.Locations{
-		"local":           {testgit.LocationLocal},
-		"origin":          {testgit.LocationOrigin},
-		"upstream":        {testgit.LocationUpstream},
-		"local, origin":   {testgit.LocationLocal, testgit.LocationOrigin},
-		"local, upstream": {testgit.LocationLocal, testgit.LocationUpstream},
+		"local":                   {testgit.LocationLocal},
+		"origin":                  {testgit.LocationOrigin},
+		"upstream":                {testgit.LocationUpstream},
+		"local, origin":           {testgit.LocationLocal, testgit.LocationOrigin},
+		"local, upstream":         {testgit.LocationLocal, testgit.LocationUpstream},
+		"local, origin, upstream": {testgit.LocationLocal, testgit.LocationOrigin, testgit.LocationUpstream},
 	}
 	for give, want := range tests {
 		have := testgit.NewLocations(give)
