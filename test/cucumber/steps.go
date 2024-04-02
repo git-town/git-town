d7551300262a4c7677a18c2ccbb55fbec8d8c5e7
Kevin Goslar <kevin.goslar@gmail.com>
2024-04-02 12:22:59 -0500

    steps.go

--- test/cucumber/steps.go
+++ test/cucumber/steps.go
@@ -1340,13 +1340,15 @@ func Steps(suite *godog.Suite, state *ScenarioState) {
 		state.fixture.DevRepo.CreateChildFeatureBranch(branch, parentBranch)
 		state.initialLineage.AddRow(name, "main")
 		state.fixture.DevRepo.CheckoutBranch(branch)
+		state.fixture.DevRepo.PushBranchToRemote(branch, gitdomain.RemoteOrigin)
 		for _, commit := range git.FromGherkinTable(table) {
 			state.fixture.DevRepo.CreateFile(commit.FileName, commit.FileContent)
 			state.fixture.DevRepo.StageFiles(commit.FileName)
 			state.fixture.DevRepo.CommitStagedChanges(commit.Message)
 			fmt.Printf("1111111111 %#v\n", commit.Locations)
 			if commit.Locations.Contains(git.Location(gitdomain.RemoteOrigin)) {
-				state.fixture.DevRepo.PushBranchToRemote(branch, gitdomain.RemoteOrigin)
+				fmt.Println("22222222222222222222222")
+				state.fixture.DevRepo.PushBranch()
 			}
 		}
 		if !slice.Contains(state.initialLocalBranches, branch) {
