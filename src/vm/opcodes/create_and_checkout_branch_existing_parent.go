d2589bdf899a4ad99e87e470284fc960133e03f8
Kevin Goslar <kevin.goslar@gmail.com>
2024-04-11 15:02:17 -0500

    create_and_checkout_branch_existing_parent.go

--- src/vm/opcodes/create_and_checkout_branch_existing_parent.go
+++ src/vm/opcodes/create_and_checkout_branch_existing_parent.go
@@ -21,7 +21,7 @@ func (self *CreateAndCheckoutBranchExistingParent) CreateContinueProgram() []sha
 func (self *CreateAndCheckoutBranchExistingParent) Run(args shared.RunArgs) error {
 	nearestAncestor := args.Runner.Backend.FirstExistingBranch(self.Ancestors, args.Runner.Config.FullConfig.MainBranch)
 	currentBranch := args.Runner.Backend.CurrentBranchCache.Value()
-	if self.Branch == currentBranch {
+	if nearestAncestor == currentBranch {
 		return args.Runner.Frontend.CreateAndCheckoutBranch(self.Branch)
 	}
 	return args.Runner.Frontend.CreateAndCheckoutBranchWithParent(self.Branch, nearestAncestor.Location())
