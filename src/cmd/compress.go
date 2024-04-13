46e93f8e270b58a2bb4e9d098dc82e7d611c9462
Kevin Goslar <kevin.goslar@gmail.com>
2024-04-12 20:07:05 -0500

    compress.go

--- src/cmd/compress.go
+++ src/cmd/compress.go
@@ -29,7 +29,7 @@ and choosing to fixup all commits.
 Branches must be synced before you compress them.
 
 By default, this command compresses only the branch you are on.
-With the --stack switch it compresses all branches in your stack.
+With the --stack switch it compresses all branches in the current stack.
 
 The compressed commit uses the commit message of the first commit in the branch.
 You can provide a custom commit message with the --message switch.
