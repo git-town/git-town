4d4b862a621baaab30ca3dcccaf635beca50d7c8
Kevin Goslar <kevin.goslar@gmail.com>
2024-04-15 16:48:24 -0500

    main_test.go

--- main_test.go
+++ main_test.go
@@ -40,7 +40,7 @@ func TestGodog(t *testing.T) {
 		Concurrency:   runtime.NumCPU() * concurrency,
 		StopOnFailure: true,
 		Strict:        true,
-		Paths:         []string{"features/sync/all_branches"},
+		Paths:         []string{"features/"},
 		Tags:          tags,
 	})
 	if status > 0 {
