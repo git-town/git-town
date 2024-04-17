0a2a9e92a9de956eb3dc2f741c8afe4ec1949fa0
Kevin Goslar <kevin.goslar@gmail.com>
2024-04-17 12:41:32 -0500

    list_test.go

--- src/cli/dialog/components/list/list_test.go
+++ src/cli/dialog/components/list/list_test.go
@@ -4,5 +4,19 @@ import "testing"
 
 func TestList(t *testing.T) {
 	t.Parallel()
-	t.Run()
+	t.Run("MoveCursorDown", func(t *testing.T) {
+		t.Parallel()
+		t.Run("entry above is enabled", func(t *testing.T) {
+			t.Parallel()
+		})
+		t.Run("first and second entry above are disabled, third entry above is enabled", func(t *testing.T) {
+			t.Parallel()
+		})
+		t.Run("already at beginning of the list", func(t *testing.T) {
+			t.Parallel()
+		})
+		t.Run("all entries above are disabled", func(t *testing.T) {
+			t.Parallel()
+		})
+	})
 }
