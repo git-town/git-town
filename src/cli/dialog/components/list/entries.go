619db1f035ba37fd30d052ea59d502d475b283e4
Kevin Goslar <kevin.goslar@gmail.com>
2024-04-17 14:42:02 -0500

    entries.go

--- src/cli/dialog/components/list/entries.go
+++ src/cli/dialog/components/list/entries.go
@@ -5,7 +5,7 @@ import "fmt"
 // Entries provides methods for a collection of Entry instances.
 type Entries[S fmt.Stringer] []Entry[S]
 
-// NewEnabledListEntries creates enabled BubbleListEntries for the given data types.
+// NewEnabledListEntries creates Entries for the given data types.
 func NewEntries[S fmt.Stringer](records ...S) Entries[S] {
 	result := make([]Entry[S], len(records))
 	for r, record := range records {
