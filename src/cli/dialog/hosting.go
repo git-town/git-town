b743a44cd555b644a673b0527b8a09c4d12d6343
Kevin Goslar <kevin.goslar@gmail.com>
2024-04-17 13:27:25 -0500

    config_storage.go hosting.go

--- src/cli/dialog/hosting.go
+++ src/cli/dialog/hosting.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 
 	"github.com/git-town/git-town/v14/src/cli/dialog/components"
+	"github.com/git-town/git-town/v14/src/cli/dialog/components/list"
 	"github.com/git-town/git-town/v14/src/config/configdomain"
 	"github.com/git-town/git-town/v14/src/gohacks/stringers"
 	"github.com/git-town/git-town/v14/src/messages"
@@ -21,13 +22,13 @@ Only change this if your code hosting server uses as custom URL.
 )
 
 func HostingPlatform(existingValue configdomain.HostingPlatform, inputs components.TestInput) (configdomain.HostingPlatform, bool, error) {
-	entries := []hostingPlatformEntry{
+	entries := list.NewEntries(
 		hostingPlatformAutoDetect,
 		hostingPlatformBitBucket,
 		hostingPlatformGitea,
 		hostingPlatformGitHub,
 		hostingPlatformGitLab,
-	}
+	)
 	cursor := indexOfHostingPlatform(existingValue, entries)
 	newValue, aborted, err := components.RadioList(entries, cursor, hostingPlatformTitle, HostingPlatformHelp, inputs)
 	fmt.Printf(messages.CodeHosting, components.FormattedSelection(newValue.String(), aborted))
