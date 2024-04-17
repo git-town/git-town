142c3ac89290953a370a12702d0538db8d5ae7ee
Kevin Goslar <kevin.goslar@gmail.com>
2024-04-17 14:12:00 -0500

    author.go

--- src/git/gitdomain/author.go
+++ src/git/gitdomain/author.go
@@ -3,6 +3,7 @@ package gitdomain
 // Author represents the author of a commit in the format "name <email>"
 type Author string
 
+// implements fmt.Stringer
 func (self Author) String() string {
 	return string(self)
 }
