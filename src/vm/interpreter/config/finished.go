8afebbbfa3531439cabe3e152551455bcc09c4e3
Kevin Goslar <kevin.goslar@gmail.com>
2024-04-18 11:43:47 -0500

    Remove obsolete TODO (#3374)

--- src/vm/interpreter/config/finished.go
+++ src/vm/interpreter/config/finished.go
@@ -13,8 +13,6 @@ import (
 
 // Finished is called when a Git Town command that only changes configuration has finished successfully.
 func Finished(args FinishedArgs) error {
-	// TODO: extract the code to load a config snapshot into a reusable function
-	//       since it exists in multiple places
 	configGitAccess := gitconfig.Access{Runner: args.Runner.Backend.Runner}
 	globalSnapshot, _, err := configGitAccess.LoadGlobal(false)
 	if err != nil {
