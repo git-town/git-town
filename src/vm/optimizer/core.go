339387d3a34731399d838b401c1393ac396a1afc
Kevin Goslar <kevin.goslar@gmail.com>
2024-04-19 06:33:49 -0500

    core.go

--- src/vm/optimizer/core.go
+++ src/vm/optimizer/core.go
@@ -7,6 +7,9 @@ import (
 	"github.com/git-town/git-town/v14/src/vm/program"
 )
 
+// Optimize improves the performance of the given program by re-arranging its opcodes.
+// It doesn't change the behavior of the program.
+// This is similar to optimizers in compilers.
 func Optimize(prog program.Program) program.Program {
 	return RemoveDuplicateCheckout(prog)
 }
