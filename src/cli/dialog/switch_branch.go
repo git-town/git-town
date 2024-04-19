62f2b91bbe0b1126cb6f38bac69f446356ab1e0f
Kevin Goslar <kevin.goslar@gmail.com>
2024-04-19 11:32:37 -0500

    switch_branch.go en.go

--- src/cli/dialog/switch_branch.go
+++ src/cli/dialog/switch_branch.go
@@ -79,8 +79,8 @@ func (self SwitchModel) View() string {
 	}
 	s := strings.Builder{}
 	if self.UncommittedChanges {
+		s.WriteString("\n")
 		s.WriteString(colors.Bold().Styled(messages.SwitchUncommittedChanges))
-		s.WriteString("123\n")
 		s.WriteString("\n")
 	}
 	window := slice.Window(slice.WindowArgs{
