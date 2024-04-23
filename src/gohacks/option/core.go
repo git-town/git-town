c140dc8b43677665e35a6e09cf522d2ec4fb939c
Kevin Goslar <kevin.goslar@gmail.com>
2024-04-23 13:12:43 -0500

    core.go unpack.go unpack_test.go

--- src/gohacks/option/core.go
+++
@@ -1,7 +0,0 @@
-// Package option helps use pointers similar to Rust option types.
-//
-// One of the uses of nil in Go is to express optionality:
-// A function that might or might not return a type typically returns a pointer to the type.
-// If the pointer is nil, it means there is no value.
-// This package helps use such poor-man option types.
-package option
