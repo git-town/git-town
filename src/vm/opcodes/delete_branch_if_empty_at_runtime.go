30514c749fd0771b52b09512cf50f248aaf223c1
Kevin Goslar <kevin.goslar@gmail.com>
2024-04-23 13:37:16 -0500

    delete_branch_if_empty_at_runtime.go

--- src/vm/opcodes/delete_branch_if_empty_at_runtime.go
+++ src/vm/opcodes/delete_branch_if_empty_at_runtime.go
@@ -15,25 +15,29 @@ type DeleteBranchIfEmptyAtRuntime struct {
 }
 
 func (self *DeleteBranchIfEmptyAtRuntime) Run(args shared.RunArgs) error {
-	parent := args.Lineage.Parent(self.Branch)
-	hasUnmergedChanges, err := args.Runner.Backend.BranchHasUnmergedChanges(self.Branch, parent)
-	if err != nil {
-		return err
-	}
-	if hasUnmergedChanges {
-		args.PrependOpcodes(&QueueMessage{
-			Message: fmt.Sprintf(messages.BranchDeletedHasUnmergedChanges, self.Branch),
-		})
-	} else {
-		args.PrependOpcodes(
-			&CheckoutParent{CurrentBranch: self.Branch},
-			&DeleteLocalBranch{Branch: self.Branch},
-			&RemoveBranchFromLineage{
-				Branch: self.Branch,
-			},
-			&QueueMessage{
-				Message: fmt.Sprintf(messages.BranchDeleted, self.Branch),
+	parentPtr := args.Lineage.Parent(self.Branch)
+	if parentPtr != nil {
+		parent := *parentPtr
+		hasUnmergedChanges, err := args.Runner.Backend.BranchHasUnmergedChanges(self.Branch, parent)
+		if err != nil {
+			return err
+		}
+		if hasUnmergedChanges {
+			args.PrependOpcodes(&QueueMessage{
+				Message: fmt.Sprintf(messages.BranchDeletedHasUnmergedChanges, self.Branch),
 			})
+			return nil
+		}
 	}
+	args.PrependOpcodes(
+		&CheckoutParent{CurrentBranch: self.Branch},
+		&DeleteLocalBranch{Branch: self.Branch},
+		&RemoveBranchFromLineage{
+			Branch: self.Branch,
+		},
+		&QueueMessage{
+			Message: fmt.Sprintf(messages.BranchDeleted, self.Branch),
+		},
+	)
 	return nil
 }
