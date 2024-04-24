c95e10a44f2ffc46bfd4a3a8bebaaa1e506292bd
Kevin Goslar <kevin.goslar@gmail.com>
2024-04-24 18:10:53 -0500

    is_nil.go

--- src/gohacks/is_nil.go
+++ src/gohacks/is_nil.go
@@ -7,12 +7,11 @@ func IsNil(val any) bool {
 	if val == nil {
 		return true
 	}
-	v := reflect.ValueOf(val)
-	k := v.Kind()
-	switch k {
+	value := reflect.ValueOf(val)
+	switch value.Kind() {
 	case reflect.Chan, reflect.Func, reflect.Map, reflect.Pointer,
 		reflect.UnsafePointer, reflect.Interface, reflect.Slice:
-		return v.IsNil()
+		return value.IsNil()
 	}
 	return false
 }
