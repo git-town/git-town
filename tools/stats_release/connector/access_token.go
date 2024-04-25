23e020ca29f552e712a34c74735d58294759b7a6
Kevin Goslar <kevin.goslar@gmail.com>
2024-04-25 10:33:42 -0500

    Use Key constants for accessing config

--- tools/stats_release/connector/access_token.go
+++ tools/stats_release/connector/access_token.go
@@ -3,10 +3,12 @@ package connector
 import (
 	"os/exec"
 	"strings"
+
+	"github.com/git-town/git-town/v14/src/config/gitconfig"
 )
 
 func loadAccessToken() string {
-	process := exec.Command("git", "config", "--get", "git-town.github-token")
+	process := exec.Command("git", "config", "--get", gitconfig.KeyGithubToken.String())
 	output, err := process.Output()
 	if err != nil {
 		panic(err.Error())
