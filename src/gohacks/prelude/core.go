f16a07ac45a22915183f9b65462296f806c6b342
Kevin Goslar <kevin.goslar@gmail.com>
2024-04-25 16:12:13 -0500

    core.go

--- src/gohacks/prelude/core.go
+++ src/gohacks/prelude/core.go
@@ -1,3 +1,5 @@
 // Package is the "standard library" for this codebase.
-// It provides things that are accessible without package namespace.
+// It provides programming facilities that are ubiquitously used
+// and so basic that they should be accessible as part of the language,
+// i.e. without package namespace.
 package prelude
