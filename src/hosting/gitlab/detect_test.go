13da10807decd2da4e82080570be64a337ee38ef
Kevin Goslar <kevin.goslar@gmail.com>
2024-04-25 16:44:39 -0500

    detect_test.go

--- src/hosting/gitlab/detect_test.go
+++ src/hosting/gitlab/detect_test.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/git-town/git-town/v14/src/config/configdomain"
 	"github.com/git-town/git-town/v14/src/git/giturl"
+	. "github.com/git-town/git-town/v14/src/gohacks/prelude"
 	"github.com/git-town/git-town/v14/src/hosting/gitlab"
 	"github.com/shoenig/test/must"
 )
@@ -14,22 +15,22 @@ func TestDetect(t *testing.T) {
 
 	t.Run("GitLab SaaS", func(t *testing.T) {
 		t.Parallel()
-		must.True(t, gitlab.Detect(giturl.Parse("git@gitlab.com:git-town/docs.git"), configdomain.HostingPlatformNone))
+		must.True(t, gitlab.Detect(giturl.Parse("git@gitlab.com:git-town/docs.git"), None[configdomain.HostingPlatform]()))
 	})
 
 	t.Run("hosted service type provided manually", func(t *testing.T) {
 		t.Parallel()
-		must.True(t, gitlab.Detect(giturl.Parse("git@custom-url.com:git-town/docs.git"), configdomain.HostingPlatformGitLab))
+		must.True(t, gitlab.Detect(giturl.Parse("git@custom-url.com:git-town/docs.git"), Some(configdomain.HostingPlatformGitLab)))
 	})
 
 	t.Run("repo is hosted by another hosting platform", func(t *testing.T) {
 		t.Parallel()
-		must.False(t, gitlab.Detect(giturl.Parse("git@github.com:git-town/git-town.git"), configdomain.HostingPlatformNone))
+		must.False(t, gitlab.Detect(giturl.Parse("git@github.com:git-town/git-town.git"), None[configdomain.HostingPlatform]()))
 	})
 
 	t.Run("no origin remote", func(t *testing.T) {
 		t.Parallel()
 		var originURL *giturl.Parts
-		must.False(t, gitlab.Detect(originURL, configdomain.HostingPlatformNone))
+		must.False(t, gitlab.Detect(originURL, None[configdomain.HostingPlatform]()))
 	})
 }
