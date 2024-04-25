46451d6139ae129f99f4f54cab40c5c46196edb2
Kevin Goslar <kevin.goslar@gmail.com>
2024-04-25 17:53:45 -0500

    detect_test.go

--- src/hosting/gitea/detect_test.go
+++ src/hosting/gitea/detect_test.go
@@ -12,10 +12,10 @@ func TestDetect(t *testing.T) {
 	t.Parallel()
 	var emptyURL *giturl.Parts
 	tests := map[*giturl.Parts]bool{
-		giturl.Parse("git@gitea.com:git-town/docs.git"):      true,
-		giturl.Parse("git@custom-url.com:git-town/docs.git"): false,
-		giturl.Parse("git@github.com:git-town/git-town.git"): false,
-		emptyURL: false,
+		giturl.Parse("git@gitea.com:git-town/docs.git"):      true,  // SAAS URL
+		giturl.Parse("git@custom-url.com:git-town/docs.git"): false, // custom URL
+		giturl.Parse("git@github.com:git-town/git-town.git"): false, // other hosting service URL
+		emptyURL: false, // empty URL
 	}
 	for give, want := range tests {
 		have := gitea.Detect(give)
