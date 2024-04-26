b7dab4d6b8b54a4e0c31bb1527e5bbefdb7cc515
Kevin Goslar <kevin.goslar@gmail.com>
2024-04-26 10:37:11 -0500

    ship.go

--- src/cmd/ship.go
+++ src/cmd/ship.go
@@ -200,13 +200,13 @@ func determineShipConfig(args []string, repo *execute.OpenRepoResult, dryRun, ve
 	if !hasTargetBranch {
 		return nil, branchesSnapshot, stashSize, false, fmt.Errorf(messages.ShipBranchHasNoParent, branchNameToShip)
 	}
-	err = ensureParentBranchIsMainOrPerennialBranch(branchNameToShip, targetBranchName, &repo.Runner.Config.FullConfig, repo.Runner.Config.FullConfig.Lineage)
-	if err != nil {
-		return nil, branchesSnapshot, stashSize, false, err
-	}
 	targetBranch := branchesSnapshot.Branches.FindByLocalName(targetBranchName)
 	if targetBranch == nil {
 		return nil, branchesSnapshot, stashSize, false, fmt.Errorf(messages.BranchDoesntExist, targetBranchName)
+	}
+	err = ensureParentBranchIsMainOrPerennialBranch(branchNameToShip, targetBranchName, &repo.Runner.Config.FullConfig, repo.Runner.Config.FullConfig.Lineage)
+	if err != nil {
+		return nil, branchesSnapshot, stashSize, false, err
 	}
 	var proposal *hostingdomain.Proposal
 	childBranches := repo.Runner.Config.FullConfig.Lineage.Children(branchNameToShip)
