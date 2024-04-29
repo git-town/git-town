374c33edcf01da5ea6384d165b3223224c3546ee
Kevin Goslar <kevin.goslar@gmail.com>
2024-04-29 08:29:42 -0500

    some_p.go

--- src/gohacks/prelude/some_p.go
+++ src/gohacks/prelude/some_p.go
@@ -1,6 +1,7 @@
 package prelude
 
-// Some instantiates a new Option containing the given value.
+// SomeP instantiates a new OptionP containing the given value.
+// The value must exist, i.e. the pointer must not be nil.
 func SomeP[T any](value *T) OptionP[T] {
 	if value == nil {
 		panic("You gave nil to SomeP")
