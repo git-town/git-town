e17882672bd6042df6c2f91566e1a27598045474
Kevin Goslar <kevin.goslar@gmail.com>
2024-04-29 13:39:12 -0500

    validated_config.go

--- src/config/configdomain/validated_config.go
+++ src/config/configdomain/validated_config.go
@@ -34,7 +34,36 @@ type ValidatedConfig struct {
 }
 
 func NewValidatedConfig(unvalidated UnvalidatedConfig) ValidatedConfig {
-	//
+	validatedGitUserEmail := validateGitUserEmail(unvalidated.GitUserEmail)
+	validatedGitUserName := validateGitUserName(unvalidated.GitUserName)
+	validatedLineage := validateLineage(unvalidated.Lineage)
+	validatedMainBranch := validateMainBranch(unvalidated.MainBranch)
+	validatedPerennialBranches := validatePerennialBranches(unvalidated.PerennialBranches)
+	return ValidatedConfig{
+		Aliases:                  unvalidated.Aliases,
+		ContributionBranches:     unvalidated.ContributionBranches,
+		GitHubToken:              unvalidated.GitHubToken,
+		GitLabToken:              unvalidated.GitLabToken,
+		GitUserEmail:             validatedGitUserEmail,
+		GitUserName:              validatedGitUserName,
+		GiteaToken:               unvalidated.GiteaToken,
+		HostingOriginHostname:    unvalidated.HostingOriginHostname,
+		HostingPlatform:          unvalidated.HostingPlatform,
+		Lineage:                  validatedLineage,
+		MainBranch:               validatedMainBranch,
+		ObservedBranches:         unvalidated.ObservedBranches,
+		Offline:                  unvalidated.Offline,
+		ParkedBranches:           unvalidated.ParkedBranches,
+		PerennialBranches:        validatedPerennialBranches,
+		PerennialRegex:           unvalidated.PerennialRegex,
+		PushHook:                 unvalidated.PushHook,
+		PushNewBranches:          unvalidated.PushNewBranches,
+		ShipDeleteTrackingBranch: unvalidated.ShipDeleteTrackingBranch,
+		SyncBeforeShip:           unvalidated.SyncBeforeShip,
+		SyncFeatureStrategy:      unvalidated.SyncFeatureStrategy,
+		SyncPerennialStrategy:    unvalidated.SyncPerennialStrategy,
+		SyncUpstream:             unvalidated.SyncUpstream,
+	}
 }
 
 func (self *ValidatedConfig) BranchType(branch gitdomain.LocalBranchName) BranchType {
