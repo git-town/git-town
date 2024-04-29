da20ba6cbb65e5e04ce80ab305ddb12f5cfbe5bb
Kevin Goslar <kevin.goslar@gmail.com>
2024-04-29 17:22:58 -0500

    validated_config.go main_and_perennial.go

--- src/config/configdomain/validated_config.go
+++ src/config/configdomain/validated_config.go
@@ -4,7 +4,6 @@ import (
 	"github.com/git-town/git-town/v14/src/git/gitdomain"
 	. "github.com/git-town/git-town/v14/src/gohacks/prelude"
 	"github.com/git-town/git-town/v14/src/gohacks/slice"
-	"github.com/git-town/git-town/v14/src/validate"
 )
 
 // ValidatedConfig is validated UnvalidatedConfig
@@ -32,39 +31,6 @@ type ValidatedConfig struct {
 	SyncFeatureStrategy      SyncFeatureStrategy
 	SyncPerennialStrategy    SyncPerennialStrategy
 	SyncUpstream             SyncUpstream
-}
-
-func NewValidatedConfig(unvalidated UnvalidatedConfig) ValidatedConfig {
-	validatedMainBranch, validatedPerennialBranches, err := validate.MainAndPerennials(unvalidated.MainBranch, unvalidated.PerennialBranches)
-
-	validatedGitUserEmail := validateGitUserEmail(unvalidated.GitUserEmail)
-	validatedGitUserName := validateGitUserName(unvalidated.GitUserName)
-	validatedLineage := validateLineage(unvalidated.Lineage)
-	return ValidatedConfig{
-		Aliases:                  unvalidated.Aliases,
-		ContributionBranches:     unvalidated.ContributionBranches,
-		GitHubToken:              unvalidated.GitHubToken,
-		GitLabToken:              unvalidated.GitLabToken,
-		GitUserEmail:             validatedGitUserEmail,
-		GitUserName:              validatedGitUserName,
-		GiteaToken:               unvalidated.GiteaToken,
-		HostingOriginHostname:    unvalidated.HostingOriginHostname,
-		HostingPlatform:          unvalidated.HostingPlatform,
-		Lineage:                  validatedLineage,
-		MainBranch:               validatedMainBranch,
-		ObservedBranches:         unvalidated.ObservedBranches,
-		Offline:                  unvalidated.Offline,
-		ParkedBranches:           unvalidated.ParkedBranches,
-		PerennialBranches:        validatedPerennialBranches,
-		PerennialRegex:           unvalidated.PerennialRegex,
-		PushHook:                 unvalidated.PushHook,
-		PushNewBranches:          unvalidated.PushNewBranches,
-		ShipDeleteTrackingBranch: unvalidated.ShipDeleteTrackingBranch,
-		SyncBeforeShip:           unvalidated.SyncBeforeShip,
-		SyncFeatureStrategy:      unvalidated.SyncFeatureStrategy,
-		SyncPerennialStrategy:    unvalidated.SyncPerennialStrategy,
-		SyncUpstream:             unvalidated.SyncUpstream,
-	}
 }
 
 func (self *ValidatedConfig) BranchType(branch gitdomain.LocalBranchName) BranchType {
---
+++ src/validate/main_and_perennial.go
@@ -0,0 +1,33 @@
+package validate
+
+func ValidateConfig(unvalidated UnvalidatedConfig) ValidatedConfig {
+	validatedMainBranch, validatedPerennialBranches, err := validate.MainAndPerennials(unvalidated.MainBranch, unvalidated.PerennialBranches)
+	validatedGitUserEmail := validateGitUserEmail(unvalidated.GitUserEmail)
+	validatedGitUserName := validateGitUserName(unvalidated.GitUserName)
+	validatedLineage := validateLineage(unvalidated.Lineage)
+	return ValidatedConfig{
+		Aliases:                  unvalidated.Aliases,
+		ContributionBranches:     unvalidated.ContributionBranches,
+		GitHubToken:              unvalidated.GitHubToken,
+		GitLabToken:              unvalidated.GitLabToken,
+		GitUserEmail:             validatedGitUserEmail,
+		GitUserName:              validatedGitUserName,
+		GiteaToken:               unvalidated.GiteaToken,
+		HostingOriginHostname:    unvalidated.HostingOriginHostname,
+		HostingPlatform:          unvalidated.HostingPlatform,
+		Lineage:                  validatedLineage,
+		MainBranch:               validatedMainBranch,
+		ObservedBranches:         unvalidated.ObservedBranches,
+		Offline:                  unvalidated.Offline,
+		ParkedBranches:           unvalidated.ParkedBranches,
+		PerennialBranches:        validatedPerennialBranches,
+		PerennialRegex:           unvalidated.PerennialRegex,
+		PushHook:                 unvalidated.PushHook,
+		PushNewBranches:          unvalidated.PushNewBranches,
+		ShipDeleteTrackingBranch: unvalidated.ShipDeleteTrackingBranch,
+		SyncBeforeShip:           unvalidated.SyncBeforeShip,
+		SyncFeatureStrategy:      unvalidated.SyncFeatureStrategy,
+		SyncPerennialStrategy:    unvalidated.SyncPerennialStrategy,
+		SyncUpstream:             unvalidated.SyncUpstream,
+	}
+}
