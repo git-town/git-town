e89fbc24e705354cf366070bf68a6133c67545f8
Kevin Goslar <kevin.goslar@gmail.com>
2024-05-01 13:46:31 -0500

    enter_main_branch.go

--- src/cmd/debug/enter_main_branch.go
+++ src/cmd/debug/enter_main_branch.go
@@ -6,6 +6,7 @@ import (
 	"github.com/git-town/git-town/v14/src/cli/dialog"
 	"github.com/git-town/git-town/v14/src/cli/dialog/components"
 	"github.com/git-town/git-town/v14/src/git/gitdomain"
+	. "github.com/git-town/git-town/v14/src/gohacks/prelude"
 	"github.com/spf13/cobra"
 )
 
@@ -14,7 +15,7 @@ func enterMainBranchCmd() *cobra.Command {
 		Use: "main-branch",
 		RunE: func(_ *cobra.Command, _ []string) error {
 			localBranches := gitdomain.NewLocalBranchNames("main", "branch-1", "branch-2", "branch-3", "branch-4", "branch-5", "branch-6", "branch-7", "branch-8", "branch-9", "branch-A", "branch-B")
-			main := gitdomain.NewLocalBranchName("main")
+			main := Some(gitdomain.NewLocalBranchName("main"))
 			dialogInputs := components.LoadTestInputs(os.Environ())
 			_, _, err := dialog.MainBranch(localBranches, main, dialogInputs.Next())
 			return err
