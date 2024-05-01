6734783dcbf99da422bbd29d5c24558c64180dcc
Kevin Goslar <kevin.goslar@gmail.com>
2024-05-01 06:53:36 -0500

    offline.go

--- src/cmd/offline.go
+++ src/cmd/offline.go
@@ -2,17 +2,20 @@ package cmd
 
 import (
 	"fmt"
+	"os"
 
+	"github.com/git-town/git-town/v14/src/cli/dialog/components"
 	"github.com/git-town/git-town/v14/src/cli/flags"
 	"github.com/git-town/git-town/v14/src/cli/format"
 	"github.com/git-town/git-town/v14/src/cmd/cmdhelpers"
-	"github.com/git-town/git-town/v14/src/config"
 	"github.com/git-town/git-town/v14/src/config/configdomain"
 	"github.com/git-town/git-town/v14/src/config/gitconfig"
 	"github.com/git-town/git-town/v14/src/execute"
+	"github.com/git-town/git-town/v14/src/git/gitdomain"
 	"github.com/git-town/git-town/v14/src/gohacks"
 	"github.com/git-town/git-town/v14/src/messages"
 	"github.com/git-town/git-town/v14/src/undo/undoconfig"
+	"github.com/git-town/git-town/v14/src/validate"
 	configInterpreter "github.com/git-town/git-town/v14/src/vm/interpreter/config"
 	"github.com/spf13/cobra"
 )
@@ -54,7 +57,7 @@ func executeOffline(args []string, verbose bool) error {
 	case 0:
 		displayOfflineStatus(repo.UnvalidatedConfig.Config)
 	case 1:
-		err = setOfflineStatus(args[0], repo.UnvalidatedConfig)
+		err = setOfflineStatus(args[0], repo)
 		if err != nil {
 			return err
 		}
@@ -75,10 +78,20 @@ func displayOfflineStatus(config configdomain.UnvalidatedConfig) {
 	fmt.Println(format.Bool(config.Offline.Bool()))
 }
 
-func setOfflineStatus(text string, config config.ValidatedConfig) error {
+func setOfflineStatus(text string, repo *execute.OpenRepoResult) error {
+	dialogTestInputs := components.LoadTestInputs(os.Environ())
+	branchesSnapshot, err := repo.BackendCommands.BranchesSnapshot()
+	if err != nil {
+		return err
+	}
+	localBranches := branchesSnapshot.Branches.LocalBranches()
+	validatedConfig, err := validate.Config(repo.UnvalidatedConfig, gitdomain.LocalBranchNames{}, localBranches, &repo.BackendCommands, &dialogTestInputs)
+	if err != nil {
+		return err
+	}
 	value, err := gohacks.ParseBool(text)
 	if err != nil {
 		return fmt.Errorf(messages.ValueInvalid, gitconfig.KeyOffline, text)
 	}
-	return config.SetOffline(configdomain.Offline(value))
+	return validatedConfig.SetOffline(configdomain.Offline(value))
 }
