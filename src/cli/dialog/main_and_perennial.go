64aa92b5a136020f2150c35ea1999bb870c50f22
Kevin Goslar <kevin.goslar@gmail.com>
2024-05-02 16:41:35 -0500

    main_and_perennial.go

--- src/cli/dialog/main_and_perennial.go
+++ src/cli/dialog/main_and_perennial.go
@@ -10,21 +10,21 @@ import (
 	"github.com/git-town/git-town/v14/src/messages"
 )
 
-func MainAndPerennials(args MainAndPerennialsArgs) (mainBranch gitdomain.LocalBranchName, additionalPerennials gitdomain.LocalBranchNames, aborted bool, err error) {
+func MainAndPerennials(args MainAndPerennialsArgs) (mainBranch gitdomain.LocalBranchName, perennials gitdomain.LocalBranchNames, aborted bool, err error) {
 	unvalidatedMain, hasMain := args.UnvalidatedMain.Get()
 	if hasMain {
-		return unvalidatedMain, additionalPerennials, false, nil
+		return unvalidatedMain, args.UnvalidatedPerennials, false, nil
 	}
 	if args.HasConfigFile {
-		return unvalidatedMain, additionalPerennials, false, errors.New(messages.ConfigMainbranchInConfigFile)
+		return unvalidatedMain, args.UnvalidatedPerennials, false, errors.New(messages.ConfigMainbranchInConfigFile)
 	}
 	fmt.Print(messages.ConfigNeeded)
 	mainBranch, aborted, err = MainBranch(args.LocalBranches, args.GetDefaultBranch(), args.DialogInputs.Next())
 	if err != nil || aborted {
-		return mainBranch, additionalPerennials, aborted, err
+		return mainBranch, args.UnvalidatedPerennials, aborted, err
 	}
-	additionalPerennials, aborted, err = PerennialBranches(args.LocalBranches, args.UnvalidatedPerennials, mainBranch, args.DialogInputs.Next())
-	return mainBranch, additionalPerennials, aborted, err
+	perennials, aborted, err = PerennialBranches(args.LocalBranches, args.UnvalidatedPerennials, mainBranch, args.DialogInputs.Next())
+	return mainBranch, perennials, aborted, err
 }
 
 type MainAndPerennialsArgs struct {
