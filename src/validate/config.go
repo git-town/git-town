914fc4e8b90c0e8f2a8bcddc44920cae94a9aa42
Kevin Goslar <kevin.goslar@gmail.com>
2024-05-02 10:54:06 -0500

    config.go

---
+++ src/validate/config.go
@@ -0,0 +1,79 @@
+package validate
+
+import (
+	"errors"
+
+	"github.com/git-town/git-town/v14/src/cli/dialog"
+	"github.com/git-town/git-town/v14/src/cli/dialog/components"
+	"github.com/git-town/git-town/v14/src/config"
+	"github.com/git-town/git-town/v14/src/config/configdomain"
+	"github.com/git-town/git-town/v14/src/git"
+	"github.com/git-town/git-town/v14/src/git/gitdomain"
+	"github.com/git-town/git-town/v14/src/messages"
+)
+
+func Config(args ConfigArgs) (validatedResult config.ValidatedConfig, aborted bool, err error) {
+	// enter and save main and perennials
+	validatedMain, additionalPerennials, aborted, err := dialog.MainAndPerennials(dialog.MainAndPerennialsArgs{
+		UnvalidatedMain:       args.Unvalidated.Config.MainBranch,
+		UnvalidatedPerennials: args.Unvalidated.Config.PerennialBranches,
+	})
+	if err != nil || aborted {
+		return validatedResult, aborted, err
+	}
+	if err = args.Unvalidated.SetMainBranch(validatedMain); err != nil {
+		return validatedResult, false, err
+	}
+	if len(additionalPerennials) > 0 {
+		newPerennials := append(args.Unvalidated.Config.PerennialBranches, additionalPerennials...)
+		if err = args.Unvalidated.SetPerennialBranches(newPerennials); err != nil {
+			return validatedResult, false, err
+		}
+	}
+
+	// check Git user data
+	if args.Unvalidated.Config.GitUserEmail.IsNone() {
+		return validatedResult, false, errors.New(messages.GitUserEmailMissing)
+	}
+	if args.Unvalidated.Config.GitUserName.IsNone() {
+		return validatedResult, false, errors.New(messages.GitUserNameMissing)
+	}
+
+	// enter and save missing parent branches
+	additionalLineage, additionalPerennials, abort, err := dialog.Lineage(dialog.LineageArgs{
+		BranchesToVerify: args.BranchesToValidate,
+		Config:           args.Unvalidated.Config,
+		DefaultChoice:    validatedMain,
+		DialogTestInputs: args.TestInputs,
+		LocalBranches:    args.LocalBranches,
+		MainBranch:       validatedMain,
+	})
+	if err != nil || abort {
+		return validatedResult, abort, err
+	}
+	for branch, parent := range additionalLineage {
+		args.Unvalidated.SetParent(branch, parent)
+	}
+	if len(additionalPerennials) > 0 {
+		newPerennials := append(args.Unvalidated.Config.PerennialBranches, additionalPerennials...)
+		if err = args.Unvalidated.SetPerennialBranches(newPerennials); err != nil {
+			return validatedResult, false, err
+		}
+	}
+	validatedConfig := configdomain.ValidatedConfig{
+		UnvalidatedConfig: args.Unvalidated.Config,
+		MainBranch:        validatedMain,
+	}
+	vConfig := config.ValidatedConfig{
+		Config: validatedConfig,
+	}
+	return vConfig, false, nil
+}
+
+type ConfigArgs struct {
+	Backend            *git.BackendCommands
+	BranchesToValidate gitdomain.LocalBranchNames
+	LocalBranches      gitdomain.LocalBranchNames
+	TestInputs         *components.TestInputs
+	Unvalidated        config.UnvalidatedConfig
+}
