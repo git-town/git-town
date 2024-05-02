c37d216ab3d24680768511388d303df44d85cec6
Kevin Goslar <kevin.goslar@gmail.com>
2024-05-02 16:37:41 -0500

    main_and_perennial.go

--- src/cli/dialog/main_and_perennial.go
+++ src/cli/dialog/main_and_perennial.go
@@ -13,15 +13,15 @@ import (
 func MainAndPerennials(args MainAndPerennialsArgs) (mainBranch gitdomain.LocalBranchName, additionalPerennials gitdomain.LocalBranchNames, aborted bool, err error) {
 	unvalidatedMain, hasMain := args.UnvalidatedMain.Get()
 	if hasMain {
-		return unvalidatedMain, args.UnvalidatedPerennials, false, nil
+		return unvalidatedMain, additionalPerennials, false, nil
 	}
 	if args.HasConfigFile {
-		return unvalidatedMain, args.UnvalidatedPerennials, false, errors.New(messages.ConfigMainbranchInConfigFile)
+		return unvalidatedMain, additionalPerennials, false, errors.New(messages.ConfigMainbranchInConfigFile)
 	}
 	fmt.Print(messages.ConfigNeeded)
 	mainBranch, aborted, err = MainBranch(args.LocalBranches, args.GetDefaultBranch(), args.DialogInputs.Next())
 	if err != nil || aborted {
-		return mainBranch, args.UnvalidatedPerennials, aborted, err
+		return mainBranch, additionalPerennials, aborted, err
 	}
 	additionalPerennials, aborted, err = PerennialBranches(args.LocalBranches, args.UnvalidatedPerennials, mainBranch, args.DialogInputs.Next())
 	return mainBranch, additionalPerennials, aborted, err
