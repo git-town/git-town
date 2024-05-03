441434983da78fac3c8c99b2c486e952cc4d20ba
Kevin Goslar <kevin.goslar@gmail.com>
2024-05-03 07:08:03 -0500

    load_repo_snapshot.go

--- src/execute/load_repo_snapshot.go
+++ src/execute/load_repo_snapshot.go
@@ -3,6 +3,7 @@ package execute
 import (
 	"github.com/git-town/git-town/v14/src/cli/dialog/components"
 	"github.com/git-town/git-town/v14/src/config/configdomain"
+	"github.com/git-town/git-town/v14/src/git"
 	"github.com/git-town/git-town/v14/src/git/gitdomain"
 	"github.com/git-town/git-town/v14/src/validate"
 )
@@ -72,9 +73,11 @@ func LoadRepoSnapshot(args LoadRepoSnapshotArgs) (gitdomain.BranchesSnapshot, gi
 }
 
 type LoadRepoSnapshotArgs struct {
+	Backend               *git.BackendCommands
 	Config                *configdomain.UnvalidatedConfig
 	DialogTestInputs      components.TestInputs
 	Fetch                 bool
+	Frontend              *git.FrontendCommands
 	HandleUnfinishedState bool
 	Repo                  *OpenRepoResult
 	RepoStatus            gitdomain.RepoStatus
