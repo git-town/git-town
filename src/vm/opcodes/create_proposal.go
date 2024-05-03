a9f4d30f9455d45c497aeebe6877e53cf1107328
Kevin Goslar <kevin.goslar@gmail.com>
2024-05-03 07:19:33 -0500

    create_proposal.go

--- src/vm/opcodes/create_proposal.go
+++ src/vm/opcodes/create_proposal.go
@@ -8,7 +8,8 @@ import (
 
 // CreateProposal creates a new proposal for the current branch.
 type CreateProposal struct {
-	Branch gitdomain.LocalBranchName
+	Branch     gitdomain.LocalBranchName
+	MainBranch gitdomain.LocalBranchName
 	undeclaredOpcodeMethods
 }
 
@@ -20,7 +21,7 @@ func (self *CreateProposal) CreateContinueProgram() []shared.Opcode {
 
 func (self *CreateProposal) Run(args shared.RunArgs) error {
 	parentBranch := args.Runner.Config.Config.Lineage[self.Branch]
-	prURL, err := args.Connector.NewProposalURL(self.Branch, parentBranch)
+	prURL, err := args.Connector.NewProposalURL(self.Branch, parentBranch, self.MainBranch)
 	if err != nil {
 		return err
 	}
