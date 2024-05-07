f293824ae2abfe53f98b5d97698db9578609813f
Kevin Goslar <kevin.goslar@gmail.com>
2024-05-07 14:41:30 -0500

    config.go

--- src/validate/config.go
+++ src/validate/config.go
@@ -6,6 +6,7 @@ import (
 	"github.com/git-town/git-town/v14/src/cli/dialog"
 	"github.com/git-town/git-town/v14/src/cli/dialog/components"
 	"github.com/git-town/git-town/v14/src/config"
+	"github.com/git-town/git-town/v14/src/config/configdomain"
 	"github.com/git-town/git-town/v14/src/git"
 	"github.com/git-town/git-town/v14/src/git/gitdomain"
 	"github.com/git-town/git-town/v14/src/gohacks"
