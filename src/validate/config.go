46bc88ed95d3ed0fcbd38cd43bdca899ecb72023
Kevin Goslar <kevin.goslar@gmail.com>
2024-05-07 14:31:01 -0500

    Cleanup outdated config only on sync

--- src/validate/config.go
+++ src/validate/config.go
@@ -2,13 +2,10 @@ package validate
 
 import (
 	"errors"
-	"fmt"
 
 	"github.com/git-town/git-town/v14/src/cli/dialog"
 	"github.com/git-town/git-town/v14/src/cli/dialog/components"
 	"github.com/git-town/git-town/v14/src/config"
-	"github.com/git-town/git-town/v14/src/config/configdomain"
-	"github.com/git-town/git-town/v14/src/config/gitconfig"
 	"github.com/git-town/git-town/v14/src/git"
 	"github.com/git-town/git-town/v14/src/git/gitdomain"
 	. "github.com/git-town/git-town/v14/src/gohacks/prelude"
@@ -75,13 +72,6 @@ func Config(args ConfigArgs) (validatedResult config.Config, aborted bool, err e
 		}
 	}
 
-	// remove outdated lineage
-	err = args.Unvalidated.RemoveOutdatedConfiguration(args.LocalBranches)
-	if err != nil {
-		return validatedResult, false, err
-	}
-	err = cleanupPerennialParentEntries(args.Unvalidated.Config.Lineage, validatedPerennials, args.Unvalidated.GitConfig, args.FinalMessages)
-
 	return args.Unvalidated, false, err
 }
 
@@ -92,18 +82,4 @@ type ConfigArgs struct {
 	LocalBranches      gitdomain.LocalBranchNames
 	TestInputs         components.TestInputs
 	Unvalidated        config.Config
-}
-
-// cleanupPerennialParentEntries removes outdated entries from the configuration.
-func cleanupPerennialParentEntries(lineage configdomain.Lineage, perennialBranches gitdomain.LocalBranchNames, access gitconfig.Access, finalMessages stringslice.Collector) error {
-	for _, perennialBranch := range perennialBranches {
-		if lineage.Parent(perennialBranch).IsSome() {
-			if err := access.RemoveLocalConfigValue(gitconfig.NewParentKey(perennialBranch)); err != nil {
-				return err
-			}
-			lineage.RemoveBranch(perennialBranch)
-			finalMessages.Add(fmt.Sprintf(messages.PerennialBranchRemovedParentEntry, perennialBranch))
-		}
-	}
-	return nil
 }
