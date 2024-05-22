2fa6e44f0fe63dee316e14e3146bc1859c3e82ab
Kevin Goslar <kevin.goslar@gmail.com>
2024-05-22 16:17:27 -0500

    sync_perennial_branch.go

--- src/sync/sync_perennial_branch.go
+++ src/sync/sync_perennial_branch.go
@@ -7,11 +7,13 @@ import (
 
 // PerennialBranchProgram adds the opcodes to sync the perennial branch with the given name.
 func PerennialBranchProgram(branch gitdomain.BranchInfo, args BranchProgramArgs) {
-	if branch.HasTrackingBranch() {
-		updateCurrentPerennialBranchOpcode(args.Program, branch.RemoteName, args.Config.SyncPerennialStrategy)
+	if remoteBranch, hasRemoteBranch := branch.RemoteName.Get(); hasRemoteBranch {
+		updateCurrentPerennialBranchOpcode(args.Program, remoteBranch, args.Config.SyncPerennialStrategy)
 	}
-	if branch.LocalName == args.Config.MainBranch && args.Remotes.HasUpstream() && args.Config.SyncUpstream.Bool() {
-		args.Program.Add(&opcodes.FetchUpstream{Branch: args.Config.MainBranch})
-		args.Program.Add(&opcodes.RebaseBranch{Branch: gitdomain.NewBranchName("upstream/" + args.Config.MainBranch.String())})
+	if localBranch, hasLocalBranch := branch.LocalName.Get(); hasLocalBranch {
+		if localBranch == args.Config.MainBranch && args.Remotes.HasUpstream() && args.Config.SyncUpstream.Bool() {
+			args.Program.Add(&opcodes.FetchUpstream{Branch: args.Config.MainBranch})
+			args.Program.Add(&opcodes.RebaseBranch{Branch: gitdomain.NewBranchName("upstream/" + args.Config.MainBranch.String())})
+		}
 	}
 }
