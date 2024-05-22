898e10fb998489492d06f9313329deb1fdeb5f03
Kevin Goslar <kevin.goslar@gmail.com>
2024-05-22 11:52:42 -0500

    Provide needed data directly (#3541)

--- src/sync/sync_deleted_branch.go
+++ src/sync/sync_deleted_branch.go
@@ -11,8 +11,8 @@ import (
 )
 
 // syncDeletedBranchProgram adds opcodes that sync a branch that was deleted at origin to the given program.
-func syncDeletedBranchProgram(list *program.Program, branch gitdomain.BranchInfo, parentOtherWorktree bool, args BranchProgramArgs) {
-	switch args.Config.BranchType(branch.LocalName) {
+func syncDeletedBranchProgram(list *program.Program, branch gitdomain.LocalBranchName, parentOtherWorktree bool, args BranchProgramArgs) {
+	switch args.Config.BranchType(branch) {
 	case configdomain.BranchTypeFeatureBranch:
 		syncDeletedFeatureBranchProgram(list, branch, parentOtherWorktree, args)
 	case configdomain.BranchTypePerennialBranch, configdomain.BranchTypeMainBranch:
@@ -24,40 +24,39 @@ func syncDeletedBranchProgram(list *program.Program, branch gitdomain.BranchInfo
 
 // syncDeletedFeatureBranchProgram syncs a feare branch whose remote has been deleted.
 // The parent branch must have been fully synced before calling this function.
-func syncDeletedFeatureBranchProgram(list *program.Program, branch gitdomain.BranchInfo, parentOtherWorktree bool, args BranchProgramArgs) {
-	list.Add(&opcodes.Checkout{Branch: branch.LocalName})
-	pullParentBranchOfCurrentFeatureBranchOpcode(featureBranchArgs{
+func syncDeletedFeatureBranchProgram(list *program.Program, branch gitdomain.LocalBranchName, parentOtherWorktree bool, args BranchProgramArgs) {
+	list.Add(&opcodes.Checkout{Branch: branch})
+	pullParentBranchOfCurrentFeatureBranchOpcode(pullParentBranchOfCurrentFeatureBranchOpcodeArgs{
 		branch:              branch,
-		offline:             args.Config.Offline,
 		parentOtherWorktree: parentOtherWorktree,
 		program:             list,
 		syncStrategy:        args.Config.SyncFeatureStrategy,
 	})
-	list.Add(&opcodes.DeleteBranchIfEmptyAtRuntime{Branch: branch.LocalName})
+	list.Add(&opcodes.DeleteBranchIfEmptyAtRuntime{Branch: branch})
 }
 
-func syncDeletedObservedBranchProgram(list *program.Program, branch gitdomain.BranchInfo, args BranchProgramArgs) {
+func syncDeletedObservedBranchProgram(list *program.Program, branch gitdomain.LocalBranchName, args BranchProgramArgs) {
 	RemoveBranchFromLineage(RemoveBranchFromLineageArgs{
-		Branch:  branch.LocalName,
+		Branch:  branch,
 		Lineage: args.Config.Lineage,
 		Parent:  args.Config.MainBranch,
 		Program: list,
 	})
-	list.Add(&opcodes.RemoveFromObservedBranches{Branch: branch.LocalName})
+	list.Add(&opcodes.RemoveFromObservedBranches{Branch: branch})
 	list.Add(&opcodes.Checkout{Branch: args.Config.MainBranch})
-	list.Add(&opcodes.DeleteLocalBranch{Branch: branch.LocalName})
-	list.Add(&opcodes.QueueMessage{Message: fmt.Sprintf(messages.BranchDeleted, branch.LocalName)})
+	list.Add(&opcodes.DeleteLocalBranch{Branch: branch})
+	list.Add(&opcodes.QueueMessage{Message: fmt.Sprintf(messages.BranchDeleted, branch)})
 }
 
-func syncDeletedPerennialBranchProgram(list *program.Program, branch gitdomain.BranchInfo, args BranchProgramArgs) {
+func syncDeletedPerennialBranchProgram(list *program.Program, branch gitdomain.LocalBranchName, args BranchProgramArgs) {
 	RemoveBranchFromLineage(RemoveBranchFromLineageArgs{
-		Branch:  branch.LocalName,
+		Branch:  branch,
 		Lineage: args.Config.Lineage,
 		Parent:  args.Config.MainBranch,
 		Program: list,
 	})
-	list.Add(&opcodes.RemoveFromPerennialBranches{Branch: branch.LocalName})
+	list.Add(&opcodes.RemoveFromPerennialBranches{Branch: branch})
 	list.Add(&opcodes.Checkout{Branch: args.Config.MainBranch})
-	list.Add(&opcodes.DeleteLocalBranch{Branch: branch.LocalName})
-	list.Add(&opcodes.QueueMessage{Message: fmt.Sprintf(messages.BranchDeleted, branch.LocalName)})
+	list.Add(&opcodes.DeleteLocalBranch{Branch: branch})
+	list.Add(&opcodes.QueueMessage{Message: fmt.Sprintf(messages.BranchDeleted, branch)})
 }
