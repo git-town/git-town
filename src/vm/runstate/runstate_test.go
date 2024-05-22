c5793aa178d4a73ddf30c8d9c57eef27bd33d3eb
Kevin Goslar <kevin.goslar@gmail.com>
2024-05-22 07:06:49 -0500

    runstate_test.go

--- src/vm/runstate/runstate_test.go
+++ src/vm/runstate/runstate_test.go
@@ -5,6 +5,7 @@ import (
 	"testing"
 
 	"github.com/git-town/git-town/v14/src/git/gitdomain"
+	. "github.com/git-town/git-town/v14/src/gohacks/prelude"
 	"github.com/git-town/git-town/v14/src/undo/undoconfig"
 	"github.com/git-town/git-town/v14/src/vm/opcodes"
 	"github.com/git-town/git-town/v14/src/vm/program"
@@ -38,17 +39,17 @@ func TestRunState(t *testing.T) {
 				Active: "branch-1",
 				Branches: gitdomain.BranchInfos{
 					gitdomain.BranchInfo{
-						LocalName:  "branch-1",
-						LocalSHA:   "111111",
-						RemoteName: "origin/branch-1",
-						RemoteSHA:  "222222",
+						LocalName:  Some(gitdomain.NewLocalBranchName("branch-1")),
+						LocalSHA:   Some(gitdomain.NewSHA("111111")),
+						RemoteName: Some(gitdomain.NewRemoteBranchName("origin/branch-1")),
+						RemoteSHA:  Some(gitdomain.NewSHA("222222")),
 						SyncStatus: gitdomain.SyncStatusNotInSync,
 					},
 					gitdomain.BranchInfo{
-						LocalName:  "branch-2",
-						LocalSHA:   "333333",
-						RemoteName: gitdomain.EmptyRemoteBranchName(),
-						RemoteSHA:  gitdomain.EmptySHA(),
+						LocalName:  Some(gitdomain.NewLocalBranchName("branch-2")),
+						LocalSHA:   Some(gitdomain.NewSHA("333333")),
+						RemoteName: None[gitdomain.RemoteBranchName](),
+						RemoteSHA:  None[gitdomain.SHA](),
 						SyncStatus: gitdomain.SyncStatusLocalOnly,
 					},
 				},
