ba8464165fb90828ac356d81104fcbef7d971b44
Kevin Goslar <kevin.goslar@gmail.com>
2024-05-27 07:30:58 -0500

    pretty_test.go

--- test/helpers/pretty_test.go
+++ test/helpers/pretty_test.go
@@ -9,7 +9,7 @@ import (
 // this function exists only to use pretty somewhere so that the half-intelligent Go module system
 // doesn't auto-remove it from go.mod.
 func TestPretty(t *testing.T) {
+	t.Parallel()
 	a := 1
-	b := 2
-	pretty.Ldiff(t, a, b)
+	pretty.Ldiff(t, a, a)
 }
