6fe4bfb8148f7e5e2133c26f64027c46a4d75de3
Kevin Goslar <kevin.goslar@gmail.com>
2024-05-27 15:51:30 -0500

    main_test.go

--- main_test.go
+++ main_test.go
@@ -40,7 +40,7 @@ func TestGodog(t *testing.T) {
 		Concurrency:   runtime.NumCPU() * concurrency,
 		StopOnFailure: true,
 		Strict:        true,
-		Paths:         []string{"features/sync/current_branch/feature_branch/rebase_sync_strategy/offline"},
+		Paths:         []string{"features/"},
 		Tags:          tags,
 	})
 	if status > 0 {
