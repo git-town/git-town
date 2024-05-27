af3bc9cdc1ffd54867f47f4bbdcf82b784732cbb
Kevin Goslar <kevin.goslar@gmail.com>
2024-05-27 14:20:52 -0500

    Proper comparison and serialization of Options (#3548)

--- src/gohacks/prelude/option_test.go
+++ src/gohacks/prelude/option_test.go
@@ -11,6 +11,80 @@ import (
 func TestOption(t *testing.T) {
 	t.Parallel()
 
+	t.Run("comparison", func(t *testing.T) {
+		t.Parallel()
+		t.Run("int", func(t *testing.T) {
+			t.Parallel()
+			t.Run("equal", func(t *testing.T) {
+				a := Some(1)
+				b := Some(1)
+				must.True(t, a.Equal(b))
+			})
+			t.Run("not equal", func(t *testing.T) {
+				a := Some(1)
+				b := Some(2)
+				must.False(t, a.Equal(b))
+			})
+			t.Run("Some and None", func(t *testing.T) {
+				a := Some(1)
+				b := None[int]()
+				must.False(t, a.Equal(b))
+			})
+			t.Run("None and Some", func(t *testing.T) {
+				a := Some(1)
+				b := None[int]()
+				must.False(t, a.Equal(b))
+			})
+			t.Run("None and None", func(t *testing.T) {
+				a := None[int]()
+				b := None[int]()
+				must.True(t, a.Equal(b))
+			})
+			t.Run("Some(Default) and None", func(t *testing.T) {
+				a := Some(0)
+				b := None[int]()
+				must.False(t, a.Equal(b))
+			})
+		})
+	})
+
+	t.Run("MarshalJSON", func(t *testing.T) {
+		t.Parallel()
+		t.Run("Some", func(t *testing.T) {
+			t.Parallel()
+			value := Some(12)
+			json, err := value.MarshalJSON()
+			must.NoError(t, err)
+			must.Eq(t, "12", string(json))
+		})
+		t.Run("None", func(t *testing.T) {
+			t.Parallel()
+			value := None[int]()
+			json, err := value.MarshalJSON()
+			must.NoError(t, err)
+			must.Eq(t, "null", string(json))
+		})
+	})
+
+	t.Run("UnmarshalJSON", func(t *testing.T) {
+		t.Run("Some", func(t *testing.T) {
+			t.Parallel()
+			json := "12"
+			value := None[int]()
+			err := value.UnmarshalJSON([]byte(json))
+			must.NoError(t, err)
+			must.Eq(t, Some(12), value)
+		})
+		t.Run("None", func(t *testing.T) {
+			t.Parallel()
+			json := "null"
+			value := None[int]()
+			err := value.UnmarshalJSON([]byte(json))
+			must.NoError(t, err)
+			must.Eq(t, None[int](), value)
+		})
+	})
+
 	t.Run("String", func(t *testing.T) {
 		t.Parallel()
 		t.Run("Some(struct that implements fmt.Stringer)", func(t *testing.T) {
