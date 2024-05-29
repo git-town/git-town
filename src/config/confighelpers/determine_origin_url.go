fa7cb72da688e55f1bdf0878c32bcb987cf4223c
Kevin Goslar <kevin.goslar@gmail.com>
2024-05-29 16:25:29 +0000

    Remove OriginURLCache

--- src/config/confighelpers/determine_origin_url.go
+++ src/config/confighelpers/determine_origin_url.go
@@ -6,12 +6,7 @@ import (
 	. "github.com/git-town/git-town/v14/src/gohacks/prelude"
 )
 
-func DetermineOriginURL(originURL string, originOverride Option[configdomain.HostingOriginHostname], originURLCache configdomain.OriginURLCache) Option[giturl.Parts] {
-	cached, has := originURLCache[originURL]
-	if has {
-		return Some(cached)
-	}
-
+func DetermineOriginURL(originURL string, originOverride Option[configdomain.HostingOriginHostname]) Option[giturl.Parts] {
 	url, hasURL := giturl.Parse(originURL).Get()
 	if !hasURL {
 		return None[giturl.Parts]()
@@ -19,6 +14,5 @@ func DetermineOriginURL(originURL string, originOverride Option[configdomain.Hos
 	if value, has := originOverride.Get(); has {
 		url.Host = value.String()
 	}
-	originURLCache[originURL] = url
 	return Some(url)
 }
