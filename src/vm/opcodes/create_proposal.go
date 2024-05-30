bb5a9a41ecc16141dace55b95eb4d2a2d69522ca
Kevin Goslar <kevin.goslar@gmail.com>
2024-05-30 11:53:54 -0500

    Add extra check for when creating proposals (#3606)

--- src/vm/opcodes/create_proposal.go
+++ src/vm/opcodes/create_proposal.go
@@ -3,6 +3,7 @@ package opcodes
 import (
 	"github.com/git-town/git-town/v14/src/browser"
 	"github.com/git-town/git-town/v14/src/git/gitdomain"
+	"github.com/git-town/git-town/v14/src/hosting/hostingdomain"
 	"github.com/git-town/git-town/v14/src/vm/shared"
 )
 
@@ -21,6 +22,9 @@ func (self *CreateProposal) CreateContinueProgram() []shared.Opcode {
 
 func (self *CreateProposal) Run(args shared.RunArgs) error {
 	parentBranch := args.Config.Config.Lineage[self.Branch]
+	if args.Connector == nil {
+		return hostingdomain.UnsupportedServiceError()
+	}
 	prURL, err := args.Connector.NewProposalURL(self.Branch, parentBranch, self.MainBranch)
 	if err != nil {
 		return err
