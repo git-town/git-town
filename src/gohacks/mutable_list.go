b73ecc9cfa7b336a6d2cd408d4878dd86b9e0543
Kevin Goslar <kevin.goslar@gmail.com>
2024-06-07 11:20:02 -0500

    mutable_list.go mutable_list_test.go

--- src/gohacks/mutable_list.go
+++ src/gohacks/mutable_list.go
@@ -4,15 +4,13 @@ type MutableList[E any, L ~[]E] struct {
 	list *L
 }
 
-func NewMutableList[E any, L ~[]E]() MutableList[E, L] {
-	list := make(L, 0)
-	return MutableList[E, L]{list: &list}
-}
-
 func (self MutableList[E, L]) Append(element E) {
 	*self.list = append(*self.list, element)
 }
 
 func (self MutableList[E, L]) List() L {
+	if self.list == nil {
+		return make(L, 0)
+	}
 	return *self.list
 }
--- src/gohacks/mutable_list_test.go
+++ src/gohacks/mutable_list_test.go
@@ -14,14 +14,14 @@ func TestMutableList(t *testing.T) {
 	t.Run(".List", func(t *testing.T) {
 		t.Run("empty mutable list", func(t *testing.T) {
 			t.Parallel()
-			list := gohacks.NewMutableList[gitdomain.LocalBranchName, gitdomain.LocalBranchNames]()
+			list := gohacks.MutableList[gitdomain.LocalBranchName, gitdomain.LocalBranchNames]{}
 			have := list.List()
 			want := gitdomain.LocalBranchNames{}
 			must.Eq(t, want, have)
 		})
 		t.Run("populated mutable list", func(t *testing.T) {
 			t.Parallel()
-			list := gohacks.NewMutableList[gitdomain.LocalBranchName, gitdomain.LocalBranchNames]()
+			list := gohacks.MutableList[gitdomain.LocalBranchName, gitdomain.LocalBranchNames]{}
 			list.Append(gitdomain.NewLocalBranchName("branch-1"))
 			list.Append(gitdomain.NewLocalBranchName("branch-2"))
 			have := list.List()
