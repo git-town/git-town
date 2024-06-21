09c496dd489afa5c8f46f058f916bac066ba2635
Kevin Goslar <kevin.goslar@gmail.com>
2024-06-20 19:19:35 -0500

    Fix flaky unit test

--- src/config/configdomain/lineage_test.go
+++ src/config/configdomain/lineage_test.go
@@ -1,6 +1,8 @@
 package configdomain_test
 
 import (
+	"cmp"
+	"slices"
 	"testing"
 
 	"github.com/git-town/git-town/v14/src/config/configdomain"
@@ -333,6 +335,9 @@ func TestLineage(t *testing.T) {
 			lineage.Add(branch1, branch2)
 			lineage.Add(branch3, branch4)
 			have := lineage.Entries()
+			slices.SortFunc(have, func(a, b configdomain.LineageEntry) int {
+				return cmp.Compare(a.Child, b.Child)
+			})
 			want := []configdomain.LineageEntry{
 				{
 					Child:  branch1,
