44765148e0aab2ed510bf2a9f01c3850b01bb590
Kevin Goslar <kevin.goslar@gmail.com>
2024-06-20 19:44:45 -0500

    factory_test.go

--- test/fixture/factory_test.go
+++ test/fixture/factory_test.go
@@ -2,7 +2,6 @@ package fixture_test
 
 import (
 	"os"
-	"path/filepath"
 	"testing"
 
 	"github.com/git-town/git-town/v14/test/fixture"
@@ -12,19 +11,10 @@ import (
 func TestFixtureFactory(t *testing.T) {
 	t.Parallel()
 
-	t.Run("NewFixtureFactory", func(t *testing.T) {
-		t.Parallel()
-		dir := t.TempDir()
-		_ = fixture.NewFactory(dir)
-		memoizedPath := filepath.Join(dir, "memoized")
-		_, err := os.Stat(memoizedPath)
-		must.False(t, os.IsNotExist(err))
-	})
-
 	t.Run("CreateFixture", func(t *testing.T) {
 		t.Parallel()
-		dir := t.TempDir()
-		gm := fixture.NewFactory(dir)
+		gm := fixture.CreateFactory()
+		defer gm.Remove()
 		result := gm.CreateFixture("foo")
 		_, err := os.Stat(result.DevRepo.WorkingDir)
 		must.False(t, os.IsNotExist(err))
