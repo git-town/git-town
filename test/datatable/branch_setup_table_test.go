b1bfcaa4e12d3ca3fb1f4333d758d31fe465086e
Kevin Goslar <kevin.goslar@gmail.com>
2024-07-02 10:38:19 -0500

    branch_setup_table_test.go

--- test/datatable/branch_setup_table_test.go
+++ test/datatable/branch_setup_table_test.go
@@ -14,6 +14,7 @@ import (
 
 func TestParseBranchSetupTable(t *testing.T) {
 	t.Parallel()
+
 	t.Run("normal table", func(t *testing.T) {
 		t.Parallel()
 		give := &godog.Table{
@@ -56,6 +57,7 @@ func TestParseBranchSetupTable(t *testing.T) {
 		}
 		must.Eq(t, want, have)
 	})
+
 	t.Run("no parents given", func(t *testing.T) {
 		t.Parallel()
 		give := &godog.Table{
