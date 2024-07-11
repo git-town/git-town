e90c3f7f0ffffc9a261b48ff3fe07f9630cab1a1
Kevin Goslar <kevin.goslar@gmail.com>
2024-07-11 13:42:24 -0500

    get_parent.go root.go

---
+++ src/cmd/config/get_parent.go
@@ -0,0 +1,51 @@
+package config
+
+import (
+	"fmt"
+
+	"github.com/git-town/git-town/v14/src/cli/flags"
+	"github.com/git-town/git-town/v14/src/cmd/cmdhelpers"
+	"github.com/git-town/git-town/v14/src/execute"
+	"github.com/git-town/git-town/v14/src/git/gitdomain"
+	"github.com/spf13/cobra"
+)
+
+const getParentDesc = "Displays the parent branch for the current or given branch"
+
+func getParentCommand() *cobra.Command {
+	addVerboseFlag, readVerboseFlag := flags.Verbose()
+	cmd := cobra.Command{
+		Use:   "get-parent [branch]",
+		Args:  cobra.MaximumNArgs(1),
+		Short: getParentDesc,
+		Long:  cmdhelpers.Long(getParentDesc),
+		RunE: func(cmd *cobra.Command, args []string) error {
+			return executeGetParent(args, readVerboseFlag(cmd))
+		},
+	}
+	addVerboseFlag(&cmd)
+	return &cmd
+}
+
+func executeGetParent(args []string, verbose bool) error {
+	repo, err := execute.OpenRepo(execute.OpenRepoArgs{
+		DryRun:           false,
+		OmitBranchNames:  true,
+		PrintCommands:    true,
+		ValidateGitRepo:  true,
+		ValidateIsOnline: false,
+		Verbose:          verbose,
+	})
+	if err != nil {
+		return err
+	}
+	var branch gitdomain.LocalBranchName
+	if len(args) == 0 {
+		branch, err = repo.Git.CurrentBranch(repo.Backend)
+	} else {
+		branch = gitdomain.NewLocalBranchName(args[0])
+	}
+	parent := repo.UnvalidatedConfig.Config.Value.Lineage.Parent(branch)
+	fmt.Println(parent)
+	return nil
+}
