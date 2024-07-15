a268d044fbb501995219434076093f5fca9ec961
Kevin Goslar <kevin.goslar@gmail.com>
2024-07-15 17:00:25 -0500

    dryrun.go

--- src/config/configdomain/dryrun.go
+++ src/config/configdomain/dryrun.go
@@ -3,10 +3,10 @@ package configdomain
 // indicates whether a Git Town command should execute the commands or only display them
 type DryRun bool
 
-func (self DryRun) IsTrue() bool {
-	return bool(self)
-}
-
 func (self DryRun) IsFalse() bool {
 	return !bool(self)
+}
+
+func (self DryRun) IsTrue() bool {
+	return bool(self)
 }
