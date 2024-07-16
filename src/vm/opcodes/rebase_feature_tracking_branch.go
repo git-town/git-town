fa19d7e49ab575cf15c1ff44536edb8e48e08247
Kevin Goslar <kevin.goslar@gmail.com>
2024-07-15 20:48:17 -0500

    rebase_feature_tracking_branch.go

--- src/vm/opcodes/rebase_feature_tracking_branch.go
+++ src/vm/opcodes/rebase_feature_tracking_branch.go
@@ -8,30 +8,25 @@ import (
 
 // RebaseFeatureTrackingBranch rebases the current feature branch against its tracking branch.
 type RebaseFeatureTrackingBranch struct {
-	RemoteBranch            gitdomain.RemoteBranchName
 	PushBranches            configdomain.PushBranches
+	RemoteBranch            gitdomain.RemoteBranchName
 	undeclaredOpcodeMethods `exhaustruct:"optional"`
 }
 
 func (self *RebaseFeatureTrackingBranch) Run(args shared.RunArgs) error {
-	if self.PushBranches {
-		// Try to force-push the local branch with lease and includes to the remote branch.
-		err := args.Git.ForcePushBranchSafely(args.Frontend, args.Config.Config.NoPushHook())
-		if err == nil {
-			// The force-push succeeded --> the remote branch didn't contain new commits, we are done.
-			return nil
-		}
+	// Try to force-push the local branch with lease and includes to the remote branch.
+	err := args.Git.ForcePushBranchSafely(args.Frontend, args.Config.Config.NoPushHook())
+	if err == nil {
+		// The force-push succeeded --> the remote branch didn't contain new commits, we are done.
+		return nil
 	}
 	// The force-push failed --> the remote branch contains new commits.
 	// We need to integrate them into the local branch.
-	opcodes := []shared.Opcode{
+	args.PrependOpcodes(
 		// Rebase the local commits against the remote commits.
 		&RebaseBranch{Branch: self.RemoteBranch.BranchName()},
-	}
-	// Now try force-pushing again.
-	if self.PushBranches {
-		opcodes = append(opcodes, &RebaseFeatureTrackingBranch{RemoteBranch: self.RemoteBranch})
-	}
-	args.PrependOpcodes(opcodes...)
+		// Now try force-pushing again.
+		&RebaseFeatureTrackingBranch{RemoteBranch: self.RemoteBranch, PushBranches: self.PushBranches},
+	)
 	return nil
 }
