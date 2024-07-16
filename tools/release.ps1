0bdcf8ff19e1c95c290fd3d50617044989ccaa8f
Kevin Goslar <kevin.goslar@gmail.com>
2024-07-15 21:36:29 -0500

    Makefile RELEASE version.feature root.go release.ps1 install install.sh

--- tools/release.ps1
+++ tools/release.ps1
@@ -8,7 +8,7 @@ param (
 $ErrorActionPreference = "Stop"
 
 # the Git Town version to release
-$env:VERSION = "v14.3.0"
+$env:VERSION = "v14.3.1"
 
 # dependencies
 Set-Variable -Name "GoMsiVersion" -Value "1.0.2" -Option Constant
