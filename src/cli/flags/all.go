b9f40e0010cbe408dff7bebe3b1bdf81f9ceb483
Kevin Goslar <kevin.goslar@gmail.com>
2024-07-23 09:38:46 -0500

    Fix documentation (#3745)

--- src/cli/flags/all.go
+++ src/cli/flags/all.go
@@ -1,6 +1,6 @@
 package flags
 
-// DryRun provides mistake-safe access to the "--dry-run" Cobra command-line flag.
+// type-safe access to the "--all" command-line flag
 func All() (AddFunc, ReadBoolFlagFunc) {
 	return Bool("all", "a", "Sync all local branches", FlagTypeNonPersistent)
 }
