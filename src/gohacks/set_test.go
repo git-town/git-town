be620c9a17fd33de336533329460da9a8eb6e6a4
Kevin Goslar <kevin.goslar@gmail.com>
2024-07-29 11:07:58 -0500

    set_test.go

--- src/gohacks/set_test.go
+++ src/gohacks/set_test.go
@@ -10,14 +10,15 @@ import (
 func TestSet(t *testing.T) {
 	t.Parallel()
 
-	t.Run("Add, Values", func(t *testing.T) {
+	t.Run("Add", func(t *testing.T) {
 		t.Parallel()
 		set := gohacks.NewSet[int]()
-		must.False(t, set.Contains(1))
 		set.Add(1)
 		must.Eq(t, []int{1}, set.Values())
 		set.Add(1)
 		must.Eq(t, []int{1}, set.Values())
+		set.Add(2)
+		must.Eq(t, []int{1, 2}, set.Values())
 	})
 
 	t.Run("AddSet", func(t *testing.T) {
@@ -34,5 +35,45 @@ func TestSet(t *testing.T) {
 		must.True(t, set.Contains(1))
 		must.True(t, set.Contains(2))
 		must.False(t, set.Contains(3))
+	})
+
+	t.Run("NewSet", func(t *testing.T) {
+		t.Parallel()
+
+		t.Run("no initial value", func(t *testing.T) {
+			t.Parallel()
+			set := gohacks.NewSet[int]()
+			must.Eq(t, []int{}, set.Values())
+		})
+
+		t.Run("one initial value", func(t *testing.T) {
+			t.Parallel()
+			set := gohacks.NewSet(1)
+			must.Eq(t, []int{1}, set.Values())
+		})
+
+		t.Run("multiple initial values", func(t *testing.T) {
+			t.Parallel()
+			set := gohacks.NewSet(1, 2)
+			must.Eq(t, []int{1, 2}, set.Values())
+		})
+	})
+
+	t.Run("Values", func(t *testing.T) {
+		t.Parallel()
+
+		t.Run("no values", func(t *testing.T) {
+			set := gohacks.NewSet[int]()
+			have := set.Values()
+			want := []int{}
+			must.Eq(t, want, have)
+		})
+
+		t.Run("with values", func(t *testing.T) {
+			set := gohacks.NewSet(1, 2)
+			have := set.Values()
+			want := []int{1, 2}
+			must.Eq(t, want, have)
+		})
 	})
 }
