079d699a6e2fd9d50cb4431165e828025a8816e9
Kevin Goslar <kevin.goslar@gmail.com>
2024-07-30 16:09:07 -0500

    merge.go

--- src/gohacks/mapstools/merge.go
+++ src/gohacks/mapstools/merge.go
@@ -2,6 +2,7 @@ package mapstools
 
 import "maps"
 
+// provides the given maps merged together
 func Merge[K comparable, V any](map1, map2 map[K]V) map[K]V {
 	result := maps.Clone(map1)
 	maps.Copy(result, map2)
