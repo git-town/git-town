14f01b88bd5a3749a2972c9931cf82743707f981
Kevin Goslar <kevin.goslar@gmail.com>
2024-07-31 10:16:39 -0500

    Extract ErrorCollector fro FailureCollector (#3789)

--- src/execute/failure_collector_test.go
+++ src/execute/failure_collector_test.go
@@ -14,6 +14,7 @@ func TestCollector(t *testing.T) {
 	t.Parallel()
 
 	t.Run("BranchesSyncStatus", func(t *testing.T) {
+		t.Parallel()
 		t.Run("returns the given value", func(t *testing.T) {
 			t.Parallel()
 			fc := execute.FailureCollector{}
@@ -38,41 +39,6 @@ func TestCollector(t *testing.T) {
 			err := errors.New("test error")
 			have = fc.BranchInfos(branchInfos, err)
 			must.Eq(t, branchInfos, have)
-		})
-
-		t.Run("captures the first error it receives", func(t *testing.T) {
-			t.Parallel()
-			fc := execute.FailureCollector{}
-			fc.Check(errors.New("first"))
-			fc.Check(errors.New("second"))
-			must.ErrorContains(t, fc.Err, "first")
-		})
-	})
-
-	t.Run("Check", func(t *testing.T) {
-		t.Parallel()
-		t.Run("captures the first error it receives", func(t *testing.T) {
-			fc := execute.FailureCollector{}
-			fc.Check(nil)
-			must.Nil(t, fc.Err)
-			fc.Check(errors.New("first"))
-			fc.Check(errors.New("second"))
-			must.ErrorContains(t, fc.Err, "first")
-		})
-		t.Run("indicates whether it received an error", func(t *testing.T) {
-			fc := execute.FailureCollector{}
-			must.False(t, fc.Check(nil))
-			must.True(t, fc.Check(errors.New("")))
-			must.True(t, fc.Check(nil))
-		})
-	})
-
-	t.Run("Fail", func(t *testing.T) {
-		t.Parallel()
-		t.Run("registers the given error", func(t *testing.T) {
-			fc := execute.FailureCollector{}
-			fc.Fail("failed %s", "reason")
-			must.ErrorContains(t, fc.Err, "failed reason")
 		})
 	})
 }
