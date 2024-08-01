c02a6fd2c91c886d1eefd8068e91c5679fed8bb4
Kevin Goslar <kevin.goslar@gmail.com>
2024-08-01 10:55:03 -0500

    aliases.go single_snapshot.go

--- src/config/configdomain/aliases.go
+++ src/config/configdomain/aliases.go
@@ -7,14 +7,12 @@ type Aliases map[AliasableCommand]string
 
 func NewAliasesFromSnapshot(snapshot SingleSnapshot) (Aliases, error) {
 	result := Aliases{}
-	for key, value := range snapshot {
-		if key.IsAliasKey() {
-			aliasableCommand, has := AliasableCommandForKey(key).Get()
-			if !has {
-				return result, fmt.Errorf("not an aliasable command: %q", key)
-			}
-			result[aliasableCommand] = value
+	for key, value := range snapshot.AliasKeys() {
+		aliasableCommand, has := AliasableCommandForKey(key).Get()
+		if !has {
+			return result, fmt.Errorf("not an aliasable command: %q", key)
 		}
+		result[aliasableCommand] = value
 	}
 	return result, nil
 }
--- src/config/configdomain/single_snapshot.go
+++ src/config/configdomain/single_snapshot.go
@@ -2,3 +2,13 @@ package configdomain
 
 // SingleSnapshot contains all of the local or global Git metadata config settings.
 type SingleSnapshot map[Key]string
+
+func (self SingleSnapshot) AliasKeys() map[Key]string {
+	result := map[Key]string{}
+	for key, value := range self {
+		if key.IsAliasKey() {
+			result[key] = value
+		}
+	}
+	return result
+}
