085ef3dec4f942a6a0eb5dfe831cb07fdf774ee9
Kevin Goslar <kevin.goslar@gmail.com>
2024-08-02 13:11:20 -0500

    single_snapshot.go

--- src/config/configdomain/single_snapshot.go
+++ src/config/configdomain/single_snapshot.go
@@ -3,6 +3,7 @@ package configdomain
 // SingleSnapshot contains all of the local or global Git metadata config settings.
 type SingleSnapshot map[Key]string
 
+// provides all the keys that describe aliases for Git Town commands
 func (self SingleSnapshot) AliasKeys() map[Key]string {
 	result := map[Key]string{}
 	for key, value := range self {
@@ -13,6 +14,7 @@ func (self SingleSnapshot) AliasKeys() map[Key]string {
 	return result
 }
 
+// provides all the keys that describe lineage entries
 func (self SingleSnapshot) LineageKeys() map[Key]string {
 	result := map[Key]string{}
 	for key, value := range self {
