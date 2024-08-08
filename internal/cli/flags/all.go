db2f95c658c4d99d1a571b9e72c7c3ed6e2b426d
Kevin Goslar <kevin.goslar@gmail.com>
2024-08-08 17:38:41 -0500

    Newtype for syncing all branches (#3833)

--- internal/cli/flags/all.go
+++ internal/cli/flags/all.go
@@ -1,6 +1,29 @@
 package flags
 
-// type-safe access to the "--all" command-line flag
-func All() (AddFunc, ReadBoolFlagFunc) {
-	return Bool("all", "a", "sync all local branches", FlagTypeNonPersistent)
+import (
+	"fmt"
+
+	"github.com/git-town/git-town/v15/internal/config/configdomain"
+	"github.com/git-town/git-town/v15/internal/messages"
+	"github.com/spf13/cobra"
+)
+
+const allLong = "all"
+
+// type-safe access to the CLI arguments of type configdomain.SyncAllBranches
+func All() (AddFunc, ReadAllFlagFunc) {
+	addFlag := func(cmd *cobra.Command) {
+		cmd.PersistentFlags().BoolP(allLong, "a", false, "sync all local branches")
+	}
+	readFlag := func(cmd *cobra.Command) configdomain.SyncAllBranches {
+		value, err := cmd.Flags().GetBool(allLong)
+		if err != nil {
+			panic(fmt.Sprintf(messages.FlagStringDoesntExist, cmd.Name(), allLong))
+		}
+		return configdomain.SyncAllBranches(value)
+	}
+	return addFlag, readFlag
 }
+
+// the type signature for the function that reads the dry-run flag from the args to the given Cobra command
+type ReadAllFlagFunc func(*cobra.Command) configdomain.SyncAllBranches
