28d520d901fc066f0dc1127131a645605df22105
Kevin Goslar <kevin.goslar@gmail.com>
2024-08-08 07:12:27 -0500

    determine_remote_url_test.go

--- internal/config/confighelpers/determine_remote_url_test.go
+++ internal/config/confighelpers/determine_remote_url_test.go
@@ -25,6 +25,7 @@ func TestDetermineRemoteURL(t *testing.T) {
 		}
 		must.Eq(t, want, have)
 	})
+
 	t.Run("HTTPS URL", func(t *testing.T) {
 		t.Parallel()
 		have, has := confighelpers.DetermineRemoteURL("https://github.com/git-town/docs.git", configdomain.ParseHostingOriginHostname("")).Get()
@@ -37,6 +38,7 @@ func TestDetermineRemoteURL(t *testing.T) {
 		}
 		must.Eq(t, want, have)
 	})
+
 	t.Run("GitLab handbook repo on gitlab.com", func(t *testing.T) {
 		t.Parallel()
 		have, has := confighelpers.DetermineRemoteURL("git@gitlab.com:gitlab-com/www-gitlab-com.git", configdomain.ParseHostingOriginHostname("")).Get()
@@ -49,6 +51,7 @@ func TestDetermineRemoteURL(t *testing.T) {
 		}
 		must.Eq(t, want, have)
 	})
+
 	t.Run("GitLab repository inside a group", func(t *testing.T) {
 		t.Parallel()
 		have, has := confighelpers.DetermineRemoteURL("git@gitlab.com:gitlab-org/quality/triage-ops.git", configdomain.ParseHostingOriginHostname("")).Get()
@@ -61,6 +64,7 @@ func TestDetermineRemoteURL(t *testing.T) {
 		}
 		must.Eq(t, want, have)
 	})
+
 	t.Run("self-hosted GitLab server without URL override", func(t *testing.T) {
 		t.Parallel()
 		have, has := confighelpers.DetermineRemoteURL("git@self-hosted-gitlab.com:git-town/git-town.git", configdomain.ParseHostingOriginHostname("")).Get()
@@ -73,6 +77,7 @@ func TestDetermineRemoteURL(t *testing.T) {
 		}
 		must.Eq(t, want, have)
 	})
+
 	t.Run("self-hosted GitLab server with URL override", func(t *testing.T) {
 		t.Parallel()
 		have, has := confighelpers.DetermineRemoteURL("git@self-hosted-gitlab.com:git-town/git-town.git", configdomain.ParseHostingOriginHostname("override.com")).Get()
@@ -85,6 +90,7 @@ func TestDetermineRemoteURL(t *testing.T) {
 		}
 		must.Eq(t, want, have)
 	})
+
 	t.Run("custom SSH identity with hostname override", func(t *testing.T) {
 		t.Parallel()
 		have, has := confighelpers.DetermineRemoteURL("git@my-ssh-identity.com:git-town/git-town.git", configdomain.ParseHostingOriginHostname("gitlab.com")).Get()
