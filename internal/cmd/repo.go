410d3626de15b2259fbef38603d6219d9aa5cfc2
Kevin Goslar <kevin.goslar@gmail.com>
2024-08-08 07:09:42 -0500

    repo.go

--- internal/cmd/repo.go
+++ internal/cmd/repo.go
@@ -9,21 +9,23 @@ import (
 	"github.com/git-town/git-town/v15/internal/cmd/cmdhelpers"
 	"github.com/git-town/git-town/v15/internal/config/configdomain"
 	"github.com/git-town/git-town/v15/internal/execute"
-	"github.com/git-town/git-town/v15/internal/git/gitdomain"
+	. "github.com/git-town/git-town/v15/internal/gohacks/prelude"
+	"github.com/git-town/git-town/v15/internal/hosting"
+	"github.com/git-town/git-town/v15/internal/hosting/hostingdomain"
 	"github.com/spf13/cobra"
 )
 
 const repoDesc = "Open the repository homepage in the browser"
 
 const repoHelp = `
-Supported for repositories hosted on GitHub, GitLab, Gitea, and Bitbucket. Derives the Git provider from the "origin" remote. You can override this detection with "git config %s <DRIVER>" where DRIVER is "github", "gitlab", "gitea", or "bitbucket".
+Supported for repositories hosted on GitHub, GitLab, Gitea, and Bitbucket. Derives the Git provider from the given remote if one exists, otherwise the "origin" remote. You can override remote detection with "git config %s <DRIVER>" where DRIVER is "github", "gitlab", "gitea", or "bitbucket".
 
 When using SSH identities, run "git config %s <HOSTNAME>" where HOSTNAME matches what is in your ssh config file.`
 
 func repoCommand() *cobra.Command {
 	addVerboseFlag, readVerboseFlag := flags.Verbose()
 	cmd := cobra.Command{
-		Use:   "repo",
+		Use:   "repo [remote]",
 		Args:  cobra.MaximumNArgs(1),
 		Short: repoDesc,
 		Long:  cmdhelpers.Long(repoDesc, fmt.Sprintf(repoHelp, configdomain.KeyHostingPlatform, configdomain.KeyHostingOriginHostname)),
@@ -47,17 +49,37 @@ func executeRepo(args []string, verbose configdomain.Verbose) error {
 	if err != nil {
 		return err
 	}
-	var remoteName gitdomain.Remote
-	if len(args) == 0 {
-		remoteName = gitdomain.RemoteOrigin
-	} else {
-		remoteName = gitdomain.NewRemote(args[0])
-	}
-	url, err := repo.UnvalidatedConfig.GitConfig.RemoteURL(remoteName)
+	data, err := determineRepoData(args, repo)
 	if err != nil {
 		return err
 	}
-	browser.Open(url, repo.Frontend, repo.Backend)
+	browser.Open(data.connector.RepositoryURL(), repo.Frontend, repo.Backend)
 	print.Footer(verbose, repo.CommandsCounter.Get(), repo.FinalMessages.Result())
 	return nil
+}
+
+func determineRepoData(args []string, repo execute.OpenRepoResult) (data repoData, err error) {
+	var connectorOpt Option[hostingdomain.Connector]
+	if originURL, hasOriginURL := repo.UnvalidatedConfig.OriginURL().Get(); hasOriginURL {
+		connectorOpt, err = hosting.NewConnector(hosting.NewConnectorArgs{
+			Config:          repo.UnvalidatedConfig.Config.Get(),
+			HostingPlatform: repo.UnvalidatedConfig.Config.Value.HostingPlatform,
+			Log:             print.Logger{},
+			OriginURL:       originURL,
+		})
+		if err != nil {
+			return data, err
+		}
+	}
+	connector, hasConnector := connectorOpt.Get()
+	if !hasConnector {
+		return data, hostingdomain.UnsupportedServiceError()
+	}
+	return repoData{
+		connector: connector,
+	}, err
+}
+
+type repoData struct {
+	connector hostingdomain.Connector
 }
