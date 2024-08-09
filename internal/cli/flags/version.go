704225919d1801ae851b0756d01558a311b7282f
Kevin Goslar <kevin.goslar@gmail.com>
2024-08-08 21:25:59 -0500

    version.go

--- internal/cli/flags/version.go
+++ internal/cli/flags/version.go
@@ -7,11 +7,11 @@ import (
 	"github.com/spf13/cobra"
 )
 
-const versionLong = "display the version number"
+const versionLong = "version"
 
 func Version() (AddFunc, ReadBoolFlagFunc) {
 	addFlag := func(cmd *cobra.Command) {
-		cmd.PersistentFlags().BoolP(versionLong, "V", false, versionLong)
+		cmd.PersistentFlags().BoolP(versionLong, "V", false, "display the version number")
 	}
 	readFlag := func(cmd *cobra.Command) bool {
 		value, err := cmd.Flags().GetBool(versionLong)
