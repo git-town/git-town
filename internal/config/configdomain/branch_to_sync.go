7434384546c884f70b7d9ec9a6621ed3b00d6169
Kevin Goslar <kevin.goslar@gmail.com>
2024-08-20 15:35:23 -0500

    branch_to_sync.go

--- internal/config/configdomain/branch_to_sync.go
+++ internal/config/configdomain/branch_to_sync.go
@@ -5,7 +5,7 @@ import (
 	. "github.com/git-town/git-town/v15/pkg/prelude"
 )
 
-// all the information needed about a branch to sync
+// all the information needed to sync a branch
 type BranchToSync struct {
 	BranchInfo         gitdomain.BranchInfo
 	FirstCommitMessage Option[gitdomain.CommitMessage]
