a27c2d1127fd4a1d28f5cb8cccfa9569aae1973f
Kevin Goslar <kevin.goslar@gmail.com>
2024-08-23 17:23:09 -0500

    proposal_override.go

--- internal/hosting/hostingdomain/proposal_override.go
+++ internal/hosting/hostingdomain/proposal_override.go
@@ -3,7 +3,10 @@ package hostingdomain
 import "os"
 
 const (
-	OverrideKey        = "GIT_TOWN_TEST_PROPOSAL"
+	// the key under which the proposal API lookup override gets stored in the environment variables
+	OverrideKey = "GIT_TOWN_TEST_PROPOSAL"
+
+	// the content to use in the OverrideKey environment variable to simulate the API returning that no proposal exists
 	OverrideNoProposal = "(no proposal)"
 )
 
