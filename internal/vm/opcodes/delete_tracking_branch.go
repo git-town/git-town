3a954d295b81cf96f0f92bd6ab4d1d4a12140b2a
Kevin Goslar <kevin.goslar@gmail.com>
2024-08-28 14:45:08 -0500

    delete_tracking_branch.go

--- internal/vm/opcodes/delete_tracking_branch.go
+++ internal/vm/opcodes/delete_tracking_branch.go
@@ -12,5 +12,8 @@ type DeleteTrackingBranch struct {
 }
 
 func (self *DeleteTrackingBranch) Run(args shared.RunArgs) error {
+	// TODO: add AllowError setting and if true, ignore the error here
+	// this is useful when shipping via API
+	// since there is nothing the user can do to retry this in a meaningful way
 	return args.Git.DeleteTrackingBranch(args.Frontend, self.Branch)
 }
