49e816eb4bf69da400ed5e609eb952b37687334f
Kevin Goslar <kevin.goslar@gmail.com>
2024-08-28 10:18:26 -0500

    index_test.go

--- internal/gohacks/slice/index_test.go
+++ internal/gohacks/slice/index_test.go
@@ -10,11 +10,19 @@ import (
 
 func TestIndex(t *testing.T) {
 	t.Parallel()
+
 	t.Run("contains element", func(t *testing.T) {
 		t.Parallel()
 		haystack := []int{1, 2, 3}
 		have := slice.Index(haystack, 2)
 		want := Some(1)
+		must.Eq(t, want, have)
+	})
+	t.Run("does not contain the element", func(t *testing.T) {
+		t.Parallel()
+		haystack := []int{1, 2, 3}
+		have := slice.Index(haystack, 4)
+		want := None[int]()
 		must.Eq(t, want, have)
 	})
 }
