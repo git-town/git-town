0a152b5dc65698a36cf6ae565fbbd8f3f46dd4bf
Kevin Goslar <kevin.goslar@gmail.com>
2024-09-03 13:32:37 -0500

    perennial_regex.go verified_regex.go

--- internal/config/configdomain/perennial_regex.go
+++ internal/config/configdomain/perennial_regex.go
@@ -1,31 +1,15 @@
 package configdomain
 
-import (
-	"regexp"
+import . "github.com/git-town/git-town/v16/pkg/prelude"
 
-	"github.com/git-town/git-town/v16/internal/git/gitdomain"
-	. "github.com/git-town/git-town/v16/pkg/prelude"
-)
-
-// PerennialRegex contains the "branches.perennial-regex" setting.
 type PerennialRegex struct {
-	regex *regexp.Regexp
-	text  string
-}
-
-// MatchesBranch indicates whether the given branch matches this PerennialRegex.
-func (self PerennialRegex) MatchesBranch(branch gitdomain.LocalBranchName) bool {
-	return self.regex.MatchString(branch.String())
-}
-
-func (self PerennialRegex) String() string {
-	return self.text
+	VerifiedRegex
 }
 
 func ParsePerennialRegex(value string) (Option[PerennialRegex], error) {
-	if value == "" {
-		return None[PerennialRegex](), nil
+	verifiedRegexOpt, err := parseRegex(value)
+	if verifiedRegex, hasVerifiedRegex := verifiedRegexOpt.Get(); hasVerifiedRegex {
+		return Some(PerennialRegex{VerifiedRegex: verifiedRegex}), err
 	}
-	re, err := regexp.Compile(value)
-	return Some(PerennialRegex{regex: re, text: value}), err
+	return None[PerennialRegex](), err
 }
---
+++ internal/config/configdomain/verified_regex.go
@@ -0,0 +1,31 @@
+package configdomain
+
+import (
+	"regexp"
+
+	"github.com/git-town/git-town/v16/internal/git/gitdomain"
+	. "github.com/git-town/git-town/v16/pkg/prelude"
+)
+
+// a regular expression in the Git Town configuration
+type VerifiedRegex struct {
+	regex *regexp.Regexp
+	text  string
+}
+
+// MatchesBranch indicates whether the given branch matches this VerifiedRegex.
+func (self VerifiedRegex) MatchesBranch(branch gitdomain.LocalBranchName) bool {
+	return self.regex.MatchString(branch.String())
+}
+
+func (self VerifiedRegex) String() string {
+	return self.text
+}
+
+func parseRegex(value string) (Option[VerifiedRegex], error) {
+	if value == "" {
+		return None[VerifiedRegex](), nil
+	}
+	re, err := regexp.Compile(value)
+	return Some(VerifiedRegex{regex: re, text: value}), err
+}
