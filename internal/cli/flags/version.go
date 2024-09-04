78b7294ddbd0e6bf96206633d05f4bfb4fd45cfe
Kevin Goslar <kevin.goslar@gmail.com>
2024-09-04 10:18:16 -0500

    Only the root command supports the version flag now

--- internal/cli/flags/version.go
+++ internal/cli/flags/version.go
@@ -8,7 +8,7 @@ const versionLong = "version"
 
 func Version() (AddFunc, ReadBoolFlagFunc) {
 	addFlag := func(cmd *cobra.Command) {
-		cmd.PersistentFlags().BoolP(versionLong, "V", false, "display the version number")
+		cmd.Flags().BoolP(versionLong, "V", false, "display the version number")
 	}
 	readFlag := func(cmd *cobra.Command) bool {
 		value, err := cmd.Flags().GetBool(versionLong)
