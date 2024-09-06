d4273ebf3f1b5426bf5edfa29fbdabe96510faea
Kevin Goslar <kevin.goslar@gmail.com>
2024-09-06 13:10:38 -0500

    save_test.go

--- internal/config/configfile/save_test.go
+++ internal/config/configfile/save_test.go
@@ -282,8 +282,8 @@ main = "main"
 # See also the "perennial-regex" setting.
 perennials = []
 
-# Branches matching this regular expression are treated as feature branches.
-# This setting is effective only when used together with the "default-branch-type" setting.
+# All branches whose name matches this regular expression
+# are also considered perennial branches.
 #
 # If you are not sure, leave this empty.
 perennial-regex = ""
