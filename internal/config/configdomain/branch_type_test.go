17dd59bb6b8c6a070f6a5dd991246e0a9a5215e3
Kevin Goslar <kevin.goslar@gmail.com>
2024-09-06 20:32:21 -0500

    branch_type_test.go

--- internal/config/configdomain/branch_type_test.go
+++ internal/config/configdomain/branch_type_test.go
@@ -10,6 +10,7 @@ import (
 
 func TestBranchType(t *testing.T) {
 	t.Parallel()
+
 	t.Run("ParseBranchType", func(t *testing.T) {
 		t.Parallel()
 		tests := map[string]Option[configdomain.BranchType]{
