4a9814155623907ad2b7b4edbedb0d0bc32ee3e7
Kevin Goslar <kevin.goslar@gmail.com>
2024-09-11 16:36:19 -0500

    eueu

---
+++ internal/cli/flags/display_types.go
@@ -0,0 +1,26 @@
+package flags
+
+import (
+	"github.com/git-town/git-town/v16/internal/config/configdomain"
+	"github.com/spf13/cobra"
+)
+
+const displayTypesLong = "details"
+
+// type-safe access to the CLI arguments of type configdomain.DryRun
+func DryRun() (AddFunc, ReadDryRunFlagFunc) {
+	addFlag := func(cmd *cobra.Command) {
+		cmd.Flags().BoolP(dryRunLong, "", false, "print but do not run the Git commands")
+	}
+	readFlag := func(cmd *cobra.Command) configdomain.DryRun {
+		value, err := cmd.Flags().GetBool(dryRunLong)
+		if err != nil {
+			panic(err)
+		}
+		return configdomain.DryRun(value)
+	}
+	return addFlag, readFlag
+}
+
+// the type signature for the function that reads the dry-run flag from the args to the given Cobra command
+type ReadDryRunFlagFunc func(*cobra.Command) configdomain.DryRun
