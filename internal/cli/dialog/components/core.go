0770e36dbbc40cedec305e5e738ff0d8e3a80bf2
Kevin Goslar <kevin.goslar@gmail.com>
2024-09-15 15:47:53 -0500

    core.go

--- internal/cli/dialog/components/core.go
+++ internal/cli/dialog/components/core.go
@@ -1,4 +1,4 @@
-// Package dialog contains shared components to build full dialog screens out of.
+// Package components contains shared components to build full dialog screens out of.
 package components
 
 import tea "github.com/charmbracelet/bubbletea"
