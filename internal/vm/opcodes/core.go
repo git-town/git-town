5dd6ea4b32f2faa8c9ceab3f3639bd57cdc7ef0d
Kevin Goslar <kevin.goslar@gmail.com>
2024-09-15 15:35:54 -0500

    core.go core.go core.go

--- internal/vm/opcodes/core.go
+++ internal/vm/opcodes/core.go
@@ -1,4 +1,4 @@
-// Package opcode defines the individual operations that the Git Town VM can execute.
+// Package opcodes defines the individual operations that the Git Town VM can execute.
 // All opcodes implement the shared.Opcode interface.
 package opcodes
 
