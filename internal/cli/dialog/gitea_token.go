186368730f0cb4b98ad364c2f52595c59441bfb7
Kevin Goslar <kevin.goslar@gmail.com>
2024-09-23 15:29:33 +0000

    Improve dialog wording (#4051)

--- internal/cli/dialog/gitea_token.go
+++ internal/cli/dialog/gitea_token.go
@@ -12,11 +12,11 @@ import (
 const (
 	giteaTokenTitle = `Gitea API token`
 	giteaTokenHelp  = `
-If you have an API token for Gitea,
-and want to ship branches from the CLI,
-please enter it now.
+Git Town can update pull requests and ship branches on gitea for you.
+To enable this, please enter a gitea API token.
+More info at https://www.git-town.com/preferences/gitea-token.
 
-It's okay to leave this empty.
+If you leave this empty, Git Town will not use the gitea API.
 
 `
 )
