45fa52127e8db9fcd79be831ab92fd4b8a6b45ee
Kevin Goslar <kevin.goslar@gmail.com>
2024-09-27 06:41:26 -0500

    sync_branches.go

--- internal/sync/sync_branches.go
+++ internal/sync/sync_branches.go
@@ -1,21 +1,23 @@
 package sync
 
-import (
-	"github.com/git-town/git-town/v16/internal/config/configdomain"
-	"github.com/git-town/git-town/v16/internal/git/gitdomain"
-	. "github.com/git-town/git-town/v16/pkg/prelude"
-)
+import "github.com/git-town/git-town/v16/internal/config/configdomain"
 
 // BranchesProgram syncs all given branches.
 func BranchesProgram(args BranchesProgramArgs) {
+	for _, branchToSync := range args.BranchesToSync {
+		BranchProgram(branchToSync.BranchInfo, BranchProgramArgs{
+			BranchInfos:        args.BranchInfos,
+			Config:             args.Config,
+			FirstCommitMessage: branchToSync.FirstCommitMessage,
+			InitialBranch:      args.InitialBranch,
+			Remotes:            args.Remotes,
+			Program:            args.Program,
+			PushBranches:       args.PushBranches,
+		})
+	}
 }
 
 type BranchesProgramArgs struct {
 	BranchProgramArgs
 	BranchesToSync []configdomain.BranchToSync
-	DryRun         configdomain.DryRun
-	HasOpenChanges bool
-	InitialBranch  gitdomain.LocalBranchName
-	PreviousBranch Option[gitdomain.LocalBranchName]
-	ShouldPushTags bool
 }
