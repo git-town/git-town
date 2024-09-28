d45278df1785364576776e5d695ccb5e5b24d685
Kevin Goslar <kevin.goslar@gmail.com>
2024-09-27 20:33:54 -0500

    checkout_if_needed.go

--- internal/vm/opcodes/checkout_if_needed.go
+++ internal/vm/opcodes/checkout_if_needed.go
@@ -19,6 +19,5 @@ func (self *CheckoutIfNeeded) Run(args shared.RunArgs) error {
 	if existingBranch == self.Branch {
 		return nil
 	}
-	args.PrependOpcodes(&Checkout{Branch: self.Branch})
-	return nil
+	return args.Git.CheckoutBranch(args.Frontend, self.Branch, false)
 }
