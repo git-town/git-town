f3b163540fe1e1e8a2c5bae428ebda61d348162b
Kevin Goslar <kevin.goslar@gmail.com>
2024-09-27 21:01:43 -0500

    core.go merge_branch_no_edit.go merge_parent.go

--- internal/vm/opcodes/core.go
+++ internal/vm/opcodes/core.go
@@ -86,6 +86,7 @@ func Types() []shared.Opcode {
 		&ForcePushCurrentBranch{},
 		&DeleteBranchIfEmptyAtRuntime{},
 		&Merge{},
+		&MergeBranchNoEdit{},
 		&MergeParent{},
 		&PreserveCheckoutHistory{},
 		&PullCurrentBranch{},
