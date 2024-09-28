ca2ae1126964f912712fd7806a5035161c30521b
Kevin Goslar <kevin.goslar@gmail.com>
2024-09-27 20:51:17 -0500

    core.go

--- internal/vm/opcodes/core.go
+++ internal/vm/opcodes/core.go
@@ -67,6 +67,7 @@ func Types() []shared.Opcode {
 		&CommitNoEdit{},
 		&ConnectorMergeProposal{},
 		&ContinueMerge{},
+		&ContinueRebase{},
 		&ContinueRebaseIfNeeded{},
 		&CreateAndCheckoutBranchExistingParent{},
 		&CreateBranch{},
