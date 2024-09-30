34765460ff73008fd2be5141399ee7d94db98125
Kevin Goslar <kevin.goslar@gmail.com>
2024-09-30 15:30:26 -0500

    reset_current_branch_to_sha.go

--- internal/vm/opcodes/reset_current_branch_to_sha.go
+++ internal/vm/opcodes/reset_current_branch_to_sha.go
@@ -5,7 +5,7 @@ import (
 	"github.com/git-town/git-town/v16/internal/vm/shared"
 )
 
-// ResetCurrentBranchToSHAIfNeeded undoes all commits on the current branch
+// ResetCurrentBranchToSHA undoes all commits on the current branch
 // all the way until the given SHA.
 type ResetCurrentBranchToSHA struct {
 	Hard                    bool
