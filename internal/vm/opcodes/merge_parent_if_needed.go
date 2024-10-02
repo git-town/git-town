61fecf139e66748a2407f05a12c33dc17471d99e
Kevin Goslar <kevin.goslar@gmail.com>
2024-10-02 17:39:32 -0500

    merge_parent_if_needed.go [skip ci]

--- internal/vm/opcodes/merge_parent_if_needed.go
+++ internal/vm/opcodes/merge_parent_if_needed.go
@@ -7,7 +7,7 @@ import (
 	"github.com/git-town/git-town/v16/internal/vm/shared"
 )
 
-// MergeParent merges the branch that at runtime is the parent branch of the given branch into the given branch.
+// merges the branch that at runtime is the parent branch of the given branch into the given branch
 type MergeParentIfNeeded struct {
 	Branch                  gitdomain.LocalBranchName
 	undeclaredOpcodeMethods `exhaustruct:"optional"`
