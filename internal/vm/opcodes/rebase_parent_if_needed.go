27154c08fe40c519ea19d68f7407c0e90e55a624
Kevin Goslar <kevin.goslar@gmail.com>
2024-10-02 18:09:45 -0500

    rebase_parent_if_needed.go [skip ci]

--- internal/vm/opcodes/rebase_parent_if_needed.go
+++ internal/vm/opcodes/rebase_parent_if_needed.go
@@ -35,7 +35,7 @@ func (self *RebaseParentIfNeeded) Run(args shared.RunArgs) error {
 			})
 			break
 		}
-		// here the parent isn't local --> sync with its tracking branch, then also sync the grandparent until we find a local ancestor
+		// here the parent isn't local --> sync with its tracking branch, then try again with the grandparent until we find a local ancestor
 		parentTrackingName := parent.AtRemote(gitdomain.RemoteOrigin)
 		program = append(program, &RebaseBranch{
 			Branch: parentTrackingName.BranchName(),
