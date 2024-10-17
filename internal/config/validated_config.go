cb1de51347bb950aff21784d45ccefc10f8ffcd3
Kevin Goslar <kevin.goslar@gmail.com>
2024-10-17 11:45:33 -0500

    validated_config.go

--- internal/config/validated_config.go
+++ internal/config/validated_config.go
@@ -2,6 +2,7 @@ package config
 
 import (
 	"github.com/git-town/git-town/v16/internal/config/configdomain"
+	. "github.com/git-town/git-town/v16/pkg/prelude"
 )
 
 // Config provides type-safe access to Git Town configuration settings
@@ -19,6 +20,15 @@ func (self *ValidatedConfig) Reload() {
 	_, self.GlobalGitConfig, _ = self.GitConfig.LoadGlobal(false) // we ignore the Git cache here because reloading a config in the middle of a Git Town command doesn't change the cached initial state of the repo
 	_, self.LocalGitConfig, _ = self.GitConfig.LoadLocal(false)   // we ignore the Git cache here because reloading a config in the middle of a Git Town command doesn't change the cached initial state of the repo
 	unvalidateConfig := configdomain.NewUnvalidatedConfig(self.ConfigFile, self.GlobalGitConfig, self.LocalGitConfig)
+	self.UnvalidatedConfig = &UnvalidatedConfig{
+		Config:          NewMutable(&unvalidateConfig),
+		ConfigFile:      self.ConfigFile,
+		DryRun:          self.DryRun,
+		GitConfig:       self.GitConfig,
+		GitVersion:      self.GitVersion,
+		GlobalGitConfig: self.GlobalGitConfig,
+		LocalGitConfig:  self.LocalGitConfig,
+	}
 	self.Config = configdomain.ValidatedConfig{
 		UnvalidatedConfig: &unvalidateConfig,
 		GitUserEmail:      self.Config.GitUserEmail,
