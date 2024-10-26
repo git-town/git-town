3c0d5b45b02a066681ad3a951542daca0c0cca6d
Kevin Goslar <kevin.goslar@gmail.com>
2024-10-26 16:01:55 -0500

    test_commands.go

--- test/commands/test_commands.go
+++ test/commands/test_commands.go
@@ -278,6 +278,22 @@ func (self *TestCommands) FilesInCommit(sha gitdomain.SHA) []string {
 	return strings.Split(output, "\n")
 }
 
+func (self *TestCommands) GitConfig(scope configdomain.ConfigScope, name configdomain.Key) Option[string] {
+	args := []string{"config"}
+	switch scope {
+	case configdomain.ConfigScopeGlobal:
+		args = append(args, "--global")
+	case configdomain.ConfigScopeLocal:
+		args = append(args, "--local")
+	}
+	args = append(args, "--get", name.String())
+	output, err := self.Query("git", args...)
+	if err != nil {
+		return None[string]()
+	}
+	return Some(output)
+}
+
 // HasBranchesOutOfSync indicates whether one or more local branches are out of sync with their tracking branch.
 func (self *TestCommands) HasBranchesOutOfSync() (bool, string) {
 	output := self.MustQuery("git", "for-each-ref", "--format=%(refname:short) %(upstream:track)", "refs/heads")
@@ -340,22 +356,6 @@ func (self *TestCommands) LocalBranchesMainFirst(mainBranch gitdomain.LocalBranc
 	}
 	branches = slice.Hoist(branches, mainBranch)
 	return branches, nil
-}
-
-func (self *TestCommands) GitConfig(scope configdomain.ConfigScope, name configdomain.Key) Option[string] {
-	args := []string{"config"}
-	switch scope {
-	case configdomain.ConfigScopeGlobal:
-		args = append(args, "--global")
-	case configdomain.ConfigScopeLocal:
-		args = append(args, "--local")
-	}
-	args = append(args, "--get", name.String())
-	output, err := self.Query("git", args...)
-	if err != nil {
-		return None[string]()
-	}
-	return Some(output)
 }
 
 func (self *TestCommands) MergeBranch(branch gitdomain.LocalBranchName) error {
