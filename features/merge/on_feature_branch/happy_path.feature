08498e9d5fab94b99bd97509e57c11f27d0740bd
Kevin Goslar <kevin.goslar@gmail.com>
2024-11-04 09:17:12 -0600

    happy_path.feature branch.feature parent.feature core.go merge.go

---
+++ features/merge/on_feature_branch/happy_path.feature
@@ -0,0 +1,40 @@
+Feature: merging a branch in a stack with its parent
+
+  Background:
+    Given a Git repo with origin
+    And the branches
+      | NAME  | TYPE    | PARENT | LOCATIONS     |
+      | alpha | feature | main   | local, origin |
+      | beta  | feature | alpha  | local, origin |
+    And the commits
+      | BRANCH | LOCATION      | MESSAGE      |
+      | alpha  | local, origin | alpha commit |
+      | beta   | local, origin | beta commit  |
+    And the current branch is "beta"
+    And Git Town setting "sync-feature-strategy" is "merge"
+    When I run "git-town merge"
+
+  @this
+  Scenario: result
+    Then it runs the commands
+      | BRANCH | COMMAND             |
+      | beta   | git merge alpha     |
+      |        | git branch -D alpha |
+    And the current branch is still "beta"
+    And this lineage exists now
+      | BRANCH   | PARENT   |
+      | existing | main     |
+      | new      | existing |
+    And these commits exist now
+      | BRANCH  | LOCATION      | MESSAGE     | FILE NAME | FILE CONTENT |
+      | feature | local, origin | the feature | file      | content 3    |
+
+  Scenario: undo
+    When I run "git-town undo"
+    Then it runs the commands
+      | BRANCH   | COMMAND               |
+      | new      | git checkout existing |
+      | existing | git branch -D new     |
+    And the current branch is now "existing"
+    And the initial commits exist now
+    And the initial lineage exists now
---
+++ internal/cmd/merge.go
@@ -0,0 +1,166 @@
+package cmd
+
+import (
+	"errors"
+	"fmt"
+
+	"github.com/git-town/git-town/v16/internal/cli/flags"
+	"github.com/git-town/git-town/v16/internal/cmd/cmdhelpers"
+	"github.com/git-town/git-town/v16/internal/config"
+	"github.com/git-town/git-town/v16/internal/config/configdomain"
+	"github.com/git-town/git-town/v16/internal/execute"
+	"github.com/git-town/git-town/v16/internal/git/gitdomain"
+	"github.com/git-town/git-town/v16/internal/messages"
+	configInterpreter "github.com/git-town/git-town/v16/internal/vm/interpreter/config"
+	. "github.com/git-town/git-town/v16/pkg/prelude"
+	"github.com/spf13/cobra"
+)
+
+const mergeDesc = "Merges the current branch in a stack with its parent"
+
+const mergeHelp = `
+Merges the current branch with its parent branch.
+Both branches must be feature branches and be in sync with their tracking branches.
+`
+
+func mergeCmd() *cobra.Command {
+	addVerboseFlag, readVerboseFlag := flags.Verbose()
+	cmd := cobra.Command{
+		Use:     "merge",
+		Args:    cobra.NoArgs,
+		GroupID: "refactoring",
+		Short:   mergeDesc,
+		Long:    cmdhelpers.Long(mergeDesc, mergeHelp),
+		RunE: func(cmd *cobra.Command, args []string) error {
+			return executeMerge(args, readVerboseFlag(cmd))
+		},
+	}
+	addVerboseFlag(&cmd)
+	return &cmd
+}
+
+func executeMerge(args []string, verbose configdomain.Verbose) error {
+	repo, err := execute.OpenRepo(execute.OpenRepoArgs{
+		DryRun:           false,
+		PrintBranchNames: false,
+		PrintCommands:    true,
+		ValidateGitRepo:  true,
+		ValidateIsOnline: false,
+		Verbose:          verbose,
+	})
+	if err != nil {
+		return err
+	}
+	data, err := determineObserveData(args, repo)
+	if err != nil {
+		return err
+	}
+	err = validateObserveData(data)
+	if err != nil {
+		return err
+	}
+	branchNames := data.branchesToObserve.Keys()
+	if err = repo.UnvalidatedConfig.NormalConfig.AddToObservedBranches(branchNames...); err != nil {
+		return err
+	}
+	if err = removeNonObserveBranchTypes(data.branchesToObserve, repo.UnvalidatedConfig); err != nil {
+		return err
+	}
+	printObservedBranches(branchNames)
+	if checkout, hasCheckout := data.checkout.Get(); hasCheckout {
+		if err = repo.Git.CheckoutBranch(repo.Frontend, checkout, false); err != nil {
+			return err
+		}
+	}
+	return configInterpreter.Finished(configInterpreter.FinishedArgs{
+		Backend:               repo.Backend,
+		BeginBranchesSnapshot: Some(data.branchesSnapshot),
+		BeginConfigSnapshot:   repo.ConfigSnapshot,
+		Command:               "observe",
+		CommandsCounter:       repo.CommandsCounter,
+		FinalMessages:         repo.FinalMessages,
+		Git:                   repo.Git,
+		RootDir:               repo.RootDir,
+		TouchedBranches:       branchNames.BranchNames(),
+		Verbose:               verbose,
+	})
+}
+
+type observeData struct {
+	branchInfos       gitdomain.BranchInfos
+	branchesSnapshot  gitdomain.BranchesSnapshot
+	branchesToObserve configdomain.BranchesAndTypes
+	checkout          Option[gitdomain.LocalBranchName]
+}
+
+func printObservedBranches(branches gitdomain.LocalBranchNames) {
+	for _, branch := range branches {
+		fmt.Printf(messages.ObservedBranchIsNowObserved, branch)
+	}
+}
+
+func removeNonObserveBranchTypes(branches configdomain.BranchesAndTypes, config config.UnvalidatedConfig) error {
+	for branchName, branchType := range branches {
+		switch branchType {
+		case configdomain.BranchTypeContributionBranch:
+			if err := config.NormalConfig.RemoveFromContributionBranches(branchName); err != nil {
+				return err
+			}
+		case configdomain.BranchTypeParkedBranch:
+			if err := config.NormalConfig.RemoveFromParkedBranches(branchName); err != nil {
+				return err
+			}
+		case configdomain.BranchTypePrototypeBranch:
+			if err := config.NormalConfig.RemoveFromPrototypeBranches(branchName); err != nil {
+				return err
+			}
+		case
+			configdomain.BranchTypeFeatureBranch,
+			configdomain.BranchTypeObservedBranch,
+			configdomain.BranchTypeMainBranch,
+			configdomain.BranchTypePerennialBranch:
+		}
+	}
+	return nil
+}
+
+func determineObserveData(args []string, repo execute.OpenRepoResult) (observeData, error) {
+	branchesSnapshot, err := repo.Git.BranchesSnapshot(repo.Backend)
+	if err != nil {
+		return observeData{}, err
+	}
+	branchesToObserve, branchToCheckout, err := execute.BranchesToMark(args, branchesSnapshot, repo.UnvalidatedConfig)
+	return observeData{
+		branchInfos:       branchesSnapshot.Branches,
+		branchesSnapshot:  branchesSnapshot,
+		branchesToObserve: branchesToObserve,
+		checkout:          branchToCheckout,
+	}, err
+}
+
+func validateObserveData(data observeData) error {
+	for branchName, branchType := range data.branchesToObserve {
+		switch branchType {
+		case configdomain.BranchTypeMainBranch:
+			return errors.New(messages.MainBranchCannotObserve)
+		case configdomain.BranchTypePerennialBranch:
+			return errors.New(messages.PerennialBranchCannotObserve)
+		case configdomain.BranchTypeObservedBranch:
+			return fmt.Errorf(messages.BranchIsAlreadyObserved, branchName)
+		case
+			configdomain.BranchTypeFeatureBranch,
+			configdomain.BranchTypeContributionBranch,
+			configdomain.BranchTypeParkedBranch,
+			configdomain.BranchTypePrototypeBranch:
+		}
+		hasLocalBranch := data.branchInfos.HasLocalBranch(branchName)
+		hasRemoteBranch := data.branchInfos.HasMatchingTrackingBranchFor(branchName)
+		if !hasLocalBranch && !hasRemoteBranch {
+			return fmt.Errorf(messages.BranchDoesntExist, branchName)
+		}
+		if hasLocalBranch && !hasRemoteBranch {
+			return fmt.Errorf(messages.ObserveBranchIsLocal, branchName)
+		}
+	}
+	return nil
+}
