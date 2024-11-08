6faa21afb5213ba4682b51e9940ff9002b79d60e
Kevin Goslar <kevin.goslar@gmail.com>
2024-11-08 16:38:55 -0600

    connector.go

--- internal/hosting/gitlab/connector.go
+++ internal/hosting/gitlab/connector.go
@@ -29,99 +29,21 @@ func (self Connector) FindProposalFn() Option[func(branch, target gitdomain.Loca
 	if self.Data.APIToken.IsNone() && len(hostingdomain.ReadProposalOverride()) == 0 {
 		return None[func(branch, target gitdomain.LocalBranchName) (Option[hostingdomain.Proposal], error)]()
 	}
-	return Some(func(branch, target gitdomain.LocalBranchName) (Option[hostingdomain.Proposal], error) {
-		self.log.Start(messages.APIProposalLookupStart)
-		proposalURLOverride := hostingdomain.ReadProposalOverride()
-		if len(proposalURLOverride) > 0 {
-			self.log.Ok()
-			if proposalURLOverride == hostingdomain.OverrideNoProposal {
-				return None[hostingdomain.Proposal](), nil
-			}
-			return Some(hostingdomain.Proposal{
-				MergeWithAPI: true,
-				Number:       123,
-				Source:       branch,
-				Target:       target,
-				Title:        "title",
-				URL:          proposalURLOverride,
-			}), nil
-		}
-		opts := &gitlab.ListProjectMergeRequestsOptions{
-			State:        gitlab.Ptr("opened"),
-			SourceBranch: gitlab.Ptr(branch.String()),
-			TargetBranch: gitlab.Ptr(target.String()),
-		}
-		mergeRequests, _, err := self.client.MergeRequests.ListProjectMergeRequests(self.projectPath(), opts)
-		if err != nil {
-			self.log.Failed(err.Error())
-			return None[hostingdomain.Proposal](), err
-		}
-		switch len(mergeRequests) {
-		case 0:
-			self.log.Success("none")
-			return None[hostingdomain.Proposal](), nil
-		case 1:
-			proposal := parseMergeRequest(mergeRequests[0])
-			self.log.Success(strconv.Itoa(proposal.Number))
-			return Some(proposal), nil
-		default:
-			return None[hostingdomain.Proposal](), fmt.Errorf(messages.ProposalMultipleFromToFound, len(mergeRequests), branch, target)
-		}
-	})
+	return Some(self.findProposal)
 }
 
 func (self Connector) SearchProposalFn() Option[func(branch gitdomain.LocalBranchName) (Option[hostingdomain.Proposal], error)] {
 	if self.APIToken.IsNone() {
 		return None[func(branch gitdomain.LocalBranchName) (Option[hostingdomain.Proposal], error)]()
 	}
-	return Some(func(branch gitdomain.LocalBranchName) (Option[hostingdomain.Proposal], error) {
-		self.log.Start(messages.APIParentBranchLookupStart, branch.String())
-		opts := &gitlab.ListProjectMergeRequestsOptions{
-			State:        gitlab.Ptr("opened"),
-			SourceBranch: gitlab.Ptr(branch.String()),
-		}
-		mergeRequests, _, err := self.client.MergeRequests.ListProjectMergeRequests(self.projectPath(), opts)
-		if err != nil {
-			self.log.Failed(err.Error())
-			return None[hostingdomain.Proposal](), err
-		}
-		switch len(mergeRequests) {
-		case 0:
-			self.log.Success("none")
-			return None[hostingdomain.Proposal](), nil
-		case 1:
-			proposal := parseMergeRequest(mergeRequests[0])
-			self.log.Success(proposal.Target.String())
-			return Some(proposal), nil
-		default:
-			return None[hostingdomain.Proposal](), fmt.Errorf(messages.ProposalMultipleFromFound, len(mergeRequests), branch)
-		}
-	})
+	return Some(self.searchProposal)
 }
 
 func (self Connector) SquashMergeProposalFn() Option[func(number int, message gitdomain.CommitMessage) error] {
 	if self.APIToken.IsNone() {
 		return None[func(number int, message gitdomain.CommitMessage) error]()
 	}
-	return Some(func(number int, message gitdomain.CommitMessage) error {
-		if number <= 0 {
-			return errors.New(messages.ProposalNoNumberGiven)
-		}
-		self.log.Start(messages.HostingGitlabMergingViaAPI, number)
-		// the GitLab API wants the full commit message in the body
-		_, _, err := self.client.MergeRequests.AcceptMergeRequest(self.projectPath(), number, &gitlab.AcceptMergeRequestOptions{
-			SquashCommitMessage: gitlab.Ptr(message.String()),
-			Squash:              gitlab.Ptr(true),
-			// the branch will be deleted by Git Town
-			ShouldRemoveSourceBranch: gitlab.Ptr(false),
-		})
-		if err != nil {
-			self.log.Failed(err.Error())
-			return err
-		}
-		self.log.Ok()
-		return nil
-	})
+	return Some(self.squashMergeProposal)
 }
 
 func (self Connector) UpdateProposalSourceFn() Option[func(number int, _ gitdomain.LocalBranchName, finalMessages stringslice.Collector) error] {
@@ -132,18 +54,104 @@ func (self Connector) UpdateProposalTargetFn() Option[func(number int, target gi
 	if self.APIToken.IsNone() {
 		return None[func(number int, target gitdomain.LocalBranchName, _ stringslice.Collector) error]()
 	}
-	return Some(func(number int, target gitdomain.LocalBranchName, _ stringslice.Collector) error {
-		self.log.Start(messages.HostingGitlabUpdateMRViaAPI, number, target)
-		_, _, err := self.client.MergeRequests.UpdateMergeRequest(self.projectPath(), number, &gitlab.UpdateMergeRequestOptions{
-			TargetBranch: gitlab.Ptr(target.String()),
-		})
-		if err != nil {
-			self.log.Failed(err.Error())
-			return err
-		}
+	return Some(self.updateProposalTarget)
+}
+
+func (self Connector) findProposal(branch, target gitdomain.LocalBranchName) (Option[hostingdomain.Proposal], error) {
+	self.log.Start(messages.APIProposalLookupStart)
+	proposalURLOverride := hostingdomain.ReadProposalOverride()
+	if len(proposalURLOverride) > 0 {
 		self.log.Ok()
-		return nil
+		if proposalURLOverride == hostingdomain.OverrideNoProposal {
+			return None[hostingdomain.Proposal](), nil
+		}
+		return Some(hostingdomain.Proposal{
+			MergeWithAPI: true,
+			Number:       123,
+			Source:       branch,
+			Target:       target,
+			Title:        "title",
+			URL:          proposalURLOverride,
+		}), nil
+	}
+	opts := &gitlab.ListProjectMergeRequestsOptions{
+		State:        gitlab.Ptr("opened"),
+		SourceBranch: gitlab.Ptr(branch.String()),
+		TargetBranch: gitlab.Ptr(target.String()),
+	}
+	mergeRequests, _, err := self.client.MergeRequests.ListProjectMergeRequests(self.projectPath(), opts)
+	if err != nil {
+		self.log.Failed(err.Error())
+		return None[hostingdomain.Proposal](), err
+	}
+	switch len(mergeRequests) {
+	case 0:
+		self.log.Success("none")
+		return None[hostingdomain.Proposal](), nil
+	case 1:
+		proposal := parseMergeRequest(mergeRequests[0])
+		self.log.Success(strconv.Itoa(proposal.Number))
+		return Some(proposal), nil
+	default:
+		return None[hostingdomain.Proposal](), fmt.Errorf(messages.ProposalMultipleFromToFound, len(mergeRequests), branch, target)
+	}
+}
+
+func (self Connector) searchProposal(branch gitdomain.LocalBranchName) (Option[hostingdomain.Proposal], error) {
+	self.log.Start(messages.APIParentBranchLookupStart, branch.String())
+	opts := &gitlab.ListProjectMergeRequestsOptions{
+		State:        gitlab.Ptr("opened"),
+		SourceBranch: gitlab.Ptr(branch.String()),
+	}
+	mergeRequests, _, err := self.client.MergeRequests.ListProjectMergeRequests(self.projectPath(), opts)
+	if err != nil {
+		self.log.Failed(err.Error())
+		return None[hostingdomain.Proposal](), err
+	}
+	switch len(mergeRequests) {
+	case 0:
+		self.log.Success("none")
+		return None[hostingdomain.Proposal](), nil
+	case 1:
+		proposal := parseMergeRequest(mergeRequests[0])
+		self.log.Success(proposal.Target.String())
+		return Some(proposal), nil
+	default:
+		return None[hostingdomain.Proposal](), fmt.Errorf(messages.ProposalMultipleFromFound, len(mergeRequests), branch)
+	}
+}
+
+func (self Connector) squashMergeProposal(number int, message gitdomain.CommitMessage) error {
+	if number <= 0 {
+		return errors.New(messages.ProposalNoNumberGiven)
+	}
+	self.log.Start(messages.HostingGitlabMergingViaAPI, number)
+	// the GitLab API wants the full commit message in the body
+	_, _, err := self.client.MergeRequests.AcceptMergeRequest(self.projectPath(), number, &gitlab.AcceptMergeRequestOptions{
+		SquashCommitMessage: gitlab.Ptr(message.String()),
+		Squash:              gitlab.Ptr(true),
+		// the branch will be deleted by Git Town
+		ShouldRemoveSourceBranch: gitlab.Ptr(false),
 	})
+	if err != nil {
+		self.log.Failed(err.Error())
+		return err
+	}
+	self.log.Ok()
+	return nil
+}
+
+func (self Connector) updateProposalTarget(number int, target gitdomain.LocalBranchName, _ stringslice.Collector) error {
+	self.log.Start(messages.HostingGitlabUpdateMRViaAPI, number, target)
+	_, _, err := self.client.MergeRequests.UpdateMergeRequest(self.projectPath(), number, &gitlab.UpdateMergeRequestOptions{
+		TargetBranch: gitlab.Ptr(target.String()),
+	})
+	if err != nil {
+		self.log.Failed(err.Error())
+		return err
+	}
+	self.log.Ok()
+	return nil
 }
 
 // NewGitlabConfig provides GitLab configuration data if the current repo is hosted on GitLab,
