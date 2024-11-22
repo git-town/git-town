018f61c369cecce4272e4a53a33b3577b212f3ee
Kevin Goslar <kevin.goslar@gmail.com>
2024-11-22 14:22:30 -0600

    merge_parent_if_needed.go

--- internal/vm/opcodes/merge_parent_if_needed.go
+++ internal/vm/opcodes/merge_parent_if_needed.go
@@ -41,7 +41,7 @@ func (self *MergeParentIfNeeded) Run(args shared.RunArgs) error {
 			})
 			break
 		}
-		// here the parent isn't local --> sync with its tracking branch, then try again with the grandparent until we find a local ancestor
+		// here the parent isn't local --> sync with its tracking branch if it exists, then try again with the grandparent until we find a local ancestor
 		parentIsRemote := branchInfos.HasMatchingTrackingBranchFor(parent)
 		if parentIsRemote {
 			parentTrackingBranch := parent.AtRemote(gitdomain.RemoteOrigin)
