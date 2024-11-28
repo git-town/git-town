7a1c1dfc264dc43998fa8285b3c6802beb83d2b5
Gabriel Augendre <gabriel.augendre@ovhcloud.com>
2024-11-28 13:59:02 +0100

    fix typo

--- internal/hosting/bitbucketdatacenter/detect.go
+++ internal/hosting/bitbucketdatacenter/detect.go
@@ -2,7 +2,7 @@ package bitbucketdatacenter
 
 import "github.com/git-town/git-town/v16/internal/git/giturl"
 
-// Detect always return false because wa can't guess a self-hosted URL.
+// Detect always return false because we can't guess a self-hosted URL.
 func Detect(_ giturl.Parts) bool {
 	return false
 }
