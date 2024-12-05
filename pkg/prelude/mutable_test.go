c98c6f3c6421c0f8922d3ed68c33a37045ff17e4
Kevin Goslar <kevin.goslar@gmail.com>
2024-12-04 20:36:45 -0600

    Remove LocalBranchNames.Prepend

--- pkg/prelude/mutable_test.go
+++ pkg/prelude/mutable_test.go
@@ -3,7 +3,6 @@ package prelude_test
 import (
 	"testing"
 
-	"github.com/git-town/git-town/v16/internal/git/gitdomain"
 	"github.com/git-town/git-town/v16/internal/gohacks"
 	. "github.com/git-town/git-town/v16/pkg/prelude"
 	"github.com/shoenig/test/must"
@@ -11,17 +10,6 @@ import (
 
 func TestMutable(t *testing.T) {
 	t.Parallel()
-
-	t.Run("modify the encapsulated value directly", func(t *testing.T) {
-		t.Parallel()
-		branchNames := gitdomain.LocalBranchNames{}
-		mutable := NewMutable(&branchNames)
-		mutable.Value.Prepend("branch-1")
-		mutable.Value.Prepend("branch-2")
-		want := gitdomain.NewLocalBranchNames("branch-2", "branch-1")
-		must.Eq(t, &want, mutable.Value)
-		must.Eq(t, want, branchNames)
-	})
 
 	t.Run("remains mutable when called by value", func(t *testing.T) {
 		t.Parallel()
