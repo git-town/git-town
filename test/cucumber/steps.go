2b9bf6cc348e8bd7e77fc7f812a50d40f7cf98a6
Kevin Goslar <kevin.goslar@gmail.com>
2024-12-05 21:22:42 -0600

    steps.go

--- test/cucumber/steps.go
+++ test/cucumber/steps.go
@@ -467,20 +467,6 @@ func defineSteps(sc *godog.ScenarioContext) {
 		return nil
 	})
 
-	sc.Step(`^Git Town prints an error like:$`, func(ctx context.Context, expected *godog.DocString) error {
-		state := ctx.Value(keyScenarioState).(*ScenarioState)
-		state.runExitCodeChecked = true
-		regex := regexp.MustCompile(expected.Content)
-		have := stripansi.Strip(state.runOutput.GetOrPanic())
-		if !regex.MatchString(have) {
-			return fmt.Errorf("text not found:\n%s\n\nactual text:\n%s", expected.Content, state.runOutput.GetOrDefault())
-		}
-		if exitCode := state.runExitCode.GetOrPanic(); exitCode == 0 {
-			return fmt.Errorf("unexpected exit code %d", exitCode)
-		}
-		return nil
-	})
-
 	sc.Step(`^Git Town prints no output$`, func(ctx context.Context) error {
 		state := ctx.Value(keyScenarioState).(*ScenarioState)
 		output := state.runOutput.GetOrPanic()
