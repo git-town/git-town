06ea951f01b121c161d90ec09b695789b0917690
Kevin Goslar <kevin.goslar@gmail.com>
2024-12-12 07:36:37 -0500

    Improve method name (#4305)

--- internal/config/gitconfig/access.go
+++ internal/config/gitconfig/access.go
@@ -92,15 +92,6 @@ func (self *Access) SetConfigValue(scope configdomain.ConfigScope, key configdom
 	return self.Run("git", args...)
 }
 
-// updates a custom Git alias (not set up by Git Town)
-func (self *Access) UpdateDeprecatedCustomSetting(scope configdomain.ConfigScope, key configdomain.Key, oldValue, newValue string) {
-	fmt.Println(colors.Cyan().Styled(fmt.Sprintf(messages.SettingDeprecatedValueMessage, scope, key, oldValue, newValue)))
-	err := self.SetConfigValue(scope, key, newValue)
-	if err != nil {
-		fmt.Printf(messages.SettingCannotWrite, scope, key, err)
-	}
-}
-
 func (self *Access) UpdateDeprecatedSetting(scope configdomain.ConfigScope, oldKey, newKey configdomain.Key, value string) {
 	fmt.Println(colors.Cyan().Styled(fmt.Sprintf(messages.SettingDeprecatedMessage, scope, oldKey, newKey)))
 	err := self.RemoveConfigValue(scope, oldKey)
@@ -110,6 +101,15 @@ func (self *Access) UpdateDeprecatedSetting(scope configdomain.ConfigScope, oldK
 	err = self.SetConfigValue(scope, newKey, value)
 	if err != nil {
 		fmt.Printf(messages.SettingCannotWrite, scope, newKey, err)
+	}
+}
+
+// updates a custom Git alias (not set up by Git Town)
+func (self *Access) UpdateExternalGitTownAlias(scope configdomain.ConfigScope, key configdomain.Key, oldValue, newValue string) {
+	fmt.Println(colors.Cyan().Styled(fmt.Sprintf(messages.SettingDeprecatedValueMessage, scope, key, oldValue, newValue)))
+	err := self.SetConfigValue(scope, key, newValue)
+	if err != nil {
+		fmt.Printf(messages.SettingCannotWrite, scope, key, err)
 	}
 }
 
@@ -157,7 +157,7 @@ func (self *Access) load(scope configdomain.ConfigScope, updateOutdated bool) (c
 					configKey = update.After.Key
 					value = update.After.Value
 				} else if IsGitTownAlias(value) && value == update.Before.Value {
-					self.UpdateDeprecatedCustomSetting(scope, configdomain.Key(key), update.Before.Value, update.After.Value)
+					self.UpdateExternalGitTownAlias(scope, configdomain.Key(key), update.Before.Value, update.After.Value)
 					configKey = update.After.Key
 					value = update.After.Value
 				}
