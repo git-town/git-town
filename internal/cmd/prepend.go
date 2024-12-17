c9c3af2c59bccc2153d05c8167f631ed108419f5
Kevin Goslar <kevin.goslar@gmail.com>
2024-12-17 10:39:25 -0600

    prepend.go

--- internal/cmd/prepend.go
+++ internal/cmd/prepend.go
@@ -292,7 +292,6 @@ func prependProgram(data prependData, finalMessages stringslice.Collector) progr
 			Remotes:             data.remotes,
 		})
 	}
-
 	prog.Value.Add(&opcodes.BranchCreateAndCheckoutExistingParent{
 		Ancestors: data.newParentCandidates,
 		Branch:    data.targetBranch,
