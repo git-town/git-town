685bc31f1c1aecc871eb0f40d8d877545316d776
Kevin Goslar <kevin.goslar@gmail.com>
2024-12-18 16:49:59 -0600

    save_load_test.go

--- internal/vm/statefile/save_load_test.go
+++ internal/vm/statefile/save_load_test.go
@@ -101,8 +101,8 @@ func TestLoadSave(t *testing.T) {
 				&opcodes.Merge{Branch: "branch"},
 				&opcodes.MergeAbort{},
 				&opcodes.MergeContinue{},
-				&opcodes.MergeParentResolvePhantomConflicts{CurrentParent: "parent", OriginalParentName: Some(gitdomain.NewLocalBranchName("original-parent")), OriginalParentSHA: Some(gitdomain.NewSHA("123456"))},
 				&opcodes.MergeParentIfNeeded{Branch: "branch", OriginalParentName: Some(gitdomain.NewLocalBranchName("original-parent")), OriginalParentSHA: Some(gitdomain.NewSHA("123456"))},
+				&opcodes.MergeParentResolvePhantomConflicts{CurrentParent: "parent", OriginalParentName: Some(gitdomain.NewLocalBranchName("original-parent")), OriginalParentSHA: Some(gitdomain.NewSHA("123456"))},
 				&opcodes.MergeSquashProgram{Authors: []gitdomain.Author{"author 1 <one@acme.com>", "author 2 <two@acme.com>"}, Branch: "branch", CommitMessage: Some(gitdomain.CommitMessage("commit message")), Parent: "parent"},
 				&opcodes.MessageQueue{Message: "message"},
 				&opcodes.ProgramEndOfBranch{},
@@ -516,19 +516,19 @@ func TestLoadSave(t *testing.T) {
     },
     {
       "data": {
-        "CurrentParent": "parent",
-        "OriginalParentName": "original-parent",
-        "OriginalParentSHA": "123456"
-      },
-      "type": "MergeParent"
-    },
-    {
-      "data": {
         "Branch": "branch",
         "OriginalParentName": "original-parent",
         "OriginalParentSHA": "123456"
       },
       "type": "MergeParentIfNeeded"
+    },
+    {
+      "data": {
+        "CurrentParent": "parent",
+        "OriginalParentName": "original-parent",
+        "OriginalParentSHA": "123456"
+      },
+      "type": "MergeParentResolvePhantomConflicts"
     },
     {
       "data": {
