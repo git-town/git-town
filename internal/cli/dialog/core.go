c7278641c1f25333940a61cf857c0e8f25e02f99
Kevin Goslar <kevin.goslar@gmail.com>
2024-12-18 21:01:55 -0600

    core.go sync_feature_strategy.go

--- internal/cli/dialog/core.go
+++ internal/cli/dialog/core.go
@@ -1,2 +1,13 @@
 // Package dialog provides high-level screens through which the user can enter data into Git Town.
 package dialog
+
+import "github.com/git-town/git-town/v17/internal/cli/dialog/components/list"
+
+func DialogPosition[S comparable](entries list.Entries[S], needle S) int {
+	for e, entry := range entries {
+		if entry.Data == needle {
+			return e
+		}
+	}
+	return -1
+}
--- internal/cli/dialog/sync_feature_strategy.go
+++ internal/cli/dialog/sync_feature_strategy.go
@@ -2,7 +2,6 @@ package dialog
 
 import (
 	"fmt"
-	"strings"
 
 	"github.com/git-town/git-town/v17/internal/cli/dialog/components"
 	"github.com/git-town/git-town/v17/internal/cli/dialog/components/list"
@@ -22,52 +21,29 @@ hence their name.
 `
 )
 
-const (
-	syncFeatureStrategyEntryMerge    syncFeatureStrategyEntry = `merge updates from the parent and tracking branch`
-	syncFeatureStrategyEntryRebase   syncFeatureStrategyEntry = `rebase branches against their parent and tracking branch`
-	syncFeatureStrategyEntryCompress syncFeatureStrategyEntry = `compress the branch after merging parent and tracking`
-)
-
 func SyncFeatureStrategy(existing configdomain.SyncFeatureStrategy, inputs components.TestInput) (configdomain.SyncFeatureStrategy, bool, error) {
-	entries := []syncFeatureStrategyEntry{
-		syncFeatureStrategyEntryMerge,
-		syncFeatureStrategyEntryRebase,
-		syncFeatureStrategyEntryCompress,
+	entries := list.Entries[configdomain.SyncFeatureStrategy]{
+		{
+			Data:    configdomain.SyncFeatureStrategyMerge,
+			Enabled: true,
+			Text:    `merge updates from the parent and tracking branch`,
+		},
+		{
+			Data:    configdomain.SyncFeatureStrategyRebase,
+			Enabled: true,
+			Text:    `rebase branches against their parent and tracking branch`,
+		},
+		{
+			Data:    configdomain.SyncFeatureStrategyCompress,
+			Enabled: true,
+			Text:    `compress the branch after merging parent and tracking`,
+		},
 	}
-	var defaultPos int
-	switch existing {
-	case configdomain.SyncFeatureStrategyMerge:
-		defaultPos = 0
-	case configdomain.SyncFeatureStrategyRebase:
-		defaultPos = 1
-	case configdomain.SyncFeatureStrategyCompress:
-		defaultPos = 2
-	default:
-		panic("unknown sync strategy: " + existing.String())
-	}
-	selection, aborted, err := components.RadioList(list.NewEntries(entries...), defaultPos, syncFeatureStrategyTitle, SyncFeatureStrategyHelp, inputs)
+	defaultPos := DialogPosition(entries, existing)
+	selection, aborted, err := components.RadioList(entries, defaultPos, syncFeatureStrategyTitle, SyncFeatureStrategyHelp, inputs)
 	if err != nil || aborted {
 		return configdomain.SyncFeatureStrategyMerge, aborted, err
 	}
-	cutSelection, _, _ := strings.Cut(selection.String(), " ")
-	fmt.Printf(messages.SyncFeatureBranches, components.FormattedSelection(cutSelection, aborted))
-	return selection.SyncFeatureStrategy(), aborted, err
-}
-
-type syncFeatureStrategyEntry string
-
-func (self syncFeatureStrategyEntry) String() string {
-	return string(self)
-}
-
-func (self syncFeatureStrategyEntry) SyncFeatureStrategy() configdomain.SyncFeatureStrategy {
-	switch self {
-	case syncFeatureStrategyEntryMerge:
-		return configdomain.SyncFeatureStrategyMerge
-	case syncFeatureStrategyEntryRebase:
-		return configdomain.SyncFeatureStrategyRebase
-	case syncFeatureStrategyEntryCompress:
-		return configdomain.SyncFeatureStrategyCompress
-	}
-	panic("unhandled syncFeatureStrategyEntry: " + self)
+	fmt.Printf(messages.SyncFeatureBranches, components.FormattedSelection(selection.String(), aborted))
+	return selection, aborted, err
 }
