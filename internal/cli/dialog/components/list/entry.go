c6853d634d52fcdce5b7a07664b66a655444d0ab
Kevin Goslar <kevin.goslar@gmail.com>
2024-12-19 14:52:05 -0600

    entry.go

--- internal/cli/dialog/components/list/entry.go
+++ internal/cli/dialog/components/list/entry.go
@@ -3,6 +3,6 @@ package list
 // Entry is an entry in a List instance.
 type Entry[S comparable] struct {
 	Data     S
-	Disabled bool // TODO: reverse to Disabled, make optional, then remove from all the instantiations of Entry
+	Disabled bool
 	Text     string
 }
