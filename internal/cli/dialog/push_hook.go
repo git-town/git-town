54f0aa223c5e36941278dd7220be2c4ae71db0ba
Kevin Goslar <kevin.goslar@gmail.com>
2024-12-19 06:12:53 -0600

    push_hook.go push_hook.go

--- internal/cli/dialog/push_hook.go
+++ internal/cli/dialog/push_hook.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/git-town/git-town/v17/internal/cli/dialog/components"
 	"github.com/git-town/git-town/v17/internal/cli/dialog/components/list"
+	"github.com/git-town/git-town/v17/internal/cli/format"
 	"github.com/git-town/git-town/v17/internal/config/configdomain"
 	"github.com/git-town/git-town/v17/internal/messages"
 )
@@ -29,9 +30,17 @@ const (
 )
 
 func PushHook(existing configdomain.PushHook, inputs components.TestInput) (configdomain.PushHook, bool, error) {
-	entries := []pushHookEntry{
-		pushHookEntryEnabled,
-		pushHookEntryDisabled,
+	entries := list.Entries[configdomain.PushHook]{
+		{
+			Data:    true,
+			Enabled: true,
+			Text:    "enabled",
+		},
+		{
+			Data:    false,
+			Enabled: true,
+			Text:    "disabled",
+		},
 	}
 	var defaultPos int
 	if existing {
@@ -39,12 +48,12 @@ func PushHook(existing configdomain.PushHook, inputs components.TestInput) (conf
 	} else {
 		defaultPos = 1
 	}
-	selection, aborted, err := components.RadioList(list.NewEntries(entries...), defaultPos, pushHookTitle, PushHookHelp, inputs)
+	selection, aborted, err := components.RadioList(entries, defaultPos, pushHookTitle, PushHookHelp, inputs)
 	if err != nil || aborted {
 		return true, aborted, err
 	}
-	fmt.Printf(messages.PushHook, components.FormattedSelection(selection.String(), aborted))
-	return selection.PushHook(), aborted, err
+	fmt.Printf(messages.PushHook, components.FormattedSelection(format.Bool(selection.IsTrue()), aborted))
+	return selection, aborted, err
 }
 
 type pushHookEntry string
