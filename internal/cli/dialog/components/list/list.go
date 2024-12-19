e1da7fdaa723af3183fce7d6e73512b849bbac90
Kevin Goslar <kevin.goslar@gmail.com>
2024-12-18 20:31:45 -0600

    list.go

--- internal/cli/dialog/components/list/list.go
+++ internal/cli/dialog/components/list/list.go
@@ -20,7 +20,7 @@ type List[S any] struct {
 	Status       Status
 }
 
-func NewList[S fmt.Stringer](entries Entries[S], cursor int) List[S] {
+func NewList[S any](entries Entries[S], cursor int) List[S] {
 	numberLen := gohacks.NumberLength(len(entries))
 	return List[S]{
 		Status:       StatusActive,
