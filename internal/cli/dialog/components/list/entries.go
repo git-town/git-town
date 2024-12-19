73fc7946f26537e1cc4cedb1c7492ce4e13a9b65
Kevin Goslar <kevin.goslar@gmail.com>
2024-12-19 06:39:27 -0600

    entries.go

--- internal/cli/dialog/components/list/entries.go
+++ internal/cli/dialog/components/list/entries.go
@@ -7,24 +7,6 @@ import (
 // Entries provides methods for a collection of Entry instances.
 type Entries[S comparable] []Entry[S]
 
-type EntryType interface {
-	comparable
-	fmt.Stringer
-}
-
-// creates an Entries instance containing the given records
-func NewEntries[S EntryType](records ...S) Entries[S] {
-	result := make([]Entry[S], len(records))
-	for r, record := range records {
-		result[r] = Entry[S]{
-			Data:    record,
-			Enabled: true,
-			Text:    record.String(),
-		}
-	}
-	return result
-}
-
 // indicates whether all entries in this list are disabled
 func (self Entries[S]) AllDisabled() bool {
 	for _, entry := range self {
@@ -43,4 +25,23 @@ func (self Entries[S]) IndexOfData(needle S) int {
 		}
 	}
 	return -1
+}
+
+// narrower type needed to use the NewEntries convenience function
+type ComparableStringer interface {
+	comparable
+	fmt.Stringer
+}
+
+// creates an Entries instance containing the given records
+func NewEntries[S ComparableStringer](records ...S) Entries[S] {
+	result := make([]Entry[S], len(records))
+	for r, record := range records {
+		result[r] = Entry[S]{
+			Data:    record,
+			Enabled: true,
+			Text:    record.String(),
+		}
+	}
+	return result
 }
