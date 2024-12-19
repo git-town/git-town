f55dce87a46cbedc126a9c3850d79cd116172343
Kevin Goslar <kevin.goslar@gmail.com>
2024-12-19 14:50:23 -0500

    Simplify dialog architecture (#4359)

--- internal/cli/dialog/ship_delete_tracking_branch.go
+++ internal/cli/dialog/ship_delete_tracking_branch.go
@@ -2,10 +2,10 @@ package dialog
 
 import (
 	"fmt"
-	"strings"
 
 	"github.com/git-town/git-town/v17/internal/cli/dialog/components"
 	"github.com/git-town/git-town/v17/internal/cli/dialog/components/list"
+	"github.com/git-town/git-town/v17/internal/cli/format"
 	"github.com/git-town/git-town/v17/internal/config/configdomain"
 	"github.com/git-town/git-town/v17/internal/messages"
 )
@@ -21,15 +21,18 @@ merging pull requests through its UI.
 `
 )
 
-const (
-	ShipDeleteTrackingBranchEntryYes shipDeleteTrackingBranchEntry = `yes, "git town ship" should delete tracking branches`
-	ShipDeleteTrackingBranchEntryNo  shipDeleteTrackingBranchEntry = `no, my code hosting platform deletes tracking branches`
-)
-
 func ShipDeleteTrackingBranch(existing configdomain.ShipDeleteTrackingBranch, inputs components.TestInput) (configdomain.ShipDeleteTrackingBranch, bool, error) {
-	entries := []shipDeleteTrackingBranchEntry{
-		ShipDeleteTrackingBranchEntryYes,
-		ShipDeleteTrackingBranchEntryNo,
+	entries := list.Entries[bool]{
+		{
+			Data:    true,
+			Enabled: true,
+			Text:    `yes, "git town ship" should delete tracking branches`,
+		},
+		{
+			Data:    false,
+			Enabled: true,
+			Text:    `no, my code hosting platform deletes tracking branches`,
+		},
 	}
 	var defaultPos int
 	if existing {
@@ -37,31 +40,10 @@ func ShipDeleteTrackingBranch(existing configdomain.ShipDeleteTrackingBranch, in
 	} else {
 		defaultPos = 1
 	}
-	selection, aborted, err := components.RadioList(list.NewEntries(entries...), defaultPos, shipDeleteTrackingBranchTitle, ShipDeleteTrackingBranchHelp, inputs)
+	selection, aborted, err := components.RadioList(entries, defaultPos, shipDeleteTrackingBranchTitle, ShipDeleteTrackingBranchHelp, inputs)
 	if err != nil || aborted {
 		return true, aborted, err
 	}
-	fmt.Printf(messages.ShipDeletesTrackingBranches, components.FormattedSelection(selection.Short(), aborted))
-	return selection.ShipDeleteTrackingBranch(), aborted, err
-}
-
-type shipDeleteTrackingBranchEntry string
-
-func (self shipDeleteTrackingBranchEntry) ShipDeleteTrackingBranch() configdomain.ShipDeleteTrackingBranch {
-	switch self {
-	case ShipDeleteTrackingBranchEntryYes:
-		return configdomain.ShipDeleteTrackingBranch(true)
-	case ShipDeleteTrackingBranchEntryNo:
-		return configdomain.ShipDeleteTrackingBranch(false)
-	}
-	panic("unhandled shipDeleteTrackingBranchEntry: " + self)
-}
-
-func (self shipDeleteTrackingBranchEntry) Short() string {
-	start, _, _ := strings.Cut(self.String(), ",")
-	return start
-}
-
-func (self shipDeleteTrackingBranchEntry) String() string {
-	return string(self)
+	fmt.Printf(messages.ShipDeletesTrackingBranches, components.FormattedSelection(format.Bool(selection), aborted))
+	return configdomain.ShipDeleteTrackingBranch(selection), aborted, err
 }
