7067599d8efc0aa595c41f074bb7fc8a6ee7e0ae
Kevin Goslar <kevin.goslar@gmail.com>
2024-12-19 06:27:52 -0600

    dialog_position.go

--- internal/cli/dialog/components/list/dialog_position.go
+++ internal/cli/dialog/components/list/dialog_position.go
@@ -1,5 +1,6 @@
 package list
 
+// provides the position of the given needle in the given list of entries
 func DialogPosition[S comparable](entries Entries[S], needle S) int {
 	for e, entry := range entries {
 		if entry.Data == needle {
