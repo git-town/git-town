22af2abe9de6c6ab2d5040cd82614f2e32326282
Kevin Goslar <kevin.goslar@gmail.com>
2024-12-23 13:15:24 -0600

    core.go

--- internal/gohacks/cache/core.go
+++ internal/gohacks/cache/core.go
@@ -8,6 +8,3 @@ type LocalBranchWithPrevious = WithPrevious[gitdomain.LocalBranchName]
 
 // RemoteBranch is a cache for gitdomain.RemoteBranchName variables.
 type RemoteBranch = Cache[gitdomain.RemoteBranchName]
-
-// Remotes is a cache for domain.Remotes variables.
-// type Remotes = Cache[gitdomain.Remotes]
