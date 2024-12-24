c69d823e17a5a32bd0bfe8bd028fc14e0c6b5f8d
Kevin Goslar <kevin.goslar@gmail.com>
2024-12-24 16:03:21 -0600

    steps.go

--- test/cucumber/steps.go
+++ test/cucumber/steps.go
@@ -348,7 +348,6 @@ func defineSteps(sc *godog.ScenarioContext) {
 		state := ctx.Value(keyScenarioState).(*ScenarioState)
 		devRepo := state.fixture.DevRepo.GetOrPanic()
 		branch := gitdomain.NewLocalBranchName(name)
-
 		branchType := devRepo.Config.BranchType(branch)
 		if branchType != configdomain.BranchTypePrototypeBranch {
 			return fmt.Errorf(
