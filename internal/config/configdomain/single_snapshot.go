0840175d320cd051e5428cfe48287cc693330118
Kevin Goslar <kevin.goslar@gmail.com>
2024-12-26 13:05:56 -0600

    single_snapshot.go load.go

--- internal/config/configdomain/single_snapshot.go
+++ internal/config/configdomain/single_snapshot.go
@@ -23,23 +23,23 @@ func (self SingleSnapshot) Aliases() Aliases {
 	return result
 }
 
-// provides all the keys that describe lineage entries
-func (self SingleSnapshot) LineageEntries() map[LineageKey]string {
-	result := map[LineageKey]string{}
-	for key, value := range self {
-		if lineageKey, isLineageKey := ParseLineageKey(key).Get(); isLineageKey {
-			result[lineageKey] = value
-		}
-	}
-	return result
-}
-
 // provides all the keys that describe branch type overrides
 func (self SingleSnapshot) BranchTypeOverrideEntries() map[BranchTypeOverrideKey]string {
 	result := map[BranchTypeOverrideKey]string{}
 	for key, value := range self {
 		if branchTypeKey, isBranchTypeKey := ParseBranchTypeOverrideKey(key).Get(); isBranchTypeKey {
 			result[branchTypeKey] = value
+		}
+	}
+	return result
+}
+
+// provides all the keys that describe lineage entries
+func (self SingleSnapshot) LineageEntries() map[LineageKey]string {
+	result := map[LineageKey]string{}
+	for key, value := range self {
+		if lineageKey, isLineageKey := ParseLineageKey(key).Get(); isLineageKey {
+			result[lineageKey] = value
 		}
 	}
 	return result
