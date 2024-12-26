f49923a7f104f02ead4bc1a01fcd0fceabe56db6
Kevin Goslar <kevin.goslar@gmail.com>
2024-12-26 12:39:47 -0600

    doc.go runner.go

--- test/datatable/doc.go
+++ test/datatable/doc.go
@@ -1,2 +1,8 @@
 // Package datatable supports comparing Gherkin tables in test code.
 package datatable
+
+import "github.com/git-town/git-town/v17/internal/git/gitdomain"
+
+type runner interface {
+	SHAsForCommit(name string) gitdomain.SHAs
+}
