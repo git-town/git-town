22f5940ac8390273a98f0c384d40fb1572d09675
Kevin Goslar <kevin.goslar@gmail.com>
2024-12-26 12:37:41 -0600

    config.go doc.go

--- internal/subshell/doc.go
+++ internal/subshell/doc.go
@@ -1,2 +1,10 @@
 // Package subshell provides facilities to execute CLI commands in subshells.
 package subshell
+
+import "time"
+
+// the number of times Git Town should retry when there is another Git process running
+const concurrentGitRetries = 5
+
+// the amount of time Git Town should wait between retries when there is another Git process running
+const concurrentGitRetryDelay = 1 * time.Second
