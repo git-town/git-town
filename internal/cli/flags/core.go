431c882cc4226dcd731d36465274a5d8a28ad7fd
Kevin Goslar <kevin.goslar@gmail.com>
2024-12-26 12:35:46 -0600

    core.go doc.go core.go doc.go

--- internal/cli/flags/core.go
+++
@@ -1,6 +0,0 @@
-package flags
-
-import "github.com/spf13/cobra"
-
-// AddFunc defines the type signature for helper functions that add a CLI flag to a Cobra command.
-type AddFunc func(*cobra.Command)
