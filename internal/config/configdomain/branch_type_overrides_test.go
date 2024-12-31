918ce59eb2e8a8ec184fad9444f44128f6b212da
Kevin Goslar <kevin.goslar@gmail.com>
2024-12-31 16:16:41 -0600

    branch_type_overrides_test.go

--- internal/config/configdomain/branch_type_overrides_test.go
+++ internal/config/configdomain/branch_type_overrides_test.go
@@ -34,6 +34,8 @@ func TestBranchTypeOverrides(t *testing.T) {
 		snapshot := configdomain.SingleSnapshot{
 			"git-town-branch.branch-1.branchtype": "feature",
 			"git-town-branch.branch-2.branchtype": "observed",
+			"git-town-branch.branch-3.parent":     "main",
+			"git-town.prototype-branches":         "foo",
 		}
 		removeFunc := func(configdomain.Key) error { return nil }
 		have, err := configdomain.NewBranchTypeOverridesInSnapshot(snapshot, removeFunc)
