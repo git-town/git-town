fec4ea7112d2a21ff9850f7679446762cbf0fcb3
Kevin Goslar <kevin.goslar@gmail.com>
2024-12-30 19:55:34 -0600

    root.go show.go

---
+++ internal/cmd/status/show.go
@@ -0,0 +1,62 @@
+package status
+
+import (
+	"fmt"
+
+	"github.com/git-town/git-town/v17/internal/cli/flags"
+	"github.com/git-town/git-town/v17/internal/cli/print"
+	"github.com/git-town/git-town/v17/internal/cmd/cmdhelpers"
+	"github.com/git-town/git-town/v17/internal/config/configdomain"
+	"github.com/git-town/git-town/v17/internal/execute"
+	"github.com/spf13/cobra"
+)
+
+const statusShowDesc = "Displays the complete runstate details"
+
+func showRunstateCommand() *cobra.Command {
+	addVerboseFlag, readVerboseFlag := flags.Verbose()
+	cmd := cobra.Command{
+		Use:   "show",
+		Args:  cobra.NoArgs,
+		Short: statusShowDesc,
+		Long:  cmdhelpers.Long(statusShowDesc),
+		RunE: func(cmd *cobra.Command, _ []string) error {
+			verbose, err := readVerboseFlag(cmd)
+			if err != nil {
+				return err
+			}
+			return executeStatusShow(verbose)
+		},
+	}
+	addVerboseFlag(&cmd)
+	return &cmd
+}
+
+func executeStatusShow(verbose configdomain.Verbose) error {
+	repo, err := execute.OpenRepo(execute.OpenRepoArgs{
+		DryRun:           false,
+		PrintBranchNames: true,
+		PrintCommands:    true,
+		ValidateGitRepo:  true,
+		ValidateIsOnline: false,
+		Verbose:          verbose,
+	})
+	if err != nil {
+		return err
+	}
+	data, err := loadDisplayStatusData(repo.RootDir)
+	if err != nil {
+		return err
+	}
+	showStatus(data)
+	print.Footer(verbose, *repo.CommandsCounter.Value, []string{})
+	return nil
+}
+
+func showStatus(data displayStatusData) {
+	state, hasState := data.state.Get()
+	if !hasState {
+		return
+	}
+	fmt.Println(state.String())
+}
