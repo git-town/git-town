767a03fe4bbc26a62c7a0def53c604460365bec9
Kevin Goslar <kevin.goslar@gmail.com>
2025-01-01 06:39:11 -0600

    remove_branch.go doc.go

--- internal/vm/opcodes/doc.go
+++ internal/vm/opcodes/doc.go
@@ -59,7 +59,6 @@ func All() []shared.Opcode {
 		&BranchTrackingDelete{},
 		&BranchTypeOverrideSet{},
 		&BranchTypeOverrideRemove{},
-		&BranchesParkedRemove{},
 		&BranchesPerennialAdd{},
 		&BranchesPerennialRemove{},
 		&BranchesPrototypeAdd{},
