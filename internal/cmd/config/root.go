94996f96e6e79f6fa2e9044006e3213f4225666f
Stephen Wade <stephen@stephenwade.me>
2025-01-12 20:38:24 +0000

    root.go sync-prototype-strategy.md

--- internal/cmd/config/root.go
+++ internal/cmd/config/root.go
@@ -91,7 +91,7 @@ func printConfig(config config.UnvalidatedConfig) {
 	print.Entry("run pre-push hook", format.Bool(bool(config.NormalConfig.PushHook)))
 	print.Entry("feature sync strategy", config.NormalConfig.SyncFeatureStrategy.String())
 	print.Entry("perennial sync strategy", config.NormalConfig.SyncPerennialStrategy.String())
-	print.Entry("sync-prototype strategy", config.NormalConfig.SyncPrototypeStrategy.String())
+	print.Entry("prototype sync strategy", config.NormalConfig.SyncPrototypeStrategy.String())
 	print.Entry("sync tags", format.Bool(config.NormalConfig.SyncTags.IsTrue()))
 	print.Entry("sync with upstream", format.Bool(config.NormalConfig.SyncUpstream.IsTrue()))
 	fmt.Println()
