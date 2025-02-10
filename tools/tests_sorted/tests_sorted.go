a5b81af6c64999beb6fe8b93b6af90a7b4e91235
Kevin Goslar <kevin.goslar@gmail.com>
2025-02-10 10:57:00 -0600

    append_all_missing_test.go tests_sorted.go

--- tools/tests_sorted/tests_sorted.go
+++ tools/tests_sorted/tests_sorted.go
@@ -12,7 +12,6 @@ import (
 
 // file paths to ignore
 var ignorePaths = []string{ //nolint:gochecknoglobals
-	"internal/gohacks/slice/append_all_missing_test.go",
 	"internal/gohacks/slice/contains_any_test.go",
 	"internal/gohacks/slice/find_many_test.go",
 	"internal/gohacks/slice/get_all_test.go",
