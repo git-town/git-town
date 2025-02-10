631f7f8d92454cb4269a79f8dbab9f3316cbe753
Kevin Goslar <kevin.goslar@gmail.com>
2025-02-10 10:48:28 -0600

    lineage_test.go tests_sorted.go

--- tools/tests_sorted/tests_sorted.go
+++ tools/tests_sorted/tests_sorted.go
@@ -12,7 +12,6 @@ import (
 
 // file paths to ignore
 var ignorePaths = []string{ //nolint:gochecknoglobals
-	"internal/config/configdomain/lineage_test.go",
 	"internal/config/configdomain/ship_delete_tracking_branch_test.go",
 	"internal/config/configdomain/sync_strategy_test.go",
 	"internal/git/commands_test.go",
