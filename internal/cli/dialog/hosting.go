028deefd85d8cc7e94eeadb390630aae154a4b9e
Kevin Goslar <kevin.goslar@gmail.com>
2025-02-14 16:32:23 -0600

    ARCHITECTURE hosting.go en.go

--- internal/cli/dialog/hosting.go
+++ internal/cli/dialog/hosting.go
@@ -52,6 +52,6 @@ func HostingPlatform(existingValue Option[configdomain.HostingPlatform], inputs 
 		return optA.Equal(optB)
 	})
 	newValue, aborted, err := components.RadioList(entries, cursor, hostingPlatformTitle, HostingPlatformHelp, inputs)
-	fmt.Printf(messages.CodeHosting, components.FormattedSelection(newValue.GetOrElse("auto-detect").String(), aborted))
+	fmt.Printf(messages.Forge, components.FormattedSelection(newValue.GetOrElse("auto-detect").String(), aborted))
 	return newValue, aborted, err
 }
