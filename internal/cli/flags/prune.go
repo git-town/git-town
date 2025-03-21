61008d557d759e696a86d00eb4a5a7234ddec5d8
Kevin Goslar <kevin.goslar@gmail.com>
2025-03-21 10:22:49 -0500

    prune.go

--- internal/cli/flags/prune.go
+++ internal/cli/flags/prune.go
@@ -8,16 +8,16 @@ import (
 const pruneLong = "prune"
 
 // type-safe access to the CLI arguments of type configdomain.Prune
-func Prune() (AddFunc, ReadPrototypeFlagFunc) {
+func Prune() (AddFunc, ReadPruneFlagFunc) {
 	addFlag := func(cmd *cobra.Command) {
-		cmd.Flags().BoolP(prototypeLong, "p", false, "create a prototype branch")
+		cmd.Flags().BoolP(pruneLong, "", false, "prune empty branches")
 	}
-	readFlag := func(cmd *cobra.Command) (configdomain.Prototype, error) {
-		value, err := cmd.Flags().GetBool(prototypeLong)
-		return configdomain.Prototype(value), err
+	readFlag := func(cmd *cobra.Command) (configdomain.Prune, error) {
+		value, err := cmd.Flags().GetBool(pruneLong)
+		return configdomain.Prune(value), err
 	}
 	return addFlag, readFlag
 }
 
-// ReadPrototypeFlagFunc is the type signature for the function that reads the "prototype" flag from the args to the given Cobra command.
-type ReadPrototypeFlagFunc func(*cobra.Command) (configdomain.Prototype, error)
+// ReadPruneFlagFunc is the type signature for the function that reads the "prune" flag from the args to the given Cobra command.
+type ReadPruneFlagFunc func(*cobra.Command) (configdomain.Prune, error)
