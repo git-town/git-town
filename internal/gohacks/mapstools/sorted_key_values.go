d811cfdde62844ab47e87ebb28d9194dea94dca8
Kevin Goslar <kevin.goslar@gmail.com>
2025-03-29 12:31:12 -0500

    sorted_key_values.go

--- internal/gohacks/mapstools/sorted_key_values.go
+++ internal/gohacks/mapstools/sorted_key_values.go
@@ -7,6 +7,7 @@ import (
 	"slices"
 )
 
+// SortedKeyValues provides the keys and values of the given map sorted alphabetically by key.
 func SortedKeyValues[Map ~map[K]V, K cmp.Ordered, V any](data Map) iter.Seq2[K, V] {
 	return func(yield func(K, V) bool) {
 		for _, key := range slices.Sorted(maps.Keys(data)) {
