724abb02c2f7da32cb5f5fcf24e8d6663ffb4218
Kevin Goslar <kevin.goslar@gmail.com>
2025-04-02 05:13:35 -0500

    Improve names

--- internal/vm/statefile/save_load_test.go
+++ internal/vm/statefile/save_load_test.go
@@ -115,7 +115,7 @@ func TestLoadSave(t *testing.T) {
 				&opcodes.RebaseBranch{Branch: "branch"},
 				&opcodes.RebaseContinue{},
 				&opcodes.RebaseContinueIfNeeded{},
-				&opcodes.RebaseOntoKeepDeleted{BranchToRebaseAgainst: "branch-1", BranchToRebaseOnto: "branch-2"},
+				&opcodes.RebaseOntoKeepDeleted{BranchToRebaseOnto: "branch-2", CommitsToRemove: "branch-1"},
 				&opcodes.RebaseOntoRemoveDeleted{BranchToRebaseAgainst: "branch-1", BranchToRebaseOnto: "branch-2", Upstream: Some(gitdomain.NewLocalBranchName("upstream"))},
 				&opcodes.RebaseParentIfNeeded{Branch: "branch"},
 				&opcodes.RebaseTrackingBranch{RemoteBranch: "origin/branch"},
@@ -600,8 +600,8 @@ func TestLoadSave(t *testing.T) {
     },
     {
       "data": {
-        "BranchToRebaseAgainst": "branch-1",
-        "BranchToRebaseOnto": "branch-2"
+        "BranchToRebaseOnto": "branch-2",
+        "CommitsToRemove": "branch-1"
       },
       "type": "RebaseOntoKeepDeleted"
     },
