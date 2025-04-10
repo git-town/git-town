c7d6efc676a90159d42bbef1319fdd87db5125ec
Kevin Goslar <kevin.goslar@gmail.com>
2025-04-10 06:03:09 -0500

    commit.go

--- internal/config/configdomain/commit.go
+++ internal/config/configdomain/commit.go
@@ -3,10 +3,10 @@ package configdomain
 // when creating a new branch, whether to commit the currently staged changes into that new branch
 type Commit bool
 
-func (self Commit) IsTrue() bool {
-	return bool(self)
-}
-
 func (self Commit) IsFalse() bool {
 	return !self.IsTrue()
+}
+
+func (self Commit) IsTrue() bool {
+	return bool(self)
 }
