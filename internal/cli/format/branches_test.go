85e249021b384826818e81f80a8569cc56514975
Kevin Goslar <kevin.goslar@gmail.com>
2025-04-12 07:18:46 -0500

    branches_test.go

--- internal/cli/format/branches_test.go
+++ internal/cli/format/branches_test.go
@@ -10,12 +10,14 @@ import (
 
 func TestBranchNames(t *testing.T) {
 	t.Parallel()
+
 	t.Run("empty", func(t *testing.T) {
 		t.Parallel()
 		have := format.BranchNames(gitdomain.LocalBranchNames{})
 		want := "(none)"
 		must.EqOp(t, want, have)
 	})
+
 	t.Run("normal", func(t *testing.T) {
 		t.Parallel()
 		have := format.BranchNames(gitdomain.LocalBranchNames{"one", "two"})
