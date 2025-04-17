03f537cacdccc616cb75797d051785d8881abbe2
Kevin Goslar <kevin.goslar@gmail.com>
2025-04-17 11:56:06 -0500

    Prototype of the dialog with shorter SHAs

--- internal/cli/dialog/commits_to_beam.go
+++ internal/cli/dialog/commits_to_beam.go
@@ -5,25 +5,27 @@ import (
 
 	"github.com/git-town/git-town/v19/internal/cli/dialog/components"
 	"github.com/git-town/git-town/v19/internal/cli/dialog/components/list"
+	"github.com/git-town/git-town/v19/internal/git"
 	"github.com/git-town/git-town/v19/internal/git/gitdomain"
 	"github.com/git-town/git-town/v19/internal/messages"
 )
 
-const (
-	commitsToBeamTitle = `Select the commits to beam into branch %q`
-	shaLength          = 7
-)
+const commitsToBeamTitle = `Select the commits to beam into branch %q`
 
 // lets the user select commits to beam to the target branch
-func CommitsToBeam(commits []gitdomain.Commit, targetBranch gitdomain.LocalBranchName, inputs components.TestInput) (gitdomain.Commits, bool, error) {
+func CommitsToBeam(commits []gitdomain.Commit, targetBranch gitdomain.LocalBranchName, git git.Commands, querier gitdomain.Querier, inputs components.TestInput) (gitdomain.Commits, bool, error) {
 	if len(commits) == 0 {
 		return gitdomain.Commits{}, false, nil
 	}
 	entries := make(list.Entries[gitdomain.Commit], len(commits))
 	for c, commit := range commits {
+		shortSHA, err := git.ShortenSHA(querier, commit.SHA)
+		if err != nil {
+			return gitdomain.Commits{}, false, err
+		}
 		entries[c] = list.Entry[gitdomain.Commit]{
 			Data: commit,
-			Text: fmt.Sprintf("%s (%s)", commit.Message.String(), commit.SHA.TruncateTo(shaLength).String()),
+			Text: fmt.Sprintf("%s (%s)", commit.Message.String(), shortSHA),
 		}
 	}
 	selection, aborted, err := components.CheckList(entries, []int{}, fmt.Sprintf(commitsToBeamTitle, targetBranch), "", inputs)
