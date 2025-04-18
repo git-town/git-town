f57b09a5c7502736cd672ce092049093fe0793f7
Kevin Goslar <kevin.goslar@gmail.com>
2025-04-18 16:38:02 -0500

    Extract logic to format opcodes (#4731)

---
+++ internal/vm/shared/opcode_test.go
@@ -0,0 +1,29 @@
+package shared_test
+
+import (
+	"testing"
+
+	"github.com/git-town/git-town/v19/internal/config/configdomain"
+	"github.com/git-town/git-town/v19/internal/vm/opcodes"
+	"github.com/git-town/git-town/v19/internal/vm/shared"
+	"github.com/shoenig/test/must"
+)
+
+func TestOpcode(t *testing.T) {
+	t.Parallel()
+
+	t.Run("String", func(t *testing.T) {
+		t.Parallel()
+		give := []shared.Opcode{
+			&opcodes.MergeAbort{},
+			&opcodes.BranchTypeOverrideSet{Branch: "branch", BranchType: configdomain.BranchTypePerennialBranch},
+		}
+		have := shared.RenderOpcodes(give, "")
+		want := `
+Program:
+1: &opcodes.MergeAbort{undeclaredOpcodeMethods:opcodes.undeclaredOpcodeMethods{}}
+2: &opcodes.BranchTypeOverrideSet{Branch:"branch", BranchType:"perennial", undeclaredOpcodeMethods:opcodes.undeclaredOpcodeMethods{}}
+`[1:]
+		must.EqOp(t, want, have)
+	})
+}
