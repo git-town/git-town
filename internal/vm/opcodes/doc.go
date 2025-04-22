e09b0e848ba764b5287d7415e7962856cabd71c0
Stephen Wade <stephen@stephenwade.me>
2025-04-22 02:09:38 +0000

    Add missing opcode to internal/vm/opcodes/doc.go

--- internal/vm/opcodes/doc.go
+++ internal/vm/opcodes/doc.go
@@ -46,6 +46,7 @@ func All() []shared.Opcode {
 		&BranchCurrentResetToParent{},
 		&BranchCurrentResetToSHA{},
 		&BranchCurrentResetToSHAIfNeeded{},
+		&BranchDeleteIfEmptyAtRuntime{},
 		&BranchWithRemoteGoneDeleteIfEmptyAtRuntime{},
 		&BranchEnsureShippableChanges{},
 		&BranchLocalDelete{},
