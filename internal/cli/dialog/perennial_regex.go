63be0c6a94514b0d1e502a618b4df87f56fbb3f4
Kevin Goslar <kevin.goslar@gmail.com>
2025-04-25 15:59:20 -0500

    perennial_regex.go

--- internal/cli/dialog/perennial_regex.go
+++ internal/cli/dialog/perennial_regex.go
@@ -15,6 +15,8 @@ const (
 Any branch name matching this regular expression
 will be treated as a perennial branch.
 
+Example: ^release-.+
+
 If you're not sure what to enter here,
 it's safe to leave it blank.
 
