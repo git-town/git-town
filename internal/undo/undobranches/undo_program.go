f8c7f35c704e795508d9794a8c51ed71b46b71af
Kevin Goslar <kevin.goslar@gmail.com>
2025-04-28 16:49:20 -0500

    undo_program.go

--- internal/undo/undobranches/undo_program.go
+++ internal/undo/undobranches/undo_program.go
@@ -1,8 +1,6 @@
 package undobranches
 
 import (
-	"fmt"
-
 	"github.com/git-town/git-town/v19/internal/config"
 	"github.com/git-town/git-town/v19/internal/git/gitdomain"
 	"github.com/git-town/git-town/v19/internal/gohacks/stringslice"
@@ -12,9 +10,7 @@ import (
 func DetermineUndoBranchesProgram(beginBranchesSnapshot, endBranchesSnapshot gitdomain.BranchesSnapshot, undoablePerennialCommits []gitdomain.SHA, validatedConfig config.ValidatedConfig, touchedBranches []gitdomain.BranchName, undoAPIProgram program.Program, finalMessages stringslice.Collector) program.Program {
 	branchSpans := NewBranchSpans(beginBranchesSnapshot, endBranchesSnapshot)
 	branchSpans = branchSpans.KeepOnly(touchedBranches)
-	fmt.Println("BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB BRANCH SPANS TOUCHED ONLY", branchSpans)
 	branchChanges := branchSpans.Changes()
-	fmt.Println("CCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCC CHANGES", branchChanges)
 	return branchChanges.UndoProgram(BranchChangesUndoProgramArgs{
 		BeginBranch:              beginBranchesSnapshot.Active.GetOrDefault(),
 		Config:                   validatedConfig,
