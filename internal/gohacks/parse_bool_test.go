19a1b28ae8cd8f7e07aa61a0685e4f7509cf7014
Kevin Goslar <kevin.goslar@gmail.com>
2025-04-29 13:04:50 -0500

    parse_bool_test.go

--- internal/gohacks/parse_bool_test.go
+++ internal/gohacks/parse_bool_test.go
@@ -11,6 +11,42 @@ import (
 func TestParseBool(t *testing.T) {
 	t.Parallel()
 
+	t.Run("ParseBool", func(t *testing.T) {
+		t.Parallel()
+		t.Run("valid inputs", func(t *testing.T) {
+			tests := map[string]bool{
+				"yes":      true,
+				"Yes":      true,
+				"YES":      true,
+				"no":       false,
+				"on":       true,
+				"off":      false,
+				"true":     true,
+				"false":    false,
+				"enable":   true,
+				"enabled":  true,
+				"disable":  false,
+				"disabled": false,
+				"1":        true,
+				"0":        false,
+			}
+			for give, want := range tests {
+				have, err := gohacks.ParseBool(give, "test")
+				must.NoError(t, err)
+				must.Eq(t, want, have)
+			}
+		})
+
+		t.Run("invalid inputs", func(t *testing.T) {
+			t.Parallel()
+			tests := []string{"", "zonk"}
+			for _, give := range tests {
+				_, err := gohacks.ParseBool(give, "test")
+				must.Error(t, err)
+			}
+		})
+	})
+
 	t.Run("ParseBoolOpt", func(t *testing.T) {
 		t.Parallel()
 		tests := map[string]Option[bool]{
