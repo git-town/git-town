26f5b140de9db027fe889a79870b0c14d56d58ff
Kevin Goslar <kevin.goslar@gmail.com>
2025-04-30 10:17:01 -0500

    status reset: correctly indicate existence of file

--- internal/vm/statefile/delete.go
+++ internal/vm/statefile/delete.go
@@ -9,21 +9,21 @@ import (
 )
 
 // Delete removes the stored run state from disk.
-func Delete(repoDir gitdomain.RepoRootDir) error {
+func Delete(repoDir gitdomain.RepoRootDir) (bool, error) {
 	filename, err := FilePath(repoDir)
 	if err != nil {
-		return err
+		return false, err
 	}
 	_, err = os.Stat(filename)
 	if err != nil {
 		if os.IsNotExist(err) {
-			return nil
+			return false, nil
 		}
-		return fmt.Errorf(messages.FileStatProblem, filename, err)
+		return false, fmt.Errorf(messages.FileStatProblem, filename, err)
 	}
 	err = os.Remove(filename)
 	if err != nil {
-		return fmt.Errorf(messages.FileDeleteProblem, filename, err)
+		return false, fmt.Errorf(messages.FileDeleteProblem, filename, err)
 	}
-	return nil
+	return true, nil
 }
