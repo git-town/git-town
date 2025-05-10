97fe0b0256ea090f24263da5b170efd364d787df
Flavio <kevin.goslar@gmail.com>
2025-05-10 06:21:17 -0500

    sync_deleted_branch.go

--- internal/cmd/sync/sync_deleted_branch.go
+++ internal/cmd/sync/sync_deleted_branch.go
@@ -55,7 +55,8 @@ func syncDeletedFeatureBranchProgram(prog Mutable[program.Program], branch gitdo
 			previousParentSHA: parentLastRunSHA,
 			program:           prog,
 			syncStrategy:      args.Config.NormalConfig.SyncFeatureStrategy,
-			trackingBranch:    None[gitdomain.RemoteBranchName](),
+			// we sync a branch whose remote was deleted here, so we know for sure there is no tracking branch
+			trackingBranch: None[gitdomain.RemoteBranchName](),
 		})
 		prog.Value.Add(&opcodes.BranchWithRemoteGoneDeleteIfEmptyAtRuntime{Branch: branch})
 	}
