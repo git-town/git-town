a056dbad6643736dd66a2bdb0d03ca673e0e58b9
Flavio <kevin.goslar@gmail.com>
2025-05-10 13:31:46 -0500

    offline.go

--- internal/config/configdomain/offline.go
+++ internal/config/configdomain/offline.go
@@ -10,12 +10,12 @@ import (
 // Offline is a new-type for the "offline" configuration setting.
 type Offline bool
 
-func (self Offline) IsOnline() bool {
-	return !self.IsOffline()
-}
-
 func (self Offline) IsOffline() bool {
 	return bool(self)
+}
+
+func (self Offline) IsOnline() bool {
+	return !self.IsOffline()
 }
 
 func (self Offline) String() string {
