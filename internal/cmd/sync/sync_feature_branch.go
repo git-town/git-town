4ef59ac6d23015f5af118e673917e9d00ffc5b49
Kevin Goslar <kevin.goslar@gmail.com>
2025-05-12 05:56:45 -0500

    Move rebasing feature branches into an expandable opcode (#4912)

--- internal/cmd/sync/sync_feature_branch.go
+++ internal/cmd/sync/sync_feature_branch.go
@@ -72,29 +72,14 @@ func syncFeatureBranchMerge(args featureBranchArgs) {
 	)
 }
 
+// TODO: inline all these helper functions since all they do now is add a single opcode
 func syncFeatureBranchRebase(args featureBranchArgs) {
 	args.program.Value.Add(
-		&opcodes.RebaseParentsUntilLocal{
-			Branch:      args.localName,
-			PreviousSHA: args.parentLastRunSHA,
+		&opcodes.SyncFeatureBranchRebase{
+			Branch:           args.localName,
+			ParentLastRunSHA: args.parentLastRunSHA,
+			PushBranches:     args.pushBranches,
+			TrackingBranch:   args.trackingBranch,
 		},
 	)
-	if trackingBranch, hasTrackingBranch := args.trackingBranch.Get(); hasTrackingBranch {
-		if args.offline.IsOnline() {
-			args.program.Value.Add(
-				&opcodes.RebaseTrackingBranch{
-					RemoteBranch: trackingBranch,
-					PushBranches: args.pushBranches,
-				},
-				&opcodes.RebaseParentsUntilLocal{
-					Branch:      args.localName,
-					PreviousSHA: args.parentLastRunSHA,
-				},
-				&opcodes.PushCurrentBranchForceIfNeeded{
-					CurrentBranch:   args.localName,
-					ForceIfIncludes: true,
-				},
-			)
-		}
-	}
 }
