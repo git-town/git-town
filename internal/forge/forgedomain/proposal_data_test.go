0379c9596cd7a67846ba54c8cb2286bcd14c8f1c
Flavio <kevin.goslar@gmail.com>
2025-05-14 15:56:03 -0500

    proposal_data_test.go

---
+++ internal/forge/forgedomain/proposal_data_test.go
@@ -0,0 +1,54 @@
+package forgedomain_test
+
+import (
+	"encoding/json"
+	"fmt"
+	"testing"
+
+	"github.com/git-town/git-town/v20/internal/forge/forgedomain"
+	. "github.com/git-town/git-town/v20/pkg/prelude"
+	"github.com/shoenig/test/must"
+)
+
+func TestBitbucketCloudProposalData(t *testing.T) {
+	t.Parallel()
+
+	t.Run("MarshalJSON", func(t *testing.T) {
+		t.Parallel()
+		data := forgedomain.BitbucketCloudProposalData{
+			ProposalData: forgedomain.ProposalData{
+				Body:         Some("body"),
+				MergeWithAPI: true,
+				Number:       123,
+				Source:       "source",
+				Target:       "target",
+				Title:        "title",
+				URL:          "url",
+			},
+			CloseSourceBranch: true,
+			Draft:             true,
+		}
+		serialized, err := json.MarshalIndent(data, "", "  ")
+		must.NoError(t, err)
+		fmt.Println(string(serialized))
+		want := `
+{
+  "Body": "body",
+  "MergeWithAPI": true,
+  "Number": 123,
+  "Source": "source",
+  "Target": "target",
+  "Title": "title",
+  "URL": "url",
+  "CloseSourceBranch": true,
+  "Draft": true
+}`[1:]
+		must.EqOp(t, want, string(serialized))
+
+		var data2 forgedomain.BitbucketCloudProposalData
+		json.Unmarshal(serialized, &data2)
+		must.Eq(t, data, data2)
+		must.True(t, data2.CloseSourceBranch)
+		must.True(t, data2.Draft)
+	})
+}
