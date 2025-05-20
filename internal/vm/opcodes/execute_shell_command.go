1820cb7867cc82b13fcda61c2b5b4bf7f7f20453
Flavio <kevin.goslar@gmail.com>
2025-05-20 10:08:09 -0500

    execute_shell_command.go

--- internal/vm/opcodes/execute_shell_command.go
+++ internal/vm/opcodes/execute_shell_command.go
@@ -1,8 +1,6 @@
 package opcodes
 
-import (
-	"github.com/git-town/git-town/v20/internal/vm/shared"
-)
+import "github.com/git-town/git-town/v20/internal/vm/shared"
 
 // FetchUpstream brings the Git history of the local repository
 // up to speed with activities that happened in the upstream remote.
