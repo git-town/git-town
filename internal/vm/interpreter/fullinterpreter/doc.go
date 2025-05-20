0beaed7d974548220c2541335daab0b780a1676f
Flavio <kevin.goslar@gmail.com>
2025-05-20 05:11:39 -0500

    doc.go

--- internal/vm/interpreter/fullinterpreter/doc.go
+++ internal/vm/interpreter/fullinterpreter/doc.go
@@ -1,2 +1,2 @@
-// Package full implements the virtual machine for executing steps of Git Town commands.
+// Package fullinterpreter implements the virtual machine for executing steps of Git Town commands.
 package fullinterpreter
