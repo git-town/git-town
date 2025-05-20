eaad98f4de1934c912054131795c567838c2a34b
Flavio <kevin.goslar@gmail.com>
2025-05-20 10:09:08 -0500

    exit_to_shell.go

--- internal/vm/opcodes/exit_to_shell.go
+++ internal/vm/opcodes/exit_to_shell.go
@@ -1,7 +1,5 @@
 package opcodes
 
-// FetchUpstream brings the Git history of the local repository
-// up to speed with activities that happened in the upstream remote.
 type ExitToShell struct {
 	undeclaredOpcodeMethods `exhaustruct:"optional"`
 }
