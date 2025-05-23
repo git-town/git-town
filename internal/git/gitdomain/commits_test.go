fd8f1dbc42f4edf9d652b7181edfd3216fa4c0ae
Kevin Goslar <kevin.goslar@gmail.com>
2025-05-22 19:43:09 -0500

    commits_test.go

--- internal/git/gitdomain/commits_test.go
+++ internal/git/gitdomain/commits_test.go
@@ -9,6 +9,7 @@ import (
 
 func TestCommits(t *testing.T) {
 	t.Parallel()
+
 	t.Run("FindByCommitMessage", func(t *testing.T) {
 		t.Parallel()
 		t.Run("contains a commit with the message", func(t *testing.T) {
