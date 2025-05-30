d3623f615f7038765463b2df3574de7d11927bd3
Flavio <kevin.goslar@gmail.com>
2025-05-30 17:12:36 -0500

    doc.go

--- internal/state/runstate/doc.go
+++ internal/state/runstate/doc.go
@@ -1,2 +1,2 @@
-// Package runstatefile stores Git Town runstate on disk.
+// Package runstate implements the in-memory and on-disk representations of the mutable state of the Git Town interpreter.
 package runstate
