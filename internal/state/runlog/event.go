00dce011d67de67ae18395bc835a43d3c6fa5fcd
Flavio <kevin.goslar@gmail.com>
2025-05-30 14:49:04 -0500

    event.go

--- internal/state/runlog/event.go
+++ internal/state/runlog/event.go
@@ -3,6 +3,6 @@ package runlog
 type Event string
 
 const (
-	EventStart Event = "start" // a Git Town command has started
-	EventEnd   Event = "end"   // a Git Town command has finished or errored
+	EventStart Event = "start" // before a Git Town command starts making changes to the repo
+	EventEnd   Event = "end"   // after a Git Town command finished making changes to the repo
 )
