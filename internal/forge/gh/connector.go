7c0de7dd82332c58010f52fedb7e247bc8537e52
Kevin Goslar <kevin.goslar@gmail.com>
2025-06-02 16:24:36 -0500

    connector.go

--- internal/forge/gh/connector.go
+++ internal/forge/gh/connector.go
@@ -1,6 +1,7 @@
 package gh
 
 import (
+	"encoding/json"
 	"fmt"
 
 	"github.com/git-town/git-town/v21/internal/cli/print"
@@ -13,7 +14,13 @@ import (
 // via the GitHub API.
 type Connector struct {
 	forgedomain.Data
-	log print.Logger
+	runner Runner
+	ghPath string // full path of the gh executable
+	log    print.Logger
+}
+
+type Runner interface {
+	Query(executable string, args ...string) (string, error)
 }
 
 func (self Connector) DefaultProposalMessage(data forgedomain.ProposalData) string {
@@ -21,11 +28,48 @@ func (self Connector) DefaultProposalMessage(data forgedomain.ProposalData) stri
 }
 
 func (self Connector) FindProposalFn() Option[func(branch, target gitdomain.LocalBranchName) (Option[forgedomain.Proposal], error)] {
-	if len(forgedomain.ReadProposalOverride()) > 0 {
-		return Some(self.findProposalViaOverride)
+	return Some(self.findProposal)
+}
+
+func (self Connector) findProposal(branch, target gitdomain.LocalBranchName) (Option[forgedomain.Proposal], error) {
+	out, err := self.runner.Query(self.ghPath, "pr", "list", "--head="+branch.String(), "--base="+target.String(), "--json=number,title,body,mergeable,headRefName,baseRefName,url")
+	if err != nil {
+		return None[forgedomain.Proposal](), err
 	}
-	if self.APIToken.IsSome() {
-		return Some(self.findProposalViaAPI)
+	var parsed []ghData
+	err = json.Unmarshal([]byte(out), &parsed)
+	if err != nil {
+		return None[forgedomain.Proposal](), err
 	}
-	return None[func(branch, target gitdomain.LocalBranchName) (Option[forgedomain.Proposal], error)]()
+	if len(parsed) == 0 {
+		return None[forgedomain.Proposal](), nil
+	}
+	if len(parsed) > 1 {
+		return None[forgedomain.Proposal](), fmt.Errorf("found more than one pull request: %d", len(parsed))
+	}
+	pr := parsed[0]
+	proposal := forgedomain.Proposal{
+		Data: forgedomain.ProposalData{
+			Body:         NewOption(pr.Body),
+			MergeWithAPI: pr.MergeStateStatus == "",
+			Number:       pr.Number,
+			Source:       gitdomain.NewLocalBranchName(pr.HeadRefName),
+			Target:       gitdomain.NewLocalBranchName(pr.BaseRefName),
+			Title:        pr.Title,
+			URL:          pr.URL,
+		},
+		ForgeType: forgedomain.ForgeTypeGitHub,
+	}
+	return Some(proposal), nil
+}
+
+type ghData struct {
+	BaseRefName      string `json:"baseRefName"`
+	Body             string `json:"body"`
+	HeadRefName      string `json:"headRefName"`
+	MergeStateStatus string `json:"mergeStateStatus"`
+	Mergeable        string `json:"mergeable"`
+	Number           int    `json:"number"`
+	Title            string `json:"title"`
+	URL              string `json:"url"`
 }
