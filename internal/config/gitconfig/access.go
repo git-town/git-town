abfd236b4b8df6abffd8edba16e20134d09cdfb9
Kevin Goslar <kevin.goslar@gmail.com>
2025-06-24 14:45:49 -0500

    access.go

--- internal/config/gitconfig/access.go
+++ internal/config/gitconfig/access.go
@@ -11,17 +11,13 @@ import (
 	"github.com/git-town/git-town/v21/internal/config/configdomain"
 	"github.com/git-town/git-town/v21/internal/git/gitdomain"
 	"github.com/git-town/git-town/v21/internal/messages"
+	"github.com/git-town/git-town/v21/internal/subshell/subshelldomain"
 	. "github.com/git-town/git-town/v21/pkg/prelude"
 )
 
-type Runner interface {
-	Query(executable string, args ...string) (string, error)
-	Run(executable string, args ...string) error
-}
-
 // Access provides typesafe access to the Git configuration on disk.
 type Access struct {
-	Runner
+	Runner subshelldomain.RunnerQuerier
 }
 
 func (self *Access) Load(scopeOpt Option[configdomain.ConfigScope], updateOutdated configdomain.UpdateOutdatedSettings) (configdomain.SingleSnapshot, error) {
@@ -83,7 +79,7 @@ func (self *Access) Load(scopeOpt Option[configdomain.ConfigScope], updateOutdat
 }
 
 func (self *Access) RemoteURL(remote gitdomain.Remote) Option[string] {
-	output, err := self.Query("git", "remote", "get-url", remote.String())
+	output, err := self.Runner.Query("git", "remote", "get-url", remote.String())
 	if err != nil {
 		// NOTE: it's okay to ignore the error here.
 		// If we get an error here, we simply don't use the origin remote.
@@ -98,17 +94,17 @@ func (self *Access) RemoveConfigValue(scope configdomain.ConfigScope, key config
 		args = append(args, "--global")
 	}
 	args = append(args, "--unset", key.String())
-	return self.Run("git", args...)
+	return self.Runner.Run("git", args...)
 }
 
 // removeLocalConfigurationValue deletes the configuration value with the given key from the local Git Town configuration.
 func (self *Access) RemoveLocalConfigValue(key configdomain.Key) error {
-	return self.Run("git", "config", "--unset", key.String())
+	return self.Runner.Run("git", "config", "--unset", key.String())
 }
 
 // RemoveLocalGitConfiguration removes all Git Town configuration.
 func (self *Access) RemoveLocalGitConfiguration(localSnapshot configdomain.SingleSnapshot) error {
-	err := self.Run("git", "config", "--remove-section", "git-town")
+	err := self.Runner.Run("git", "config", "--remove-section", "git-town")
 	if err != nil {
 		var exitErr *exec.ExitError
 		if errors.As(err, &exitErr) {
@@ -122,7 +118,7 @@ func (self *Access) RemoveLocalGitConfiguration(localSnapshot configdomain.Singl
 	}
 	for key := range localSnapshot {
 		if strings.HasPrefix(key.String(), "git-town-branch.") {
-			err = self.Run("git", "config", "--unset", key.String())
+			err = self.Runner.Run("git", "config", "--unset", key.String())
 			if err != nil {
 				return fmt.Errorf(messages.ConfigRemoveError, err)
 			}
@@ -138,7 +134,7 @@ func (self *Access) SetConfigValue(scope configdomain.ConfigScope, key configdom
 		args = append(args, "--global")
 	}
 	args = append(args, key.String(), value)
-	return self.Run("git", args...)
+	return self.Runner.Run("git", args...)
 }
 
 func (self *Access) UpdateDeprecatedSetting(scope configdomain.ConfigScope, oldKey, newKey configdomain.Key, value string) {
