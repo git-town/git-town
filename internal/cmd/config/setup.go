161d3ab53ecbb74c56d34d748eedf4691a4be4dd
Kevin Goslar <kevin.goslar@gmail.com>
2025-06-26 14:49:58 -0500

    setup.go

--- internal/cmd/config/setup.go
+++ internal/cmd/config/setup.go
@@ -771,6 +771,7 @@ func saveGitHubConnectorType(oldType, newType Option[forgedomain.GitHubConnector
 	}
 	return gitCommands.RemoveGitHubConnectorType(frontend)
 }
+
 func saveGitHubToken(oldToken, newToken Option[configdomain.GitHubToken], scope configdomain.ConfigScope, githubConnectorType Option[forgedomain.GitHubConnectorType], gitCommands git.Commands, frontend subshelldomain.Runner) error {
 	if connectorType, has := githubConnectorType.Get(); has {
 		if connectorType == forgedomain.GitHubConnectorTypeGh {
