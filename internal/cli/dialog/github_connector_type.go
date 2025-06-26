ea9b74ce31cecdcfd011a3f35939250abf0fa117
Kevin Goslar <kevin.goslar@gmail.com>
2025-06-26 11:34:41 -0500

    github_connector_type.go

--- internal/cli/dialog/github_connector_type.go
+++ internal/cli/dialog/github_connector_type.go
@@ -17,11 +17,12 @@ const (
 Git Town supports two ways to connect to GitHub:
 
 1. GitHub API:
-   - Git Town talks directly with the GitHub API
-	 - it uses an access token that you create for your GitHub account and provide on the next screen
-2. GitHub's "gh" tool:
-   - this is safer and more convenient
-	 - but you need to install this application
+   Git Town talks directly with the GitHub API.
+	 This uses an access token for your account
+	 that you provide on the next screen.
+2. GitHub's "gh" application:
+   This doesn't require an access token
+	 but you need to install and configure the gh tool.
 
 `
 )
