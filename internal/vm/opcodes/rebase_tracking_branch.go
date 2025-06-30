27a698cef30eb633934edbd12e740760423adee1
Kevin Goslar <kevin.goslar@gmail.com>
2025-06-30 14:35:30 -0500

    errored.go execute.go exit_to_shell.go finished.go conflict_phantom_resolve.go connector_proposal_merge.go lineage_parent_set.go merge_parent_resolve_phantom_conflicts.go rebase_onto_keep_deleted.go rebase_onto_remove_deleted.go rebase_tracking_branch.go stash_pop.go json.go program.go

--- internal/vm/opcodes/rebase_tracking_branch.go
+++ internal/vm/opcodes/rebase_tracking_branch.go
@@ -16,8 +16,7 @@ type RebaseTrackingBranch struct {
 func (self *RebaseTrackingBranch) Run(args shared.RunArgs) error {
 	if self.PushBranches {
 		// force-push-with-lease-if-includes here first, this avoids phantom merge conflicts from amended local commits
-		err := args.Git.ForcePushBranchSafely(args.Frontend, args.Config.Value.NormalConfig.NoPushHook(), true)
-		if err == nil {
+		if err := args.Git.ForcePushBranchSafely(args.Frontend, args.Config.Value.NormalConfig.NoPushHook(), true); err == nil {
 			// The force-push succeeded --> the remote branch didn't contain new commits, we are done.
 			return nil
 		}
