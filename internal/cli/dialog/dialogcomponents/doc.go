9be093f185e9259bd1b565ad7f54b2ae7018caf5
Kevin Goslar <kevin.goslar@gmail.com>
2025-07-04 06:42:30 -0500

    doc.go

--- internal/cli/dialog/dialogcomponents/doc.go
+++ internal/cli/dialog/dialogcomponents/doc.go
@@ -1,2 +1,2 @@
-// Package components contains shared components to build full dialog screens out of.
+// Package dialogcomponents contains shared components to build full dialog screens out of.
 package dialogcomponents
