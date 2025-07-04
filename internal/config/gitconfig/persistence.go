c9bf052362ab1ca0406423c5862613bcc3ae1aab
Kevin Goslar <kevin.goslar@gmail.com>
2025-07-04 12:11:12 -0500

    persistence.go unvalidated_config.go

--- internal/config/gitconfig/persistence.go
+++ internal/config/gitconfig/persistence.go
@@ -11,11 +11,11 @@ import (
 
 // Persistence provides high-level access to the Git CLI.
 type Persistence struct {
-	io IO
+	IO IO
 }
 
 func (self Persistence) RemoteURL(remote gitdomain.Remote) Option[string] {
-	output, err := self.io.Shell.Query("git", "remote", "get-url", remote.String())
+	output, err := self.IO.Shell.Query("git", "remote", "get-url", remote.String())
 	if err != nil {
 		// NOTE: it's okay to ignore the error here.
 		// If we get an error here, we simply don't use the origin remote.
@@ -26,149 +26,149 @@ func (self Persistence) RemoteURL(remote gitdomain.Remote) Option[string] {
 
 func (self Persistence) RemoveBranchTypeOverride(branch gitdomain.LocalBranchName) error {
 	key := configdomain.NewBranchTypeOverrideKeyForBranch(branch)
-	return self.io.RemoveConfigValue(configdomain.ConfigScopeLocal, key.Key)
+	return self.IO.RemoveConfigValue(configdomain.ConfigScopeLocal, key.Key)
 }
 
 func (self Persistence) RemoveCreatePrototypeBranches() error {
-	return self.io.RemoveLocalConfigValue(configdomain.KeyDeprecatedCreatePrototypeBranches)
+	return self.IO.RemoveLocalConfigValue(configdomain.KeyDeprecatedCreatePrototypeBranches)
 }
 
 func (self Persistence) RemoveDevRemote() error {
-	return self.io.RemoveLocalConfigValue(configdomain.KeyDevRemote)
+	return self.IO.RemoveLocalConfigValue(configdomain.KeyDevRemote)
 }
 
 func (self Persistence) RemoveFeatureRegex() error {
-	return self.io.RemoveLocalConfigValue(configdomain.KeyFeatureRegex)
+	return self.IO.RemoveLocalConfigValue(configdomain.KeyFeatureRegex)
 }
 
 func (self Persistence) RemoveMainBranch() error {
-	return self.io.RemoveLocalConfigValue(configdomain.KeyMainBranch)
+	return self.IO.RemoveLocalConfigValue(configdomain.KeyMainBranch)
 }
 
 func (self Persistence) RemoveNewBranchType() error {
-	return self.io.RemoveLocalConfigValue(configdomain.KeyNewBranchType)
+	return self.IO.RemoveLocalConfigValue(configdomain.KeyNewBranchType)
 }
 
 func (self Persistence) RemoveParent(parent gitdomain.LocalBranchName) error {
-	return self.io.RemoveLocalConfigValue(configdomain.NewParentKey(parent))
+	return self.IO.RemoveLocalConfigValue(configdomain.NewParentKey(parent))
 }
 
 func (self Persistence) RemovePerennialBranches() error {
-	return self.io.RemoveLocalConfigValue(configdomain.KeyPerennialBranches)
+	return self.IO.RemoveLocalConfigValue(configdomain.KeyPerennialBranches)
 }
 
 func (self Persistence) RemovePerennialRegex() error {
-	return self.io.RemoveLocalConfigValue(configdomain.KeyPerennialRegex)
+	return self.IO.RemoveLocalConfigValue(configdomain.KeyPerennialRegex)
 }
 
 func (self Persistence) RemovePushHook() error {
-	return self.io.RemoveLocalConfigValue(configdomain.KeyPushHook)
+	return self.IO.RemoveLocalConfigValue(configdomain.KeyPushHook)
 }
 
 func (self Persistence) RemoveShareNewBranches() error {
-	return self.io.RemoveLocalConfigValue(configdomain.KeyShareNewBranches)
+	return self.IO.RemoveLocalConfigValue(configdomain.KeyShareNewBranches)
 }
 
 func (self Persistence) RemoveShipDeleteTrackingBranch() error {
-	return self.io.RemoveLocalConfigValue(configdomain.KeyShipDeleteTrackingBranch)
+	return self.IO.RemoveLocalConfigValue(configdomain.KeyShipDeleteTrackingBranch)
 }
 
 func (self Persistence) RemoveShipStrategy() error {
-	return self.io.RemoveLocalConfigValue(configdomain.KeyShipStrategy)
+	return self.IO.RemoveLocalConfigValue(configdomain.KeyShipStrategy)
 }
 
 func (self Persistence) RemoveSyncFeatureStrategy() error {
-	return self.io.RemoveLocalConfigValue(configdomain.KeySyncFeatureStrategy)
+	return self.IO.RemoveLocalConfigValue(configdomain.KeySyncFeatureStrategy)
 }
 
 func (self Persistence) RemoveSyncPerennialStrategy() error {
-	return self.io.RemoveLocalConfigValue(configdomain.KeySyncPerennialStrategy)
+	return self.IO.RemoveLocalConfigValue(configdomain.KeySyncPerennialStrategy)
 }
 
 func (self Persistence) RemoveSyncPrototypeStrategy() error {
-	return self.io.RemoveLocalConfigValue(configdomain.KeySyncPrototypeStrategy)
+	return self.IO.RemoveLocalConfigValue(configdomain.KeySyncPrototypeStrategy)
 }
 
 func (self Persistence) RemoveSyncTags() error {
-	return self.io.RemoveLocalConfigValue(configdomain.KeySyncTags)
+	return self.IO.RemoveLocalConfigValue(configdomain.KeySyncTags)
 }
 
 func (self Persistence) RemoveSyncUpstream() error {
-	return self.io.RemoveLocalConfigValue(configdomain.KeySyncUpstream)
+	return self.IO.RemoveLocalConfigValue(configdomain.KeySyncUpstream)
 }
 
 func (self Persistence) SetBranchTypeOverride(branch gitdomain.LocalBranchName, branchType configdomain.BranchType) error {
-	return self.io.SetConfigValue(configdomain.ConfigScopeLocal, configdomain.NewBranchTypeOverrideKeyForBranch(branch).Key, branchType.String())
+	return self.IO.SetConfigValue(configdomain.ConfigScopeLocal, configdomain.NewBranchTypeOverrideKeyForBranch(branch).Key, branchType.String())
 }
 
 func (self Persistence) SetDevRemote(value gitdomain.Remote) error {
-	return self.io.SetConfigValue(configdomain.ConfigScopeLocal, configdomain.KeyDevRemote, value.String())
+	return self.IO.SetConfigValue(configdomain.ConfigScopeLocal, configdomain.KeyDevRemote, value.String())
 }
 
 func (self Persistence) SetFeatureRegex(value configdomain.FeatureRegex) error {
-	return self.io.SetConfigValue(configdomain.ConfigScopeLocal, configdomain.KeyFeatureRegex, value.String())
+	return self.IO.SetConfigValue(configdomain.ConfigScopeLocal, configdomain.KeyFeatureRegex, value.String())
 }
 
 func (self Persistence) SetMainBranch(value gitdomain.LocalBranchName) error {
-	return self.io.SetConfigValue(configdomain.ConfigScopeLocal, configdomain.KeyMainBranch, value.String())
+	return self.IO.SetConfigValue(configdomain.ConfigScopeLocal, configdomain.KeyMainBranch, value.String())
 }
 
 func (self Persistence) SetNewBranchType(value configdomain.BranchType) error {
-	return self.io.SetConfigValue(configdomain.ConfigScopeLocal, configdomain.KeyNewBranchType, value.String())
+	return self.IO.SetConfigValue(configdomain.ConfigScopeLocal, configdomain.KeyNewBranchType, value.String())
 }
 
 func (self Persistence) SetOffline(value configdomain.Offline) error {
-	return self.io.SetConfigValue(configdomain.ConfigScopeGlobal, configdomain.KeyOffline, value.String())
+	return self.IO.SetConfigValue(configdomain.ConfigScopeGlobal, configdomain.KeyOffline, value.String())
 }
 
 func (self Persistence) SetParent(child, parent gitdomain.LocalBranchName) error {
-	return self.io.SetConfigValue(configdomain.ConfigScopeLocal, configdomain.NewParentKey(child), parent.String())
+	return self.IO.SetConfigValue(configdomain.ConfigScopeLocal, configdomain.NewParentKey(child), parent.String())
 }
 
 func (self Persistence) SetPerennialBranches(branches gitdomain.LocalBranchNames) error {
-	return self.io.SetConfigValue(configdomain.ConfigScopeLocal, configdomain.KeyPerennialBranches, branches.Join(" "))
+	return self.IO.SetConfigValue(configdomain.ConfigScopeLocal, configdomain.KeyPerennialBranches, branches.Join(" "))
 }
 
 func (self Persistence) SetPerennialRegex(value configdomain.PerennialRegex) error {
-	return self.io.SetConfigValue(configdomain.ConfigScopeLocal, configdomain.KeyPerennialRegex, value.String())
+	return self.IO.SetConfigValue(configdomain.ConfigScopeLocal, configdomain.KeyPerennialRegex, value.String())
 }
 
 func (self Persistence) SetPushHook(value configdomain.PushHook) error {
-	return self.io.SetConfigValue(configdomain.ConfigScopeLocal, configdomain.KeyPushHook, strconv.FormatBool(value.IsTrue()))
+	return self.IO.SetConfigValue(configdomain.ConfigScopeLocal, configdomain.KeyPushHook, strconv.FormatBool(value.IsTrue()))
 }
 
 func (self Persistence) SetShareNewBranches(value configdomain.ShareNewBranches, scope configdomain.ConfigScope) error {
-	return self.io.SetConfigValue(scope, configdomain.KeyShareNewBranches, value.String())
+	return self.IO.SetConfigValue(scope, configdomain.KeyShareNewBranches, value.String())
 }
 
 func (self Persistence) SetShipDeleteTrackingBranch(value configdomain.ShipDeleteTrackingBranch, scope configdomain.ConfigScope) error {
-	return self.io.SetConfigValue(scope, configdomain.KeyShipDeleteTrackingBranch, strconv.FormatBool(value.IsTrue()))
+	return self.IO.SetConfigValue(scope, configdomain.KeyShipDeleteTrackingBranch, strconv.FormatBool(value.IsTrue()))
 }
 
 func (self Persistence) SetShipStrategy(value configdomain.ShipStrategy, scope configdomain.ConfigScope) error {
-	return self.io.SetConfigValue(scope, configdomain.KeyShipStrategy, value.String())
+	return self.IO.SetConfigValue(scope, configdomain.KeyShipStrategy, value.String())
 }
 
 func (self Persistence) SetSyncFeatureStrategy(value configdomain.SyncFeatureStrategy) error {
-	return self.io.SetConfigValue(configdomain.ConfigScopeLocal, configdomain.KeySyncFeatureStrategy, value.String())
+	return self.IO.SetConfigValue(configdomain.ConfigScopeLocal, configdomain.KeySyncFeatureStrategy, value.String())
 }
 
 func (self Persistence) SetSyncPerennialStrategy(value configdomain.SyncPerennialStrategy) error {
-	return self.io.SetConfigValue(configdomain.ConfigScopeLocal, configdomain.KeySyncPerennialStrategy, value.String())
+	return self.IO.SetConfigValue(configdomain.ConfigScopeLocal, configdomain.KeySyncPerennialStrategy, value.String())
 }
 
 func (self Persistence) SetSyncPrototypeStrategy(value configdomain.SyncPrototypeStrategy) error {
-	return self.io.SetConfigValue(configdomain.ConfigScopeLocal, configdomain.KeySyncPrototypeStrategy, value.String())
+	return self.IO.SetConfigValue(configdomain.ConfigScopeLocal, configdomain.KeySyncPrototypeStrategy, value.String())
 }
 
 func (self Persistence) SetSyncTags(value configdomain.SyncTags) error {
-	return self.io.SetConfigValue(configdomain.ConfigScopeLocal, configdomain.KeySyncTags, value.String())
+	return self.IO.SetConfigValue(configdomain.ConfigScopeLocal, configdomain.KeySyncTags, value.String())
 }
 
 func (self Persistence) SetSyncUpstream(value configdomain.SyncUpstream, scope configdomain.ConfigScope) error {
-	return self.io.SetConfigValue(scope, configdomain.KeySyncUpstream, strconv.FormatBool(value.IsTrue()))
+	return self.IO.SetConfigValue(scope, configdomain.KeySyncUpstream, strconv.FormatBool(value.IsTrue()))
 }
 
 func (self Persistence) SetUnknownBranchType(value configdomain.BranchType) error {
-	return self.io.SetConfigValue(configdomain.ConfigScopeLocal, configdomain.KeyUnknownBranchType, value.String())
+	return self.IO.SetConfigValue(configdomain.ConfigScopeLocal, configdomain.KeyUnknownBranchType, value.String())
 }
--- internal/config/unvalidated_config.go
+++ internal/config/unvalidated_config.go
@@ -34,9 +34,9 @@ func (self *UnvalidatedConfig) MainAndPerennials() gitdomain.LocalBranchNames {
 }
 
 func (self *UnvalidatedConfig) Reload() (globalSnapshot, localSnapshot, unscopedSnapshot configdomain.SingleSnapshot) {
-	globalSnapshot, _ = self.NormalConfig.GitIO.Load(Some(configdomain.ConfigScopeGlobal), configdomain.UpdateOutdatedNo) // we ignore the Git cache here because reloading a config in the middle of a Git Town command doesn't change the cached initial state of the repo
-	localSnapshot, _ = self.NormalConfig.GitIO.Load(Some(configdomain.ConfigScopeLocal), configdomain.UpdateOutdatedNo)   // we ignore the Git cache here because reloading a config in the middle of a Git Town command doesn't change the cached initial state of the repo
-	unscopedSnapshot, _ = self.NormalConfig.GitIO.Load(None[configdomain.ConfigScope](), configdomain.UpdateOutdatedNo)   // we ignore the Git cache here because reloading a config in the middle of a Git Town command doesn't change the cached initial state of the repo
+	globalSnapshot, _ = self.NormalConfig.GitPersistence.IO.Load(Some(configdomain.ConfigScopeGlobal), configdomain.UpdateOutdatedNo) // we ignore the Git cache here because reloading a config in the middle of a Git Town command doesn't change the cached initial state of the repo
+	localSnapshot, _ = self.NormalConfig.GitPersistence.IO.Load(Some(configdomain.ConfigScopeLocal), configdomain.UpdateOutdatedNo)   // we ignore the Git cache here because reloading a config in the middle of a Git Town command doesn't change the cached initial state of the repo
+	unscopedSnapshot, _ = self.NormalConfig.GitPersistence.IO.Load(None[configdomain.ConfigScope](), configdomain.UpdateOutdatedNo)   // we ignore the Git cache here because reloading a config in the middle of a Git Town command doesn't change the cached initial state of the repo
 	unscopedGitConfig, _ := configdomain.NewPartialConfigFromSnapshot(unscopedSnapshot, false, nil)
 	envConfig := envconfig.Load()
 	unvalidatedConfig, normalConfig := mergeConfigs(mergeConfigsArgs{
@@ -50,7 +50,7 @@ func (self *UnvalidatedConfig) Reload() (globalSnapshot, localSnapshot, unscoped
 		DryRun:           self.NormalConfig.DryRun,
 		EnvConfig:        envConfig,
 		GitConfig:        unscopedGitConfig,
-		GitIO:            self.NormalConfig.GitIO,
+		GitPersistence:   self.NormalConfig.GitPersistence,
 		GitVersion:       self.NormalConfig.GitVersion,
 		NormalConfigData: normalConfig,
 	}
