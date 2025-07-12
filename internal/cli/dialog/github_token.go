47c94c649184262b734c8f8ad20fe03ce650132e
Kevin Goslar <kevin.goslar@gmail.com>
2025-07-12 12:28:28 -0500

    github_token.go setup.go [skip ci]

--- internal/cli/dialog/github_token.go
+++ internal/cli/dialog/github_token.go
@@ -1,18 +1,8 @@
 package dialog
 
-import (
-	"fmt"
-
-	"github.com/git-town/git-town/v21/internal/cli/dialog/dialogcomponents"
-	"github.com/git-town/git-town/v21/internal/cli/dialog/dialogdomain"
-	"github.com/git-town/git-town/v21/internal/forge/forgedomain"
-	"github.com/git-town/git-town/v21/internal/messages"
-	. "github.com/git-town/git-town/v21/pkg/prelude"
-)
-
 const (
-	githubTokenTitle = `GitHub API token`
-	gitHubTokenHelp  = `
+	GitHubTokenTitle = `GitHub API token`
+	GitHubTokenHelp  = `
 Git Town can update pull requests
 and ship branches on your behalf
 using the GitHub API.
@@ -28,16 +18,3 @@ with the GitHub API.
 
 `
 )
-
-// GitHubToken lets the user enter the GitHub API token.
-func GitHubToken(oldValue Option[forgedomain.GitHubToken], inputs dialogcomponents.TestInput) (Option[forgedomain.GitHubToken], dialogdomain.Exit, error) {
-	text, exit, err := dialogcomponents.TextField(dialogcomponents.TextFieldArgs{
-		ExistingValue: oldValue.String(),
-		Help:          gitHubTokenHelp,
-		Prompt:        "Your GitHub API token: ",
-		TestInput:     inputs,
-		Title:         githubTokenTitle,
-	})
-	fmt.Printf(messages.GitHubToken, dialogcomponents.FormattedSecret(text, exit))
-	return forgedomain.ParseGitHubToken(text), exit, err
-}
