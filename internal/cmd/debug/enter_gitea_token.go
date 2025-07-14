43d8c8c224ba933e87ca71e51fcf08e4182fdee6
Kevin Goslar <kevin.goslar@gmail.com>
2025-07-14 14:20:32 -0500

    enter_gitea_token.go enter_github_token.go

--- internal/cmd/debug/enter_gitea_token.go
+++ internal/cmd/debug/enter_gitea_token.go
@@ -6,6 +6,7 @@ import (
 	"github.com/git-town/git-town/v21/internal/cli/dialog"
 	"github.com/git-town/git-town/v21/internal/cli/dialog/dialogcomponents"
 	"github.com/git-town/git-town/v21/internal/forge/forgedomain"
+	"github.com/git-town/git-town/v21/internal/messages"
 	. "github.com/git-town/git-town/v21/pkg/prelude"
 	"github.com/spf13/cobra"
 )
@@ -15,7 +16,17 @@ func enterGiteaToken() *cobra.Command {
 		Use: "gitea-token",
 		RunE: func(_ *cobra.Command, _ []string) error {
 			dialogInputs := dialogcomponents.LoadTestInputs(os.Environ())
-			_, _, err := dialog.GiteaToken(None[forgedomain.GiteaToken](), dialogInputs.Next())
+			_, _, err := dialog.ConfigStringDialog(dialog.ConfigStringDialogArgs[forgedomain.GiteaToken]{
+				ConfigFileValue: None[forgedomain.GiteaToken](),
+				HelpText:        dialog.GiteaTokenHelp,
+				Inputs:          dialogInputs,
+				LocalValue:      None[forgedomain.GiteaToken](),
+				ParseFunc:       dialog.WrapParseFunc(forgedomain.ParseGiteaToken),
+				Prompt:          "Your Gitea token: ",
+				ResultMessage:   messages.GiteaToken,
+				Title:           dialog.GiteaTokenTitle,
+				UnscopedValue:   None[forgedomain.GiteaToken](),
+			})
 			return err
 		},
 	}
--- internal/cmd/debug/enter_github_token.go
+++ internal/cmd/debug/enter_github_token.go
@@ -6,6 +6,7 @@ import (
 	"github.com/git-town/git-town/v21/internal/cli/dialog"
 	"github.com/git-town/git-town/v21/internal/cli/dialog/dialogcomponents"
 	"github.com/git-town/git-town/v21/internal/forge/forgedomain"
+	"github.com/git-town/git-town/v21/internal/messages"
 	. "github.com/git-town/git-town/v21/pkg/prelude"
 	"github.com/spf13/cobra"
 )
@@ -15,7 +16,17 @@ func enterGitHubToken() *cobra.Command {
 		Use: "github-token",
 		RunE: func(_ *cobra.Command, _ []string) error {
 			dialogInputs := dialogcomponents.LoadTestInputs(os.Environ())
-			_, _, err := dialog.GitHubToken(None[forgedomain.GitHubToken](), dialogInputs.Next())
+			_, _, err := dialog.ConfigStringDialog(dialog.ConfigStringDialogArgs[forgedomain.GitHubToken]{
+				ConfigFileValue: None[forgedomain.GitHubToken](),
+				HelpText:        dialog.GitHubTokenHelp,
+				Inputs:          dialogInputs,
+				LocalValue:      None[forgedomain.GitHubToken](),
+				ParseFunc:       dialog.WrapParseFunc(forgedomain.ParseGitHubToken),
+				Prompt:          "Your GitHub token: ",
+				ResultMessage:   messages.GitHubToken,
+				Title:           dialog.GitHubTokenTitle,
+				UnscopedValue:   None[forgedomain.GitHubToken](),
+			})
 			return err
 		},
 	}
