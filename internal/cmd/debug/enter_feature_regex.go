916c2c433fe759c4809aae561cdcf327473121bd
Kevin Goslar <kevin.goslar@gmail.com>
2025-07-14 14:22:08 -0500

    enter_feature_regex.go

--- internal/cmd/debug/enter_feature_regex.go
+++ internal/cmd/debug/enter_feature_regex.go
@@ -6,6 +6,7 @@ import (
 	"github.com/git-town/git-town/v21/internal/cli/dialog"
 	"github.com/git-town/git-town/v21/internal/cli/dialog/dialogcomponents"
 	"github.com/git-town/git-town/v21/internal/config/configdomain"
+	"github.com/git-town/git-town/v21/internal/messages"
 	. "github.com/git-town/git-town/v21/pkg/prelude"
 	"github.com/spf13/cobra"
 )
@@ -15,7 +16,17 @@ func enterFeatureRegex() *cobra.Command {
 		Use: "feature-regex",
 		RunE: func(_ *cobra.Command, _ []string) error {
 			dialogInputs := dialogcomponents.LoadTestInputs(os.Environ())
-			_, _, err := dialog.FeatureRegex(None[configdomain.FeatureRegex](), dialogInputs.Next())
+			_, _, err := dialog.ConfigStringDialog(dialog.ConfigStringDialogArgs[configdomain.FeatureRegex]{
+				ConfigFileValue: None[configdomain.FeatureRegex](),
+				HelpText:        dialog.FeatureRegexHelp,
+				Inputs:          dialogInputs,
+				LocalValue:      None[configdomain.FeatureRegex](),
+				ParseFunc:       configdomain.ParseFeatureRegex,
+				Prompt:          "Your feature regex: ",
+				ResultMessage:   messages.FeatureRegex,
+				Title:           dialog.FeatureRegexTitle,
+				UnscopedValue:   None[configdomain.FeatureRegex](),
+			})
 			return err
 		},
 	}
