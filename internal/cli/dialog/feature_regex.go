14c298e80b09123d36f9413a8229812fa6e26f9d
Flavio <kevin.goslar@gmail.com>
2025-07-15 10:46:42 -0500

    feature_regex.go

--- internal/cli/dialog/feature_regex.go
+++ internal/cli/dialog/feature_regex.go
@@ -1,6 +1,7 @@
 package dialog
 
 import (
+	"cmp"
 	"fmt"
 
 	"github.com/git-town/git-town/v21/internal/cli/dialog/dialogcomponents"
@@ -23,7 +24,7 @@ is set to something other than "feature".
 )
 
 func FeatureRegex(existingValue Option[configdomain.FeatureRegex], inputs dialogcomponents.TestInputs) (Option[configdomain.FeatureRegex], dialogdomain.Exit, error) {
-	value, exit, err := dialogcomponents.TextField(dialogcomponents.TextFieldArgs{
+	value, exit, err1 := dialogcomponents.TextField(dialogcomponents.TextFieldArgs{
 		ExistingValue: existingValue.String(),
 		Help:          FeatureRegexHelp,
 		Prompt:        "Feature regex: ",
@@ -31,9 +32,6 @@ func FeatureRegex(existingValue Option[configdomain.FeatureRegex], inputs dialog
 		Title:         featureRegexTitle,
 	})
 	fmt.Printf(messages.FeatureRegex, dialogcomponents.FormattedSelection(value, exit))
-	if err != nil {
-		return None[configdomain.FeatureRegex](), exit, err
-	}
-	featureRegex, err := configdomain.ParseFeatureRegex(value)
-	return featureRegex, exit, err
+	featureRegex, err2 := configdomain.ParseFeatureRegex(value)
+	return featureRegex, exit, cmp.Or(err1, err2)
 }
