b9542af9603ff0dbf3e47deddb3219b74b0aae14
Kevin Goslar <kevin.goslar@gmail.com>
2025-07-16 18:48:17 -0500

    save.go

--- internal/config/configfile/save.go
+++ internal/config/configfile/save.go
@@ -16,41 +16,96 @@ func RenderPerennialBranches(perennials gitdomain.LocalBranchNames) string {
 	return fmt.Sprintf(`["%s"]`, perennials.Join(`", "`))
 }
 
-func RenderTOML(data configdomain.PartialConfig, mainBranch gitdomain.LocalBranchName) string {
+func RenderTOML(config configdomain.PartialConfig) string {
 	result := strings.Builder{}
 	result.WriteString("# More info around this file at https://www.git-town.com/configuration-file\n")
-	result.WriteString("\n[branches]\n")
-	result.WriteString(fmt.Sprintf("main = %q\n", mainBranch))
-	if len(data.PerennialBranches) > 0 {
-		result.WriteString(fmt.Sprintf("perennials = %s\n", RenderPerennialBranches(data.PerennialBranches)))
+
+	main, hasMain := config.MainBranch.Get()
+	hasPerennialBranches := len(config.PerennialBranches) > 0
+	perennialRegex, hasPerennialRegex := config.PerennialRegex.Get()
+	if hasMain || hasPerennialBranches || hasPerennialRegex {
+		result.WriteString("\n[branches]\n")
+		if hasMain {
+			result.WriteString(fmt.Sprintf("main = %q\n", main))
+		}
+		if hasPerennialBranches {
+			result.WriteString(fmt.Sprintf("perennials = %s\n", RenderPerennialBranches(config.PerennialBranches)))
+		}
+		if hasPerennialRegex {
+			result.WriteString(fmt.Sprintf("perennial-regex = %q\n", perennialRegex))
+		}
 	}
-	if value, has := data.PerennialRegex.Get(); has {
-		result.WriteString(fmt.Sprintf("perennial-regex = %q\n", value))
+
+	newBranchType, hasNewBranchType := config.NewBranchType.Get()
+	shareNewBranches, hasShareNewBranches := config.ShareNewBranches.Get()
+	if hasNewBranchType || hasShareNewBranches {
+		result.WriteString("\n[create]\n")
+		if hasNewBranchType {
+			result.WriteString(fmt.Sprintf("new-branch-type = %q\n", newBranchType))
+		}
+		if hasShareNewBranches {
+			result.WriteString(fmt.Sprintf("share-new-branches = %q\n", shareNewBranches))
+		}
 	}
-	result.WriteString("\n[create]\n")
-	result.WriteString(fmt.Sprintf("new-branch-type = %q\n", data.NewBranchType))
-	result.WriteString(fmt.Sprintf("share-new-branches = %q\n", data.ShareNewBranches))
-	result.WriteString("\n[hosting]\n")
-	result.WriteString(fmt.Sprintf("dev-remote = %q\n", data.DevRemote.String()))
-	if forgeType, has := data.ForgeType.Get(); has {
-		result.WriteString(fmt.Sprintf("forge-type = %q\n", forgeType))
+
+	devRemote, hasDevRemote := config.DevRemote.Get()
+	forgeType, hasForgeType := config.ForgeType.Get()
+	originHostName, hasOriginHostName := config.HostingOriginHostname.Get()
+	if hasDevRemote || hasForgeType || hasOriginHostName {
+		result.WriteString("\n[hosting]\n")
+		if hasDevRemote {
+			result.WriteString(fmt.Sprintf("dev-remote = %q\n", devRemote))
+		}
+		if hasForgeType {
+			result.WriteString(fmt.Sprintf("forge-type = %q\n", forgeType))
+		}
+		if hasOriginHostName {
+			result.WriteString(fmt.Sprintf("origin-hostname = %q\n", originHostName))
+		}
 	}
-	if hostName, has := data.HostingOriginHostname.Get(); has {
-		result.WriteString(fmt.Sprintf("origin-hostname = %q\n", hostName))
+
+	deleteTrackingBranch, hasDeleteTrackingBranch := config.ShipDeleteTrackingBranch.Get()
+	shipStrategy, hasShipStrategy := config.ShipStrategy.Get()
+	if hasDeleteTrackingBranch || hasShipStrategy {
+		result.WriteString("\n[ship]\n")
+		if hasDeleteTrackingBranch {
+			result.WriteString(fmt.Sprintf("delete-tracking-branch = %t\n", deleteTrackingBranch))
+		}
+		if hasShipStrategy {
+			result.WriteString(fmt.Sprintf("strategy = %q\n", shipStrategy))
+		}
 	}
-	result.WriteString("\n[ship]\n")
-	result.WriteString(fmt.Sprintf("delete-tracking-branch = %t\n", data.ShipDeleteTrackingBranch))
-	result.WriteString(fmt.Sprintf("strategy = %q\n", data.ShipStrategy))
-	result.WriteString("\n[sync]\n")
-	result.WriteString(fmt.Sprintf("feature-strategy = %q\n", data.SyncFeatureStrategy))
-	result.WriteString(fmt.Sprintf("perennial-strategy = %q\n", data.SyncPerennialStrategy))
-	result.WriteString(fmt.Sprintf("prototype-strategy = %q\n", data.SyncPrototypeStrategy))
-	result.WriteString(fmt.Sprintf("push-hook = %t\n", data.PushHook))
-	result.WriteString(fmt.Sprintf("tags = %t\n", data.SyncTags))
-	result.WriteString(fmt.Sprintf("upstream = %t\n", data.SyncUpstream))
+
+	syncFeatureStrategy, hasSyncFeatureStrategy := config.SyncFeatureStrategy.Get()
+	syncPerennialStrategy, hasSyncPerennialStrategy := config.SyncPerennialStrategy.Get()
+	syncPrototypeStrategy, hasSyncPrototypeStrategy := config.SyncPrototypeStrategy.Get()
+	pushHook, hasPushHook := config.PushHook.Get()
+	syncTags, hasSyncTags := config.SyncTags.Get()
+	syncUpstream, hasSyncUpstream := config.SyncUpstream.Get()
+	if hasSyncFeatureStrategy || hasSyncPerennialStrategy || hasSyncPrototypeStrategy || hasPushHook || hasSyncTags || hasSyncUpstream {
+		result.WriteString("\n[sync]\n")
+		if hasSyncFeatureStrategy {
+			result.WriteString(fmt.Sprintf("feature-strategy = %q\n", syncFeatureStrategy))
+		}
+		if hasSyncPerennialStrategy {
+			result.WriteString(fmt.Sprintf("perennial-strategy = %q\n", syncPerennialStrategy))
+		}
+		if hasSyncPrototypeStrategy {
+			result.WriteString(fmt.Sprintf("prototype-strategy = %q\n", syncPrototypeStrategy))
+		}
+		if hasPushHook {
+			result.WriteString(fmt.Sprintf("push-hook = %t\n", pushHook))
+		}
+		if hasSyncTags {
+			result.WriteString(fmt.Sprintf("tags = %t\n", syncTags))
+		}
+		if hasSyncUpstream {
+			result.WriteString(fmt.Sprintf("upstream = %t\n", syncUpstream))
+		}
+	}
 	return result.String()
 }
 
-func Save(normalConfig configdomain.PartialConfig, mainBranch gitdomain.LocalBranchName) error {
-	return os.WriteFile(FileName, []byte(RenderTOML(normalConfig, mainBranch)), 0o600)
+func Save(config configdomain.PartialConfig) error {
+	return os.WriteFile(FileName, []byte(RenderTOML(config)), 0o600)
 }
