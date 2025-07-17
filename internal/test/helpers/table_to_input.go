a25745b0f94b3cd6b7cfc384b78c96be1abd892d
Kevin Goslar <kevin.goslar@gmail.com>
2025-07-17 05:03:34 -0500

    Remove error checking in test code

--- internal/test/helpers/table_to_input.go
+++ internal/test/helpers/table_to_input.go
@@ -1,19 +1,15 @@
 package helpers
 
 import (
-	"errors"
 	"strings"
 
 	"github.com/cucumber/godog"
 	messages "github.com/cucumber/messages/go/v21"
 )
 
-func TableToInputEnv(table *godog.Table) ([]string, error) {
+func TableToInputEnv(table *godog.Table) []string {
 	result := make([]string, 0, len(table.Rows)-1)
-	keyColumn, err := detectKeysColumn(table.Rows[0])
-	if err != nil {
-		return result, err
-	}
+	keyColumn := detectKeysColumn(table.Rows[0])
 	for i := 1; i < len(table.Rows); i++ {
 		row := table.Rows[i]
 		answersEnvStyle := strings.ReplaceAll(row.Cells[keyColumn].Value, " ", "|")
@@ -21,14 +17,14 @@ func TableToInputEnv(table *godog.Table) ([]string, error) {
 			result = append(result, answersEnvStyle)
 		}
 	}
-	return result, nil
+	return result
 }
 
-func detectKeysColumn(row *messages.PickleTableRow) (int, error) {
+func detectKeysColumn(row *messages.PickleTableRow) int {
 	for i, cell := range row.Cells {
 		if cell.Value == "KEYS" {
-			return i, nil
+			return i
 		}
 	}
-	return 0, errors.New(`no table column with header "KEYS" detected`)
+	panic(`no table column with header "KEYS" detected`)
 }
