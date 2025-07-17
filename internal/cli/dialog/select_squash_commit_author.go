f30b498ad5801d6f469cecec6f025a23eff6408d
Flavio <kevin.goslar@gmail.com>
2025-07-17 09:48:26 -0500

    select_squash_commit_author.go

--- internal/cli/dialog/select_squash_commit_author.go
+++ internal/cli/dialog/select_squash_commit_author.go
@@ -10,6 +10,8 @@ import (
 	"github.com/git-town/git-town/v21/internal/messages"
 )
 
+// TODO: rename this file to squash_commit_author.go
+
 const squashCommitAuthorTitle = `Squash commit author`
 
 // SelectSquashCommitAuthor allows the user to select an author amongst a given list of authors.
