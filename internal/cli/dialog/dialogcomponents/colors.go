7f591c0511aa081ea106967d06a0a6540ee14576
Kevin Goslar <kevin.goslar@gmail.com>
2025-07-18 11:13:44 -0500

    colors.go unknown_branch_type.go

--- internal/cli/dialog/dialogcomponents/colors.go
+++ internal/cli/dialog/dialogcomponents/colors.go
@@ -5,7 +5,6 @@ import (
 
 	"github.com/git-town/git-town/v21/internal/cli/colors"
 	"github.com/git-town/git-town/v21/internal/cli/dialog/dialogdomain"
-	. "github.com/git-town/git-town/v21/pkg/prelude"
 )
 
 // FormattedToken provides the given API token in a printable format.
@@ -31,12 +30,12 @@ func FormattedSelection(selection string, exit dialogdomain.Exit) string {
 }
 
 // FormattedSelection provides the given dialog choice in a printable format.
-func FormattedOptionalSelection(selection Option[fmt.Stringer], exit dialogdomain.Exit) string {
+func FormattedOptionalSelection(value fmt.Stringer, has bool, exit dialogdomain.Exit) string {
 	if exit {
 		return colors.Red().Styled("(aborted)")
 	}
-	if selected, has := selection.Get(); has {
-		return colors.Green().Styled(selected.String())
+	if has {
+		return colors.Green().Styled(value.String())
 	}
 	return colors.Green().Styled("(use global setting)")
 }
--- internal/cli/dialog/unknown_branch_type.go
+++ internal/cli/dialog/unknown_branch_type.go
@@ -23,12 +23,10 @@ Git Town cannot determine their type any other way.
 
 func UnknownBranchType(unvalidatedConfig config.UnvalidatedConfig, inputs dialogcomponents.TestInputs) (Option[configdomain.UnknownBranchType], dialogdomain.Exit, error) {
 	entries := make(list.Entries[Option[configdomain.UnknownBranchType]], 0, 5)
-	fmt.Println("11111111111111111111111111111", unvalidatedConfig.GitGlobal.UnknownBranchType)
 	if globalValue, has := unvalidatedConfig.GitGlobal.UnknownBranchType.Get(); has {
-		fmt.Println("22222222222222", globalValue)
 		entries = append(entries, list.Entry[Option[configdomain.UnknownBranchType]]{
 			Data: None[configdomain.UnknownBranchType](),
-			Text: fmt.Sprintf("use global value (%s)", globalValue),
+			Text: fmt.Sprintf("use global setting (%s)", globalValue),
 		})
 	}
 	entries = appendEntry(entries, configdomain.BranchTypeContributionBranch)
@@ -38,7 +36,8 @@ func UnknownBranchType(unvalidatedConfig config.UnvalidatedConfig, inputs dialog
 	entries = appendEntry(entries, configdomain.BranchTypePrototypeBranch)
 	defaultPos := determinePos(entries, unvalidatedConfig)
 	selection, exit, err := dialogcomponents.RadioList(entries, defaultPos, unknownBranchTypeTitle, UnknownBranchTypeHelp, inputs, "unknown-branch-type")
-	fmt.Printf(messages.UnknownBranchType, dialogcomponents.FormattedSelection(selection.String(), exit))
+	value, has := selection.Get()
+	fmt.Printf(messages.UnknownBranchType, dialogcomponents.FormattedOptionalSelection(value, has, exit))
 	return selection, exit, err
 }
 
