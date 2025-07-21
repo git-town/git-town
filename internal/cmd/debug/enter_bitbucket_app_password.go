9d9020395705d56cef74d4111af218d0d4382b65
Kevin Goslar <kevin.goslar@gmail.com>
2025-07-21 13:57:33 -0500

    setup assistant: make bitbucket credentials aware of global settings (#5214)
    part of #5201

--- internal/cmd/debug/enter_bitbucket_app_password.go
+++ internal/cmd/debug/enter_bitbucket_app_password.go
@@ -15,7 +15,11 @@ func enterBitbucketAppPassword() *cobra.Command {
 		Use: "bitbucket-app-password",
 		RunE: func(_ *cobra.Command, _ []string) error {
 			dialogInputs := dialogcomponents.LoadTestInputs(os.Environ())
-			_, _, err := dialog.BitbucketAppPassword(None[forgedomain.BitbucketAppPassword](), dialogInputs)
+			_, _, err := dialog.BitbucketAppPassword(dialog.Args[forgedomain.BitbucketAppPassword]{
+				Global: None[forgedomain.BitbucketAppPassword](),
+				Inputs: dialogInputs,
+				Local:  None[forgedomain.BitbucketAppPassword](),
+			})
 			return err
 		},
 	}
