0b826e684ae05913a54fbff1108d3d5ef0662938
Kevin Goslar <kevin.goslar@gmail.com>
2025-07-21 16:15:23 -0500

    setup assistant: make aware of global sync-feature-strategy (#5219)
    part of #5201

--- internal/cmd/debug/enter_sync_feature_strategy.go
+++ internal/cmd/debug/enter_sync_feature_strategy.go
@@ -6,6 +6,7 @@ import (
 	"github.com/git-town/git-town/v21/internal/cli/dialog"
 	"github.com/git-town/git-town/v21/internal/cli/dialog/dialogcomponents"
 	"github.com/git-town/git-town/v21/internal/config/configdomain"
+	. "github.com/git-town/git-town/v21/pkg/prelude"
 	"github.com/spf13/cobra"
 )
 
@@ -14,7 +15,11 @@ func enterSyncFeatureStrategy() *cobra.Command {
 		Use: "sync-feature-strategy",
 		RunE: func(_ *cobra.Command, _ []string) error {
 			dialogTestInputs := dialogcomponents.LoadTestInputs(os.Environ())
-			_, _, err := dialog.SyncFeatureStrategy(configdomain.SyncFeatureStrategyMerge, dialogTestInputs)
+			_, _, err := dialog.SyncFeatureStrategy(dialog.Args[configdomain.SyncFeatureStrategy]{
+				Global: None[configdomain.SyncFeatureStrategy](),
+				Inputs: dialogTestInputs,
+				Local:  Some(configdomain.SyncFeatureStrategyMerge),
+			})
 			return err
 		},
 	}
