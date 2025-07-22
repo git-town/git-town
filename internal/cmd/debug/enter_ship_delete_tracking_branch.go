a77a317b2931d67f6177bf062ffbe2a1c0926e27
Kevin Goslar <kevin.goslar@gmail.com>
2025-07-21 19:39:08 -0500

    ship_delete_tracking_branch.go setup.go enter_ship_delete_tracking_branch.go

--- internal/cmd/debug/enter_ship_delete_tracking_branch.go
+++ internal/cmd/debug/enter_ship_delete_tracking_branch.go
@@ -5,6 +5,8 @@ import (
 
 	"github.com/git-town/git-town/v21/internal/cli/dialog"
 	"github.com/git-town/git-town/v21/internal/cli/dialog/dialogcomponents"
+	"github.com/git-town/git-town/v21/internal/config/configdomain"
+	. "github.com/git-town/git-town/v21/pkg/prelude"
 	"github.com/spf13/cobra"
 )
 
@@ -13,7 +15,11 @@ func enterShipDeleteTrackingBranch() *cobra.Command {
 		Use: "ship-delete-tracking-branch",
 		RunE: func(_ *cobra.Command, _ []string) error {
 			dialogTestInputs := dialogcomponents.LoadTestInputs(os.Environ())
-			_, _, err := dialog.ShipDeleteTrackingBranch(true, dialogTestInputs)
+			_, _, err := dialog.ShipDeleteTrackingBranch(dialog.Args[configdomain.ShipDeleteTrackingBranch]{
+				Global: None[configdomain.ShipDeleteTrackingBranch](),
+				Inputs: dialogTestInputs,
+				Local:  None[configdomain.ShipDeleteTrackingBranch](),
+			})
 			return err
 		},
 	}
