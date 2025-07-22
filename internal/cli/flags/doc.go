dc24968dff8ef2e069f5b33dc90d20c07b171abb
Flavio <kevin.goslar@gmail.com>
2025-07-22 11:36:04 -0500

    doc.go

--- internal/cli/flags/doc.go
+++ internal/cli/flags/doc.go
@@ -3,9 +3,7 @@
 // enforces that the programmer didn't forget to define or read the flag.
 package flags
 
-import (
-	"github.com/spf13/cobra"
-)
+import "github.com/spf13/cobra"
 
 // AddFunc defines the type signature for helper functions that add a CLI flag to a Cobra command.
 type AddFunc func(*cobra.Command)
