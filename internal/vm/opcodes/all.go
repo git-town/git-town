fb62a6a2c0ef6ae1ebc5d39bbccdaccafb0668c7
Kevin Goslar <kevin.goslar@gmail.com>
2025-07-25 16:51:33 -0500

    all.go

--- internal/vm/opcodes/all.go
+++ internal/vm/opcodes/all.go
@@ -56,6 +56,7 @@ func All() []shared.Opcode {
 		&ConflictMergePhantomResolveAll{},
 		&ConflictPhantomResolve{},
 		&ConflictRebasePhantomFinalize{},
+		&ConflictRebasePhantomResolveAll{},
 		&ConnectorProposalMerge{},
 		&ExecuteShellCommand{},
 		&ExitToShell{},
