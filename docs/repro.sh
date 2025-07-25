60f4e7dd872db94a9d7ee659c8225983c5b8fcc6
Kevin Goslar <kevin.goslar@gmail.com>
2025-07-25 06:15:43 -0500

    Simplify repro template

--- docs/repro.sh
+++ docs/repro.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env sh
+set -ex
 
 # Template for submitting Git Town bug reproductions
 #
@@ -8,42 +9,18 @@
 # To get you going, this script creates two branches and runs "git town sync".
 # Please change this to create the conditions under which the bug happens.
 
-set -ex
-
+echo "CREATE THE TEST REPO"
 rm -rf test || true
-
-echo "CREATE THE TEST FOLDER"
-git init test
-cd test
-git commit --allow-empty -m "initial"
-
-echo "CONFIGURE GIT TOWN"
+git init test && cd test && git commit --allow-empty -m "initial"
+git town status reset
 git config set git-town.main-branch "main"
 git config set git-town.new-branch-type "feature"
 git config set git-town.sync-feature-strategy "rebase"
 
 echo "CREATE BRANCH A"
 git town hack branch-A
-echo "
-line 1a
-line 2a
-" >file
+echo "content A" >file
 git add file && git commit -m commit-A
 
-echo "CREATE BRANCH B"
-git town hack branch-B
-echo "
-line 1b
-line 2b
-" >file
-git add file && git commit -m commit-B
-
-echo "SHIP BRANCH A"
-git checkout main
-git merge --squash branch-A
-git commit -m "branch A shipped"
-git branch -d branch-A
-
-echo "SYNC BRANCH B"
-git checkout branch-B
+echo "SYNC BRANCH A"
 git town sync
