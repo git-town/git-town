32224dd1ab363f72f89c6534f129d47d84946e97
Kevin Goslar <kevin.goslar@gmail.com>
2025-07-25 16:31:36 -0500

    rebase_parents_until_local.go

--- internal/vm/opcodes/rebase_parents_until_local.go
+++ internal/vm/opcodes/rebase_parents_until_local.go
@@ -56,14 +56,6 @@ func (self *RebaseParentsUntilLocal) Run(args shared.RunArgs) error {
 			// Here we rebase onto the new parent, while removing the commits that the parent had in the last run.
 			// This removes old versions of commits that were amended by the user.
 			// The new commits of the parent get added back during the rebase.
-			//
-			// TODO: this doesn't work correctly if the main branch contains a new commit.
-			// Then we have a proper merge conflict, which shouldn't get auto-resolved.
-			// So we need some sort of phantom merge conflict detection here.
-			//
-			// Rather than calling RebaseOntoKeepDeleted, we should call RebaseOntoResolvePhantomConflicts.
-			// This opcode then checks each conflicting file and if it identifies a phantom merge conflict,
-			// it auto-resolves it. It leaves the other conflicts open to let the user resolve them.
 			program = append(program, &RebaseOntoResolvePhantomConflicts{
 				BranchToRebaseOnto: branchToRebase,
 				CommitsToRemove:    parentSHAPreviousRun.Location(),
