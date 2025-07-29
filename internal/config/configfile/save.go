d30953b31d9354b1d4b8075398935ce3bb2f6587
Kevin Goslar <kevin.goslar@gmail.com>
2025-07-29 08:18:34 -0500

    save.go save_test.go

--- internal/config/configfile/save.go
+++ internal/config/configfile/save.go
@@ -64,6 +64,14 @@ func RenderTOML(data configdomain.PartialConfig) string {
 		}
 	}
 
+	proposalShowLineage, hasProposalShowLineage := data.ProposalsShowLineage.Get()
+	if hasProposalShowLineage {
+		result.WriteString("\n[propose]\n")
+		if hasProposalShowLineage {
+			result.WriteString(fmt.Sprintf("lineage = %q\n", proposalShowLineage))
+		}
+	}
+
 	deleteTrackingBranch, hasDeleteTrackingBranch := data.ShipDeleteTrackingBranch.Get()
 	shipStrategy, hasShipStrategy := data.ShipStrategy.Get()
 	if hasDeleteTrackingBranch || hasShipStrategy {
