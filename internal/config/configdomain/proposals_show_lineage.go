d0e4dc27dd591c99ee897c953914cbb2a03da21c
Kevin Goslar <kevin.goslar@gmail.com>
2025-07-29 06:17:57 -0500

    key.go proposals_show_lineage.go proposals_show_lineage_test.go parse_snapshot.go en.go

--- internal/config/configdomain/proposals_show_lineage.go
+++ internal/config/configdomain/proposals_show_lineage.go
@@ -1,14 +1,35 @@
 package configdomain
 
+import (
+	"fmt"
+	"strings"
+
+	"github.com/git-town/git-town/v21/internal/messages"
+	. "github.com/git-town/git-town/v21/pkg/prelude"
+)
+
 // indicates whether and how proposals should display the stack lineage of the respective branch
 type ProposalsShowLineage string
 
 const (
-	ProposalsShowLineageNone = "none" // don't display lineage in proposals
-	ProposalsShowLineageCI   = "ci"   // this team has set up https://github.com/git-town/action to embed the stack lineage into proposals
-	ProposalsShowLineageCLI  = "cli"  // the Git Town CLI should embed the lineage into proposals
+	ProposalsShowLineageNone ProposalsShowLineage = "none" // don't display lineage in proposals
+	ProposalsShowLineageCI   ProposalsShowLineage = "ci"   // this team has set up https://github.com/git-town/action to embed the stack lineage into proposals
+	ProposalsShowLineageCLI  ProposalsShowLineage = "cli"  // the Git Town CLI should embed the lineage into proposals
 )
 
 func (self ProposalsShowLineage) String() string {
 	return string(self)
+}
+
+func ParseProposalsShowLineage(value string) (Option[ProposalsShowLineage], error) {
+	switch strings.ToLower(strings.TrimSpace(value)) {
+	case "":
+		return None[ProposalsShowLineage](), nil
+	case "ci":
+		return Some(ProposalsShowLineageCI), nil
+	case "cli":
+		return Some(ProposalsShowLineageCLI), nil
+	default:
+		return None[ProposalsShowLineage](), fmt.Errorf(messages.ProposalsShowLineageInvalid, value)
+	}
 }
---
+++ internal/config/configdomain/proposals_show_lineage_test.go
@@ -0,0 +1,54 @@
+package configdomain_test
+
+import (
+	"testing"
+
+	"github.com/git-town/git-town/v21/internal/config/configdomain"
+	. "github.com/git-town/git-town/v21/pkg/prelude"
+	"github.com/shoenig/test/must"
+)
+
+func TestParseProposalsShowLineage(t *testing.T) {
+	t.Parallel()
+	tests := []struct {
+		give string
+		want Option[configdomain.ProposalsShowLineage]
+		err  bool
+	}{
+		{
+			give: "",
+			want: None[configdomain.ProposalsShowLineage](),
+			err:  true,
+		},
+		{
+			give: "none",
+			want: None[configdomain.ProposalsShowLineage](),
+			err:  false,
+		},
+		{
+			give: "ci",
+			want: Some(configdomain.ProposalsShowLineageCI),
+			err:  false,
+		},
+		{
+			give: "CI",
+			want: Some(configdomain.ProposalsShowLineageCI),
+			err:  false,
+		},
+		{
+			give: "cli",
+			want: Some(configdomain.ProposalsShowLineageCLI),
+			err:  false,
+		},
+		{
+			give: "CLI",
+			want: Some(configdomain.ProposalsShowLineageCLI),
+			err:  false,
+		},
+	}
+	for _, tt := range tests {
+		have, err := configdomain.ParseProposalsShowLineage(tt.give)
+		must.EqOp(t, err != nil, tt.err)
+		must.True(t, have.Equal(tt.want))
+	}
+}
