5a0ae0d5ab53fc11dd6100b5ec00fa7f17b2ab9d
Kevin Goslar <kevin.goslar@gmail.com>
2025-07-31 13:36:09 -0500

    cli_config.go

--- internal/config/cliconfig/cli_config.go
+++ internal/config/cliconfig/cli_config.go
@@ -1,6 +1,11 @@
 package cliconfig
 
-import "github.com/git-town/git-town/v21/internal/config/configdomain"
+import (
+	"github.com/git-town/git-town/v21/internal/config/configdomain"
+	"github.com/git-town/git-town/v21/internal/forge/forgedomain"
+	"github.com/git-town/git-town/v21/internal/git/gitdomain"
+	. "github.com/git-town/git-town/v21/pkg/prelude"
+)
 
 // CliConfig contains the generic (command-independent)
 // configuration information that can be received
@@ -8,4 +13,46 @@ import "github.com/git-town/git-town/v21/internal/config/configdomain"
 type CliConfig struct {
 	DryRun  configdomain.DryRun
 	Verbose configdomain.Verbose
+}
+
+func NewCliConfig(dryRun Option[configdomain.DryRun], verbose Option[configdomain.Verbose]) configdomain.PartialConfig {
+	return configdomain.PartialConfig{
+		Aliases:                  configdomain.Aliases{},
+		BitbucketAppPassword:     None[forgedomain.BitbucketAppPassword](),
+		BitbucketUsername:        None[forgedomain.BitbucketUsername](),
+		BranchTypeOverrides:      configdomain.BranchTypeOverrides{},
+		CodebergToken:            None[forgedomain.CodebergToken](),
+		ContributionRegex:        None[configdomain.ContributionRegex](),
+		DevRemote:                None[gitdomain.Remote](),
+		DryRun:                   dryRun,
+		FeatureRegex:             None[configdomain.FeatureRegex](),
+		ForgeType:                None[forgedomain.ForgeType](),
+		GitHubConnectorType:      None[forgedomain.GitHubConnectorType](),
+		GitHubToken:              None[forgedomain.GitHubToken](),
+		GitLabConnectorType:      None[forgedomain.GitLabConnectorType](),
+		GitLabToken:              None[forgedomain.GitLabToken](),
+		GitUserEmail:             None[gitdomain.GitUserEmail](),
+		GitUserName:              None[gitdomain.GitUserName](),
+		GiteaToken:               None[forgedomain.GiteaToken](),
+		HostingOriginHostname:    None[configdomain.HostingOriginHostname](),
+		Lineage:                  configdomain.Lineage{},
+		MainBranch:               None[gitdomain.LocalBranchName](),
+		NewBranchType:            None[configdomain.NewBranchType](),
+		ObservedRegex:            None[configdomain.ObservedRegex](),
+		Offline:                  None[configdomain.Offline](),
+		PerennialBranches:        gitdomain.LocalBranchNames{},
+		PerennialRegex:           None[configdomain.PerennialRegex](),
+		ProposalsShowLineage:     None[configdomain.ProposalsShowLineage](),
+		PushHook:                 None[configdomain.PushHook](),
+		ShareNewBranches:         None[configdomain.ShareNewBranches](),
+		ShipDeleteTrackingBranch: None[configdomain.ShipDeleteTrackingBranch](),
+		ShipStrategy:             None[configdomain.ShipStrategy](),
+		SyncFeatureStrategy:      None[configdomain.SyncFeatureStrategy](),
+		SyncPerennialStrategy:    None[configdomain.SyncPerennialStrategy](),
+		SyncPrototypeStrategy:    None[configdomain.SyncPrototypeStrategy](),
+		SyncTags:                 None[configdomain.SyncTags](),
+		SyncUpstream:             None[configdomain.SyncUpstream](),
+		UnknownBranchType:        None[configdomain.UnknownBranchType](),
+		Verbose:                  verbose,
+	}
 }
