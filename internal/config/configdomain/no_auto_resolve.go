7c0fe2f61f690f03d51ceff5a0958f0e07684358
Kevin Goslar <kevin.goslar@gmail.com>
2025-07-31 12:32:21 -0500

    no_auto_resolve.go

--- internal/config/configdomain/no_auto_resolve.go
+++ internal/config/configdomain/no_auto_resolve.go
@@ -3,12 +3,12 @@ package configdomain
 // indicates whether a Git Town command should not auto-resolve phantom merge conflicts
 type NoAutoResolve bool
 
-func (self NoAutoResolve) IsTrue() bool {
-	return bool(self)
-}
-
 func (self NoAutoResolve) IsFalse() bool {
 	return !self.IsTrue()
+}
+
+func (self NoAutoResolve) IsTrue() bool {
+	return bool(self)
 }
 
 func (self NoAutoResolve) ShouldAutoResolve() bool {
