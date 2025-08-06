2e62bbbda8ac9d68d86243da48b306e405bb4e59
Kevin Goslar <kevin.goslar@gmail.com>
2025-08-06 10:48:45 -0500

    More specific name for merge conflict info struct (#5337)

--- internal/git/commands.go
+++ internal/git/commands.go
@@ -473,21 +473,21 @@ func (self *Commands) FetchUpstream(runner subshelldomain.Runner, branch gitdoma
 	return runner.Run("git", "fetch", gitdomain.RemoteUpstream.String(), branch.String())
 }
 
-func (self *Commands) FileConflictFullInfo(querier subshelldomain.Querier, quickInfo FileConflictQuickInfo, parentLocation gitdomain.Location, rootBranch gitdomain.LocalBranchName) (MergeConflictFullInfo, error) {
+func (self *Commands) FileConflictFullInfo(querier subshelldomain.Querier, quickInfo FileConflictQuickInfo, parentLocation gitdomain.Location, rootBranch gitdomain.LocalBranchName) (MergeConflictInfo, error) {
 	rootBlob := None[BlobInfo]()
 	parentBlob := None[BlobInfo]()
 	if currentBranchBlobInfo, has := quickInfo.CurrentBranchChange.Get(); has {
 		var err error
 		rootBlob, err = self.ContentBlobInfo(querier, rootBranch.Location(), currentBranchBlobInfo.FilePath)
 		if err != nil {
-			return MergeConflictFullInfo{}, err
+			return MergeConflictInfo{}, err
 		}
 		parentBlob, err = self.ContentBlobInfo(querier, parentLocation, currentBranchBlobInfo.FilePath)
 		if err != nil {
-			return MergeConflictFullInfo{}, err
+			return MergeConflictInfo{}, err
 		}
 	}
-	result := MergeConflictFullInfo{
+	result := MergeConflictInfo{
 		Current: quickInfo.CurrentBranchChange,
 		Parent:  parentBlob,
 		Root:    rootBlob,
@@ -495,8 +495,8 @@ func (self *Commands) FileConflictFullInfo(querier subshelldomain.Querier, quick
 	return result, nil
 }
 
-func (self *Commands) FileConflictFullInfos(querier subshelldomain.Querier, quickInfos []FileConflictQuickInfo, parentLocation gitdomain.Location, rootBranch gitdomain.LocalBranchName) ([]MergeConflictFullInfo, error) {
-	result := make([]MergeConflictFullInfo, len(quickInfos))
+func (self *Commands) FileConflictFullInfos(querier subshelldomain.Querier, quickInfos []FileConflictQuickInfo, parentLocation gitdomain.Location, rootBranch gitdomain.LocalBranchName) ([]MergeConflictInfo, error) {
+	result := make([]MergeConflictInfo, len(quickInfos))
 	for q, quickInfo := range quickInfos {
 		fullInfo, err := self.FileConflictFullInfo(querier, quickInfo, parentLocation, rootBranch)
 		if err != nil {
