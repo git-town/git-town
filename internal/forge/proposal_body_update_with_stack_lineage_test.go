500293fd1235ba026ec8f467311112a060796fec
Kevin Goslar <kevin.goslar@gmail.com>
2025-08-11 18:08:02 -0500

    proposal_body_update_with_stack_lineage_test.go

--- internal/forge/proposal_body_update_with_stack_lineage_test.go
+++ internal/forge/proposal_body_update_with_stack_lineage_test.go
@@ -1,6 +1,7 @@
 package forge_test
 
 import (
+	"fmt"
 	"testing"
 
 	"github.com/git-town/git-town/v21/internal/forge"
@@ -31,156 +32,157 @@ func TestUpdateProposalBodyUpdateWithStackLineage(t *testing.T) {
 			description: "Empty body with non-empty lineage content",
 			currentBody: "",
 			lineage: `
-			main
-				feat-a
-					feat-b
-			`,
+					main
+						feat-a
+							feat-b
+					`,
 			want: `<!-- branch-stack -->
 
-			main
-				feat-a
-					feat-b
+					main
+						feat-a
+							feat-b
 
-<!-- branch-stack-end -->`,
+		<!-- branch-stack-end -->`,
 		},
-		{
-			description: "Proposal Body with multiple line body",
-			currentBody: `
-Git-town is a town of Gitters.
-These Gitters are the stackers of tomorrow
-			`,
-			lineage: `
-			main
-				feat-a
-					feat-b
-			`,
-			want: `
-Git-town is a town of Gitters.
-These Gitters are the stackers of tomorrow
+		// 		{
+		// 			description: "Proposal Body with multiple line body",
+		// 			currentBody: `
+		// Git-town is a town of Gitters.
+		// These Gitters are the stackers of tomorrow
+		// 			`,
+		// 			lineage: `
+		// 			main
+		// 				feat-a
+		// 					feat-b
+		// 			`,
+		// 			want: `
+		// Git-town is a town of Gitters.
+		// These Gitters are the stackers of tomorrow
 
+		// <!-- branch-stack -->
 
-<!-- branch-stack -->
+		// 			main
+		// 				feat-a
+		// 					feat-b
 
-			main
-				feat-a
-					feat-b
+		// <!-- branch-stack-end -->`,
+		// 		},
+		// 		{
+		// 			description: "Proposal with template where branch-stack hidden comment is in the middle of the proposal body",
+		// 			currentBody: `
+		// Git-town is a town of Gitters.
+		// <!-- branch-stack -->
+		// 			Please check the box that apply
+		// 			- [ ] Add Tests
+		// 			- [ ] Wrote Documentation
+		// 			- [ ] Fixed Infra
+		// 			`,
+		// 			lineage: `
+		// 			main
+		// 				feat-a
+		// 					feat-b
+		// 			`,
+		// 			want: `
+		// Git-town is a town of Gitters.
+		// <!-- branch-stack -->
 
-<!-- branch-stack-end -->`,
-		},
-		{
-			description: "Proposal with template where branch-stack hidden comment is in the middle of the proposal body",
-			currentBody: `
-Git-town is a town of Gitters.
-<!-- branch-stack -->
-			Please check the box that apply
-			- [ ] Add Tests
-			- [ ] Wrote Documentation
-			- [ ] Fixed Infra
-			`,
-			lineage: `
-			main
-				feat-a
-					feat-b
-			`,
-			want: `
-Git-town is a town of Gitters.
-<!-- branch-stack -->
+		// 			main
+		// 				feat-a
+		// 					feat-b
 
-			main
-				feat-a
-					feat-b
+		// <!-- branch-stack-end -->
+		// 			Please check the box that apply
+		// 			- [ ] Add Tests
+		// 			- [ ] Wrote Documentation
+		// 			- [ ] Fixed Infra
+		// 			`,
+		// 		},
+		// 		{
+		// 			description: "Proposal existing proposal lineage in the middle of the body is updated",
+		// 			currentBody: `
+		// Git-town is a town of Gitters.
+		// <!-- branch-stack -->
 
-<!-- branch-stack-end -->
-			Please check the box that apply
-			- [ ] Add Tests
-			- [ ] Wrote Documentation
-			- [ ] Fixed Infra
-			`,
-		},
-		{
-			description: "Proposal existing proposal lineage in the middle of the body is updated",
-			currentBody: `
-Git-town is a town of Gitters.
-<!-- branch-stack -->
+		// 			main
+		// 				feat-a
 
-			main
-				feat-a
+		// <!-- branch-stack-end -->
+		// 			Please check the box that apply
+		// 			- [ ] Add Tests
+		// 			- [ ] Wrote Documentation
+		// 			- [ ] Fixed Infra
+		// 			`,
+		// 			lineage: `
+		// 			main
+		// 				feat-a
+		// 					feat-b
+		// 						feat-c
+		// 							feat-d
+		// 			`,
+		// 			want: `
+		// Git-town is a town of Gitters.
+		// <!-- branch-stack -->
 
-<!-- branch-stack-end -->
-			Please check the box that apply
-			- [ ] Add Tests
-			- [ ] Wrote Documentation
-			- [ ] Fixed Infra
-			`,
-			lineage: `
-			main
-				feat-a
-					feat-b
-						feat-c
-							feat-d
-			`,
-			want: `
-Git-town is a town of Gitters.
-<!-- branch-stack -->
+		// 			main
+		// 				feat-a
+		// 					feat-b
+		// 						feat-c
+		// 							feat-d
 
-			main
-				feat-a
-					feat-b
-						feat-c
-							feat-d
+		// <!-- branch-stack-end -->
+		// 			Please check the box that apply
+		// 			- [ ] Add Tests
+		// 			- [ ] Wrote Documentation
+		// 			- [ ] Fixed Infra
+		// 			`,
+		// 		},
+		// 		{
+		// 			description: "Proposal existing proposal lineage at the end of the body",
+		// 			currentBody: `
+		// Git-town is a town of Gitters.
+		// Please check the box that apply
+		// 	- [ ] Add Tests
+		// 	- [ ] Wrote Documentation
+		// 	- [ ] Fixed Infra
+		// <!-- branch-stack -->
 
-<!-- branch-stack-end -->
-			Please check the box that apply
-			- [ ] Add Tests
-			- [ ] Wrote Documentation
-			- [ ] Fixed Infra
-			`,
-		},
-		{
-			description: "Proposal existing proposal lineage at the end of the body",
-			currentBody: `
-Git-town is a town of Gitters.
-Please check the box that apply
-	- [ ] Add Tests
-	- [ ] Wrote Documentation
-	- [ ] Fixed Infra
-<!-- branch-stack -->
+		// 			main
+		// 				feat-a
 
-			main
-				feat-a
+		// <!-- branch-stack-end -->
+		// 			`,
+		// 			lineage: `
+		// 			main
+		// 				feat-a
+		// 					feat-b
+		// 						feat-c
+		// 							feat-d
+		// 			`,
+		// 			want: `
+		// Git-town is a town of Gitters.
+		// Please check the box that apply
+		// 	- [ ] Add Tests
+		// 	- [ ] Wrote Documentation
+		// 	- [ ] Fixed Infra
+		// <!-- branch-stack -->
 
-<!-- branch-stack-end -->
-			`,
-			lineage: `
-			main
-				feat-a
-					feat-b
-						feat-c
-							feat-d
-			`,
-			want: `
-Git-town is a town of Gitters.
-Please check the box that apply
-	- [ ] Add Tests
-	- [ ] Wrote Documentation
-	- [ ] Fixed Infra
-<!-- branch-stack -->
+		// 			main
+		// 				feat-a
+		// 					feat-b
+		// 						feat-c
+		// 							feat-d
 
-			main
-				feat-a
-					feat-b
-						feat-c
-							feat-d
-
-<!-- branch-stack-end -->
-			`,
-		},
+		// <!-- branch-stack-end -->
+		// 			`,
+		// 		},
 	} {
 		t.Run(tc.description, func(t *testing.T) {
 			t.Parallel()
 			// act
 			actual := forge.ProposalBodyUpdateWithStackLineage(tc.currentBody, tc.lineage)
 			// assert
+			fmt.Println(tc.want)
+			fmt.Println(actual)
 			must.EqOp(t, tc.want, actual)
 		})
 	}
