59534149909ae9dd222f62e31d9a423de978ce62
Kevin Goslar <kevin.goslar@gmail.com>
2025-08-12 16:51:03 -0500

    all.go

--- internal/vm/opcodes/all.go
+++ internal/vm/opcodes/all.go
@@ -96,6 +96,7 @@ func All() []shared.Opcode {
 		&RebaseContinue{},
 		&RebaseOntoRemoveDeleted{},
 		&RebaseOnto{},
+		&RebaseParentLocal{},
 		&RebaseParentRemote{},
 		&RebaseParentsUntilLocal{},
 		&RebaseTrackingBranch{},
