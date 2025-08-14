3041d07d49dcf32c5ef1d254122be51fba32c06d
Kevin Goslar <kevin.goslar@gmail.com>
2025-08-14 09:25:51 -0500

    optioncompare_in_tests.go

--- tools/optioncompare_in_tests/optioncompare_in_tests.go
+++ tools/optioncompare_in_tests/optioncompare_in_tests.go
@@ -50,20 +50,20 @@ func (self *wrongCompareFinder) Visit(node ast.Node) ast.Visitor {
 	}
 
 	// ensure if the second argument is a call to "Some"
-	someCallExpr := self.getSomeCall(callExpr.Args[1])
+	someCallExpr := getSomeCall(callExpr.Args[1])
 	if someCallExpr == nil {
 		return self
 	}
 
 	// Transform must.Eq(t, Some(x), y) to must.True(t, y.EqualSome(x))
-	self.transformToEqualSome(callExpr, someCallExpr, callExpr.Args[2])
+	transformToEqualSome(callExpr, someCallExpr, callExpr.Args[2])
 	self.modified = true
 
 	return self
 }
 
 // getSomeCall checks if the given expression is a call to "Some" and returns the call expression if so
-func (self *wrongCompareFinder) getSomeCall(expr ast.Expr) *ast.CallExpr {
+func getSomeCall(expr ast.Expr) *ast.CallExpr {
 	callExpr, ok := expr.(*ast.CallExpr)
 	if !ok {
 		return nil
@@ -79,30 +79,6 @@ func (self *wrongCompareFinder) getSomeCall(expr ast.Expr) *ast.CallExpr {
 		return callExpr
 	}
 	return nil
-}
-
-// transformToEqualSome transforms must.Eq(t, Some(x), y) to must.True(t, y.EqualSome(x))
-func (self *wrongCompareFinder) transformToEqualSome(mustEqCall *ast.CallExpr, someCall *ast.CallExpr, yArg ast.Expr) {
-	// Change the selector from "Eq" to "True"
-	selectorExpr := mustEqCall.Fun.(*ast.SelectorExpr)
-	selectorExpr.Sel.Name = "True"
-
-	// Create y.EqualSome(x) where x is the argument to Some
-	equalSomeCall := &ast.CallExpr{
-		Fun: &ast.SelectorExpr{
-			X: yArg,
-			Sel: &ast.Ident{
-				Name: "EqualSome",
-			},
-		},
-		Args: someCall.Args, // Use the arguments from the Some() call
-	}
-
-	// Replace the arguments: keep t (first arg), replace second and third with y.EqualSome(x)
-	mustEqCall.Args = []ast.Expr{
-		mustEqCall.Args[0], // keep t
-		equalSomeCall,      // y.EqualSome(x)
-	}
 }
 
 // isTestFile checks if the file is a unit test file
@@ -148,6 +124,30 @@ func shouldSkipPath(path string) bool {
 		return true
 	}
 	return false
+}
+
+// transformToEqualSome transforms must.Eq(t, Some(x), y) to must.True(t, y.EqualSome(x))
+func transformToEqualSome(mustEqCall *ast.CallExpr, someCall *ast.CallExpr, yArg ast.Expr) {
+	// Change the selector from "Eq" to "True"
+	selectorExpr := mustEqCall.Fun.(*ast.SelectorExpr)
+	selectorExpr.Sel.Name = "True"
+
+	// Create y.EqualSome(x) where x is the argument to Some
+	equalSomeCall := &ast.CallExpr{
+		Fun: &ast.SelectorExpr{
+			X: yArg,
+			Sel: &ast.Ident{
+				Name: "EqualSome",
+			},
+		},
+		Args: someCall.Args, // Use the arguments from the Some() call
+	}
+
+	// Replace the arguments: keep t (first arg), replace second and third with y.EqualSome(x)
+	mustEqCall.Args = []ast.Expr{
+		mustEqCall.Args[0], // keep t
+		equalSomeCall,      // y.EqualSome(x)
+	}
 }
 
 func main() {
