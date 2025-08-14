edf4bff47cddac4fe606f3eed0790af7b133b06a
Kevin Goslar <kevin.goslar@gmail.com>
2025-08-14 12:36:01 -0500

    sync_branches.go

--- internal/cmd/sync/sync_branches.go
+++ internal/cmd/sync/sync_branches.go
@@ -5,6 +5,23 @@ import (
 	"github.com/git-town/git-town/v21/internal/vm/opcodes"
 )
 
+/*
+
+New sync workflow:
+
+- sync each stack strictly from root to leafs
+- when syncing a branch:
+  - sync-onto its parent to update the branch and remove the old commits
+	  - the old commits are those that the branch had at the end of the previous sync
+		- if no previous commits are known, rebase against the parent normally
+	- force-push-with-lease to the tracking branch
+	  - if that fails, rebase against the tracking branch and then force-push-with-lease again
+- when syncing a branch whose remote is gone:
+  - sync-onto its parent
+	- if the branch is now empty: delete it
+
+*/
+
 // BranchesProgram syncs all given branches.
 func BranchesProgram(branchesToSync configdomain.BranchesToSync, args BranchProgramArgs) {
 	for _, branchToSync := range branchesToSync {
