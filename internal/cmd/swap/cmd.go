c792025ce4104f54184eaee283d6270d83b273f4
Yadi Abdalhalim <abdalhalim.yaadata@gmail.com>
2025-08-15 02:57:20 -0500

    enhance: update proposal stack lineage during git-town swap

--- internal/cmd/swap/cmd.go
+++ internal/cmd/swap/cmd.go
@@ -391,6 +391,18 @@ func swapProgram(repo execute.OpenRepoResult, data swapData, finalMessages strin
 		},
 		program: prog,
 	})
+	connector, hasConnector := data.connector.Get()
+	if data.config.NormalConfig.ProposalsShowLineage == forgedomain.ProposalsShowLineageCLI && hasConnector {
+		swapUpdateProposalStackLineagesProgram(swapUpdateProposalStackLineagesProgramArg{
+			program: prog,
+			proposalStackLineageArgs: forge.ProposalStackLineageArgs{
+				Connector:                connector,
+				CurrentBranch:            data.branchToSwapName,
+				Lineage:                  data.config.NormalConfig.Lineage,
+				MainAndPerennialBranches: data.config.MainAndPerennials(),
+			},
+		})
+	}
 	cmdhelpers.Wrap(prog, cmdhelpers.WrapOptions{
 		DryRun:                   data.config.NormalConfig.DryRun,
 		InitialStashSize:         data.stashSize,
