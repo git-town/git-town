ae3a1821ed3d10cce89e71bfeed5bd76d2a50657
Kevin Goslar <kevin.goslar@gmail.com>
2025-08-15 11:12:49 -0500

    en.go

--- internal/messages/en.go
+++ internal/messages/en.go
@@ -118,7 +118,6 @@ Please upgrade to the new format: create.new-branch-type = "prototype"`
 	DiffParentNoFeatureBranch           = "you can only diff-parent feature branches"
 	DiffProblem                         = "cannot list diff of %q and %q: %w"
 	DirCurrentProblem                   = "cannot determine the current directory"
-	DownMultipleChildren                = "branch %q has multiple children: %s"
 	DownNoChild                         = "branch %q has no children"
 	DryRun                              = "In dry run mode. No commands will be run. When run in normal mode, the command output will appear beneath the command. Some commands will only be run if necessary. For example: 'git push' will run if and only if there are local commits not on origin."
 
