d323a3a2bf943816d07db5320061ba86cf5579c6
Yadi Abdalhalim <abdalhalim.yaadata@gmail.com>
2025-08-15 03:44:13 -0500

    enhance: implement undo for proposal body update opcode

--- internal/vm/opcodes/proposal_update_body.go
+++ internal/vm/opcodes/proposal_update_body.go
@@ -2,6 +2,7 @@ package opcodes
 
 import (
 	"errors"
+	"fmt"
 
 	"github.com/git-town/git-town/v22/internal/forge/forgedomain"
 	"github.com/git-town/git-town/v22/internal/messages"
@@ -11,6 +12,10 @@ import (
 type ProposalUpdateBody struct {
 	Proposal    forgedomain.Proposal
 	UpdatedBody string
+}
+
+func (self *ProposalUpdateBody) AutomaticUndoError() error {
+	return fmt.Errorf(messages.ProposalUpdateBodyProblem, self.Proposal.Data.Data().Number)
 }
 
 func (self *ProposalUpdateBody) Run(args shared.RunArgs) error {
@@ -23,4 +28,17 @@ func (self *ProposalUpdateBody) Run(args shared.RunArgs) error {
 		return errors.New(messages.UpdateProposalBodyUnsupported)
 	}
 	return proposalBodyUpdater.UpdateProposalBody(self.Proposal.Data, self.UpdatedBody)
+}
+
+func (self *ProposalUpdateBody) ShouldUndoOnError() bool {
+	return true
+}
+
+func (self *ProposalUpdateBody) UndoExternalChanges() []shared.Opcode {
+	return []shared.Opcode{
+		&ProposalUpdateBody{
+			Proposal:    self.Proposal,
+			UpdatedBody: self.Proposal.Data.Data().Body.GetOrZero(),
+		},
+	}
 }
