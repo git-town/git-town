ea166dfe725f7d99ea753713152a754065cce8dd
Kevin Goslar <kevin.goslar@gmail.com>
2025-08-18 18:12:04 -0500

    Provide all values to env config

--- internal/config/envconfig/load.go
+++ internal/config/envconfig/load.go
@@ -1,9 +1,52 @@
 package envconfig
 
-import "github.com/git-town/git-town/v21/internal/config/configdomain"
+import (
+	"github.com/git-town/git-town/v21/internal/config/configdomain"
+	"github.com/git-town/git-town/v21/internal/forge/forgedomain"
+	"github.com/git-town/git-town/v21/internal/git/gitdomain"
+	. "github.com/git-town/git-town/v21/pkg/prelude"
+)
 
 func Load() configdomain.PartialConfig {
-	partialConfig := configdomain.EmptyPartialConfig()
-	partialConfig.GitHubToken = GitHubAPIToken()
-	return partialConfig
+	gitHubToken := GitHubAPIToken()
+	return configdomain.PartialConfig{
+		Aliases:                  configdomain.Aliases{},
+		AutoResolve:              None[configdomain.AutoResolve](),
+		BitbucketAppPassword:     None[forgedomain.BitbucketAppPassword](),
+		BitbucketUsername:        None[forgedomain.BitbucketUsername](),
+		BranchTypeOverrides:      configdomain.BranchTypeOverrides{},
+		CodebergToken:            None[forgedomain.CodebergToken](),
+		ContributionRegex:        None[configdomain.ContributionRegex](),
+		DevRemote:                None[gitdomain.Remote](),
+		DryRun:                   None[configdomain.DryRun](),
+		FeatureRegex:             None[configdomain.FeatureRegex](),
+		ForgeType:                None[forgedomain.ForgeType](),
+		GitHubConnectorType:      None[forgedomain.GitHubConnectorType](),
+		GitHubToken:              gitHubToken,
+		GitLabConnectorType:      None[forgedomain.GitLabConnectorType](),
+		GitLabToken:              None[forgedomain.GitLabToken](),
+		GitUserEmail:             None[gitdomain.GitUserEmail](),
+		GitUserName:              None[gitdomain.GitUserName](),
+		GiteaToken:               None[forgedomain.GiteaToken](),
+		HostingOriginHostname:    None[configdomain.HostingOriginHostname](),
+		Lineage:                  configdomain.Lineage{},
+		MainBranch:               None[gitdomain.LocalBranchName](),
+		NewBranchType:            None[configdomain.NewBranchType](),
+		ObservedRegex:            None[configdomain.ObservedRegex](),
+		Offline:                  None[configdomain.Offline](),
+		PerennialBranches:        gitdomain.LocalBranchNames{},
+		PerennialRegex:           None[configdomain.PerennialRegex](),
+		ProposalsShowLineage:     None[forgedomain.ProposalsShowLineage](),
+		PushHook:                 None[configdomain.PushHook](),
+		ShareNewBranches:         None[configdomain.ShareNewBranches](),
+		ShipDeleteTrackingBranch: None[configdomain.ShipDeleteTrackingBranch](),
+		ShipStrategy:             None[configdomain.ShipStrategy](),
+		SyncFeatureStrategy:      None[configdomain.SyncFeatureStrategy](),
+		SyncPerennialStrategy:    None[configdomain.SyncPerennialStrategy](),
+		SyncPrototypeStrategy:    None[configdomain.SyncPrototypeStrategy](),
+		SyncTags:                 None[configdomain.SyncTags](),
+		SyncUpstream:             None[configdomain.SyncUpstream](),
+		UnknownBranchType:        None[configdomain.UnknownBranchType](),
+		Verbose:                  None[configdomain.Verbose](),
+	}
 }
