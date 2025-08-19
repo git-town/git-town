f29cdac6de048cc68c5f9e2b27f6aea8e7af4bd5
Kevin Goslar <kevin.goslar@gmail.com>
2025-08-19 11:14:56 -0500

    env_vars_test.go

--- internal/config/envconfig/env_vars_test.go
+++ internal/config/envconfig/env_vars_test.go
@@ -18,5 +18,11 @@ func TestEnvVars(t *testing.T) {
 			have := envVars.Get("SYNC_TAGS")
 			must.EqOp(t, "yes", have)
 		})
+		t.Run("lookup by alternative name", func(t *testing.T) {
+			t.Parallel()
+			envVars := envconfig.NewEnvVars([]string{"SYNC_TAGS=yes"})
+			have := envVars.Get("OTHER_NAME", "OTHER_NAME_2", "SYNC_TAGS")
+			must.EqOp(t, "yes", have)
+		})
 	})
 }
