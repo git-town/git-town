5d70d83034b1fc53b7ec6e8dffd3c00583081717
Kevin Goslar <kevin.goslar@gmail.com>
2025-08-18 20:42:46 -0500

    load_test.go

--- internal/config/envconfig/load_test.go
+++ internal/config/envconfig/load_test.go
@@ -8,29 +8,31 @@ import (
 )
 
 func TestLoad(t *testing.T) {
-	t.Parallel()
-	t.Run("none set", func(t *testing.T) {
+	t.Run("GitHub Token", func(t *testing.T) {
 		t.Parallel()
-		env := envconfig.NewImmutableEnvironment([]string{})
-		have := envconfig.Load(env)
-		must.True(t, have.GitHubToken.IsNone())
-	})
-	t.Run("GITHUB_TOKEN is set", func(t *testing.T) {
-		t.Parallel()
-		env := envconfig.NewImmutableEnvironment([]string{"GITHUB_TOKEN=my-token"})
-		have := envconfig.Load(env)
-		must.True(t, have.GitHubToken.EqualSome("my-token"))
-	})
-	t.Run("GITHUB_AUTH_TOKEN is set", func(t *testing.T) {
-		t.Parallel()
-		env := envconfig.NewImmutableEnvironment([]string{"GITHUB_AUTH_TOKEN=my-auth-token"})
-		have := envconfig.Load(env)
-		must.True(t, have.GitHubToken.EqualSome("my-auth-token"))
-	})
-	t.Run("GITHUB_TOKEN and GITHUB_AUTH_TOKEN are set", func(t *testing.T) {
-		t.Parallel()
-		env := envconfig.NewImmutableEnvironment([]string{"GITHUB_AUTH_TOKEN=my-auth-token", "GITHUB_TOKEN=my-token"})
-		have := envconfig.Load(env)
-		must.True(t, have.GitHubToken.EqualSome("my-token"))
+		t.Run("none set", func(t *testing.T) {
+			t.Parallel()
+			env := envconfig.NewImmutableEnvironment([]string{})
+			have := envconfig.Load(env)
+			must.True(t, have.GitHubToken.IsNone())
+		})
+		t.Run("GITHUB_TOKEN is set", func(t *testing.T) {
+			t.Parallel()
+			env := envconfig.NewImmutableEnvironment([]string{"GITHUB_TOKEN=my-token"})
+			have := envconfig.Load(env)
+			must.True(t, have.GitHubToken.EqualSome("my-token"))
+		})
+		t.Run("GITHUB_AUTH_TOKEN is set", func(t *testing.T) {
+			t.Parallel()
+			env := envconfig.NewImmutableEnvironment([]string{"GITHUB_AUTH_TOKEN=my-auth-token"})
+			have := envconfig.Load(env)
+			must.True(t, have.GitHubToken.EqualSome("my-auth-token"))
+		})
+		t.Run("GITHUB_TOKEN and GITHUB_AUTH_TOKEN are set", func(t *testing.T) {
+			t.Parallel()
+			env := envconfig.NewImmutableEnvironment([]string{"GITHUB_AUTH_TOKEN=my-auth-token", "GITHUB_TOKEN=my-token"})
+			have := envconfig.Load(env)
+			must.True(t, have.GitHubToken.EqualSome("my-token"))
+		})
 	})
 }
