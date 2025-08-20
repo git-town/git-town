429c37f278c85036a3892a1a2e649f669038a55d
Kevin Goslar <kevin.goslar@gmail.com>
2025-08-20 07:08:35 -0500

    detached.go append

--- internal/cli/flags/detached.go
+++ internal/cli/flags/detached.go
@@ -6,19 +6,27 @@ import (
 	"github.com/spf13/cobra"
 )
 
-const detachedLong = "detached"
+const (
+	detachedLong = "detached"
+	noDetached   = "no-" + detachedLong
+)
 
 // type-safe access to the CLI arguments of type configdomain.Detached
 func Detached() (AddFunc, ReadDetachedFlagFunc) {
 	addFlag := func(cmd *cobra.Command) {
 		cmd.Flags().BoolP(detachedLong, "d", false, "don't update the perennial root branch")
+		cmd.Flags().Bool(noDetached, true, "")
 	}
 	readFlag := func(cmd *cobra.Command) (Option[configdomain.Detached], error) {
-		if !cmd.Flags().Changed(detachedLong) {
-			return None[configdomain.Detached](), nil
+		if cmd.Flags().Changed(detachedLong) {
+			value, err := cmd.Flags().GetBool(detachedLong)
+			return Some(configdomain.Detached(value)), err
 		}
-		value, err := cmd.Flags().GetBool(detachedLong)
-		return Some(configdomain.Detached(value)), err
+		if cmd.Flags().Changed(noDetached) {
+			value, err := cmd.Flags().GetBool(noDetached)
+			return Some(configdomain.Detached(!value)), err
+		}
+		return None[configdomain.Detached](), nil
 	}
 	return addFlag, readFlag
 }
--- website/src/commands/append.md
+++ website/src/commands/append.md
@@ -45,13 +45,17 @@ branch with the main branch as its parent.
 Adding the `--prototype` aka `-p` switch creates a
 [prototype branch](../branch-types.md#prototype-branches).
 
-#### `-d`<br>`--detached`
+#### `-d`<br>`--detached`<br>`--no-detached`
 
 The `--detached` aka `-d` flag enables
 [detached mode](../preferences/detached.md) for the current command. Detached
 mode does not pull updates from the main or perennial branch at the root of your
 branch hierarchy. This allows you to keep your branches in sync with each other
 and decide when to pull in changes from other developers.
+
+If detached mode is enabled through
+[configuration data](../preferences/detached.md), the `--no-detached` flag
+disables detached mode for the current command.
 
 #### `-c`<br>`--commit`
 
