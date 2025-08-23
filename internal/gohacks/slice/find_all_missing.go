7916b37da79265afa0ab208d7305dd00fb78abd4
Kevin Goslar <kevin.goslar@gmail.com>
2025-08-23 13:02:19 -0500

    find_all_missing.go

--- internal/gohacks/slice/find_all_missing.go
+++ internal/gohacks/slice/find_all_missing.go
@@ -7,9 +7,9 @@ import (
 // FindAllMissing provides all entries in additional that are not contained in existing.
 func FindAllMissing[S ~[]C, C comparable](existing, additional S) S { //nolint:ireturn
 	result := S{}
-	for a := range additional {
-		if !slices.Contains(existing, additional[a]) {
-			result = append(result, additional[a])
+	for _, element := range additional {
+		if !slices.Contains(existing, element) {
+			result = append(result, element)
 		}
 	}
 	return result
