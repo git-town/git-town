7a971a49463ccf8fc9c7e330c72492491030874b
Yadi Abdalhalim <abdalhalim.yaadata@gmail.com>
2025-08-24 01:43:59 -0400

    enhance: add swap proposal target program

---
+++ internal/cmd/swap/swap_proposal_targets_program.go
@@ -0,0 +1,37 @@
+package swap
+
+import (
+	"github.com/git-town/git-town/v21/internal/git/gitdomain"
+	"github.com/git-town/git-town/v21/internal/vm/opcodes"
+	"github.com/git-town/git-town/v21/internal/vm/program"
+	. "github.com/git-town/git-town/v21/pkg/prelude"
+)
+
+type swapProposalTargetsProgramArg struct {
+	current     swapBranch
+	grandParent gitdomain.LocalBranchName
+	parent      swapBranch
+	program     Mutable[program.Program]
+}
+
+func swapProposalTargetsProgram(args swapProposalTargetsProgramArg) {
+	currentBranchProposal, currentBranchHasProposal := args.current.proposal.Get()
+	parentBranchProposal, parentBranchHasProposal := args.parent.proposal.Get()
+	if !currentBranchHasProposal && !parentBranchHasProposal {
+		return
+	}
+	if parentBranchHasProposal {
+		args.program.Value.Add(&opcodes.ProposalUpdateTarget{
+			NewBranch: args.current.name,
+			OldBranch: args.grandParent,
+			Proposal:  parentBranchProposal,
+		})
+	}
+	if currentBranchHasProposal {
+		args.program.Value.Add(&opcodes.ProposalUpdateTarget{
+			NewBranch: args.grandParent,
+			OldBranch: args.parent.name,
+			Proposal:  currentBranchProposal,
+		})
+	}
+}
