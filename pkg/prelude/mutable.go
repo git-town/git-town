5f7bc9a57be4630459440dcbd037fdd56f21c6c8
Kevin Goslar <kevin.goslar@gmail.com>
2025-09-01 06:37:18 -0500

    mutable.go optional_mutable.go

--- pkg/prelude/mutable.go
+++ pkg/prelude/mutable.go
@@ -3,9 +3,8 @@ package prelude
 // Mutable respresents a mutable value.
 // A Mutable is always mutable, even if passed by value.
 type Mutable[T any] struct {
-	// the enclosed mutable value, okay to mutate it directly
-	value       *T
 	initialized bool
+	value       *T
 }
 
 // provides an non-mutable copy of the contained mutable value
@@ -14,6 +13,19 @@ func (self Mutable[T]) Immutable() T { //nolint:ireturn
 	return *self.value
 }
 
+// provides an non-mutable copy of the contained mutable value
+func (self Mutable[T]) Value() *T {
+	self.verify()
+	return self.value
+}
+
+// provides an non-mutable copy of the contained mutable value
+func (self Mutable[T]) verify() {
+	if !self.initialized {
+		panic("Found a Mutable instance that wasn't created with the NewMutable constructor function")
+	}
+}
+
 func NewMutable[T any](value *T) Mutable[T] {
-	return Mutable[T]{value, true}
+	return Mutable[T]{true, value}
 }
--- pkg/prelude/optional_mutable.go
+++ pkg/prelude/optional_mutable.go
@@ -7,11 +7,13 @@ import (
 
 // OptionalMutable represents a value that is both optional and mutable.
 type OptionalMutable[T any] struct {
-	Value *T
+	Value       *T
+	initialized bool
 }
 
 // Get provides null-safe mutable access to the contained value.
 func (self OptionalMutable[T]) Get() (value *T, hasValue bool) {
+	self.verify()
 	if self.IsSome() {
 		return self.Value, true
 	}
@@ -20,6 +22,7 @@ func (self OptionalMutable[T]) Get() (value *T, hasValue bool) {
 
 // GetOrPanic provides unsafe mutable access to the contained value.
 func (self OptionalMutable[T]) GetOrPanic() *T {
+	self.verify()
 	if value, has := self.Get(); has {
 		return value
 	}
@@ -28,16 +31,19 @@ func (self OptionalMutable[T]) GetOrPanic() *T {
 
 // IsNone indicates whether this option instance contains nothing.
 func (self OptionalMutable[T]) IsNone() bool {
+	self.verify()
 	return self.Value == nil
 }
 
 // IsSome indicates whether this option instance contains a value.
 func (self OptionalMutable[T]) IsSome() bool {
+	self.verify()
 	return self.Value != nil
 }
 
 // MarshalJSON is used when serializing this type to JSON.
 func (self OptionalMutable[T]) MarshalJSON() ([]byte, error) {
+	self.verify()
 	if value, hasValue := self.Get(); hasValue {
 		return json.Marshal(*value)
 	}
@@ -47,12 +53,14 @@ func (self OptionalMutable[T]) MarshalJSON() ([]byte, error) {
 // String provides the string serialization of the contained value.
 // None gets serialized into an empty string.
 func (self OptionalMutable[T]) String() string {
+	self.verify()
 	return self.StringOr("")
 }
 
 // StringOr provideds the string serialization of the contained value.
 // None gets serialized into the given alternative string representation.
 func (self OptionalMutable[T]) StringOr(other string) string {
+	self.verify()
 	if self.IsSome() {
 		return fmt.Sprint(self.Value)
 	}
@@ -61,6 +69,7 @@ func (self OptionalMutable[T]) StringOr(other string) string {
 
 // ToOption provides an immutable copy of this OptionalMut.
 func (self OptionalMutable[T]) ToOption() Option[T] {
+	self.verify()
 	if value, hasValue := self.Get(); hasValue {
 		return Some(*value)
 	}
@@ -69,6 +78,7 @@ func (self OptionalMutable[T]) ToOption() Option[T] {
 
 // UnmarshalJSON is used when de-serializing this type from JSON.
 func (self *OptionalMutable[T]) UnmarshalJSON(b []byte) error {
+	self.verify()
 	if string(b) == "null" {
 		self.Value = nil
 		return nil
@@ -78,9 +88,15 @@ func (self *OptionalMutable[T]) UnmarshalJSON(b []byte) error {
 	return json.Unmarshal(b, &self.Value)
 }
 
+func (self *OptionalMutable[T]) verify() {
+	if !self.initialized {
+		panic("Found an OptionalMutable that wasn't created by the MutableNone or MutableSome constructor function")
+	}
+}
+
 // MutableNone instantiates an empty MutableOption.
 func MutableNone[T any]() OptionalMutable[T] {
-	return OptionalMutable[T]{nil}
+	return OptionalMutable[T]{nil, true}
 }
 
 // MutableSome instantiates a new OptionP containing the given value.
@@ -89,5 +105,5 @@ func MutableSome[T any](value *T) OptionalMutable[T] {
 	if value == nil {
 		panic("Cannot create a SomeP out of a nil pointer")
 	}
-	return OptionalMutable[T]{value}
+	return OptionalMutable[T]{value, true}
 }
