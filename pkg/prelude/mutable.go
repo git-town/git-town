11e1d88fe0a47e914bece8688cea4139acf820d4
Kevin Goslar <kevin.goslar@gmail.com>
2025-09-01 06:24:29 -0500

    Move constructor to the correct file

--- pkg/prelude/mutable.go
+++ pkg/prelude/mutable.go
@@ -12,20 +12,6 @@ func (self Mutable[T]) Immutable() T { //nolint:ireturn
 	return *self.Value
 }
 
-// MutableNone instantiates an empty MutableOption.
-func MutableNone[T any]() OptionalMutable[T] {
-	return OptionalMutable[T]{nil}
-}
-
-// MutableSome instantiates a new OptionP containing the given value.
-// The value must exist, i.e. the pointer must not be nil.
-func MutableSome[T any](value *T) OptionalMutable[T] {
-	if value == nil {
-		panic("Cannot create a SomeP out of a nil pointer")
-	}
-	return OptionalMutable[T]{value}
-}
-
 func NewMutable[T any](value *T) Mutable[T] {
 	return Mutable[T]{value}
 }
