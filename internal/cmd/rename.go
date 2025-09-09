09151aa26d3be60b12cddf5ad782611e49cf4c66
Kevin Goslar <kevin.goslar@gmail.com>
2025-09-09 06:45:07 -0500

    rename.go

--- internal/cmd/rename.go
+++ internal/cmd/rename.go
@@ -315,8 +315,8 @@ func renameProgram(repo execute.OpenRepoResult, data renameData, finalMessages s
 			updateChildBranchProposalsToBranch(prog.Value, data.proposalsOfChildBranches, data.newBranch)
 			proposal, hasProposal := data.proposal.Get()
 			connector, hasConnector := data.connector.Get()
-			connectorCanUpdateProposalSource := hasConnector && connector.UpdateProposalSourceFn().IsSome()
-			if hasProposal && hasConnector && connectorCanUpdateProposalSource {
+			_, isAPIConnector := connector.(forgedomain.APIConnector)
+			if hasProposal && hasConnector && isAPIConnector {
 				prog.Value.Add(&opcodes.ProposalUpdateSource{
 					NewBranch: data.newBranch,
 					OldBranch: data.oldBranch.LocalBranchName(),
