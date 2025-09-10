527f89cade875e561285cc4bd31b6442aadcb872
Kevin Goslar <kevin.goslar@gmail.com>
2025-09-10 10:45:05 -0500

    auth_connector.go

--- internal/forge/bitbucketdatacenter/auth_connector.go
+++ internal/forge/bitbucketdatacenter/auth_connector.go
@@ -3,14 +3,12 @@ package bitbucketdatacenter
 import (
 	"github.com/git-town/git-town/v21/internal/cli/print"
 	"github.com/git-town/git-town/v21/internal/forge/forgedomain"
-	. "github.com/git-town/git-town/v21/pkg/prelude"
 )
 
 // type-check to ensure conformance to the Connector interface
 var (
 	bbdcAPIConnector AuthConnector
-	_                forgedomain.AuthVerifier = bbdcAPIConnector
-	_                forgedomain.Connector    = bbdcAPIConnector
+	_                forgedomain.Connector = bbdcAPIConnector
 )
 
 // AuthConnector provides access to the API of Bitbucket installations.
@@ -19,13 +17,4 @@ type AuthConnector struct {
 	log      print.Logger
 	token    string
 	username string
-}
-
-// TODO: delete this, it doesn't do anything
-func (self AuthConnector) VerifyConnection() forgedomain.VerifyConnectionResult {
-	return forgedomain.VerifyConnectionResult{
-		AuthenticatedUser:   None[string](),
-		AuthenticationError: nil,
-		AuthorizationError:  nil,
-	}
 }
