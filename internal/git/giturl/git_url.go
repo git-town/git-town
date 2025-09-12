f674f97c19d926c48de0f86a213a47de7b5e65dc
Kevin Goslar <kevin.goslar@gmail.com>
2025-09-12 10:46:22 -0500

    git_url.go git_url_test.go

--- internal/git/giturl/git_url.go
+++ internal/git/giturl/git_url.go
@@ -3,16 +3,18 @@ package giturl
 
 import (
 	"regexp"
+	"strings"
 
 	. "github.com/git-town/git-town/v21/pkg/prelude"
 )
 
 // Parts contains recognized parts of a Git URL.
 type Parts struct {
-	User Option[string] // optional username
-	Host string         // hostname of the Git server
-	Org  string         // name of the organization that the repo is in
-	Repo string         // name of the repository
+	User       Option[string] // optional username
+	Host       string         // hostname of the Git server
+	Org        string         // name of the organization that the repo is in
+	Repo       string         // name of the repository
+	Supergroup Option[string] // optional over-arching grouping
 }
 
 func Parse(url string) Option[Parts] {
@@ -22,15 +24,14 @@ func Parse(url string) Option[Parts] {
 		// capture "user@"
 		`(?P<user>.*@)?` +
 		// capture "host:" or "host/"
-		`(?P<host>.*?[:/])` +
+		`(?P<host>.*?)` +
 		// ignore the port
-		`(?:\.+\/)?` +
-		// capture "org/"
-		`(?P<org>.*\/)` +
-		// capture "repo"
-		`(?P<repo>.*?)` +
+		`(?:[:/]\[^/]*\/)?` +
+		// capture "supergroup/org/repo" path
+		`(.*)` +
 		// ignore trailing ".git"
-		`(?:\.git)?$`
+		`(?:\.git)?` +
+		`$`
 	regex := regexp.MustCompile(pattern)
 	matches := regex.FindStringSubmatch(url)
 	if matches == nil {
@@ -39,11 +40,25 @@ func Parse(url string) Option[Parts] {
 		// Remotes on the filesystem are not an error condition.
 		return None[Parts]()
 	}
+	parts := strings.Split(matches[3], "/")
+	var supergroup Option[string]
+	var org string
+	var repo string
+	switch len(parts) {
+	case 2:
+		org = parts[0]
+		repo = parts[1]
+	case 3:
+		supergroup = Some(parts[0])
+		org = parts[1]
+		repo = parts[2]
+	}
 	return Some(Parts{
-		Host: trimLast(matches[2]),
-		Org:  trimLast(matches[3]),
-		Repo: matches[4],
-		User: NewOption(trimLast(matches[1])),
+		Host:       trimLast(matches[2]),
+		Supergroup: supergroup,
+		Org:        trimLast(org),
+		Repo:       repo,
+		User:       NewOption(trimLast(matches[1])),
 	})
 }
 
