149770379ca4d8ab85d6032c301890fe291fac77
Kevin Goslar <kevin.goslar@gmail.com>
2025-09-12 20:00:51 -0500

    checkout_history_preserve.go

--- internal/vm/opcodes/checkout_history_preserve.go
+++ internal/vm/opcodes/checkout_history_preserve.go
@@ -18,12 +18,12 @@ func (self *CheckoutHistoryPreserve) Run(args shared.RunArgs) error {
 		// the branch cache is not initialized --> there were no branch changes --> no need to restore the branch history
 		return nil
 	}
-	actualPreviousBranch, hasActualPreviousBranch := args.Git.CurrentBranchCache.Previous().Get()
+	cachedPreviousBranch, hasCachedPreviousBranch := args.Git.CurrentBranchCache.Previous().Get()
 	// remove the current branch from the list of previous branch candidates because the current branch should never also be the previous branch
 	candidates := slice.GetAll(self.PreviousBranchCandidates)
 	candidatesWithoutCurrent := slice.Remove(candidates, cachedCurrentBranch)
-	expectedPreviousBranch, hasExpectedPreviousBranch := args.Git.FirstExistingBranch(args.Backend, candidatesWithoutCurrent...).Get()
-	if !hasExpectedPreviousBranch || !hasActualPreviousBranch || actualPreviousBranch == expectedPreviousBranch {
+	existingPreviousBranch, hasExistingPreviousBranch := args.Git.FirstExistingBranch(args.Backend, candidatesWithoutCurrent...).Get()
+	if !hasExistingPreviousBranch || !hasCachedPreviousBranch || cachedPreviousBranch == existingPreviousBranch {
 		return nil
 	}
 	// We	need to ignore errors here because failing to set the Git branch history
@@ -31,7 +31,7 @@ func (self *CheckoutHistoryPreserve) Run(args shared.RunArgs) error {
 	// This operation can fail for a number of reasons like the previous branch being
 	// checked out in another worktree, or concurrent Git access
 	args.PrependOpcodes(
-		&CheckoutUncached{Branch: expectedPreviousBranch},
+		&CheckoutUncached{Branch: existingPreviousBranch},
 		&CheckoutUncached{Branch: cachedCurrentBranch},
 	)
 	return nil
