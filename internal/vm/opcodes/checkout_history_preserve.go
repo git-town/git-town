c12799950be2ed5f12794ad3db5a0b0217ecf110
Kevin Goslar <kevin.goslar@gmail.com>
2025-09-12 20:28:45 -0500

    Use Option in CacheWithPrevious (#5537)

--- internal/vm/opcodes/checkout_history_preserve.go
+++ internal/vm/opcodes/checkout_history_preserve.go
@@ -13,17 +13,17 @@ type CheckoutHistoryPreserve struct {
 }
 
 func (self *CheckoutHistoryPreserve) Run(args shared.RunArgs) error {
-	if !args.Git.CurrentBranchCache.Initialized() {
+	cachedCurrentBranch, hasCachedCurrentBranch := args.Git.CurrentBranchCache.Get()
+	if !hasCachedCurrentBranch {
 		// the branch cache is not initialized --> there were no branch changes --> no need to restore the branch history
 		return nil
 	}
-	currentBranch := args.Git.CurrentBranchCache.Value()
-	actualPreviousBranch := args.Git.CurrentBranchCache.Previous()
+	cachedPreviousBranch, hasCachedPreviousBranch := args.Git.CurrentBranchCache.GetPrevious()
 	// remove the current branch from the list of previous branch candidates because the current branch should never also be the previous branch
 	candidates := slice.GetAll(self.PreviousBranchCandidates)
-	candidatesWithoutCurrent := slice.Remove(candidates, currentBranch)
-	expectedPreviousBranch, hasExpectedPreviousBranch := args.Git.FirstExistingBranch(args.Backend, candidatesWithoutCurrent...).Get()
-	if !hasExpectedPreviousBranch || actualPreviousBranch == expectedPreviousBranch {
+	candidatesWithoutCurrent := slice.Remove(candidates, cachedCurrentBranch)
+	existingPreviousBranch, hasExistingPreviousBranch := args.Git.FirstExistingBranch(args.Backend, candidatesWithoutCurrent...).Get()
+	if !hasExistingPreviousBranch || !hasCachedPreviousBranch || cachedPreviousBranch == existingPreviousBranch {
 		return nil
 	}
 	// We	need to ignore errors here because failing to set the Git branch history
@@ -31,8 +31,8 @@ func (self *CheckoutHistoryPreserve) Run(args shared.RunArgs) error {
 	// This operation can fail for a number of reasons like the previous branch being
 	// checked out in another worktree, or concurrent Git access
 	args.PrependOpcodes(
-		&CheckoutUncached{Branch: expectedPreviousBranch},
-		&CheckoutUncached{Branch: currentBranch},
+		&CheckoutUncached{Branch: existingPreviousBranch},
+		&CheckoutUncached{Branch: cachedCurrentBranch},
 	)
 	return nil
 }
