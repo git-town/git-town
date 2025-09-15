7d29200490d2244ed3acd206a1de5505ccf9c9a1
Yaadata (Yadi) <36752216+yaadata@users.noreply.github.com>
2025-09-15 07:45:34 -0400

    [internal/cmd/swap] enhance: update proposal stack lineage during git-town swap (#5424)

--- internal/cmd/swap/cmd.go
+++ internal/cmd/swap/cmd.go
@@ -378,6 +378,17 @@ func swapProgram(repo execute.OpenRepoResult, data swapData, finalMessages strin
 			program:     prog,
 		})
 	}
+	connector, hasConnector := data.connector.Get()
+	if data.config.NormalConfig.ProposalsShowLineage == forgedomain.ProposalsShowLineageCLI && hasConnector {
+		if proposalFinder, canFindProposals := connector.(forgedomain.ProposalFinder); canFindProposals {
+			swapUpdateProposalStackLineagesProgram(prog, forge.ProposalStackLineageArgs{
+				Connector:                proposalFinder,
+				CurrentBranch:            data.currentBranchName,
+				Lineage:                  data.config.NormalConfig.Lineage,
+				MainAndPerennialBranches: data.config.MainAndPerennials(),
+			})
+		}
+	}
 	cmdhelpers.Wrap(prog, cmdhelpers.WrapOptions{
 		DryRun:                   data.config.NormalConfig.DryRun,
 		InitialStashSize:         data.stashSize,
