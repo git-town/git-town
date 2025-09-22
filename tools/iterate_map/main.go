2d3a1aeb711ce306e6e224ad0094128c9a210a3a
Kevin Goslar <kevin.goslar@gmail.com>
2025-09-22 08:14:37 -0500

    main.go

--- tools/iterate_map/main.go
+++ tools/iterate_map/main.go
@@ -55,51 +55,52 @@ type mapIterationVisitor struct {
 	typeInfo *types.Info
 }
 
-func (v *mapIterationVisitor) Visit(node ast.Node) ast.Visitor {
+func (visitor *mapIterationVisitor) Visit(node ast.Node) ast.Visitor {
 	rangeStmt, isRangeStmt := node.(*ast.RangeStmt)
 	if !isRangeStmt {
-		return v
+		return visitor
 	}
-	if !v.isMapIteration(rangeStmt) {
-		return v
+	if !visitor.isMapIteration(rangeStmt) {
+		return visitor
 	}
-	pos := v.fset.Position(rangeStmt.Pos())
-	if v.hasIgnoreComment(rangeStmt) {
-		return v
+	if visitor.hasIgnoreComment(rangeStmt) {
+		return visitor
 	}
-	*v.errors++
+	*visitor.errors++
 	workDir, err := os.Getwd()
 	if err != nil {
 		fmt.Println(err.Error())
-		return v
+		return visitor
 	}
-	relPath, err := filepath.Rel(workDir, v.path)
+	relPath, err := filepath.Rel(workDir, visitor.path)
 	if err != nil {
-		relPath = v.path
+		fmt.Println(err.Error())
+		return visitor
 	}
-	fmt.Printf("%s:%d\n", relPath, pos.Line)
-	return v
+	position := visitor.fset.Position(rangeStmt.Pos())
+	fmt.Printf("%s:%d\n", relPath, position.Line)
+	return visitor
 }
 
-func (v *mapIterationVisitor) isMapIteration(rangeStmt *ast.RangeStmt) bool {
-	if v.typeInfo == nil {
+func (visitor *mapIterationVisitor) isMapIteration(rangeStmt *ast.RangeStmt) bool {
+	if visitor.typeInfo == nil {
 		return false
 	}
-	typ := v.typeInfo.TypeOf(rangeStmt.X)
+	typ := visitor.typeInfo.TypeOf(rangeStmt.X)
 	if typ == nil {
 		return false
 	}
-	return v.isMapType(typ)
+	return visitor.isMapType(typ)
 }
 
-func (v *mapIterationVisitor) hasIgnoreComment(rangeStmt *ast.RangeStmt) bool {
-	if v.file.Comments == nil {
+func (visitor *mapIterationVisitor) hasIgnoreComment(rangeStmt *ast.RangeStmt) bool {
+	if visitor.file.Comments == nil {
 		return false
 	}
-	rangePos := v.fset.Position(rangeStmt.Pos())
-	for _, commentGroup := range v.file.Comments {
+	rangePos := visitor.fset.Position(rangeStmt.Pos())
+	for _, commentGroup := range visitor.file.Comments {
 		for _, comment := range commentGroup.List {
-			commentPos := v.fset.Position(comment.Pos())
+			commentPos := visitor.fset.Position(comment.Pos())
 			if commentPos.Line == rangePos.Line && strings.HasPrefix(comment.Text, ignoreComment) {
 				return true
 			}
@@ -108,20 +109,19 @@ func (v *mapIterationVisitor) hasIgnoreComment(rangeStmt *ast.RangeStmt) bool {
 	return false
 }
 
-func (v *mapIterationVisitor) isMapType(typ types.Type) bool {
+func (visitor *mapIterationVisitor) isMapType(typ types.Type) bool {
 	switch t := typ.Underlying().(type) {
 	case *types.Map:
 		return true
 	case *types.Pointer:
-		return v.isMapType(t.Elem())
+		return visitor.isMapType(t.Elem())
 	case *types.Named:
-		return v.isMapType(t.Underlying())
+		return visitor.isMapType(t.Underlying())
 	}
 	return false
 }
 
 func shouldIgnorePath(path string) bool {
 	return strings.HasPrefix(path, "vendor/") ||
-		strings.HasPrefix(path, ".git/") ||
-		strings.Contains(path, "testdata/")
+		strings.HasPrefix(path, ".git/")
 }
