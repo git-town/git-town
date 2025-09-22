6ee7e178437d76af2d1679e76457297cb7027285
Kevin Goslar <kevin.goslar@gmail.com>
2025-09-22 07:00:31 -0500

    detect.go

--- internal/forge/detect.go
+++ internal/forge/detect.go
@@ -10,7 +10,6 @@ import (
 	"github.com/git-town/git-town/v21/internal/forge/github"
 	"github.com/git-town/git-town/v21/internal/forge/gitlab"
 	"github.com/git-town/git-town/v21/internal/git/giturl"
-	"github.com/git-town/git-town/v21/internal/gohacks/mapstools"
 	. "github.com/git-town/git-town/v21/pkg/prelude"
 )
 
@@ -18,20 +17,24 @@ func Detect(remoteURL giturl.Parts, userOverride Option[forgedomain.ForgeType]) 
 	if userOverride.IsSome() {
 		return userOverride
 	}
-	detectors := map[forgedomain.ForgeType]func(giturl.Parts) bool{
-		forgedomain.ForgeTypeAzureDevOps:         azuredevops.Detect,
-		forgedomain.ForgeTypeBitbucket:           bitbucketcloud.Detect,
-		forgedomain.ForgeTypeBitbucketDatacenter: bitbucketdatacenter.Detect,
-		forgedomain.ForgeTypeForgejo:             forgejo.Detect,
-		forgedomain.ForgeTypeGitea:               gitea.Detect,
-		forgedomain.ForgeTypeGitHub:              github.Detect,
-		forgedomain.ForgeTypeGitLab:              gitlab.Detect,
+	detectors := []detector{
+		{forgedomain.ForgeTypeAzureDevOps, azuredevops.Detect},
+		{forgedomain.ForgeTypeBitbucket, bitbucketcloud.Detect},
+		{forgedomain.ForgeTypeBitbucketDatacenter, bitbucketdatacenter.Detect},
+		{forgedomain.ForgeTypeForgejo, forgejo.Detect},
+		{forgedomain.ForgeTypeGitea, gitea.Detect},
+		{forgedomain.ForgeTypeGitHub, github.Detect},
+		{forgedomain.ForgeTypeGitLab, gitlab.Detect},
 	}
-	for forgeType := range mapstools.SortedKeys(detectors) {
-		detector := detectors[forgeType]
-		if detector(remoteURL) {
-			return Some(forgeType)
+	for _, detector := range detectors {
+		if detector.implementation(remoteURL) {
+			return Some(detector.forgeType)
 		}
 	}
 	return None[forgedomain.ForgeType]()
+}
+
+type detector struct {
+	forgeType      forgedomain.ForgeType
+	implementation func(giturl.Parts) bool
 }
