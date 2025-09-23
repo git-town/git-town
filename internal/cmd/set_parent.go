87d33eda8c5e4add7b916c96aac4f2fdd6cb7b0c
Kevin Goslar <kevin.goslar@gmail.com>
2025-09-22 19:46:40 -0500

    dependent_changes.feature independent_changes.feature merge.feature none.go set_parent.go no_parent.go set-parent

--- internal/cmd/set_parent.go
+++ internal/cmd/set_parent.go
@@ -61,6 +61,7 @@ main
 
 func setParentCommand() *cobra.Command {
 	addAutoResolveFlag, readAutoResolveFlag := flags.AutoResolve()
+	addNoParentFlag, readNoParentFlag := flags.NoParent()
 	addVerboseFlag, readVerboseFlag := flags.Verbose()
 	cmd := cobra.Command{
 		Use:     "set-parent [branch]",
@@ -70,8 +71,9 @@ func setParentCommand() *cobra.Command {
 		Long:    cmdhelpers.Long(setParentDesc, setParentHelp),
 		RunE: func(cmd *cobra.Command, args []string) error {
 			autoResolve, errAutoResolve := readAutoResolveFlag(cmd)
+			noParent, errNoParent := readNoParentFlag(cmd)
 			verbose, errVerbose := readVerboseFlag(cmd)
-			if cmp.Or(errAutoResolve, errVerbose) != nil {
+			if cmp.Or(errAutoResolve, errNoParent, errVerbose) != nil {
 				return errVerbose
 			}
 			cliConfig := cliconfig.New(cliconfig.NewArgs{
@@ -83,15 +85,16 @@ func setParentCommand() *cobra.Command {
 				Stash:        None[configdomain.Stash](),
 				Verbose:      verbose,
 			})
-			return executeSetParent(args, cliConfig)
+			return executeSetParent(args, cliConfig, noParent)
 		},
 	}
 	addAutoResolveFlag(&cmd)
+	addNoParentFlag(&cmd)
 	addVerboseFlag(&cmd)
 	return &cmd
 }
 
-func executeSetParent(args []string, cliConfig configdomain.PartialConfig) error {
+func executeSetParent(args []string, cliConfig configdomain.PartialConfig, noParent configdomain.NoParent) error {
 	repo, err := execute.OpenRepo(execute.OpenRepoArgs{
 		CliConfig:        cliConfig,
 		PrintBranchNames: true,
@@ -112,53 +115,55 @@ func executeSetParent(args []string, cliConfig configdomain.PartialConfig) error
 	}
 	var selectedParent gitdomain.LocalBranchName
 	newParentOpt := None[gitdomain.LocalBranchName]()
-	switch len(args) {
-	case 0:
-		// TODO: extract this logic into an "enterParent" function
-		excludeBranches := append(
-			gitdomain.LocalBranchNames{data.initialBranch},
-			data.config.NormalConfig.Lineage.Children(data.initialBranch)...,
-		)
-		entries := dialog.NewSwitchBranchEntries(dialog.NewSwitchBranchEntriesArgs{
-			BranchInfos:       data.branchesSnapshot.Branches,
-			BranchTypes:       []configdomain.BranchType{},
-			BranchesAndTypes:  repo.UnvalidatedConfig.UnvalidatedBranchesAndTypes(data.branchesSnapshot.Branches.Names()),
-			ExcludeBranches:   excludeBranches,
-			Lineage:           repo.UnvalidatedConfig.NormalConfig.Lineage,
-			MainBranch:        repo.UnvalidatedConfig.UnvalidatedConfig.MainBranch,
-			Regexes:           []*regexp.Regexp{},
-			ShowAllBranches:   false,
-			UnknownBranchType: repo.UnvalidatedConfig.NormalConfig.UnknownBranchType,
-		})
-		noneEntry := dialog.SwitchBranchEntry{
-			Branch:        messages.SetParentNoneOption,
-			Indentation:   "",
-			OtherWorktree: false,
-			Type:          configdomain.BranchTypeFeatureBranch,
-		}
-		entries = append(dialog.SwitchBranchEntries{noneEntry}, entries...)
-		selectedParent, exit, err = dialog.SwitchBranch(dialog.SwitchBranchArgs{
-			CurrentBranch:      None[gitdomain.LocalBranchName](),
-			Cursor:             entries.IndexOf(data.defaultChoice),
-			DisplayBranchTypes: true,
-			Entries:            entries,
-			InputName:          fmt.Sprintf("parent-branch-for-%q", data.initialBranch),
-			Inputs:             data.inputs,
-			Title:              Some(fmt.Sprintf(messages.ParentBranchTitle, data.initialBranch)),
-			UncommittedChanges: false,
-		})
-		if err != nil || exit {
-			return err
-		}
-		if selectedParent != messages.SetParentNoneOption {
+	if !noParent {
+		switch len(args) {
+		case 0:
+			// TODO: extract this logic into an "enterParent" function
+			excludeBranches := append(
+				gitdomain.LocalBranchNames{data.initialBranch},
+				data.config.NormalConfig.Lineage.Children(data.initialBranch)...,
+			)
+			entries := dialog.NewSwitchBranchEntries(dialog.NewSwitchBranchEntriesArgs{
+				BranchInfos:       data.branchesSnapshot.Branches,
+				BranchTypes:       []configdomain.BranchType{},
+				BranchesAndTypes:  repo.UnvalidatedConfig.UnvalidatedBranchesAndTypes(data.branchesSnapshot.Branches.Names()),
+				ExcludeBranches:   excludeBranches,
+				Lineage:           repo.UnvalidatedConfig.NormalConfig.Lineage,
+				MainBranch:        repo.UnvalidatedConfig.UnvalidatedConfig.MainBranch,
+				Regexes:           []*regexp.Regexp{},
+				ShowAllBranches:   false,
+				UnknownBranchType: repo.UnvalidatedConfig.NormalConfig.UnknownBranchType,
+			})
+			noneEntry := dialog.SwitchBranchEntry{
+				Branch:        messages.SetParentNoneOption,
+				Indentation:   "",
+				OtherWorktree: false,
+				Type:          configdomain.BranchTypeFeatureBranch,
+			}
+			entries = append(dialog.SwitchBranchEntries{noneEntry}, entries...)
+			selectedParent, exit, err = dialog.SwitchBranch(dialog.SwitchBranchArgs{
+				CurrentBranch:      None[gitdomain.LocalBranchName](),
+				Cursor:             entries.IndexOf(data.defaultChoice),
+				DisplayBranchTypes: true,
+				Entries:            entries,
+				InputName:          fmt.Sprintf("parent-branch-for-%q", data.initialBranch),
+				Inputs:             data.inputs,
+				Title:              Some(fmt.Sprintf(messages.ParentBranchTitle, data.initialBranch)),
+				UncommittedChanges: false,
+			})
+			if err != nil || exit {
+				return err
+			}
+			if selectedParent != messages.SetParentNoneOption {
+				newParentOpt = Some(selectedParent)
+			}
+		case 1:
+			selectedParent = gitdomain.NewLocalBranchName(args[0])
+			if !data.branchesSnapshot.Branches.HasLocalBranch(selectedParent) {
+				return fmt.Errorf(messages.BranchDoesntExist, selectedParent)
+			}
 			newParentOpt = Some(selectedParent)
 		}
-	case 1:
-		selectedParent = gitdomain.NewLocalBranchName(args[0])
-		if !data.branchesSnapshot.Branches.HasLocalBranch(selectedParent) {
-			return fmt.Errorf(messages.BranchDoesntExist, selectedParent)
-		}
-		newParentOpt = Some(selectedParent)
 	}
 	runProgram, exit := setParentProgram(newParentOpt, data)
 	if exit {
