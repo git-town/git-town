844aa7268ea48220f872a8b95e6a51a3e5fc7e95
Yadi Abdalhalim <abdalhalim.yaadata@gmail.com>
2025-10-01 22:36:29 -0500

    review: simplify conditional statement

--- internal/cmd/delete.go
+++ internal/cmd/delete.go
@@ -348,23 +348,25 @@ func deleteFeatureBranch(prog, finalUndoProgram Mutable[program.Program], data d
 		prog.Value.Add(&opcodes.BranchTrackingDelete{Branch: trackingBranchToDelete})
 	}
 	deleteLocalBranch(prog, finalUndoProgram, data)
-	if connector, hasConnector := data.connector.Get(); data.config.NormalConfig.ProposalsShowLineage == forgedomain.ProposalsShowLineageCLI && hasConnector {
-		if proposalFinder, hasProposalFinder := connector.(forgedomain.ProposalFinder); hasProposalFinder {
-			tree, err := forge.NewProposalStackLineageTree(forge.ProposalStackLineageArgs{
-				Connector:                proposalFinder,
-				CurrentBranch:            data.initialBranch,
-				Lineage:                  data.config.NormalConfig.Lineage,
-				MainAndPerennialBranches: data.config.MainAndPerennials(),
-			})
-			if err != nil {
-				fmt.Printf("failed to update proposal stack lineage: %s\n", err.Error())
-			} else {
-				for branch, proposal := range tree.BranchToProposal { // okay to iterate the map in random order
-					prog.Value.Add(&opcodes.ProposalUpdateLineage{
-						Current:         branch,
-						CurrentProposal: proposal,
-						LineageTree:     MutableSome(tree),
-					})
+	if data.config.NormalConfig.ProposalsShowLineage == forgedomain.ProposalsShowLineageCLI {
+		if connector, hasConnector := data.connector.Get(); hasConnector {
+			if proposalFinder, hasProposalFinder := connector.(forgedomain.ProposalFinder); hasProposalFinder {
+				tree, err := forge.NewProposalStackLineageTree(forge.ProposalStackLineageArgs{
+					Connector:                proposalFinder,
+					CurrentBranch:            data.initialBranch,
+					Lineage:                  data.config.NormalConfig.Lineage,
+					MainAndPerennialBranches: data.config.MainAndPerennials(),
+				})
+				if err != nil {
+					fmt.Printf("failed to update proposal stack lineage: %s\n", err.Error())
+				} else {
+					for branch, proposal := range tree.BranchToProposal { // okay to iterate the map in random order
+						prog.Value.Add(&opcodes.ProposalUpdateLineage{
+							Current:         branch,
+							CurrentProposal: proposal,
+							LineageTree:     MutableSome(tree),
+						})
+					}
 				}
 			}
 		}
