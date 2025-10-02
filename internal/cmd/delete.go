2656c987d3bb566daa2f559cb1ceafdb39651a11
Yaadata (Yadi) <36752216+yaadata@users.noreply.github.com>
2025-10-01 23:04:52 -0500

    [internal/cmd] enhance(delete): add ability to update proposal lineage on delete command (#5596)
    ## Summary
    
    Add ability to update proposal lineage when executing
    
    ```zsh
    $ git-town delete
    ```
    
    ## Testing
    
    - [x] Proposal lineage updated when deleting a branch in the stack that
    does not have a proposal
    - [x] Proposal lineage updated when deleting a branch in the stack that
    does have a proposal
    
    <!-- branch-stack -->
    
    -------------------------
     - main
       - https://github.com/git-town/git-town/pull/5596 :point_left:
    
    Stack generated by [Git Town](https://github.com/git-town/git-town)
    
    <!-- branch-stack-end -->

--- internal/cmd/delete.go
+++ internal/cmd/delete.go
@@ -348,6 +348,36 @@ func deleteFeatureBranch(prog, finalUndoProgram Mutable[program.Program], data d
 		prog.Value.Add(&opcodes.BranchTrackingDelete{Branch: trackingBranchToDelete})
 	}
 	deleteLocalBranch(prog, finalUndoProgram, data)
+	if data.config.NormalConfig.ProposalsShowLineage == forgedomain.ProposalsShowLineageCLI {
+		if connector, hasConnector := data.connector.Get(); hasConnector {
+			if proposalFinder, hasProposalFinder := connector.(forgedomain.ProposalFinder); hasProposalFinder {
+				tree, err := forge.NewProposalStackLineageTree(forge.ProposalStackLineageArgs{
+					Connector:                proposalFinder,
+					CurrentBranch:            data.initialBranch,
+					Lineage:                  data.config.NormalConfig.Lineage,
+					MainAndPerennialBranches: data.config.MainAndPerennials(),
+				})
+				if err != nil {
+					fmt.Printf("failed to update proposal stack lineage: %s\n", err.Error())
+				} else {
+					for branch, proposal := range tree.BranchToProposal { // okay to iterate the map in random order
+						// Do not update the proposal of the deleted branch.
+						// At this point, a forge (like github) would close
+						// the proposal because there is no longer a remote
+						// branch.
+						if branch == data.initialBranch {
+							continue
+						}
+						prog.Value.Add(&opcodes.ProposalUpdateLineage{
+							Current:         branch,
+							CurrentProposal: proposal,
+							LineageTree:     MutableSome(tree),
+						})
+					}
+				}
+			}
+		}
+	}
 }
 
 func deleteLocalBranch(prog, finalUndoProgram Mutable[program.Program], data deleteData) {
