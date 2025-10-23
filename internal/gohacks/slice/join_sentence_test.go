f24f661c0886ebd012be8b3f327248f511332524
Kevin Goslar <kevin.goslar@gmail.com>
2025-10-23 16:07:37 -0500

    join_sentence_test.go

--- internal/gohacks/slice/join_sentence_test.go
+++ internal/gohacks/slice/join_sentence_test.go
@@ -10,35 +10,35 @@ import (
 
 func TestJoinSentenceQuotes(t *testing.T) {
 	t.Parallel()
-	t.Run("empty", func(t *testing.T) {
+	t.Run("0 elements", func(t *testing.T) {
 		t.Parallel()
 		give := []configdomain.BranchType{}
 		have := slice.JoinSentenceQuotes(give)
 		want := ""
 		must.EqOp(t, want, have)
 	})
-	t.Run("one", func(t *testing.T) {
+	t.Run("1 element", func(t *testing.T) {
 		t.Parallel()
 		give := []configdomain.BranchType{configdomain.BranchTypeMainBranch}
 		have := slice.JoinSentenceQuotes(give)
 		want := `"main"`
 		must.EqOp(t, want, have)
 	})
-	t.Run("two", func(t *testing.T) {
+	t.Run("2 elements", func(t *testing.T) {
 		t.Parallel()
 		give := []configdomain.BranchType{configdomain.BranchTypeMainBranch, configdomain.BranchTypeFeatureBranch}
 		have := slice.JoinSentenceQuotes(give)
 		want := `"main" and "feature"`
 		must.EqOp(t, want, have)
 	})
-	t.Run("three", func(t *testing.T) {
+	t.Run("3 elements", func(t *testing.T) {
 		t.Parallel()
 		give := []configdomain.BranchType{configdomain.BranchTypeMainBranch, configdomain.BranchTypeFeatureBranch, configdomain.BranchTypeContributionBranch}
 		have := slice.JoinSentenceQuotes(give)
 		want := `"main", "feature", and "contribution"`
 		must.EqOp(t, want, have)
 	})
-	t.Run("four", func(t *testing.T) {
+	t.Run("4 elements", func(t *testing.T) {
 		t.Parallel()
 		give := []configdomain.BranchType{configdomain.BranchTypeMainBranch, configdomain.BranchTypeFeatureBranch, configdomain.BranchTypeContributionBranch, configdomain.BranchTypeObservedBranch}
 		have := slice.JoinSentenceQuotes(give)
