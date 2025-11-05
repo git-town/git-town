a74e4dd816b25e9be96c18984636bbcb34f1428c
Kevin Goslar <kevin.goslar@gmail.com>
2025-11-05 09:25:17 -0600

    split_non_empty_lines.go

--- internal/gohacks/split_non_empty_lines.go
+++ internal/gohacks/split_non_empty_lines.go
@@ -2,7 +2,7 @@ package gohacks
 
 import "strings"
 
-// splitNonEmptyLines splits the input by newlines and returns non-empty, trimmed lines.
+// SplitNonEmptyLines splits the input by newlines and returns non-empty, trimmed lines.
 func SplitNonEmptyLines(output string) []string {
 	if output == "" {
 		return []string{}
