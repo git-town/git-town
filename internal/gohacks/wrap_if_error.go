3b44674ad71cf2c1c39ebfbca19db40cc8bfa1eb
Kevin Goslar <kevin.goslar@gmail.com>
2025-11-05 13:48:02 -0600

    wrap_if_error.go [skip ci]

--- internal/gohacks/wrap_if_error.go
+++ internal/gohacks/wrap_if_error.go
@@ -5,7 +5,6 @@ import (
 )
 
 // WrapIfError wraps the given error in the given text.
-// Returns nil if the given error is nil.
 func WrapIfError(err error, text string, a ...any) error {
 	if err == nil {
 		return nil
