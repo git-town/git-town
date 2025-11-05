651eaa63edd0e157ac11e0ebecf0e3ab2601a42d
Kevin Goslar <kevin.goslar@gmail.com>
2025-11-05 10:54:57 -0600

    beam.go [skip ci]

--- internal/config/configdomain/beam.go
+++ internal/config/configdomain/beam.go
@@ -1,6 +1,6 @@
 package configdomain
 
-// Beam indicates whether a Git Town command should beam commits onto the created branch.
+// Beam indicates whether a Git Town command should beam commits to the new branch.
 type Beam bool
 
 func (self Beam) ShouldBeam() bool {
