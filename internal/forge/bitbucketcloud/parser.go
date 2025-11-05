aa5950110943937f65c1ae27d1c779a0a229099e
Kevin Goslar <kevin.goslar@gmail.com>
2025-11-05 12:35:17 -0600

    Fix Bitbucket-Cloud connector check (#5726)

--- internal/forge/bitbucketcloud/parser.go
+++ internal/forge/bitbucketcloud/parser.go
@@ -59,7 +59,7 @@ func parsePullRequest(pullRequest map[string]interface{}) (result forgedomain.Bi
 	if !ok {
 		return result, errors.New(messages.APIUnexpectedResultDataStructure)
 	}
-	source1 := pullRequest["source"]
+	source1, has := pullRequest["source"]
 	if !has {
 		return result, errors.New(messages.APIUnexpectedResultDataStructure)
 	}
