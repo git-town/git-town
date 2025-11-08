ac4e8e3eb33368f4d4f88cf7ae0816508039b61b
Kevin Goslar <kevin.goslar@gmail.com>
2025-11-07 20:06:13 -0600

    equal_ignore_whitespace_test.go

--- internal/gohacks/stringslice/equal_ignore_whitespace_test.go
+++ internal/gohacks/stringslice/equal_ignore_whitespace_test.go
@@ -4,50 +4,49 @@ import (
 	"testing"
 
 	"github.com/git-town/git-town/v22/internal/gohacks/stringslice"
+	"github.com/shoenig/test/must"
 )
 
 func TestEqualIgnoreWhitespace(t *testing.T) {
 	t.Parallel()
 
 	tests := []struct {
-		name       string
-		fileLines  []string
-		tableLines []string
-		want       bool
+		name  string
+		give1 []string
+		give2 []string
+		want  bool
 	}{
 		{
-			name:       "exact match",
-			fileLines:  []string{"one", "two"},
-			tableLines: []string{"one", "two"},
-			want:       true,
+			name:  "exact match",
+			give1: []string{"one", "two"},
+			give2: []string{"one", "two"},
+			want:  true,
 		},
 		{
-			name:       "match with different indentation",
-			fileLines:  []string{"    one", "    two"},
-			tableLines: []string{"  one", "  two"},
-			want:       true,
+			name:  "match with different indentation",
+			give1: []string{"    one", "    two"},
+			give2: []string{"  one", "  two"},
+			want:  true,
 		},
 		{
-			name:       "no match - different content",
-			fileLines:  []string{"one", "three"},
-			tableLines: []string{"one", "two"},
-			want:       false,
+			name:  "no match - different content",
+			give1: []string{"one", "three"},
+			give2: []string{"one", "two"},
+			want:  false,
 		},
 		{
-			name:       "no match - different length",
-			fileLines:  []string{"one"},
-			tableLines: []string{"one", "two"},
-			want:       false,
+			name:  "no match - different length",
+			give1: []string{"one"},
+			give2: []string{"one", "two"},
+			want:  false,
 		},
 	}
 
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			t.Parallel()
-			result := stringslice.EqualIgnoreWhitespace(tt.fileLines, tt.tableLines)
-			if result != tt.want {
-				t.Errorf("matchesTable() = %v, expected %v", result, tt.want)
-			}
+			have := stringslice.EqualIgnoreWhitespace(tt.give1, tt.give2)
+			must.EqOp(t, tt.want, have)
 		})
 	}
 }
