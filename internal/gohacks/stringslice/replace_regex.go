ccb25c7a48f77028a8b7474934baaf59db3b2bcd
Kevin Goslar <kevin.goslar@gmail.com>
2025-11-11 16:16:32 -0600

    replace_regex.go [skip ci]

--- internal/gohacks/stringslice/replace_regex.go
+++ internal/gohacks/stringslice/replace_regex.go
@@ -2,10 +2,11 @@ package stringslice
 
 import "regexp"
 
-func ReplaceRegex(lines []string, regex *regexp.Regexp, replacement string) []string {
-	result := make([]string, len(lines))
-	for i, line := range lines {
-		result[i] = regex.ReplaceAllString(line, replacement)
+// ReplaceRegex replaces all matches of the given regex with the given replacement in the given strings.
+func ReplaceRegex(texts []string, regex *regexp.Regexp, replacement string) []string {
+	result := make([]string, len(texts))
+	for t, text := range texts {
+		result[t] = regex.ReplaceAllString(text, replacement)
 	}
 	return result
 }
