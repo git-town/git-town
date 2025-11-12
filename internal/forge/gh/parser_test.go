39c1781fe741bc17c3102899636da177e3b26649
Kevin Goslar <kevin.goslar@gmail.com>
2025-11-12 09:54:09 -0600

    parser_test.go

--- internal/forge/gh/parser_test.go
+++ internal/forge/gh/parser_test.go
@@ -51,7 +51,7 @@ func TestParseJSONOutput(t *testing.T) {
 		give := `[]`
 		have, err := gh.ParseJSONOutput(give, "branch")
 		must.NoError(t, err)
-		must.Eq(t, None[forgedomain.Proposal](), have)
+		must.Eq(t, []forgedomain.Proposal{}, have)
 	})
 
 	t.Run("single result", func(t *testing.T) {
@@ -70,18 +70,20 @@ func TestParseJSONOutput(t *testing.T) {
 ]`
 		have, err := gh.ParseJSONOutput(give, "branch")
 		must.NoError(t, err)
-		want := Some(forgedomain.Proposal{
-			Data: forgedomain.ProposalData{
-				Body:         Some("GitLab also provides a CLI app. This PR adds support for it similar to GitHub.\n"),
-				MergeWithAPI: true,
-				Number:       5079,
-				Source:       "kg-glab",
-				Target:       "main",
-				Title:        "glab connector type",
-				URL:          "https://github.com/git-town/git-town/pull/5079",
+		want := []forgedomain.Proposal{
+			{
+				Data: forgedomain.ProposalData{
+					Body:         Some("GitLab also provides a CLI app. This PR adds support for it similar to GitHub.\n"),
+					MergeWithAPI: true,
+					Number:       5079,
+					Source:       "kg-glab",
+					Target:       "main",
+					Title:        "glab connector type",
+					URL:          "https://github.com/git-town/git-town/pull/5079",
+				},
+				ForgeType: forgedomain.ForgeTypeGitHub,
 			},
-			ForgeType: forgedomain.ForgeTypeGitHub,
-		})
+		}
 		must.Eq(t, want, have)
 	})
 }
