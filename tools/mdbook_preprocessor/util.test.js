1e5b4bd41d44e985d4823c70460e58acbdb22353
Kevin Goslar <kevin.goslar@gmail.com>
2025-11-17 14:21:39 -0600

    mdbook_preprocessor.js types.d.ts util.js util.test.js

--- tools/mdbook_preprocessor/util.test.js
+++ tools/mdbook_preprocessor/util.test.js
@@ -1,29 +1,29 @@
 // @ts-check
 /// <reference types="node" />
 
-import assert from "node:assert/strict";
-import { describe, it } from "node:test";
+import assert from "node:assert/strict"
+import { describe, it } from "node:test"
 
-import { extractCommand, tokenize } from "./util.js";
+import { extractCommand, tokenize } from "./util.js"
 
 describe("tokenize", () => {
   it("should split string by spaces", () => {
-    const have = "git town append";
-    const want = ["git", "town", "append"];
-    assert.deepEqual(tokenize(have), want);
-  });
+    const have = "git town append"
+    const want = ["git", "town", "append"]
+    assert.deepEqual(tokenize(have), want)
+  })
 
   it("should not split tokens wrapped by various brackets", () => {
     for (const [left, right] of [["(", ")"], ["<", ">"], ["[", "]"]]) {
-      const have = `git town append ${left}-p | --prototype${right}`;
-      const want = ["git", "town", "append", `${left}-p | --prototype${right}`];
-      assert.deepEqual(tokenize(have), want);
+      const have = `git town append ${left}-p | --prototype${right}`
+      const want = ["git", "town", "append", `${left}-p | --prototype${right}`]
+      assert.deepEqual(tokenize(have), want)
     }
-  });
+  })
 
   it("should not split tokens wrapped by nested brackets", () => {
-    const have = "git town hack [<branch name>]";
-    const want = ["git", "town", "hack", "[<branch name>]"];
-    assert.deepEqual(tokenize(have), want);
-  });
-});
+    const have = "git town hack [<branch name>]"
+    const want = ["git", "town", "hack", "[<branch name>]"]
+    assert.deepEqual(tokenize(have), want)
+  })
+})
