c24f5a1caebe96f696bbaff9436c7e15cabc7320
Kevin Goslar <kevin.goslar@gmail.com>
2025-11-19 19:10:00 -0600

    env.go

--- internal/browser/env.go
+++ internal/browser/env.go
@@ -1,6 +1,7 @@
 package browser
 
 // name of the environment variable that defines the browser to use
+// TODO: move this logic into internal/config/envconfig/load.go
 const EnvVarName = "BROWSER"
 
 // if EnvVarName content is this value, pretend that you cannot open the browser
