a4e7bd3e28a8ada6c7b5fe390d15d67b3b443dd5
Kevin Goslar <kevin.goslar@gmail.com>
2025-11-20 17:07:52 -0600

    cli_config.go branch_prefix.go load.go

--- internal/config/configdomain/branch_prefix.go
+++ internal/config/configdomain/branch_prefix.go
@@ -1,6 +1,8 @@
 package configdomain
 
 import (
+	"strings"
+
 	"github.com/git-town/git-town/v22/internal/git/gitdomain"
 	. "github.com/git-town/git-town/v22/pkg/prelude"
 )
@@ -10,7 +12,10 @@ type BranchPrefix string
 
 // Apply provides the given branch name with this prefix applied.
 func (self BranchPrefix) Apply(branch gitdomain.LocalBranchName) gitdomain.LocalBranchName {
-	return gitdomain.NewLocalBranchName(string(self) + branch.String())
+	if strings.HasPrefix(branch.String(), self.String()) {
+		return branch
+	}
+	return gitdomain.NewLocalBranchName(self.String() + branch.String())
 }
 
 func (self BranchPrefix) String() string { return string(self) }
