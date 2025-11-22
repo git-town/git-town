f00f08d02b4d457f43637b07be234488390b0c15
Kevin Goslar <kevin.goslar@gmail.com>
2025-11-21 20:41:52 -0600

    checkout_child_or_other.go

--- internal/vm/opcodes/checkout_child_or_other.go
+++ internal/vm/opcodes/checkout_child_or_other.go
@@ -21,7 +21,7 @@ func (self *CheckoutChildOrOther) Run(args shared.RunArgs) error {
 	availableBranches = availableBranches.Remove(self.Branch)
 	descendents := args.Config.Value.NormalConfig.Lineage.Descendants(self.Branch, args.Config.Value.NormalConfig.Order)
 	ancestors := args.Config.Value.NormalConfig.Lineage.Ancestors(self.Branch)
-	branches := availableBranches.Hoist(descendents...).Hoist(ancestors...)
+	branches := availableBranches.Hoist(ancestors...).Hoist(descendents...)
 	for _, branch := range branches {
 		args.PrependOpcodes(&CheckoutIfNeeded{Branch: branch})
 		return nil
