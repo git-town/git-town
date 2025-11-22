9122f673af82b52c5fa8d90af36a38489602c3a2
Kevin Goslar <kevin.goslar@gmail.com>
2025-11-22 06:31:11 -0600

    branch_with_remote_gone_delete_if_empty_at_runtime.go

--- internal/vm/opcodes/branch_with_remote_gone_delete_if_empty_at_runtime.go
+++ internal/vm/opcodes/branch_with_remote_gone_delete_if_empty_at_runtime.go
@@ -28,7 +28,7 @@ func (self *BranchWithRemoteGoneDeleteIfEmptyAtRuntime) Run(args shared.RunArgs)
 		})
 	} else {
 		args.PrependOpcodes(
-			&CheckoutDescendentOrOtherIfNeeded{
+			&CheckoutDescendentOrOther{
 				Branch: self.Branch,
 			},
 			&BranchLocalDeleteContent{
