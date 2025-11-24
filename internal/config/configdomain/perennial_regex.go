90c01e4e2d3fd63602f9e177462958829f7f695b
Kevin Goslar <kevin.goslar@gmail.com>
2025-11-24 16:52:26 -0600

    perennial_regex.go

--- internal/config/configdomain/perennial_regex.go
+++ internal/config/configdomain/perennial_regex.go
@@ -1,6 +1,7 @@
 package configdomain
 
 import (
+	"errors"
 	"fmt"
 
 	"github.com/git-town/git-town/v22/internal/messages"
@@ -11,8 +12,17 @@ type PerennialRegex struct {
 	VerifiedRegex
 }
 
-func ParsePerennialRegex(value string, source string) (Option[PerennialRegex], error) {
-	verifiedRegexOpt, err := ParseRegex(value)
+func ParsePerennialRegex(value string, source string) (PerennialRegex, error) {
+	verifiedRegex, err := ParseRegex(value)
+	perennialRegex := PerennialRegex{VerifiedRegex: verifiedRegex}
+	if len(value) == 0 {
+		return perennialRegex, errors.New("empty regex")
+	}
+	return perennialRegex, err
+}
+
+func ParsePerennialRegexOpt(value string, source string) (Option[PerennialRegex], error) {
+	verifiedRegexOpt, err := ParseRegexOpt(value)
 	if err != nil {
 		return None[PerennialRegex](), fmt.Errorf(messages.CannotParse, source, err)
 	}
