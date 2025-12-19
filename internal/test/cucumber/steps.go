cec837574c0e8708dcbe8d9e736b089acd662480
Kevin Goslar <kevin.goslar@gmail.com>
2025-12-19 10:00:48 -0600

    steps.go

--- internal/test/cucumber/steps.go
+++ internal/test/cucumber/steps.go
@@ -99,7 +99,7 @@ func InitializeSuite(ctx *godog.TestSuiteContext) {
 }
 
 func defineSteps(sc *godog.ScenarioContext) {
-	// keep-sorted start block=yes newline_separated=yes case=no by_regex=\w+
+	// keep-sorted start block=yes newline_separated=yes case=no by_regex=(\w+)\s*(\w+)
 
 	sc.Step(`^a coworker clones the repository$`, func(ctx context.Context) {
 		state := ctx.Value(keyScenarioState).(*ScenarioState)
