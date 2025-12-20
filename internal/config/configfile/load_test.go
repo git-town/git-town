35d9254ec2d1d6d0f1f3223575438df08f282fc2
Kevin Goslar <kevin.goslar@gmail.com>
2025-12-19 19:12:08 -0600

    load_test.go

--- internal/config/configfile/load_test.go
+++ internal/config/configfile/load_test.go
@@ -254,200 +254,117 @@ perennials = [
 			must.Eq(t, want, *have)
 		})
 
-		// 		t.Run("outdated entries", func(t *testing.T) {
-		// 			t.Parallel()
-		// 			giveTOML := `
-		// create-prototype-branches = true
-		// push-hook = true
-		// push-new-branches = true
-		// ship-delete-tracking-branch = false
-		// ship-strategy = "api"
-		// sync-tags = false
-		// sync-upstream = true
+		t.Run("outdated entries", func(t *testing.T) {
+			t.Parallel()
+			giveTOML := `
+create-prototype-branches = true
+push-hook = true
+push-new-branches = true
+ship-delete-tracking-branch = false
+ship-strategy = "api"
+sync-tags = false
+sync-upstream = true
 
-		// [branches]
-		// main = "main"
-		// contribution-regex = "^gittown-"
-		// default-type = "contribution"
-		// display-types = "no main perennial"
-		// feature-regex = "^kg-"
-		// observed-regex = "^dependabot\\/"
-		// order = "desc"
-		// perennials = [ "public", "staging" ]
-		// perennial-regex = "release-.*"
-		// unknown-type = "prototype"
+[branches]
+main = "main"
+default-type = "contribution"
 
-		// [create]
-		// branch-prefix = "feature-"
-		// new-branch-type = "prototype"
-		// push-new-branches = true
-		// share-new-branches = "push"
-		// stash = true
+[create]
+push-new-branches = true
 
-		// [hosting]
-		// browser = "chrome"
-		// dev-remote = "origin"
-		// forge-type = "github"
-		// github-connector = "gh"
-		// gitlab-connector = "glab"
-		// origin-hostname = "github.com"
+[sync-strategy]
+feature-branches = "merge"
+perennial-branches = "rebase"
+prototype-branches = "compress"
+`[1:]
 
-		// [propose]
-		// lineage = "cli"
+			// step 1: decode into low-level data
+			haveData, err := configfile.Decode(giveTOML)
+			must.NoError(t, err)
+			wantData := configfile.Data{
+				Branches: &configfile.Branches{
+					DefaultType: Ptr("contribution"),
+					Main:        Ptr("main"),
+				},
+				Create: &configfile.Create{
+					PushNewbranches: Ptr(true),
+				},
+				Hosting: nil,
+				Propose: nil,
+				Ship:    nil,
+				Sync:    nil,
+				SyncStrategy: &configfile.SyncStrategy{
+					FeatureBranches:   Ptr("merge"),
+					PerennialBranches: Ptr("rebase"),
+					PrototypeBranches: Ptr("compress"),
+				},
+				CreatePrototypeBranches:  Ptr(true),
+				PushHook:                 Ptr(true),
+				PushNewbranches:          Ptr(true),
+				ShipDeleteTrackingBranch: Ptr(false),
+				ShipStrategy:             Ptr("api"),
+				SyncTags:                 Ptr(false),
+				SyncUpstream:             Ptr(true),
+			}
+			must.Eq(t, wantData, *haveData)
 
-		// [ship]
-		// delete-tracking-branch = false
-		// ignore-uncommitted = true
-		// strategy = "api"
-
-		// [sync]
-		// auto-resolve = false
-		// detached = true
-		// feature-strategy = "merge"
-		// perennial-strategy = "rebase"
-		// prototype-strategy = "compress"
-		// push-hook = true
-		// tags = false
-		// upstream = true
-
-		// [sync-strategy]
-		// feature-branches = "merge"
-		// perennial-branches = "rebase"
-		// prototype-branches = "compress"
-		// `[1:]
-
-		// 			// step 1: decode into low-level data
-		// 			haveData, err := configfile.Decode(giveTOML)
-		// 			must.NoError(t, err)
-		// 			wantData := configfile.Data{
-		// 				Branches: &configfile.Branches{
-		// 					ContributionRegex: Ptr("^gittown-"),
-		// 					DefaultType:       Ptr("contribution"),
-		// 					DisplayTypes:      Ptr("no main perennial"),
-		// 					FeatureRegex:      Ptr("^kg-"),
-		// 					Main:              Ptr("main"),
-		// 					ObservedRegex:     Ptr(`^dependabot\/`),
-		// 					Order:             Ptr("desc"),
-		// 					PerennialRegex:    Ptr("release-.*"),
-		// 					Perennials:        []string{"public", "staging"},
-		// 					UnknownType:       Ptr("prototype"),
-		// 				},
-		// 				Create: &configfile.Create{
-		// 					BranchPrefix:     Ptr("feature-"),
-		// 					NewBranchType:    Ptr("prototype"),
-		// 					PushNewbranches:  Ptr(true),
-		// 					ShareNewBranches: Ptr("push"),
-		// 					Stash:            Ptr(true),
-		// 				},
-		// 				Hosting: &configfile.Hosting{
-		// 					Browser:             Ptr("chrome"),
-		// 					DevRemote:           Ptr("origin"),
-		// 					ForgeType:           Ptr("github"),
-		// 					GitHubConnectorType: Ptr("gh"),
-		// 					GitLabConnectorType: Ptr("glab"),
-		// 					OriginHostname:      Ptr("github.com"),
-		// 				},
-		// 				Propose: &configfile.Propose{
-		// 					Lineage: Ptr("cli"),
-		// 				},
-		// 				Ship: &configfile.Ship{
-		// 					DeleteTrackingBranch: Ptr(false),
-		// 					IgnoreUncommitted:    Ptr(true),
-		// 					Strategy:             Ptr("api"),
-		// 				},
-		// 				Sync: &configfile.Sync{
-		// 					AutoResolve:       Ptr(false),
-		// 					Detached:          Ptr(true),
-		// 					FeatureStrategy:   Ptr("merge"),
-		// 					PerennialStrategy: Ptr("rebase"),
-		// 					PrototypeStrategy: Ptr("compress"),
-		// 					PushHook:          Ptr(true),
-		// 					Tags:              Ptr(false),
-		// 					Upstream:          Ptr(true),
-		// 				},
-		// 				SyncStrategy: &configfile.SyncStrategy{
-		// 					FeatureBranches:   Ptr("merge"),
-		// 					PerennialBranches: Ptr("rebase"),
-		// 					PrototypeBranches: Ptr("compress"),
-		// 				},
-		// 				CreatePrototypeBranches:  Ptr(true),
-		// 				PushHook:                 Ptr(true),
-		// 				PushNewbranches:          Ptr(true),
-		// 				ShipDeleteTrackingBranch: Ptr(false),
-		// 				ShipStrategy:             Ptr("api"),
-		// 				SyncTags:                 Ptr(false),
-		// 				SyncUpstream:             Ptr(true),
-		// 			}
-		// 			must.Eq(t, wantData, *haveData)
-
-		// 			// step 2: validate into high-level data
-		// 			finalMessages := stringslice.NewCollector()
-		// 			haveConfig, err := configfile.Validate(*haveData, finalMessages)
-		// 			must.NoError(t, err)
-		// 			contributionRegex := asserts.NoError1(configdomain.ParseContributionRegex("^gittown-", "test"))
-		// 			featureRegex := asserts.NoError1(configdomain.ParseFeatureRegex("feature-", "test"))
-		// 			observedRegex := asserts.NoError1(configdomain.ParseObservedRegex("observed-", "test"))
-		// 			perennialRegex := asserts.NoError1(configdomain.ParsePerennialRegex("perennial-", "test"))
-		// 			wantConfig := configdomain.PartialConfig{
-		// 				Aliases:              configdomain.Aliases{},
-		// 				AutoResolve:          Some(configdomain.AutoResolve(false)),
-		// 				AutoSync:             None[configdomain.AutoSync](),
-		// 				BitbucketAppPassword: None[forgedomain.BitbucketAppPassword](),
-		// 				BitbucketUsername:    None[forgedomain.BitbucketUsername](),
-		// 				BranchPrefix:         Some(configdomain.BranchPrefix("feature-")),
-		// 				BranchTypeOverrides:  configdomain.BranchTypeOverrides{},
-		// 				Browser:              Some(configdomain.Browser("chrome")),
-		// 				ContributionRegex:    contributionRegex,
-		// 				Detached:             Some(configdomain.Detached(true)),
-		// 				DevRemote:            Some(gitdomain.Remote("origin")),
-		// 				DisplayTypes: Some(configdomain.DisplayTypes{
-		// 					BranchTypes: []configdomain.BranchType{configdomain.BranchTypeMainBranch, configdomain.BranchTypePerennialBranch},
-		// 					Quantifier:  configdomain.QuantifierNo,
-		// 				}),
-		// 				DryRun:                   None[configdomain.DryRun](),
-		// 				FeatureRegex:             featureRegex,
-		// 				ForgeType:                asserts.NoError1(forgedomain.ParseForgeType("github", "test")),
-		// 				ForgejoToken:             None[forgedomain.ForgejoToken](),
-		// 				GitHubConnectorType:      Some(forgedomain.GitHubConnectorTypeGh),
-		// 				GitHubToken:              None[forgedomain.GitHubToken](),
-		// 				GitLabConnectorType:      Some(forgedomain.GitLabConnectorTypeGlab),
-		// 				GitLabToken:              None[forgedomain.GitLabToken](),
-		// 				GitUserEmail:             None[gitdomain.GitUserEmail](),
-		// 				GitUserName:              None[gitdomain.GitUserName](),
-		// 				GiteaToken:               None[forgedomain.GiteaToken](),
-		// 				HostingOriginHostname:    configdomain.ParseHostingOriginHostname("github.com"),
-		// 				IgnoreUncommitted:        Some(configdomain.IgnoreUncommitted(true)),
-		// 				Lineage:                  configdomain.NewLineage(),
-		// 				MainBranch:               Some(gitdomain.NewLocalBranchName("main")),
-		// 				NewBranchType:            Some(configdomain.NewBranchType(configdomain.BranchTypePrototypeBranch)),
-		// 				ObservedRegex:            observedRegex,
-		// 				Offline:                  None[configdomain.Offline](),
-		// 				Order:                    Some(configdomain.OrderDesc),
-		// 				PerennialBranches:        gitdomain.NewLocalBranchNames("public", "staging"),
-		// 				PerennialRegex:           perennialRegex,
-		// 				ProposalsShowLineage:     Some(forgedomain.ProposalsShowLineageCLI),
-		// 				PushBranches:             None[configdomain.PushBranches](),
-		// 				PushHook:                 Some(configdomain.PushHook(true)),
-		// 				ShareNewBranches:         Some(configdomain.ShareNewBranchesPush),
-		// 				ShipDeleteTrackingBranch: Some(configdomain.ShipDeleteTrackingBranch(false)),
-		// 				ShipStrategy:             Some(configdomain.ShipStrategyAPI),
-		// 				Stash:                    Some(configdomain.Stash(true)),
-		// 				SyncFeatureStrategy:      Some(configdomain.SyncFeatureStrategyMerge),
-		// 				SyncPerennialStrategy:    Some(configdomain.SyncPerennialStrategyRebase),
-		// 				SyncPrototypeStrategy:    Some(configdomain.SyncPrototypeStrategyCompress),
-		// 				SyncTags:                 Some(configdomain.SyncTags(false)),
-		// 				SyncUpstream:             Some(configdomain.SyncUpstream(true)),
-		// 				UnknownBranchType:        Some(configdomain.UnknownBranchType(configdomain.BranchTypePrototypeBranch)),
-		// 				Verbose:                  None[configdomain.Verbose](),
-		// 			}
-		// 			haveJSON := asserts.NoError1(json.MarshalIndent(haveConfig, "", "  "))
-		// 			wantJSON := asserts.NoError1(json.MarshalIndent(wantConfig, "", "  "))
-		// 			must.EqOp(t, string(wantJSON), string(haveJSON))
-
-		// 			// step 3: serialize back into TOML
-		// 			haveTOML := configfile.RenderTOML(haveConfig)
-		// 			must.EqOp(t, giveTOML, haveTOML)
-		// 		})
+			// step 2: validate into high-level data
+			finalMessages := stringslice.NewCollector()
+			haveConfig, err := configfile.Validate(*haveData, finalMessages)
+			must.NoError(t, err)
+			wantConfig := configdomain.PartialConfig{
+				Aliases:                  configdomain.Aliases{},
+				AutoResolve:              None[configdomain.AutoResolve](),
+				AutoSync:                 None[configdomain.AutoSync](),
+				BitbucketAppPassword:     None[forgedomain.BitbucketAppPassword](),
+				BitbucketUsername:        None[forgedomain.BitbucketUsername](),
+				BranchPrefix:             None[configdomain.BranchPrefix](),
+				BranchTypeOverrides:      configdomain.BranchTypeOverrides{},
+				Browser:                  None[configdomain.Browser](),
+				ContributionRegex:        None[configdomain.ContributionRegex](),
+				Detached:                 None[configdomain.Detached](),
+				DevRemote:                None[gitdomain.Remote](),
+				DisplayTypes:             None[configdomain.DisplayTypes](),
+				DryRun:                   None[configdomain.DryRun](),
+				FeatureRegex:             None[configdomain.FeatureRegex](),
+				ForgeType:                None[forgedomain.ForgeType](),
+				ForgejoToken:             None[forgedomain.ForgejoToken](),
+				GitHubConnectorType:      None[forgedomain.GitHubConnectorType](),
+				GitHubToken:              None[forgedomain.GitHubToken](),
+				GitLabConnectorType:      None[forgedomain.GitLabConnectorType](),
+				GitLabToken:              None[forgedomain.GitLabToken](),
+				GitUserEmail:             None[gitdomain.GitUserEmail](),
+				GitUserName:              None[gitdomain.GitUserName](),
+				GiteaToken:               None[forgedomain.GiteaToken](),
+				HostingOriginHostname:    None[configdomain.HostingOriginHostname](),
+				IgnoreUncommitted:        None[configdomain.IgnoreUncommitted](),
+				Lineage:                  configdomain.NewLineage(),
+				MainBranch:               Some(gitdomain.NewLocalBranchName("main")),
+				NewBranchType:            Some(configdomain.NewBranchType(configdomain.BranchTypePrototypeBranch)),
+				ObservedRegex:            None[configdomain.ObservedRegex](),
+				Offline:                  None[configdomain.Offline](),
+				Order:                    None[configdomain.Order](),
+				PerennialBranches:        gitdomain.LocalBranchNames{},
+				PerennialRegex:           None[configdomain.PerennialRegex](),
+				ProposalsShowLineage:     None[forgedomain.ProposalsShowLineage](),
+				PushBranches:             None[configdomain.PushBranches](),
+				PushHook:                 Some(configdomain.PushHook(true)),
+				ShareNewBranches:         Some(configdomain.ShareNewBranchesPush),
+				ShipDeleteTrackingBranch: Some(configdomain.ShipDeleteTrackingBranch(false)),
+				ShipStrategy:             Some(configdomain.ShipStrategyAPI),
+				Stash:                    None[configdomain.Stash](),
+				SyncFeatureStrategy:      Some(configdomain.SyncFeatureStrategyMerge),
+				SyncPerennialStrategy:    Some(configdomain.SyncPerennialStrategyRebase),
+				SyncPrototypeStrategy:    Some(configdomain.SyncPrototypeStrategyCompress),
+				SyncTags:                 Some(configdomain.SyncTags(false)),
+				SyncUpstream:             Some(configdomain.SyncUpstream(true)),
+				UnknownBranchType:        Some(configdomain.UnknownBranchType(configdomain.BranchTypeContributionBranch)),
+				Verbose:                  None[configdomain.Verbose](),
+			}
+			haveJSON := asserts.NoError1(json.MarshalIndent(haveConfig, "", "  "))
+			wantJSON := asserts.NoError1(json.MarshalIndent(wantConfig, "", "  "))
+			must.EqOp(t, string(wantJSON), string(haveJSON))
+		})
 
 	})
 }
