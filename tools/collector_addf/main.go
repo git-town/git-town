12195c94469b77f052e3da9255189db98625b751
Kevin Goslar <kevin.goslar@gmail.com>
2025-12-23 07:10:31 -0600

    main.go

--- tools/collector_addf/main.go
+++ tools/collector_addf/main.go
@@ -74,7 +74,7 @@ func (self *addfVisitor) Visit(node ast.Node) ast.Visitor {
 			return self
 		}
 
-		// Check if there's exactly one argument and it's a call to fmt.Sprintf
+		// ensure the only argument is a call to fmt.Sprintf
 		if !self.isFmtSprintf(callExpr.Args[0]) {
 			return self
 		}
