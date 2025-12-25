7840e058f623d50b85b1c314826f2cfbdab20c75
Kevin Goslar <kevin.goslar@gmail.com>
2025-12-25 14:05:34 -0600

    Battleplan to refactor this

--- internal/proposallineage/doc.go
+++ internal/proposallineage/doc.go
@@ -2,3 +2,13 @@
 // a tree of branch lineage and associated proposals
 // into the body of proposals.
 package proposallineage
+
+/* Ideas for refactoring this:
+* 1. Separate the proposal cache from the Tree.
+     Instead, provide a loadProposal function that uses that cache internally,
+		 and if the cache doesn't have it, loads via a connector.
+		 Given that we have cached connectors now, and need those anyways to reuse already loaded proposal data,
+		 maybe we can just use the findProposal method from the CachedConnector instance here.
+* 2. Extract a "render" function from Builder. It takes a Tree instance and returns the rendered Markdown.
+* 3. See if we can get rid of all these "rebuild" methods. They don't seem to match the domain and introduce too much mutability.
+*/
