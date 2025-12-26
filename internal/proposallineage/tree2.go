05191c5ecd2761a76572787bd237b4ee33e96941
Kevin Goslar <kevin.goslar@gmail.com>
2025-12-26 07:16:41 -0600

    tree2.go

--- internal/proposallineage/tree2.go
+++ internal/proposallineage/tree2.go
@@ -13,9 +13,37 @@ type TreeNode2 struct {
 // CalculateTree provides the full lineage tree for the given branch,
 // from the perennial root to all leafs that have the given branch as a descendent.
 func CalculateTree(branch gitdomain.LocalBranchName, lineage configdomain.Lineage) TreeNode2 {
-	result := TreeNode2{
-		Branch:   branch,
+	// Find the root ancestor of the given branch
+	root := lineage.Root(branch)
+
+	// Get all branches that should be in the tree:
+	// - All ancestors from root to branch
+	// - The branch itself
+	// - All descendants of the branch
+	ancestorsAndBranch := lineage.BranchAndAncestors(branch)
+	descendants := lineage.Descendants(branch, configdomain.OrderAsc)
+	relevantBranches := append(ancestorsAndBranch, descendants...)
+
+	// Build the tree recursively from the root
+	return buildTree2(root, lineage, relevantBranches)
+}
+
+// buildTree2 recursively builds a tree node for the given branch and its children,
+// filtering to only include branches in the relevantBranches set.
+func buildTree2(current gitdomain.LocalBranchName, lineage configdomain.Lineage, relevantBranches gitdomain.LocalBranchNames) TreeNode2 {
+	node := TreeNode2{
+		Branch:   current,
 		Children: []TreeNode2{},
 	}
-	return result
+
+	// Get all children and filter to only relevant ones
+	allChildren := lineage.Children(current, configdomain.OrderAsc)
+	for _, child := range allChildren {
+		if relevantBranches.Contains(child) {
+			childNode := buildTree2(child, lineage, relevantBranches)
+			node.Children = append(node.Children, childNode)
+		}
+	}
+
+	return node
 }
