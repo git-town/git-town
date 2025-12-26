b7a60d40762f7acfc775983ab0f9a8b311f67deb
Kevin Goslar <kevin.goslar@gmail.com>
2025-12-26 09:36:12 -0600

    render.go render_test.go

---
+++ internal/proposallineage2/render.go
@@ -0,0 +1,7 @@
+package proposallineage2
+
+import "github.com/git-town/git-town/v22/internal/git/gitdomain"
+
+func Render(tree TreeNodeWithProposal, currentBranch gitdomain.LocalBranchName) string {
+	return ""
+}
