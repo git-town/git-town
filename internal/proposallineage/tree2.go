e1050acd261046edc102f530bee31646c9221dce
Kevin Goslar <kevin.goslar@gmail.com>
2025-12-26 07:39:19 -0600

    tree2.go

--- internal/proposallineage/tree2.go
+++ internal/proposallineage/tree2.go
@@ -13,34 +13,22 @@ type TreeNode2 struct {
 // CalculateTree provides the full lineage tree for the given branch,
 // from the perennial root to all leafs that have the given branch as a descendent.
 func CalculateTree(branch gitdomain.LocalBranchName, lineage configdomain.Lineage) TreeNode2 {
-	// Find the root ancestor of the given branch
 	root := lineage.Root(branch)
-
-	// Get all branches that should be in the tree:
-	// - All ancestors from root to branch
-	// - The branch itself
-	// - All descendants of the branch
 	ancestorsAndBranch := lineage.BranchAndAncestors(branch)
 	descendants := lineage.Descendants(branch, configdomain.OrderAsc)
 	relevantBranches := append(ancestorsAndBranch, descendants...)
-
-	// Build the tree recursively from the root
 	return buildTree2(root, lineage, relevantBranches)
 }
 
-// buildTree2 recursively builds a tree node for the given branch and its children,
-// filtering to only include branches in the relevantBranches set.
-func buildTree2(current gitdomain.LocalBranchName, lineage configdomain.Lineage, relevantBranches gitdomain.LocalBranchNames) TreeNode2 {
+// builds a tree2 from the given root that contains only the given relevant branches
+func buildTree2(branch gitdomain.LocalBranchName, lineage configdomain.Lineage, includeBranches gitdomain.LocalBranchNames) TreeNode2 {
 	node := TreeNode2{
-		Branch:   current,
+		Branch:   branch,
 		Children: []TreeNode2{},
 	}
-
-	// Get all children and filter to only relevant ones
-	allChildren := lineage.Children(current, configdomain.OrderAsc)
-	for _, child := range allChildren {
-		if relevantBranches.Contains(child) {
-			childNode := buildTree2(child, lineage, relevantBranches)
+	for _, child := range lineage.Children(branch, configdomain.OrderAsc) {
+		if includeBranches.Contains(child) {
+			childNode := buildTree2(child, lineage, includeBranches)
 			node.Children = append(node.Children, childNode)
 		}
 	}
