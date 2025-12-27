3aee958da7381260b0682b30e68fd18aae80668e
Kevin Goslar <kevin.goslar@gmail.com>
2025-12-26 19:39:06 -0600

    en.go proposal_create.go proposal_update_lineage.go

--- internal/vm/opcodes/proposal_update_lineage.go
+++ internal/vm/opcodes/proposal_update_lineage.go
@@ -1,39 +1,50 @@
 package opcodes
 
 import (
+	"errors"
+
 	"github.com/git-town/git-town/v22/internal/forge/forgedomain"
 	"github.com/git-town/git-town/v22/internal/git/gitdomain"
-	"github.com/git-town/git-town/v22/internal/proposallineage"
+	"github.com/git-town/git-town/v22/internal/messages"
 	"github.com/git-town/git-town/v22/internal/proposallineage2"
 	"github.com/git-town/git-town/v22/internal/vm/shared"
-	. "github.com/git-town/git-town/v22/pkg/prelude"
 )
 
 type ProposalUpdateLineage struct {
-	Branch   gitdomain.LocalBranchName
-	Proposal Option[forgedomain.Proposal]
+	Branch gitdomain.LocalBranchName
 }
 
 func (self *ProposalUpdateLineage) Run(args shared.RunArgs) error {
-	proposal, hasProposal := self.Proposal.Get()
+	connector, hasConnector := args.Connector.Get()
+	if !hasConnector {
+		return forgedomain.UnsupportedServiceError()
+	}
+	proposalFinder, canFindProposals := connector.(forgedomain.ProposalFinder)
+	if !canFindProposals {
+		return errors.New(messages.ConnectorCannotFindProposals)
+	}
+	proposalBodyUpdater, canUpdateProposalBody := connector.(forgedomain.ProposalBodyUpdater)
+	if !canUpdateProposalBody {
+		return errors.New(messages.ConnectorCannotUpdateProposalBody)
+	}
+	parentBranch, hasParentBranch := args.Config.Value.NormalConfig.Lineage.Parent(self.Branch).Get()
+	if !hasParentBranch {
+		return nil
+	}
+	proposalOpt, err := proposalFinder.FindProposal(self.Branch, parentBranch)
+	if err != nil {
+		args.FinalMessages.Addf(messages.ProposalFindProblem, err.Error())
+		return nil
+	}
+	proposal, hasProposal := proposalOpt.Get()
 	if !hasProposal {
 		return nil
 	}
 	lineageSection := proposallineage2.RenderSection(args.Config.Value.NormalConfig.Lineage, self.Branch, args.Config.Value.NormalConfig.Order, args.Connector)
-	lineageArgs := proposallineage.ProposalStackLineageArgs{
-		Connector:                forgedomain.ProposalFinderFromConnector(args.Connector),
-		CurrentBranch:            self.Branch,
-		Lineage:                  args.Config.Value.NormalConfig.Lineage,
-		MainAndPerennialBranches: args.Config.Value.MainAndPerennials(),
-		Order:                    args.Config.Value.NormalConfig.Order,
+	updatedProposalBody := proposallineage2.UpdateProposalBody(proposal.Data.Data().Body.GetOrZero(), lineageSection)
+	err = proposalBodyUpdater.UpdateProposalBody(proposal.Data, updatedProposalBody)
+	if err != nil {
+		args.FinalMessages.Addf(messages.ProposalBodyUpdateProblem, err.Error())
 	}
-	builder, hasBuilder := proposallineage.NewBuilder(lineageArgs, self.LineageTree).Get()
-	if !hasBuilder {
-		return nil
-	}
-	args.PrependOpcodes(&ProposalUpdateBody{
-		Proposal:    proposal,
-		UpdatedBody: proposallineage.Add(proposal.Data.Data().Body.GetOrZero(), builder.Build(lineageArgs)),
-	})
 	return nil
 }
