69cf488edce30b900b1ad352c5372e6e21414f2d
Kevin Goslar <kevin.goslar@gmail.com>
2025-12-27 11:56:44 -0600

    en.go [skip ci]

--- internal/messages/en.go
+++ internal/messages/en.go
@@ -183,9 +183,9 @@ Please upgrade to the new format: create.new-branch-type = "prototype"`
 	HackTooManyArguments       = "please provide only one branch to create"
 
 	IgnoreUncommitted   = "Ship ignores uncommitted changes: %s\n"
-	InputAddOrRemove    = `invalid argument %s. Please provide either "add" or "remove"`
-	InputYesOrNo        = `invalid argument: %s. Please provide either "yes" or "no".\n`
-	InvalidStatusOutput = `invalid "git status -z" output: %s`
+	InputAddOrRemove    = `invalid argument %q. Please provide either "add" or "remove"`
+	InputYesOrNo        = `invalid argument: %q. Please provide either "yes" or "no".\n`
+	InvalidStatusOutput = `invalid "git status -z" output: %q`
 
 	MainBranch                       = "Main branch: %s\n"
 	MainBranchCannotMakeContribution = "cannot make the main branch a contribution branch"
