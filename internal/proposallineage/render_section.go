2954379dea0ef93e7c05059f8707af0018aef6f5
Kevin Goslar <kevin.goslar@gmail.com>
2025-12-29 09:39:12 -0600

    render_section.go [skip ci]

--- internal/proposallineage/render_section.go
+++ internal/proposallineage/render_section.go
@@ -7,6 +7,7 @@ import (
 	. "github.com/git-town/git-town/v22/pkg/prelude"
 )
 
+// RenderSection provides the branch lineage for the given branch in Markdown format, ready to be embedded into a proposal body.
 func RenderSection(lineage configdomain.Lineage, currentBranch gitdomain.LocalBranchName, order configdomain.Order, connector Option[forgedomain.Connector]) string {
 	// step 1: calculate the structure of the lineage tree to display
 	tree := CalculateTree(currentBranch, lineage, order)
