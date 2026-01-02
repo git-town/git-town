da7a7b63868c5c53cc23217b5dfee9e48c37c55d
Kevin Goslar <kevin.goslar@gmail.com>
2026-01-01 19:36:05 -0600

    generate_json_schema_test.go

--- tools/generate_json_schema/generate_json_schema_test.go
+++ tools/generate_json_schema/generate_json_schema_test.go
@@ -11,9 +11,8 @@ func TestCamelToKebab(t *testing.T) {
 	t.Parallel()
 	tests := map[string]string{
 		"SyncStrategy":            "sync-strategy",
-		"PushNewBranches":         "push-new-branches",
 		"CreatePrototypeBranches": "create-prototype-branches",
-		"BranchType":              "branch-type",
+		"Single":                  "single",
 	}
 	for give, want := range tests {
 		t.Run(fmt.Sprintf("%s -> %s", give, want), func(t *testing.T) {
