075e4d5d39c7388564208c2fade69d07f09cfbb2
Kevin Goslar <kevin.goslar@gmail.com>
2026-01-09 06:34:01 -0600

    an_test.go

--- internal/gohacks/an_test.go
+++ internal/gohacks/an_test.go
@@ -11,18 +11,18 @@ func TestAn(t *testing.T) {
 	t.Parallel()
 
 	tests := map[string]string{
-		"apple":  "an",
+		"apple":    "an",
 		"elephant": "an",
-		"igloo":  "an",
-		"orange": "an",
+		"igloo":    "an",
+		"orange":   "an",
 		"umbrella": "an",
-		"book": "a",
-		"car": "a",
-		"dog": "a",
-		"house": "a",
-		"tree": "a",
-		"zebra": "a",
-		"": "a",
+		"book":     "a",
+		"car":      "a",
+		"dog":      "a",
+		"house":    "a",
+		"tree":     "a",
+		"zebra":    "a",
+		"":         "a",
 	}
 
 	for give, want := range tests {
