a34d7f596eb8dadd46503a5fe4551e97747bc3a5
Kevin Goslar <kevin.goslar@gmail.com>
2026-01-09 19:38:34 -0600

    summary_section.test.ts

--- text-runner/lib/summary_section.test.ts
+++ text-runner/lib/summary_section.test.ts
@@ -41,14 +41,6 @@ suite("SummarySection", () => {
           ["-h", "--help"],
         ],
       },
-      {
-        desc: "commit command",
-        give: "git town config get-parent [<branch-name>] [-v | --verbose] [-h | --help]",
-        want: [
-          ["-v", "--verbose"],
-          ["-h", "--help"],
-        ],
-      },
     ]
     for (const { desc, give, want } of tests) {
       test(desc, () => {
