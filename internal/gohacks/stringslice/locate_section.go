dc5dc1513e350eaa231d1195596e6364772cdcd1
Kevin Goslar <kevin.goslar@gmail.com>
2026-01-10 19:25:55 -0600

    locate_section.go locate_section_test.go golden.go

--- internal/gohacks/stringslice/locate_section.go
+++ internal/gohacks/stringslice/locate_section.go
@@ -1,12 +1,14 @@
 package stringslice
 
+import . "github.com/git-town/git-town/v22/pkg/prelude"
+
 // LocateSection provides the line number where the given section starts within the given lines.
-func LocateSection(lines, section []string) (start int, found bool) { //nolint:nonamedreturns
+func LocateSection(lines, section []string) Option[int] {
 	sectionLength := len(section)
 	for i := 0; i <= len(lines)-sectionLength; i++ {
 		if EqualIgnoreWhitespace(lines[i:i+sectionLength], section) {
-			return i, true
+			return Some(i)
 		}
 	}
-	return -1, false
+	return None[int]()
 }
