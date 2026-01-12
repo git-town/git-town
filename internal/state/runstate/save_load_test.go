ef20ff698f8b5acf096fd2729e1104ea8be56142
Kevin Goslar <kevin.goslar@gmail.com>
2026-01-12 08:33:36 -0600

    Remove redundant unit test

--- internal/state/runstate/save_load_test.go
+++ internal/state/runstate/save_load_test.go
@@ -20,19 +20,6 @@ import (
 func TestLoadSave(t *testing.T) {
 	t.Parallel()
 
-	t.Run("SanitizePath", func(t *testing.T) {
-		t.Parallel()
-		tests := map[string]string{
-			"/home/user/development/git-town":        "home-user-development-git-town",
-			"c:\\Users\\user\\development\\git-town": "c-users-user-development-git-town",
-		}
-		for give, want := range tests {
-			rootDir := gitdomain.NewRepoRootDir(give)
-			have := state.SanitizePath(rootDir)
-			must.EqOp(t, want, have)
-		}
-	})
-
 	t.Run("Save and Load", func(t *testing.T) {
 		t.Parallel()
 		runState := runstate.RunState{
