f8de0d0c06e596161f9c10741a76a4f52344591f
Kevin Goslar <kevin.goslar@gmail.com>
2026-01-12 15:33:41 -0600

    data.go data_table.go data_test.go

---
+++ internal/test/mockproposals/data.go
@@ -0,0 +1,54 @@
+package mockproposals
+
+import (
+	"github.com/git-town/git-town/v22/internal/forge/forgedomain"
+	"github.com/git-town/git-town/v22/internal/git/gitdomain"
+	. "github.com/git-town/git-town/v22/pkg/prelude"
+)
+
+type MockProposals struct {
+	Dir       string // path of the JSON file containing the proposal data on disk
+	Proposals []forgedomain.ProposalData
+}
+
+func (self *MockProposals) FindByID(id int) OptionalMutable[forgedomain.ProposalData] {
+	for p, proposal := range self.Proposals {
+		if proposal.Number == id {
+			return MutableSome(&self.Proposals[p])
+		}
+	}
+	return MutableNone[forgedomain.ProposalData]()
+}
+
+func (self *MockProposals) FindBySourceAndTarget(source, target gitdomain.LocalBranchName) Option[forgedomain.ProposalData] {
+	for p, proposal := range self.Proposals {
+		if proposal.Source == source && proposal.Target == target {
+			return Some(self.Proposals[p])
+		}
+	}
+	return None[forgedomain.ProposalData]()
+}
+
+// Save stores the changes made to the given proposal to disk.
+func (self *MockProposals) Save(proposal forgedomain.ProposalData) {
+	self.Update(proposal)
+	Save(self.Dir, self.Proposals)
+}
+
+func (self *MockProposals) Search(source gitdomain.LocalBranchName) []forgedomain.ProposalData {
+	result := []forgedomain.ProposalData{}
+	for _, proposal := range self.Proposals {
+		if proposal.Source == source {
+			result = append(result, proposal)
+		}
+	}
+	return result
+}
+
+func (self *MockProposals) Update(proposal forgedomain.ProposalData) {
+	for p := range self.Proposals {
+		if self.Proposals[p].Number == proposal.Number {
+			self.Proposals[p] = proposal
+		}
+	}
+}
---
+++ internal/test/mockproposals/data_table.go
@@ -0,0 +1,100 @@
+package mockproposals
+
+import (
+	"fmt"
+	"strconv"
+
+	"github.com/cucumber/godog"
+	"github.com/git-town/git-town/v22/internal/config/configdomain"
+	"github.com/git-town/git-town/v22/internal/forge/forgedomain"
+	"github.com/git-town/git-town/v22/internal/git/gitdomain"
+	"github.com/git-town/git-town/v22/internal/test/datatable"
+	"github.com/git-town/git-town/v22/internal/test/helpers"
+	. "github.com/git-town/git-town/v22/pkg/prelude"
+)
+
+func FromGherkinTable(table *godog.Table, lineage configdomain.Lineage) []forgedomain.ProposalData {
+	result := make([]forgedomain.ProposalData, 0, len(table.Rows)-1)
+	headers := helpers.TableFields(table)
+	for i := 1; i < len(table.Rows); i++ {
+		id := Some(i)
+		source := None[gitdomain.LocalBranchName]()
+		target := None[gitdomain.LocalBranchName]()
+		title := None[gitdomain.ProposalTitle]()
+		body := None[gitdomain.ProposalBody]()
+		url := None[string]()
+		for f, field := range table.Rows[i].Cells {
+			switch headers[f] {
+			case "ID":
+				value, err := strconv.Atoi(field.Value)
+				if err != nil {
+					panic(err)
+				}
+				id = Some(value)
+			case "SOURCE BRANCH":
+				source = Some(gitdomain.NewLocalBranchName(field.Value))
+			case "TARGET BRANCH":
+				target = Some(gitdomain.NewLocalBranchName(field.Value))
+			case "TITLE":
+				title = Some(gitdomain.ProposalTitle(field.Value))
+			case "BODY":
+				body = Some(gitdomain.ProposalBody(field.Value))
+			case "URL":
+				url = Some(field.Value)
+			}
+		}
+		if id.IsNone() {
+			id = Some(i)
+		}
+		if source.IsNone() {
+			panic("please provide the source branch")
+		}
+		if target.IsNone() {
+			parent, hasParent := lineage.Parent(source.GetOrPanic()).Get()
+			if !hasParent {
+				panic(fmt.Sprintf("branch %q has no parent", source.GetOrPanic()))
+			}
+			target = Some(parent)
+		}
+		if title.IsNone() {
+			title = Some(gitdomain.ProposalTitle(fmt.Sprintf("Proposal from %s to %s", source.GetOrPanic(), target.GetOrPanic())))
+		}
+		result = append(result, forgedomain.ProposalData{
+			Active:       true,
+			Body:         body,
+			MergeWithAPI: true,
+			Number:       id.GetOrPanic(),
+			Source:       source.GetOrPanic(),
+			Target:       target.GetOrPanic(),
+			Title:        title.GetOrZero(),
+			URL:          url.GetOrZero(),
+		})
+	}
+	return result
+}
+
+func ToDataTable(proposals []forgedomain.ProposalData, fields []string) datatable.DataTable {
+	result := datatable.DataTable{}
+	result.AddRow(fields...)
+	for _, proposal := range proposals {
+		row := make([]string, len(fields))
+		for f, field := range fields {
+			switch field {
+			case "ID":
+				row[f] = strconv.Itoa(proposal.Number)
+			case "SOURCE BRANCH":
+				row[f] = proposal.Source.String()
+			case "TARGET BRANCH":
+				row[f] = proposal.Target.String()
+			case "BODY":
+				row[f] = proposal.Body.GetOrZero().String()
+			case "URL":
+				row[f] = proposal.URL
+			default:
+				panic("unknown field: " + field)
+			}
+		}
+		result.AddRow(row...)
+	}
+	return result
+}
