c59ba16fe830ff5e1aadd300ed0c22de414fbb66
Kevin Goslar <kevin.goslar@gmail.com>
2026-01-13 17:31:30 -0600

    config_dir_repo.go

--- internal/config/configdomain/config_dir_repo.go
+++ internal/config/configdomain/config_dir_repo.go
@@ -3,7 +3,3 @@ package configdomain
 // ConfigDirRepo is the directory that contains the Git Town configuration for a particular Git repo.
 // Example: ~/.config/git-town/home-user-git-town.
 type ConfigDirRepo string
-
-// func (self ConfigDirRepo) String() string {
-// 	return string(self)
-// }
