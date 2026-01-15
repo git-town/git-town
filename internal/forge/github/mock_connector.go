524f2098cdf89181e5c926bccc6e90d7298f61ee
Kevin Goslar <kevin.goslar@gmail.com>
2026-01-15 16:03:06 -0600

    mock_connector.go

--- internal/forge/github/mock_connector.go
+++ internal/forge/github/mock_connector.go
@@ -2,7 +2,6 @@ package github
 
 import (
 	"fmt"
-	"strconv"
 
 	"github.com/git-town/git-town/v22/internal/cli/print"
 	"github.com/git-town/git-town/v22/internal/forge/forgedomain"
@@ -80,7 +79,7 @@ var _ forgedomain.ProposalBodyUpdater = &mockAPIConnector // type check
 
 func (self *MockConnector) UpdateProposalBody(proposalData forgedomain.ProposalInterface, newBody gitdomain.ProposalBody) error {
 	self.cache.Clear(proposalData.Data().Number)
-	self.log.Start(messages.APIProposalUpdateBody, colors.BoldGreen().Styled("#"+strconv.Itoa(proposalData.Data().Number)))
+	self.log.Start(messages.APIProposalUpdateBody, colors.BoldGreen().Styled(proposalData.Data().Number.String()))
 	proposal, hasProposal := self.Proposals.FindByID(proposalData.Data().Number).Get()
 	if !hasProposal {
 		return fmt.Errorf("proposal with id %d not found", proposalData.Data().Number)
