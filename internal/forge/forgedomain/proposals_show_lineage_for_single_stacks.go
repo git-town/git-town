34998f5dec129e9a0291a669e01a09ad0de4d582
Kevin Goslar <kevin.goslar@gmail.com>
2026-01-19 14:10:42 -0600

    proposals_show_lineage_for_single_stacks.go

--- internal/forge/forgedomain/proposals_show_lineage_for_single_stacks.go
+++ internal/forge/forgedomain/proposals_show_lineage_for_single_stacks.go
@@ -1,7 +1,13 @@
 package forgedomain
 
+import "strconv"
+
 type ProposalsShowLineageSingleStack bool
 
 func (self ProposalsShowLineageSingleStack) ShowLineage() bool {
 	return bool(self)
+}
+
+func (self ProposalsShowLineageSingleStack) String() string {
+	return strconv.FormatBool(bool(self))
 }
