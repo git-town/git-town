de6c8a2eb47b420d6f81c420f07956037b7f2e84
Kevin Goslar <kevin.goslar@gmail.com>
2026-01-19 14:08:31 -0600

    main.go

--- tools/messy_output/main.go
+++ tools/messy_output/main.go
@@ -21,7 +21,7 @@ type ScenarioInfo struct {
 }
 
 func main() {
-	dialogStepRegex := regexp.MustCompile(`^I r[au]n ".*" and enter into the dialogs?:$`)
+	dialogStepRegex := CompileRegex()
 	errors := 0
 	asserts.NoError(filepath.WalkDir("features", func(path string, d os.DirEntry, err error) error {
 		if err != nil || d.IsDir() || filepath.Ext(d.Name()) != ".feature" {
@@ -49,6 +49,10 @@ func AnalyzeScenarios(file string, scenarios []ScenarioInfo) []string {
 		}
 	}
 	return result
+}
+
+func CompileRegex() *regexp.Regexp {
+	return regexp.MustCompile(`^I r[au]n ".*" and enter into the dialogs?:$`)
 }
 
 func FindScenarios(feature *messages.Feature, dialogStepRegex *regexp.Regexp) []ScenarioInfo {
