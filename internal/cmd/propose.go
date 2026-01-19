0aab93e4011f0d3c44786679348c209e52bb3fbd
Kevin Goslar <kevin.goslar@gmail.com>
2026-01-19 09:14:33 -0600

    propose.go

--- internal/cmd/propose.go
+++ internal/cmd/propose.go
@@ -332,7 +332,7 @@ func determineProposeData(repo execute.OpenRepoResult, args proposeArgs) (propos
 	updateProposalLineage := validatedConfig.NormalConfig.ProposalsShowLineage == forgedomain.ProposalsShowLineageCLI
 	if updateProposalLineage {
 		lineageSection := proposallineage.RenderSection(validatedConfig.NormalConfig.Lineage, initialBranch, validatedConfig.NormalConfig.Order, connectorOpt)
-		bodyText = Some(gitdomain.ProposalBody(bodyText.GetOrZero().String() + "\n\n" + lineageSection))
+		bodyText = Some(proposallineage.UpdateProposalBody(bodyText.GetOrZero(), lineageSection))
 	}
 	return proposeData{
 		branchInfos:         branchesSnapshot.Branches,
