02486d80f66cb6daeff769fe68bba78dd327df3c
Kevin Goslar <kevin.goslar@gmail.com>
2026-01-24 16:05:41 -0600

    proposal_breadcrumb_single.go

--- internal/forge/forgedomain/proposal_breadcrumb_single.go
+++ internal/forge/forgedomain/proposal_breadcrumb_single.go
@@ -2,6 +2,8 @@ package forgedomain
 
 import "strconv"
 
+// ProposalBreadcrumbSingle indicates whether to add breadcrumbs to proposals of single branches,
+// i.e. branches that are not part of a stack.
 type ProposalBreadcrumbSingle bool
 
 func (self ProposalBreadcrumbSingle) String() string {
