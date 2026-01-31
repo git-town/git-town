90fd5ab8e91a40526185bc21a8c643337e3a9ced
Kevin Goslar <kevin.goslar@gmail.com>
2026-01-31 13:22:37 -0600

    newline_delineated_test.go

--- internal/gohacks/bytestream/newline_delineated_test.go
+++ internal/gohacks/bytestream/newline_delineated_test.go
@@ -1,8 +1,9 @@
-package bytestream
+package bytestream_test
 
 import (
 	"testing"
 
+	"github.com/git-town/git-town/v22/internal/gohacks/bytestream"
 	"github.com/shoenig/test/must"
 )
 
@@ -13,65 +14,65 @@ func TestNewlineDelineated(t *testing.T) {
 		t.Parallel()
 		t.Run("empty input", func(t *testing.T) {
 			t.Parallel()
-			give := NewlineDelineated([]byte{})
+			give := bytestream.NewlineDelineated([]byte{})
 			have := give.Sanitize()
-			want := Sanitized([]byte{})
+			want := bytestream.Sanitized([]byte{})
 			must.SliceEqOp(t, want, have)
 		})
 		t.Run("no secrets", func(t *testing.T) {
 			t.Parallel()
-			give := NewlineDelineated([]byte("hello world"))
+			give := bytestream.NewlineDelineated([]byte("hello world"))
 			have := give.Sanitize()
-			want := Sanitized([]byte("hello world"))
+			want := bytestream.Sanitized([]byte("hello world"))
 			must.SliceEqOp(t, want, have)
 		})
 		t.Run("deprecated Codeberg Token", func(t *testing.T) {
 			t.Parallel()
-			give := NewlineDelineated([]byte("hello\n\ngit-town.codeberg-token\n1234567890\n\nworld"))
+			give := bytestream.NewlineDelineated([]byte("hello\n\ngit-town.codeberg-token\n1234567890\n\nworld"))
 			have := give.Sanitize()
-			want := Sanitized([]byte("hello\n\ngit-town.codeberg-token\n(redacted)\n\nworld"))
+			want := bytestream.Sanitized([]byte("hello\n\ngit-town.codeberg-token\n(redacted)\n\nworld"))
 			must.SliceEqOp(t, want, have)
 		})
 		t.Run("GitHub Token", func(t *testing.T) {
 			t.Parallel()
-			give := NewlineDelineated([]byte("hello\n\ngit-town.github-token\n1234567890\n\nworld"))
+			give := bytestream.NewlineDelineated([]byte("hello\n\ngit-town.github-token\n1234567890\n\nworld"))
 			have := give.Sanitize()
-			want := Sanitized([]byte("hello\n\ngit-town.github-token\n(redacted)\n\nworld"))
+			want := bytestream.Sanitized([]byte("hello\n\ngit-town.github-token\n(redacted)\n\nworld"))
 			must.SliceEqOp(t, want, have)
 		})
 		t.Run("GitLab Token", func(t *testing.T) {
 			t.Parallel()
-			give := NewlineDelineated([]byte("hello\n\ngit-town.gitlab-token\n1234567890\n\nworld"))
+			give := bytestream.NewlineDelineated([]byte("hello\n\ngit-town.gitlab-token\n1234567890\n\nworld"))
 			have := give.Sanitize()
-			want := Sanitized([]byte("hello\n\ngit-town.gitlab-token\n(redacted)\n\nworld"))
+			want := bytestream.Sanitized([]byte("hello\n\ngit-town.gitlab-token\n(redacted)\n\nworld"))
 			must.SliceEqOp(t, want, have)
 		})
 		t.Run("Forgejo Token", func(t *testing.T) {
 			t.Parallel()
-			give := NewlineDelineated([]byte("hello\n\ngit-town.forgejo-token\n1234567890\n\nworld"))
+			give := bytestream.NewlineDelineated([]byte("hello\n\ngit-town.forgejo-token\n1234567890\n\nworld"))
 			have := give.Sanitize()
-			want := Sanitized([]byte("hello\n\ngit-town.forgejo-token\n(redacted)\n\nworld"))
+			want := bytestream.Sanitized([]byte("hello\n\ngit-town.forgejo-token\n(redacted)\n\nworld"))
 			must.SliceEqOp(t, want, have)
 		})
 		t.Run("Bitbucket Token", func(t *testing.T) {
 			t.Parallel()
-			give := NewlineDelineated([]byte("hello\n\ngit-town.bitbucket-app-password\n1234567890\n\nworld"))
+			give := bytestream.NewlineDelineated([]byte("hello\n\ngit-town.bitbucket-app-password\n1234567890\n\nworld"))
 			have := give.Sanitize()
-			want := Sanitized([]byte("hello\n\ngit-town.bitbucket-app-password\n(redacted)\n\nworld"))
+			want := bytestream.Sanitized([]byte("hello\n\ngit-town.bitbucket-app-password\n(redacted)\n\nworld"))
 			must.SliceEqOp(t, want, have)
 		})
 		t.Run("Gitea Token", func(t *testing.T) {
 			t.Parallel()
-			give := NewlineDelineated([]byte("hello\n\ngit-town.gitea-token\n1234567890\n\nworld"))
+			give := bytestream.NewlineDelineated([]byte("hello\n\ngit-town.gitea-token\n1234567890\n\nworld"))
 			have := give.Sanitize()
-			want := Sanitized([]byte("hello\n\ngit-town.gitea-token\n(redacted)\n\nworld"))
+			want := bytestream.Sanitized([]byte("hello\n\ngit-town.gitea-token\n(redacted)\n\nworld"))
 			must.SliceEqOp(t, want, have)
 		})
 		t.Run("user email", func(t *testing.T) {
 			t.Parallel()
-			give := NewlineDelineated([]byte("hello\n\nuser.email\nuser@example.com\n\nworld"))
+			give := bytestream.NewlineDelineated([]byte("hello\n\nuser.email\nuser@example.com\n\nworld"))
 			have := give.Sanitize()
-			want := Sanitized([]byte("hello\n\nuser.email\n(redacted)\n\nworld"))
+			want := bytestream.Sanitized([]byte("hello\n\nuser.email\n(redacted)\n\nworld"))
 			must.SliceEqOp(t, want, have)
 		})
 	})
