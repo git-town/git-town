3a66c66b2606494a124c3d840949df531f52dce1
Kevin Goslar <kevin.goslar@gmail.com>
2026-02-07 08:29:56 -0600

    name_only.go

--- internal/config/configdomain/name_only.go
+++ internal/config/configdomain/name_only.go
@@ -2,7 +2,3 @@ package configdomain
 
 // NameOnly indicates whether git diff-parent should display only the names of changed files.
 type NameOnly bool
-
-func (self NameOnly) DisplayNameOnly() bool {
-	return bool(self)
-}
