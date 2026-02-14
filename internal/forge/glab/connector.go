a5801f7323b309ca95db14e9778999b953f1bb62
Kevin Goslar <kevin.goslar@gmail.com>
2026-02-14 17:19:10 -0600

    connector.go

--- internal/forge/glab/connector.go
+++ internal/forge/glab/connector.go
@@ -2,12 +2,15 @@ package glab
 
 import (
 	"fmt"
+	"strings"
 
+	"github.com/git-town/git-town/v22/internal/cli/print"
 	"github.com/git-town/git-town/v22/internal/forge/forgedomain"
 	"github.com/git-town/git-town/v22/internal/forge/gitlab"
 	"github.com/git-town/git-town/v22/internal/git/gitdomain"
 	"github.com/git-town/git-town/v22/internal/messages"
 	"github.com/git-town/git-town/v22/internal/subshell/subshelldomain"
+	"github.com/git-town/git-town/v22/pkg/colors"
 	. "github.com/git-town/git-town/v22/pkg/prelude"
 )
 
@@ -22,6 +25,7 @@ var (
 type Connector struct {
 	Backend  subshelldomain.Querier
 	Frontend subshelldomain.Runner
+	log      print.Logger
 }
 
 // ============================================================================
@@ -64,20 +68,27 @@ func (self Connector) DefaultProposalMessage(data forgedomain.ProposalData) stri
 var _ forgedomain.ProposalFinder = glabConnector // type check
 
 func (self Connector) FindProposal(branch, target gitdomain.LocalBranchName) (Option[forgedomain.Proposal], error) {
+	self.log.Start(messages.APIProposalFindStart, branch, target)
 	out, err := self.Backend.Query("glab", "mr", "list", "--source-branch="+branch.String(), "--target-branch="+target.String(), "--output=json")
 	if err != nil {
+		self.log.Failed(err.Error())
 		return None[forgedomain.Proposal](), err
 	}
 	proposals, err := ParseJSONOutput(out)
 	if err != nil {
+		self.log.Failed(err.Error())
 		return None[forgedomain.Proposal](), err
 	}
 	switch len(proposals) {
 	case 0:
+		self.log.Success("none")
 		return None[forgedomain.Proposal](), nil
 	case 1:
-		return Some(proposals[0]), nil
+		proposal := proposals[0]
+		self.log.Log(fmt.Sprintf("%s (%s)", colors.BoldGreen().Styled("#"+proposal.Data.Data().Number.String()), proposal.Data.Data().Title))
+		return Some(proposal), nil
 	default:
+		self.log.Success("multiple")
 		return None[forgedomain.Proposal](), fmt.Errorf(messages.ProposalMultipleFromFound, len(proposals), branch)
 	}
 }
@@ -89,11 +100,23 @@ func (self Connector) FindProposal(branch, target gitdomain.LocalBranchName) (Op
 var _ forgedomain.ProposalSearcher = glabConnector // type check
 
 func (self Connector) SearchProposals(branch gitdomain.LocalBranchName) ([]forgedomain.Proposal, error) {
+	self.log.Start(messages.APIProposalSearchStart, branch.String())
 	out, err := self.Backend.Query("glab", "mr", "list", "--source-branch="+branch.String(), "--output=json")
 	if err != nil {
+		self.log.Failed(err.Error())
 		return []forgedomain.Proposal{}, err
 	}
-	return ParseJSONOutput(out)
+	proposals, err := ParseJSONOutput(out)
+	if err != nil {
+		self.log.Failed(err.Error())
+		return []forgedomain.Proposal{}, err
+	}
+	ids := make([]string, len(proposals))
+	for p, proposal := range proposals {
+		ids[p] = colors.BoldGreen().Styled(fmt.Sprintf("#%d", proposal.Data.Data().Number))
+	}
+	self.log.Log(strings.Join(ids, ", "))
+	return proposals, err
 }
 
 // ============================================================================
@@ -103,7 +126,10 @@ func (self Connector) SearchProposals(branch gitdomain.LocalBranchName) ([]forge
 var _ forgedomain.ProposalMerger = glabConnector // type check
 
 func (self Connector) SquashMergeProposal(number forgedomain.ProposalNumber, message gitdomain.CommitMessage) error {
-	return self.Frontend.Run("glab", "mr", "merge", "--squash", "--body="+message.String(), number.String())
+	self.log.Start(messages.ForgeGithubMergingViaAPI, colors.BoldGreen().Styled("#"+number.String()))
+	err := self.Frontend.Run("glab", "mr", "merge", "--squash", "--body="+message.String(), number.String())
+	self.log.Finished(err)
+	return err
 }
 
 // ============================================================================
@@ -113,7 +139,10 @@ func (self Connector) SquashMergeProposal(number forgedomain.ProposalNumber, mes
 var _ forgedomain.ProposalBodyUpdater = glabConnector // type check
 
 func (self Connector) UpdateProposalBody(proposalData forgedomain.ProposalInterface, updatedDescription gitdomain.ProposalBody) error {
-	return self.Frontend.Run("glab", "mr", "update", proposalData.Data().Number.String(), "--description="+updatedDescription.String())
+	self.log.Start(messages.APIProposalUpdateBody, colors.BoldGreen().Styled("#"+proposalData.Data().Number.String()))
+	err := self.Frontend.Run("glab", "mr", "update", proposalData.Data().Number.String(), "--description="+updatedDescription.String())
+	self.log.Finished(err)
+	return err
 }
 
 // ============================================================================
@@ -123,7 +152,10 @@ func (self Connector) UpdateProposalBody(proposalData forgedomain.ProposalInterf
 var _ forgedomain.ProposalTargetUpdater = glabConnector // type check
 
 func (self Connector) UpdateProposalTarget(proposalData forgedomain.ProposalInterface, target gitdomain.LocalBranchName) error {
-	return self.Frontend.Run("glab", "mr", "update", proposalData.Data().Number.String(), "--target-branch="+target.String())
+	self.log.Start(messages.APIUpdateProposalTarget, colors.BoldGreen().Styled("#"+proposalData.Data().Number.String()), colors.BoldCyan().Styled(targetName))
+	err := self.Frontend.Run("glab", "mr", "update", proposalData.Data().Number.String(), "--target-branch="+target.String())
+	self.log.Finished(err)
+	return err
 }
 
 // ============================================================================
