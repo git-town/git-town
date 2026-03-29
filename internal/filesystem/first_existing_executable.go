8534aef36d6a8b4673c880feabcffd12d23d44bd
Kevin Goslar <kevin.goslar@gmail.com>
2026-03-29 08:06:30 -0500

    first_existing_executable.go

--- internal/filesystem/first_existing_executable.go
+++ internal/filesystem/first_existing_executable.go
@@ -9,8 +9,11 @@ import (
 // FirstExistingExecutable provides the first existing executable from the given list of executable names.
 func FirstExistingExecutable(commands []string) Option[string] {
 	for _, command := range commands {
-		executable, err := exec.LookPath(command)
-		if err == nil && len(executable) > 0 {
+		if command == "" {
+			continue
+		}
+		_, err := exec.LookPath(command)
+		if err == nil {
 			return Some(command)
 		}
 	}
