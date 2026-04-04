356e7e5731a205284ad93b13cb91f528b7143058
Kevin Goslar <kevin.goslar@gmail.com>
2026-04-04 15:08:23 -0500

    format_cmp_or_test.go

--- tools/format_cmp_or/format_cmp_or_test.go
+++ tools/format_cmp_or/format_cmp_or_test.go
@@ -10,94 +10,6 @@ import (
 func TestFormatFileContent(t *testing.T) {
 	t.Parallel()
 
-	t.Run("single-line unsorted", func(t *testing.T) {
-		t.Parallel()
-		give := []byte(`
-package main
-
-import "cmp"
-
-func foo() {
-	if err := cmp.Or(errC, errA, errB); err != nil {}
-}`[1:])
-		want := []byte(`
-package main
-
-import "cmp"
-
-func foo() {
-	if err := cmp.Or(errA, errB, errC); err != nil {}
-}`[1:])
-		have := formatCmpOr.FormatFileContent("", give)
-		must.EqOp(t, string(want), string(have))
-	})
-
-	t.Run("single-line already sorted", func(t *testing.T) {
-		t.Parallel()
-		give := []byte(`
-package main
-
-import "cmp"
-
-func foo() {
-	if err := cmp.Or(errA, errB, errC); err != nil {}
-}`[1:])
-		have := formatCmpOr.FormatFileContent("", give)
-		must.EqOp(t, string(give), string(have))
-	})
-
-	t.Run("multi-line unsorted", func(t *testing.T) {
-		t.Parallel()
-		give := []byte("package main\n\nimport \"cmp\"\n\nfunc foo() {\n\terr := cmp.Or(\n\t\terrC,\n\t\terrA,\n\t\terrB,\n\t)\n}")
-		want := []byte("package main\n\nimport \"cmp\"\n\nfunc foo() {\n\terr := cmp.Or(\n\t\terrA,\n\t\terrB,\n\t\terrC,\n\t)\n}")
-		have := formatCmpOr.FormatFileContent("", give)
-		must.EqOp(t, string(want), string(have))
-	})
-
-	t.Run("multi-line already sorted", func(t *testing.T) {
-		t.Parallel()
-		give := []byte("package main\n\nimport \"cmp\"\n\nfunc foo() {\n\terr := cmp.Or(\n\t\terrA,\n\t\terrB,\n\t\terrC,\n\t)\n}")
-		have := formatCmpOr.FormatFileContent("", give)
-		must.EqOp(t, string(give), string(have))
-	})
-
-	t.Run("non-identifier args are skipped", func(t *testing.T) {
-		t.Parallel()
-		give := []byte(`
-package main
-
-import "cmp"
-
-func foo() {
-	v := cmp.Or(foo.Bar, foo.Baz)
-	_ = v
-}`[1:])
-		have := formatCmpOr.FormatFileContent("", give)
-		must.EqOp(t, string(give), string(have))
-	})
-
-	t.Run("keep-sorted calls are skipped", func(t *testing.T) {
-		t.Parallel()
-		give := []byte("package main\n\nimport \"cmp\"\n\nfunc foo() {\n\terr := cmp.Or(\n\t\t// keep-sorted start\n\t\terrC,\n\t\terrA,\n\t\t// keep-sorted end\n\t)\n\t_ = err\n}")
-		have := formatCmpOr.FormatFileContent("myfile.go", give)
-		must.EqOp(t, string(give), string(have))
-	})
-
-	t.Run("single argument is skipped", func(t *testing.T) {
-		t.Parallel()
-		give := []byte(`
-package main
-
-import "cmp"
-
-func foo() {
-	v := cmp.Or(errA)
-	_ = v
-}`[1:])
-		have := formatCmpOr.FormatFileContent("", give)
-		must.EqOp(t, string(give), string(have))
-	})
-
 	t.Run("case-insensitive sorting", func(t *testing.T) {
 		t.Parallel()
 		give := []byte(`
@@ -115,6 +27,93 @@ import "cmp"
 
 func foo() {
 	if err := cmp.Or(errAutoResolve, errDryRun, errVerbose); err != nil {}
+}`[1:])
+		have := formatCmpOr.FormatFileContent("", give)
+		must.EqOp(t, string(want), string(have))
+	})
+
+	t.Run("keep-sorted calls are skipped", func(t *testing.T) {
+		t.Parallel()
+		give := []byte("package main\n\nimport \"cmp\"\n\nfunc foo() {\n\terr := cmp.Or(\n\t\t// keep-sorted start\n\t\terrC,\n\t\terrA,\n\t\t// keep-sorted end\n\t)\n\t_ = err\n}")
+		have := formatCmpOr.FormatFileContent("myfile.go", give)
+		must.EqOp(t, string(give), string(have))
+	})
+
+	t.Run("multi-line already sorted", func(t *testing.T) {
+		t.Parallel()
+		give := []byte("package main\n\nimport \"cmp\"\n\nfunc foo() {\n\terr := cmp.Or(\n\t\terrA,\n\t\terrB,\n\t\terrC,\n\t)\n}")
+		have := formatCmpOr.FormatFileContent("", give)
+		must.EqOp(t, string(give), string(have))
+	})
+
+	t.Run("multi-line unsorted", func(t *testing.T) {
+		t.Parallel()
+		give := []byte("package main\n\nimport \"cmp\"\n\nfunc foo() {\n\terr := cmp.Or(\n\t\terrC,\n\t\terrA,\n\t\terrB,\n\t)\n}")
+		want := []byte("package main\n\nimport \"cmp\"\n\nfunc foo() {\n\terr := cmp.Or(\n\t\terrA,\n\t\terrB,\n\t\terrC,\n\t)\n}")
+		have := formatCmpOr.FormatFileContent("", give)
+		must.EqOp(t, string(want), string(have))
+	})
+
+	t.Run("non-identifier args are skipped", func(t *testing.T) {
+		t.Parallel()
+		give := []byte(`
+package main
+
+import "cmp"
+
+func foo() {
+	v := cmp.Or(foo.Bar, foo.Baz)
+	_ = v
+}`[1:])
+		have := formatCmpOr.FormatFileContent("", give)
+		must.EqOp(t, string(give), string(have))
+	})
+	t.Run("single argument is skipped", func(t *testing.T) {
+		t.Parallel()
+		give := []byte(`
+package main
+
+import "cmp"
+
+func foo() {
+	v := cmp.Or(errA)
+	_ = v
+}`[1:])
+		have := formatCmpOr.FormatFileContent("", give)
+		must.EqOp(t, string(give), string(have))
+	})
+
+	t.Run("single-line already sorted", func(t *testing.T) {
+		t.Parallel()
+		give := []byte(`
+package main
+
+import "cmp"
+
+func foo() {
+	if err := cmp.Or(errA, errB, errC); err != nil {}
+}`[1:])
+		have := formatCmpOr.FormatFileContent("", give)
+		must.EqOp(t, string(give), string(have))
+	})
+
+	t.Run("single-line unsorted", func(t *testing.T) {
+		t.Parallel()
+		give := []byte(`
+package main
+
+import "cmp"
+
+func foo() {
+	if err := cmp.Or(errC, errA, errB); err != nil {}
+}`[1:])
+		want := []byte(`
+package main
+
+import "cmp"
+
+func foo() {
+	if err := cmp.Or(errA, errB, errC); err != nil {}
 }`[1:])
 		have := formatCmpOr.FormatFileContent("", give)
 		must.EqOp(t, string(want), string(have))
