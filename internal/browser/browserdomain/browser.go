fa346555d574d9db2a66a56f7bf5a2899be9e9ed
Kevin Goslar <kevin.goslar@gmail.com>
2026-04-22 20:46:49 -0500

    browser.go

--- internal/browser/browserdomain/browser.go
+++ internal/browser/browserdomain/browser.go
@@ -8,7 +8,7 @@ const NoBrowser = Browser("(none)")
 
 // Browser indicates a custom browser to use.
 // If set to "" or "(none)", browsers are disabled.
-// IF set to anything else, Git Town considers it the browser executable to use.
+// If set to anything else, Git Town considers it the browser executable to use.
 type Browser string
 
 func (self Browser) Get() (executable string, useBrowser bool) { //nolint: nonamedreturns
