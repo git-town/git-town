4384a5556a8b88b47875649432e976ce97a29595
Kevin Goslar <kevin.goslar@gmail.com>
2026-05-02 19:57:19 -0500

    browser_enabled.go

--- internal/browser/browserdomain/browser_enabled.go
+++ internal/browser/browserdomain/browser_enabled.go
@@ -1,10 +1,6 @@
 package browserdomain
 
-import (
-	"strconv"
-
-	. "github.com/git-town/git-town/v22/pkg/prelude"
-)
+import "strconv"
 
 const NoBrowser = BrowserExecutable("(none)")
 
@@ -16,11 +12,4 @@ func (self BrowserEnabled) Enabled() bool {
 
 func (self BrowserEnabled) String() string {
 	return strconv.FormatBool(bool(self))
-}
-
-func NewBrowserEnabledFromTTY(tty HasTTY) Option[BrowserEnabled] {
-	if tty {
-		return None[BrowserEnabled]()
-	}
-	return Some(BrowserEnabled(false))
 }
