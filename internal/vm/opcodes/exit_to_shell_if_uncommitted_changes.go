280cdfdda231ae0f12febb02c6b09c85aeb08bda
Kevin Goslar <kevin.goslar@gmail.com>
2026-05-04 06:15:50 -0500

    nthnt

--- internal/vm/opcodes/exit_to_shell_if_uncommitted_changes.go
+++ internal/vm/opcodes/exit_to_shell_if_uncommitted_changes.go
@@ -5,11 +5,12 @@ import "github.com/git-town/git-town/v22/internal/vm/shared"
 type ExitToShellIfUncommittedChanges struct{}
 
 func (self *ExitToShellIfUncommittedChanges) Run(args shared.RunArgs) error {
-	hasUncommittedFiles, err := args.Git.HasUncommittedFiles()
+	hasUncommittedFiles, err := args.Git.HasUncommittedFiles(args.Backend)
 	if err != nil {
 		return err
 	}
-	if len(uncommittedFiles) > 0 {
-		args.PrependOpcodes()
+	if hasUncommittedFiles {
+		args.PrependOpcodes(&ExitToShell{})
 	}
+	return nil
 }
