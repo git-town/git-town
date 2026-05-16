bac138edc66e99daabdb05a838a0bf354aef2404
Kevin Goslar <kevin.goslar@gmail.com>
2026-05-16 08:35:21 -0500

    z_string_test.go

---
+++ internal/gohacks/z_string_test.go
@@ -0,0 +1,29 @@
+package gohacks_test
+
+import (
+	"testing"
+
+	"github.com/git-town/git-town/v23/internal/gohacks"
+	"github.com/shoenig/test/must"
+)
+
+func TestZStringLines(t *testing.T) {
+	t.Parallel()
+	tests := map[gohacks.ZString][]string{
+		"":                    {""},
+		"single":              {"single"},
+		"a\x00b":              {"a", "b"},
+		"a\x00b\x00c":         {"a", "b", "c"},
+		"\x00leading":         {"", "leading"},
+		"trailing\x00":        {"trailing", ""},
+		"a\x00\x00double":     {"a", "", "double"},
+		"mixed\r\nchars\x001": {"mixed\r\nchars", "1"},
+	}
+	for give, want := range tests {
+		t.Run(give.String(), func(t *testing.T) {
+			t.Parallel()
+			have := give.Lines()
+			must.Eq(t, want, have)
+		})
+	}
+}
