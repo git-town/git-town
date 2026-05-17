bbf85e5950a6f10fa5e586c4517b7b275c0840b4
Kevin Goslar <kevin.goslar@gmail.com>
2026-05-17 06:48:27 -0500

    trimmed.go [skip ci]

--- internal/gohacks/stringss/trimmed.go
+++ internal/gohacks/stringss/trimmed.go
@@ -3,6 +3,9 @@ package stringss
 import "strings"
 
 // Trimmed is a string that is guaranteed to have no leading and trailing whitespace.
+//
+// If you are sure that a string is already trimmed,
+// you can cast to the Trimmed type directly instead of calling Trim().
 type Trimmed string
 
 func (self Trimmed) String() string {
