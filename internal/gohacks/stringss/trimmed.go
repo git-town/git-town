9a444500dc17aa6be44a169b8d0c639d1200a7bf
Kevin Goslar <kevin.goslar@gmail.com>
2026-05-17 08:41:20 -0500

    trimmed.go

--- internal/gohacks/stringss/trimmed.go
+++ internal/gohacks/stringss/trimmed.go
@@ -3,15 +3,17 @@ package stringss
 import "strings"
 
 // Trimmed is a string that is guaranteed to have no leading and trailing whitespace.
-//
-// If you are sure that a string is already trimmed,
-// you can cast to the Trimmed type directly instead of calling Trim().
 type Trimmed string
 
 func (self Trimmed) String() string {
 	return string(self)
 }
 
+// Converts the given string to a Trimmed string by trimming leading and trailing whitespace.
+//
+// If you are certain that a string is already trimmed,
+// you can cast to the Trimmed type directly instead of calling Trim()
+// to avoid the overhead of the unnecessary trimming.
 func Trim(text string) Trimmed {
 	return Trimmed(strings.TrimSpace(text))
 }
