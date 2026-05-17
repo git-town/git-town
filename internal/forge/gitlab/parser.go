e03ceea212f0a9d10f327de2e4e96d415e31d239
Kevin Goslar <kevin.goslar@gmail.com>
2026-05-17 07:23:48 -0500

    parser.go [skip ci]

--- internal/forge/gitlab/parser.go
+++ internal/forge/gitlab/parser.go
@@ -12,8 +12,8 @@ func parseMergeRequest(mergeRequest *gitlab.BasicMergeRequest) forgedomain.Propo
 		Active:       mergeRequest.State == "opened",
 		MergeWithAPI: true,
 		Number:       forgedomain.ProposalNumber(mergeRequest.IID),
-		Source:       gitdomain.LocalBranchNameOrPanic(stringss.Trim(mergeRequest.SourceBranch)),
-		Target:       gitdomain.LocalBranchNameOrPanic(stringss.Trim(mergeRequest.TargetBranch)),
+		Source:       gitdomain.LocalBranchNameOrPanic(stringss.Trimmed(mergeRequest.SourceBranch)), // we can assume the GitLab API provides correct strings
+		Target:       gitdomain.LocalBranchNameOrPanic(stringss.Trimmed(mergeRequest.TargetBranch)), // we can assume the GitLab API provides correct strings
 		Title:        gitdomain.ProposalTitle(mergeRequest.Title),
 		Body:         gitdomain.NewProposalBodyOpt(mergeRequest.Description),
 		URL:          mergeRequest.WebURL,
