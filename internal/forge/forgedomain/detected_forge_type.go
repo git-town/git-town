fb5a69d567c72a36ef1304bcb27c67ed82df8b86
Kevin Goslar <kevin.goslar@gmail.com>
2026-06-06 05:49:46 -0500

    detected_forge_type.go detected_forge_type_test.go render_tree.go render_tree_test.go

--- internal/forge/forgedomain/detected_forge_type.go
+++ internal/forge/forgedomain/detected_forge_type.go
@@ -1,5 +1,7 @@
 package forgedomain
 
+import . "github.com/git-town/git-town/v23/pkg/prelude"
+
 // DetectedForgeType is the forge type that actually exists.
 // Its the one the user has configured,
 // or if that is "auto", the automatically detected forge type.
@@ -7,3 +9,12 @@ type DetectedForgeType ForgeType
 
 // ForgeType converts this value into a ForgeType.
 func (self DetectedForgeType) ForgeType() ForgeType { return ForgeType(self) }
+
+func IsBitbucket(forgeTypeOpt Option[DetectedForgeType]) bool {
+	detectedForgeType, hasForgeType := forgeTypeOpt.Get()
+	if !hasForgeType {
+		return false
+	}
+	forgeType := detectedForgeType.ForgeType()
+	return forgeType == ForgeTypeBitbucket || forgeType == ForgeTypeBitbucketDatacenter
+}
