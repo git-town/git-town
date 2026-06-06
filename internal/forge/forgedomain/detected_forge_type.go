76db8fcc635a38388aff344380e2dde21e5c83b6
Kevin Goslar <kevin.goslar@gmail.com>
2026-06-06 05:31:48 -0500

    detected_forge_type.go

--- internal/forge/forgedomain/detected_forge_type.go
+++ internal/forge/forgedomain/detected_forge_type.go
@@ -1,5 +1,8 @@
 package forgedomain
 
+// DetectedForgeType is the forge type that actually exists.
+// Its the one the user has configured,
+// or if that is "auto", the automatically detected forge type.
 type DetectedForgeType ForgeType
 
 func (self DetectedForgeType) ForgeType() ForgeType { return ForgeType(self) }
