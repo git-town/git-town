7b9da682ad1dceaecd80f3619855db4b65a33888
Kevin Goslar <kevin.goslar@gmail.com>
2026-06-21 07:06:14 -0500

    repo_status.go

--- internal/git/gitdomain/repo_status.go
+++ internal/git/gitdomain/repo_status.go
@@ -7,6 +7,7 @@ type RepoStatus struct {
 	UntrackedChanges bool // the repo contains files that aren't tracked by Git
 }
 
+// NeedsToCommit indicates whether the repo contains something to commit.
 func (self RepoStatus) NeedsToCommit() bool {
 	return self.OpenChanges || self.RebaseInProgress || self.Conflicts || self.UntrackedChanges
 }
